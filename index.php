0a0a099372aab55af8e7ed6d87ce40eba6dcd13a
Jesse Nickles <jesse@littlebizzy.com>
2019-10-21 16:46:54 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+
+<style>
+  <?php include 'style.css'; ?>
+</style>
