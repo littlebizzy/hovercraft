384a2a281a85918869e8c40614c8205147417a20
Jesse Nickles <jesse@littlebizzy.com>
2019-10-22 20:51:53 +0700

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,18 @@
+<?php
+
+// WooCommerce support
+add_theme_support('woocommerce');
+
+
+// featured image
+function post_image_large() {
+    global $post;
+    $args = array( 'post_type' => 'attachment', 'post_mime_type' => 'image', 'numberposts' => 1, 'orderby' => 'menu_order', 'order' => 'ASC', 'post_status' => null, 'post_parent' => $post->ID ); 
+    $attachments = get_posts($args);
+    if ($attachments) {
+            foreach ( $attachments as $attachment ) {
+                    $src = wp_get_attachment_image_src( $attachment->ID, 'large');
+                    if ($src) {echo $src[0];}
+            }
+    }
+}
