b98dfbabbd72ebe8fbbd8386d27857cc27b5064d
Jesse Nickles <jesse@littlebizzy.com>
2019-10-22 20:16:48 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,32 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width,initial-scale=1">
 
+<title><?php
+if(is_home() || is_front_page()) {
+echo $sitename;
+echo ' | ';
+echo $sitedesc;
+}
+elseif(is_404()){
+echo '404 Error (Page Not Found) - ';
+echo $sitename;
+}
+else{
+echo trim(wp_title('', false));
+echo ' - ';
+echo $sitename;
+} ?></title>
+  
 <style>
   <?php include 'style.css'; ?>
 </style>
+  
+<?php if (is_page('cart') || is_page('checkout')) { ?>
+<link rel="stylesheet" id="select2-css" href="<?php echo site_url(); ?>/wp-content/plugins/woocommerce/assets/css/select2.css">
+<?php } ?>
+
+<?php wp_head(); ?>
+</head>
+
+<body>
+  
