a6098704e25de2f7da786a851a3dfe25f49117fe
Jesse Nickles <jesse@littlebizzy.com>
2022-03-23 18:19:54 +0700

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,13 @@
+
+    			
+        <div id="scrollup"><i class="fa-solid fa-circle-arrow-up"></i></div>
+
+		<div class="clear"></div>
+
+		</div><!--inner -->
+	  </div><!-- footer -->
+
+<?php wp_footer(); ?>
+
+</body><!-- body -->
+</html><!-- html -->
