0beafd5ed2702426022e0689f379ea4bd334fb8c
Jesse Nickles <jesse@littlebizzy.com>
2022-03-23 17:11:57 +0700

    Update header.php

--- header.php
+++ header.php
@@ -1,10 +1,35 @@
 <!DOCTYPE html>
 <html lang="en-US">
-<head>
+	<head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width,initial-scale=1">
 	
 	<style>
 	<?php include 'style.css'; ?>
 	</style>
-  
+	
+	<title><?php
+		if (is_home() || is_front_page()) {
+			echo get_option('blogname');
+			echo ' | ';
+			echo get_option('blogdescription');
+		}
+		elseif (is_category()) {
+			echo single_cat_title();
+			echo ' - ';
+			echo get_option('blogname');
+		}
+		elseif (is_404()){
+			echo '404 Error (Page Not Found)';
+			echo ' - ';
+			echo get_option('blogname');
+		}
+		else {
+			echo trim(wp_title('', false));
+			echo ' - ';
+			echo get_option('blogname');
+		} ?></title>
+	<?php wp_head(); ?>
+	</head>
+	<body>
+	
