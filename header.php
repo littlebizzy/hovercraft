4dd39adc2ef9917a3b7c70adda8736bbc3de7fa9
Jesse Nickles <jesse@littlebizzy.com>
2022-03-23 16:38:03 +0700

    Create header.php

---
+++ header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	
+	<style>
+	<?php include 'style.css'; ?>
+	</style>
+  
