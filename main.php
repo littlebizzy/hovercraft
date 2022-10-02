2528cb9bfd7c84b1e9448a4307bc8afd7cc13dbd
Jesse Nickles <jesse@littlebizzy.com>
2022-10-03 02:27:59 +0700

    Create main.php

---
+++ main.php
@@ -0,0 +1,7 @@
+<div id="main">
+<div class="inner">
+<?php get_template_part('primary'); ?>
+<?php get_template_part('sidebar'); ?>
+<div class="clear"></div>
+</div><!-- inner -->
+</div><!-- main -->
