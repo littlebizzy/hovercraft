00332d78458ebccbf5df6d687c5fe32d6414de3b
Jesse Nickles <jesse@littlebizzy.com>
2022-10-03 02:30:37 +0700

    Update splash.php

--- splash.php
+++ splash.php
@@ -1,3 +1,7 @@
 <div id="splash">
+	<div class="inner">
+	<div id="welcome"><?php global $welcome; echo $welcome; ?></div>
+	<button class="cta-home">Learn More</button>
 <div class="clear"></div>
-</div>
+</div><!-- inner -->
+</div><!-- splash -->
