c6d452715619b75ed131a8db01e3a3b7ac949530
Jesse Nickles <jesse@littlebizzy.com>
2022-10-08 17:44:43 +0700

    Update splash.php

--- splash.php
+++ splash.php
@@ -1,7 +1,18 @@
-<div id="splash">
+<div id="splash" style="
+background:linear-gradient(rgba(38, 50, 56, 0.7), rgba(240, 149, 128, 0.2)), url(<?php $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID), 'large' ); echo $url ?>);
+background-position: center;
+background-size:cover;
+background-repeat:no-repeat;
+">
 	<div class="inner">
+	
 	<div id="welcome"><?php global $welcome; echo $welcome; ?></div>
-	<button class="cta-home">Learn More</button>
+	
+		<?php if (is_front_page()) : ?>
+    <button class="cta-splash">Learn More</button>
+<?php endif; ?>
+		
 <div class="clear"></div>
 </div><!-- inner -->
 </div><!-- splash -->
+
