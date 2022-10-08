273654d32a50a9da154d5bbb700e5d417d466fa8
Jesse Nickles <jesse@littlebizzy.com>
2022-10-08 17:40:17 +0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,9 +1,13 @@
 <?php
 
+// variables
 $welcome = "Stop fixing your WordPress theme, and focus on your business.";
 
 // WooCommerce support
 add_theme_support('woocommerce');
+
+// featured image support
+add_theme_support( 'post-thumbnails' );
 
 // disable Customizer (menu)
 // ref: https://stackoverflow.com/a/50912719/1718491
@@ -22,7 +26,7 @@ if ( isset( $submenu[ 'themes.php' ] ) ) {
 
 // disable Customize link (admin bar)
 // ref: https://wordpress.stackexchange.com/a/200304/152624
-add_action( 'wp_before_admin_bar_render', 'wpse200296_before_admin_bar_render' ); 
+// add_action( 'wp_before_admin_bar_render', 'wpse200296_before_admin_bar_render' ); 
 function wpse200296_before_admin_bar_render() {
     global $wp_admin_bar;
     $wp_admin_bar->remove_menu('customize');
@@ -42,20 +46,149 @@ function post_image_large() {
 }
 
 
-/**
- * Register our sidebars and widgetized areas.
- *
- */
+// enable sidebar widgets
 function hovercraft_register_sidebar() {
 
 	register_sidebar( array(
 		'name'          => 'Sidebar',
 		'id'            => 'hovercraft_sidebar',
-		'before_widget' => '<div>',
+		'before_widget' => '<div class="widget-wrapper">',
 		'after_widget'  => '</div>',
-		'before_title'  => '<h3 class="rounded">',
+		'before_title'  => '<h3 class="widget-title">',
 		'after_title'   => '</h3>',
 	) );
 
 }
 add_action( 'widgets_init', 'hovercraft_register_sidebar' );
+
+
+// enable copyright widget
+function hovercraft_register_copyright() {
+
+	register_sidebar( array(
+		'name'          => 'Copyright',
+		'id'            => 'hovercraft_copyright',
+		'before_widget' => '<div class="widget-wrapper">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
+	) );
+
+}
+add_action( 'widgets_init', 'hovercraft_register_copyright' );
+
+
+// enable footer one widget
+function hovercraft_register_footer_one() {
+
+	register_sidebar( array(
+		'name'          => 'Footer #1',
+		'id'            => 'hovercraft_footer_one',
+		'before_widget' => '<div class="widget-wrapper">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
+	) );
+
+}
+add_action( 'widgets_init', 'hovercraft_register_footer_one' );
+
+
+// enable footer two widget
+function hovercraft_register_footer_two() {
+
+	register_sidebar( array(
+		'name'          => 'Footer #2',
+		'id'            => 'hovercraft_footer_two',
+		'before_widget' => '<div class="widget-wrapper">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
+	) );
+
+}
+add_action( 'widgets_init', 'hovercraft_register_footer_two' );
+
+
+// enable footer three widget
+function hovercraft_register_footer_three() {
+
+	register_sidebar( array(
+		'name'          => 'Footer #3',
+		'id'            => 'hovercraft_footer_three',
+		'before_widget' => '<div class="widget-wrapper">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
+	) );
+
+}
+add_action( 'widgets_init', 'hovercraft_register_footer_three' );
+
+
+// enable footer four widget
+function hovercraft_register_footer_four() {
+
+	register_sidebar( array(
+		'name'          => 'Footer #4',
+		'id'            => 'hovercraft_footer_four',
+		'before_widget' => '<div class="widget-wrapper">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
+	) );
+
+}
+add_action( 'widgets_init', 'hovercraft_register_footer_four' );
+
+
+
+// register header menu
+function hovercraft_main_menu() {
+	register_nav_menu('main-menu',__( 'Main Menu' ));
+}
+add_action( 'init', 'hovercraft_main_menu' );
+
+
+
+// register header cta menu
+function hovercraft_cta_header() {
+	register_nav_menu('cta-header',__( 'Header CTA' ));
+}
+add_action( 'init', 'hovercraft_cta_header' );
+
+
+// llimit header cta to single item
+// https://www.isitwp.com/limit-amount-of-menu-items/
+add_filter( 'wp_nav_menu_objects', 'hovercraft_header_cta_limit', 10, 2 );
+
+function hovercraft_header_cta_limit($items, $args) {
+    // want our MAINMENU to have MAX of 7 items
+    if ( $args->theme_location == 'cta-header' ) {
+        $toplinks = 0;
+        foreach ( $items as $k => $v ) {
+            if ( $v->menu_item_parent == 0 ) {
+                // count how many top-level links we have so far...
+                $toplinks++;
+            }
+            // if we've passed our max # ...
+            if ( $toplinks > 1 ) {
+                unset($items[$k]);
+            }
+        }
+    }
+    return $items;
+}
+
+// disable copyright widget title
+// https://presscustomizr.com/snippet/dynamically-changing-the-widget-title-depending-on-the-context/
+function my_widget_title($id_base) {
+      if ('hovercraft_copyright' == $id_base) {
+      		return '';
+      }
+      else {
+        return $title;
+      }
+    }
+
+
