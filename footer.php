335da3baa9f1057d9ae6cfd1a93373f395fa6f6e
Jesse Nickles <jesse@littlebizzy.com>
2022-10-08 17:42:06 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,11 +1,52 @@
 <div id="footer">
 <div class="inner">
-<div id="scrollup"><i class="fa-solid fa-circle-arrow-up"></i></div>
-
+	
+	<div class="footer-section-left">
+		<?php dynamic_sidebar( 'hovercraft_footer_one' ); ?>
+	<div class="clear"></div>
+	</div><!-- footer-section-left -->
+	
+		<div class="footer-section-two">
+		<?php dynamic_sidebar( 'hovercraft_footer_two' ); ?>
+	<div class="clear"></div>
+	</div><!-- footer-section-left -->
+	
+		<div class="footer-section-three">
+		<?php dynamic_sidebar( 'hovercraft_footer_three' ); ?>
+	<div class="clear"></div>
+	</div><!-- footer-section-left -->
+	
+		<div class="footer-section-four">
+		<?php dynamic_sidebar( 'hovercraft_footer_four' ); ?>
+	<div class="clear"></div>
+	</div><!-- footer-section-left -->
+	
 <div class="clear"></div>
-
 </div><!--inner -->
 </div><!-- footer -->
+
+<div id="copyright">
+	<div class="inner">
+		
+	<div class="copyright-left">
+		&copy; <?php echo date("Y"); ?> <?php echo get_bloginfo( 'name' ); ?>. All rights reserved.
+	<div class="clear"></div>
+	</div><!-- copyright-left -->
+	
+	<div class="copyright-right">
+	
+		
+	<?php add_filter ( 'widget_title' , 'my_widget_title', 10, 1); //we use the default priority and 3 arguments in the callback function
+		dynamic_sidebar( 'hovercraft_copyright' ); remove_filter('widget_title', 'my_widget_title'); ?>
+		
+	<div class="clear"></div>
+	</div><!-- copyright-right -->
+		
+	<div class="clear"></div>
+	</div><!--inner -->
+</div><!-- copyright -->
+
+<div id="scrollup"><i class="fa-solid fa-circle-arrow-up"></i></div>
 
 <?php wp_footer(); ?>
 
