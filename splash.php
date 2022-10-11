7256a66d41a150b910343b53176733916592c077
Jesse Nickles <jesse@littlebizzy.com>
2022-10-12 01:18:03 +0700

    Update splash.php

--- splash.php
+++ splash.php
@@ -1,19 +1,27 @@
-<div id="splash" style="
+<div id="splash-wrapper">
+<div <?php if (is_front_page()) { echo 'class="splash-wide"'; } else { echo 'class="splash-fixed"'; } ?> style="
+																												
 background:linear-gradient(rgba(38, 50, 56, 0.7), rgba(240, 149, 128, 0.2)), url(<?php $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID), 'large' ); echo $url ?>);
+																												
 background: linear-gradient(270deg, #e19eae 0%, #945af5 100%), url(<?php $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID), 'large' ); echo $url ?>);
 
 background-position: center;
 background-size:cover;
 background-repeat:no-repeat;
-						
-background: #000046;  /* fallback for old browsers */
-background: -webkit-linear-gradient(to right, #1CB5E0, #000046);  /* Chrome 10-25, Safari 5.1-6 */
-background: linear-gradient(to right, #1CB5E0, #000046); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 
+background: linear-gradient(to right, #1CB5E0, #000046), url(<?php $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID), 'large' ); echo $url ?>);
+
+background:linear-gradient(rgba(38, 50, 56, 0.7), rgba(240, 149, 128, 0.2)), url(<?php $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID), 'large' ); echo $url ?>);
+																												
 ">
 	<div class="inner">
 	
-	<div id="welcome"><?php global $welcome; echo $welcome; ?></div>
+	<div class="welcome-wrapper">
+		<div class="welcome"><?php single_post_title(); ?></div>
+		<div class="welcome-text">
+			<?php the_excerpt(); ?>
+		</div><!-- welcome-text -->
+		</div><!-- welcome-wrapper -->
 	
 		<?php if (is_front_page()) : ?>
 		<?php wp_nav_menu( array( 'theme_location' => 'cta-splash', 'container_class'	=> 'cta-splash' ) ); ?>
@@ -23,4 +31,4 @@ background: linear-gradient(to right, #1CB5E0, #000046); /* W3C, IE 10+/ Edge, F
 <div class="clear"></div>
 </div><!-- inner -->
 </div><!-- splash -->
-
+</div><!-- splash-wrapper -->
