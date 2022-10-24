167a07f36883e7c59077d68b3304a47fffc9a6ea
Jesse Nickles <jesse@littlebizzy.com>
2022-10-25 04:50:44 +0700

    Create select-menu.php

---
+++ inc/select-menu.php
@@ -0,0 +1,11 @@
+function hovercraft_select_menu() {
+    wp_enqueue_script(
+        'hovercraft_select_menu', // unique name 
+        get_template_directory_uri().'/assets/js/select-menu.js', // location
+        array('jquery'), // dependencies
+        '', 
+        true
+    );
+}
+
+add_action( 'wp_enqueue_scripts', 'hovercraft_select_menu' );
