f7b40744d878d0c014698c38a9cd00b31a8b85e9
Jesse Nickles <jesse@littlebizzy.com>
2022-10-25 01:25:57 +0700

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -1,4 +1,5 @@
+<?php // get_template_part( 'template-parts/prefix', 'suffix' ); ?>
 <?php get_template_part('header'); ?>
-<?php get_template_part('splash'); ?>
-<?php get_template_part('main'); ?>
+<?php get_template_part('template-parts/splash'); ?>
+<?php get_template_part( 'template-parts/main' ); ?>
 <?php get_template_part('footer'); ?>
