a89f8e6e09f2acb1ea4d869a49345a97fe727eb8
Jesse Nickles <jesse@littlebizzy.com>
2022-10-26 02:07:10 +0700

    Update author.php

--- author.php
+++ author.php
@@ -1,4 +1,4 @@
 <?php get_template_part('header'); ?>
-<?php get_template_part('splash'); ?>
-<?php get_template_part('main'); ?>
+<?php get_template_part('template-parts/splash'); ?>
+<?php get_template_part( 'template-parts/main' ); ?>
 <?php get_template_part('footer'); ?>
