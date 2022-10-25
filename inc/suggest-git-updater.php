d724062250ffcd62ffbe590d86b55ab56a4bd05c
Jesse Nickles <jesse@littlebizzy.com>
2022-10-25 21:21:16 +0700

    Update suggest-git-updater.php

--- inc/suggest-git-updater.php
+++ inc/suggest-git-updater.php
@@ -1,9 +1,15 @@
 add_action('admin_notices', 'hovercraft_suggest_git_updater_plugin');
 
+function my_theme_dependencies() {
+  if )
+    echo '<div class="error"><p>' . __( 'Warning: The theme needs Plugin X to function', 'my-theme' ) . '</p></div>';
+}
+
 function hovercraft_suggest_git_updater_plugin() {
 
     $current_user = wp_get_current_user();
-    if(current_user_can('manage_options')) { ?>
+
+    if(current_user_can('manage_options') && ( ! is_plugin_active('git-updater/git-updater.php')) ) { ?>
     
     <div class="notice notice-info">
     <p><strong>Git Updater</strong></p>
@@ -14,3 +20,5 @@ function hovercraft_suggest_git_updater_plugin() {
     <?php } else { }
 
 }
+
+// https://wordpress.stackexchange.com/questions/39408/how-to-make-plugin-required-in-a-wp-theme-without-using-php-conditional-statemen
