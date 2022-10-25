12eb6b6d660455c4520a7a820c98b43b11cc1e22
Jesse Nickles <jesse@littlebizzy.com>
2022-10-26 01:48:20 +0700

    Update suggest-git-updater.php

--- inc/suggest-git-updater.php
+++ inc/suggest-git-updater.php
@@ -1,16 +1,13 @@
 function hovercraft_suggest_git_updater_plugin() {
 
-    $current_user = wp_get_current_user();
+    if ( ! in_array( 'git-updater/git-updater.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) { ?>
 
-    if( current_user_can('manage_options') ) && ( ! is_plugin_active('git-updater/git-updater.php') ) ) { ?>
-    
-    <div class="notice notice-info">
-    <p><strong>Git Updater</strong></p>
-    <p>HoverCraft requires the free Git Updater plugin to receive automatic updates via GitHub.</p>
-    
-    </div>
-    
-    <?php } else { }
+        <div class="notice notice-info">
+        <p><strong>Git Updater</strong></p>
+        <p>HoverCraft requires the free Git Updater plugin to receive automatic updates via GitHub.</p>
+        </div>
+
+    <?php }
 
 }
 
@@ -18,3 +15,4 @@ add_action('admin_notices', 'hovercraft_suggest_git_updater_plugin');
 
 // https://wordpress.stackexchange.com/questions/39408/how-to-make-plugin-required-in-a-wp-theme-without-using-php-conditional-statemen
 // https://gist.github.com/mlbd/b96f62e685ca3d58c17031fcc9e73c07
+// https://www.aurodigo.com/how-to-solve-fatal-error-call-to-undefined-function-is-plugin-active-problem-on-wordpress
