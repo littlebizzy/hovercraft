d589e1597e58abc9c074cd4af2eef4c7d4387f53
Jesse Nickles <jesse@littlebizzy.com>
2022-10-25 21:21:40 +0700

    Update suggest-git-updater.php

--- inc/suggest-git-updater.php
+++ inc/suggest-git-updater.php
@@ -1,10 +1,5 @@
 add_action('admin_notices', 'hovercraft_suggest_git_updater_plugin');
 
-function my_theme_dependencies() {
-  if )
-    echo '<div class="error"><p>' . __( 'Warning: The theme needs Plugin X to function', 'my-theme' ) . '</p></div>';
-}
-
 function hovercraft_suggest_git_updater_plugin() {
 
     $current_user = wp_get_current_user();
