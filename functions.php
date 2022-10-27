404ffe4a6680d4e62809b233dd6417fb20d6718b
Jesse Nickles <jesse@littlebizzy.com>
2022-10-28 01:13:37 +0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -15,6 +15,21 @@ require get_template_directory() . '/inc/enable-menu-locations.php';
 // featured images
 require get_template_directory() . '/inc/featured-images.php';
 
+// logo
+function hovercraft_logo() {
+ $defaults = array(
+ 'height'      => 150,
+ 'width'       => 150,
+ 'flex-height' => false, // <-- setting both flex-height and flex-width to false maintains an aspect ratio
+ 'flex-width'  => false
+ );
+ add_theme_support( 'custom-logo', $defaults );
+}
+
+// call the function in the hook
+add_action( 'after_setup_theme', 'hovercraft_logo' );
+
+
 // variables
 $welcome = "Stop fixing your WordPress theme, and focus on your business.";
 global $hovercraft_excerpt;
