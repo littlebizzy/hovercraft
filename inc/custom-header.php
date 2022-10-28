b1376a5bdff53490238a099cb2644c95ca63d1b4
Jesse Nickles <jesse@littlebizzy.com>
2022-10-29 03:02:42 +0700

    Create custom-header.php

---
+++ inc/custom-header.php
@@ -0,0 +1,14 @@
+<?php
+
+function hovercraft_custom_header_setup() {
+    $args = array(
+        // 'default-image'      => get_template_directory_uri() . 'img/default-image.jpg',
+        // 'default-text-color' => '000',
+        'width'              => 1600,
+        'height'             => 900,
+        'flex-width'         => true,
+        'flex-height'        => true,
+    );
+    add_theme_support( 'custom-header', $args );
+}
+add_action( 'after_setup_theme', 'hovercraft_custom_header_setup' );
