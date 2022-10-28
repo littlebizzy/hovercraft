47b226d2921503013e5eea7db519e7352e4dcf90
Jesse Nickles <jesse@littlebizzy.com>
2022-10-29 03:04:55 +0700

    Create header-video.php

---
+++ inc/header-video.php
@@ -0,0 +1,35 @@
+<?php
+
+function hovercraft_video($wp_customize) {
+// add a setting 
+    $wp_customize->add_setting('hovercraft_video',
+   array(
+      'default' => '',
+      'transport' => 'refresh',
+      'sanitize_callback' => 'absint',
+             'type' => 'theme_mod',
+   		)
+	);
+// Add a control to upload the hover video
+	$wp_customize->add_control(new WP_Customize_Media_Control( $wp_customize, 'video_upload', 
+		array(
+        	'label' => 'Background Video',
+			'description' => esc_html__( 'This is the description for the Media Control' ),
+        	'section' => 'header_image', //this is the section where the custom-logo from WordPress is
+			'mime_type' => 'video',  // Required. Can be image, audio, video, application, text
+        	'settings' => 'hovercraft_video',
+			'button_labels' => array( // Optional
+         	'select' => __( 'Select File' ),
+         	'change' => __( 'Change File' ),
+         	'default' => __( 'Default' ),
+         	'remove' => __( 'Remove' ),
+         	'placeholder' => __( 'No file selected' ),
+         	'frame_title' => __( 'Select File' ),
+         	'frame_button' => __( 'Choose File' ),
+        	'priority' => 8 // show it just below the custom-logo
+    		)
+		)
+	));
+}
+
+add_action('customize_register', 'hovercraft_video');
