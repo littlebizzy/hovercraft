8fc053a207afb7073b17b3cc3904698b1b03c184
Jesse Nickles <jesse@littlebizzy.com>
2022-10-31 05:42:34 +0700

    Update header-text.php

--- template-parts/header-text.php
+++ template-parts/header-text.php
@@ -1,8 +1,6 @@
-<a href="<?php echo home_url(); ?>">
 <div class="branding-right">
 <?php
 	echo '<div class="site-name">' . get_bloginfo('name') . '</div>';
 	echo '<div class="site-tagline">' . get_bloginfo('description') . '</div>';
 ?>
 </div><!-- branding-right -->
-</a>
