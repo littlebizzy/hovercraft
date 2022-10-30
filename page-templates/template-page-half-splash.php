be9e4084aa84e2b60946804fa4e12502d412d06b
Jesse Nickles <jesse@littlebizzy.com>
2022-10-31 04:43:26 +0700

    Create template-page-half-splash.php

---
+++ page-templates/template-page-half-splash.php
@@ -0,0 +1,5 @@
+<?php /* Template Name: Half Splash */ ?>
+<?php get_template_part('header'); ?>
+<?php get_template_part('header-page-half-splash'); ?>
+<?php get_template_part('template-parts/main'); ?>
+<?php get_template_part('footer'); ?>
