352887461cc3781eb6a730da712e6f26c9f23737
Jesse Nickles <jesse@littlebizzy.com>
2022-11-13 22:48:21 +0700

    Update archive.php

--- archive.php
+++ archive.php
@@ -1,3 +1,4 @@
 <?php get_template_part('header'); ?>
+<?php get_template_part('template-parts/header/header-page-no-splash'); ?>
 <?php get_template_part('template-parts/main'); ?>
 <?php get_template_part('footer'); ?>
