3265f11b8d9b6564419712ed2373bda12f298956
Jesse Nickles <jesse@littlebizzy.com>
2022-11-14 15:47:14 +0700

    Create back-to-top.js

---
+++ assets/js/back-to-top.js
@@ -0,0 +1,16 @@
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
+// https://codepen.io/matthewcain/pen/ZepbeR
