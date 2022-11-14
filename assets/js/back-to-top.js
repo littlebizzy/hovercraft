a6faa21e875765e3d2e1c92a12da2ad10617316b
Jesse Nickles <jesse@littlebizzy.com>
2022-11-14 16:36:41 +0700

    Update back-to-top.js

--- assets/js/back-to-top.js
+++ assets/js/back-to-top.js
@@ -1,7 +1,7 @@
-var btn = $('#button');
+var btn = jQuery('#scrollup');
 
-$(window).scroll(function() {
-  if ($(window).scrollTop() > 300) {
+jQuery(window).scroll(function() {
+  if (jQuery(window).scrollTop() > 300) {
     btn.addClass('show');
   } else {
     btn.removeClass('show');
@@ -10,7 +10,7 @@ $(window).scroll(function() {
 
 btn.on('click', function(e) {
   e.preventDefault();
-  $('html, body').animate({scrollTop:0}, '300');
+  jQuery('html, body').animate({scrollTop:0}, '300');
 });
 
 // https://codepen.io/matthewcain/pen/ZepbeR
