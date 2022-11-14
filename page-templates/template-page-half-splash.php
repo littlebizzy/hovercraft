b714a615bf07bdda212f92307110e8874973f8db
Jesse Nickles <jesse@littlebizzy.com>
2022-11-14 15:14:27 +0700

    Update template-page-half-splash.php

--- page-templates/template-page-half-splash.php
+++ page-templates/template-page-half-splash.php
@@ -1,5 +1,5 @@
 <?php /* Template Name: Half Splash */ ?>
 <?php get_template_part('header'); ?>
-<?php get_template_part('template-parts/header/header-page-half-splash'); ?>
+<?php get_template_part('template-parts/header/header-half-splash'); ?>
 <?php get_template_part('template-parts/main'); ?>
 <?php get_template_part('footer'); ?>
