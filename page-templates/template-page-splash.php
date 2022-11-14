e88ab9d5a5ab0ca0259715cb020fcc2f6f5a1704
Jesse Nickles <jesse@littlebizzy.com>
2022-11-14 15:13:06 +0700

    Update template-page-splash.php

--- page-templates/template-page-splash.php
+++ page-templates/template-page-splash.php
@@ -1,5 +1,5 @@
 <?php /* Template Name: Splash */ ?>
 <?php get_template_part('header'); ?>
-<?php get_template_part('template-parts/header/header-page-splash'); ?>
+<?php get_template_part('template-parts/header/header-splash'); ?>
 <?php get_template_part('template-parts/main'); ?>
 <?php get_template_part('footer'); ?>
