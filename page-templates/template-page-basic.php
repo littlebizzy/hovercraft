9b34fd73891f1d0949ff4cd918f23a5dec6bb5e6
Jesse Nickles <jesse@littlebizzy.com>
2022-11-15 17:45:42 +0700

    Update template-page-basic.php

--- page-templates/template-page-basic.php
+++ page-templates/template-page-basic.php
@@ -1,5 +1,5 @@
 <?php /* Template Name: Basic */ ?>
 <?php get_template_part('header'); ?>
 <?php get_template_part('template-parts/header/header-basic'); ?>
-<?php get_template_part('template-parts/content/main'); ?>
+<?php get_template_part('template-parts/content/main-basic'); ?>
 <?php get_template_part('footer'); ?>
