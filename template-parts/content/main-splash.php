a6bb39d5be788a1b9e44be0c6a338c023cfe7bc7
Jesse Nickles <jesse@littlebizzy.com>
2022-11-15 17:31:37 +0700

    Create main-splash.php

---
+++ template-parts/content/main-splash.php
@@ -0,0 +1,7 @@
+<div id="main">
+<div class="inner">
+<?php get_template_part('template-parts/content/primary-splash'); ?>
+<?php get_template_part('sidebar'); ?>
+<div class="clear"></div>
+</div><!-- inner -->
+</div><!-- main -->
