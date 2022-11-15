b66f772d8f4fa4ae4b7525249236707b8f6edf98
Jesse Nickles <jesse@littlebizzy.com>
2022-11-15 17:43:15 +0700

    Update main.php

--- template-parts/main.php
+++ template-parts/main.php
@@ -1,6 +1,6 @@
 <div id="main">
 <div class="inner">
-<?php get_template_part('template-parts/primary'); ?>
+<?php get_template_part('template-parts/content/primary-basic'); ?>
 <?php get_template_part('sidebar'); ?>
 <div class="clear"></div>
 </div><!-- inner -->
