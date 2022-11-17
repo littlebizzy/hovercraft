fe943dc2f21b59e38706f826162b3f4e653105be
Jesse Nickles <jesse@littlebizzy.com>
2022-11-17 21:41:14 +0700

    Update main-splash.php

--- template-parts/content/main-splash.php
+++ template-parts/content/main-splash.php
@@ -2,6 +2,7 @@
 <div class="inner">
 <?php get_template_part('template-parts/content/primary-splash'); ?>
 <?php get_template_part('sidebar'); ?>
+<?php get_template_part('template-parts/pagination'); ?>
 <div class="clear"></div>
 </div><!-- inner -->
 </div><!-- main -->
