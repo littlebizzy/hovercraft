80f16a1062c528f233bcfb55aadf0a901131facd
Jesse Nickles <jesse@littlebizzy.com>
2022-11-17 21:54:59 +0700

    Update archive.php

--- archive.php
+++ archive.php
@@ -1,4 +1,4 @@
 <?php get_template_part('header'); ?>
 <?php get_template_part('template-parts/header/header-basic'); ?>
-<?php get_template_part('template-parts/main'); ?>
+<?php get_template_part('template-parts/content/main-basic'); ?>
 <?php get_template_part('footer'); ?>
