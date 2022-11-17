15cee1c44f804117498eb9974b5a5bb63d907aa2
Jesse Nickles <jesse@littlebizzy.com>
2022-11-17 21:34:23 +0700

    Create pagination.php

---
+++ template-parts/pagination.php
@@ -0,0 +1,8 @@
+<?php
+
+// https://njengah.com/pagination-code-in-wordpress/
+
+?>
+
+<div class="nav-previous alignleft"><?php previous_posts_link( 'Older posts' ); ?></div>
+<div class="nav-next alignright"><?php next_posts_link( 'Newer posts' ); ?></div>
