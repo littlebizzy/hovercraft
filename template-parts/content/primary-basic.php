13f4c8e6f949a2d01b3e04c1cec7e53d298c9c1e
Jesse Nickles <jesse@littlebizzy.com>
2022-11-17 21:50:27 +0700

    Update primary-basic.php

--- template-parts/content/primary-basic.php
+++ template-parts/content/primary-basic.php
@@ -33,13 +33,13 @@
 	<?php endwhile; endif; ?>	
 <?php } ?><!-- page -->
 	
-<!-- blog archive -->
-<?php if (is_category('blog')) { ?>
+<!-- archive -->
+<?php if (is_archive()) { ?>
 	<h1><?php single_cat_title(); ?></h1>
 	<?php if (have_posts()): while (have_posts()): the_post(); ?>
 		<h4><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h4>
     <?php endwhile; endif; ?>
-<?php } ?><!-- blog archive -->
+<?php } ?><!-- archive -->
 
 
 <div class="last-modified">
