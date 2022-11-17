70cab933247edaba7b0f933c5188fcff0c858cb9
Jesse Nickles <jesse@littlebizzy.com>
2022-11-17 15:48:40 +0700

    Update primary-splash.php

--- template-parts/content/primary-splash.php
+++ template-parts/content/primary-splash.php
@@ -4,5 +4,9 @@
 	<?php the_content(); ?>
 	<?php endwhile; endif; ?>
 
+<div class="last-modified">
+	<?php echo "<span>Last modified on "; the_modified_time('F jS, Y'); echo "</span>"; ?>
+</div><!-- last-modified -->
+
 <div class="clear"></div>
 </div><!-- primary -->
