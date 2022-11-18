c0153204324af4b41688c61df33305cba889bfc3
Jesse Nickles <jesse@littlebizzy.com>
2022-11-18 18:09:11 +0700

    Update branding.php

--- template-parts/header/branding.php
+++ template-parts/header/branding.php
@@ -3,20 +3,15 @@
 	<?php if(get_theme_mod('custom_logo', '') != '') { ?>
 		<a href="<?php echo home_url(); ?>" class="custom-logo-link" rel="home">
 		<img class="site-logo" src="<?php 
-			// $logo_default_id = get_theme_mod( 'custom_logo' );
-			// $logo_default_url = wp_get_attachment_image_src( $logo_default_id , 'full' );
-			$logo_default_url = wp_get_attachment_url( get_theme_mod( 'custom_logo' );
+			$logo_default_url = wp_get_attachment_url( get_theme_mod( 'custom_logo' ) );
 			$logo_alternative_url = get_theme_mod( 'hovercraft_logo_alternative' );
 			if ( is_page_template( array( 
-				'page-templates/template-page-splash.php', 
-				'page-templates/template-page-half-splash.php',
-				'page-templates/template-page-mini-splash.php'
+				'page-templates/template-page-splash.php'
 				) ) ) {
 				echo esc_url($logo_alternative_url);
 			} else { 
-				echo esc_url($logo_default_url[0]); 
-			} ?>);
-		?>" class="custom-logo" alt="<?php echo get_bloginfo('name'); ?>" />
+				echo esc_url($logo_default_url); 
+			} ?>" class="custom-logo" alt="<?php echo get_bloginfo('name'); ?>" />
 		</a><!-- custom-logo-link -->
 	<?php } ?>
 	</div><!-- branding-left -->
