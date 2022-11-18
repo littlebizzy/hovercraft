3bda0912ac90ad0a83f616ab083dc17e1603cbbc
Jesse Nickles <jesse@littlebizzy.com>
2022-11-18 14:17:12 +0700

    Update logo-default.php

--- inc/logo-default.php
+++ inc/logo-default.php
@@ -1,14 +1,14 @@
 <?php
 
-function hovercraft_logo() {
- $defaults = array(
- //'height'      => 150,
-// 'width'       => 150,
- 'flex-height' => false, // <-- setting both flex-height and flex-width to false maintains an aspect ratio
- 'flex-width'  => false
- );
- add_theme_support( 'custom-logo', $defaults );
-}
+/** default logo */
 
-// call the function in the hook
+function hovercraft_logo() {
+    $defaults = array(
+    // 'height'      => 512,
+    // 'width'       => 512,
+    'flex-height' => false, // setting both flex-height and flex-width to false maintains aspect ratio
+    'flex-width'  => false
+    );
+    add_theme_support( 'custom-logo', $defaults );
+}
 add_action( 'after_setup_theme', 'hovercraft_logo' );
