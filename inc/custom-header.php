5f6d82fec1dfa329b12eca368934d5271937b96c
Jesse Nickles <jesse@littlebizzy.com>
2022-11-18 17:10:34 +0700

    Update custom-header.php

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -5,11 +5,10 @@
 function hovercraft_custom_header() {
     $args = array(
         // 'default-image' => get_template_directory_uri() . 'img/default-image.jpg',
-        // 'default-text-color' => '000',
         'width' => 1920,
         'height' => 1080,
         'flex-width' => true,
-        'flex-height' => true,
+        'flex-height' => true
         );
     add_theme_support( 'custom-header', $args );
 }
