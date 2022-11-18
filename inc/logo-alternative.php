44a63719bbf3878240dc6adef099501fa7852121
Jesse Nickles <jesse@littlebizzy.com>
2022-11-18 14:26:59 +0700

    Update logo-alternative.php

--- inc/logo-alternative.php
+++ inc/logo-alternative.php
@@ -1,15 +1,14 @@
 <?php
 
-function hovercraft_logo_transparent($wp_customize) {
-// add a setting 
-    $wp_customize->add_setting('hovercraft_logo_transparent');
-// Add a control to upload the hover logo
-    $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'hovercraft_logo_transparent', array(
-        'label' => 'Alternative Logo',
-        'section' => 'title_tagline', //this is the section where the custom-logo from WordPress is
-        'settings' => 'hovercraft_logo_transparent',
-        'priority' => 8 // show it just below the custom-logo
-    )));
-}
+/** alternative logo */
 
-add_action('customize_register', 'hovercraft_logo_transparent');
+function hovercraft_logo_alternative( $wp_customize ) {
+    $wp_customize->add_setting( 'hovercraft_logo_alternative' );
+    $wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize, 'hovercraft_logo_alternative', array( 
+        'label' => 'Alternative Logo',
+        'section' => 'title_tagline',
+        'settings' => 'hovercraft_logo_alternative',
+        'priority' => 8 // display just below the custom-logo
+        ) ) );
+}
+add_action( 'customize_register', 'hovercraft_logo_alternative' );
