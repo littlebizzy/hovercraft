21e5e6ef2af3248527e3b5f8881537fdb214c6c2
Jesse Nickles <jesse@littlebizzy.com>
2022-11-18 17:44:27 +0700

    Delete header-text.php

--- template-parts/header/header-text.php
+++
@@ -1,6 +0,0 @@
-<div class="branding-right">
-<?php
-	echo '<div class="site-name">' . get_bloginfo('name') . '</div>';
-	echo '<div class="site-tagline">' . get_bloginfo('description') . '</div>';
-?>
-</div><!-- branding-right -->
