7ccfc22d774b220894870aa869e6b1206acd4afc
Jesse Nickles <jesse@littlebizzy.com>
2022-11-19 15:03:08 +0700

    Update menu-locations.php

--- inc/menu-locations.php
+++ inc/menu-locations.php
@@ -1,39 +1,25 @@
 <?php
 
-// register header menu
-function hovercraft_main_menu() {
-	register_nav_menu('main-menu',__( 'Main Menu' ));
+// all menu locations
+function hovercraft_menu_locations() { 
+	register_nav_menus(
+        array(
+            'main-menu' => 'Main Menu',
+            'cta-header-primary' => 'Header Primary CTA',
+			'cta-header-secondary' => 'Header Secondary CTA',
+			'cta-hero-primary' => 'Hero Primary CTA',
+			'cta-hero-secondary' => 'Hero Secondary CTA',
+        )
+    ); 
 }
-add_action( 'init', 'hovercraft_main_menu' );
+add_action( 'init', 'hovercraft_menu_locations' );
 
 
+// limit cta-header-primary to one link
+add_filter( 'wp_nav_menu_objects', 'hovercraft_cta_header_primary_limit', 10, 2 );
 
-// register header cta menu
-function hovercraft_cta_header() {
-	register_nav_menu('cta-header',__( 'Header CTA' ));
-}
-add_action( 'init', 'hovercraft_cta_header' );
-
-// menu splash cta major
-function hovercraft_cta_splash_major() {
-	register_nav_menu('cta-splash-major',__( 'Splash CTA (Major)' ));
-}
-add_action( 'init', 'hovercraft_cta_splash_major' );
-
-// menu splash cta minor
-function hovercraft_cta_splash_minor() {
-	register_nav_menu('cta-splash-minor',__( 'Splash CTA (Minor)' ));
-}
-add_action( 'init', 'hovercraft_cta_splash_minor' );
-
-
-// llimit header cta to single item
-// https://www.isitwp.com/limit-amount-of-menu-items/
-add_filter( 'wp_nav_menu_objects', 'hovercraft_header_cta_limit', 10, 2 );
-
-function hovercraft_header_cta_limit($items, $args) {
-    // want our MAINMENU to have MAX of 7 items
-    if ( $args->theme_location == 'cta-header' ) {
+function hovercraft_cta_header_primary_limit($items, $args) {
+    if ( $args->theme_location == 'cta-header-primary' ) {
         $toplinks = 0;
         foreach ( $items as $k => $v ) {
             if ( $v->menu_item_parent == 0 ) {
@@ -49,3 +35,65 @@ function hovercraft_header_cta_limit($items, $args) {
     return $items;
 }
 
+// limit cta-header-secondary to one link
+add_filter( 'wp_nav_menu_objects', 'hovercraft_cta_header_secondary_limit', 10, 2 );
+
+function hovercraft_cta_header_secondary_limit($items, $args) {
+    if ( $args->theme_location == 'cta-header-secondary' ) {
+        $toplinks = 0;
+        foreach ( $items as $k => $v ) {
+            if ( $v->menu_item_parent == 0 ) {
+                // count how many top-level links we have so far...
+                $toplinks++;
+            }
+            // if we've passed our max # ...
+            if ( $toplinks > 1 ) {
+                unset($items[$k]);
+            }
+        }
+    }
+    return $items;
+}
+
+// limit cta-hero-primary to one link
+add_filter( 'wp_nav_menu_objects', 'hovercraft_cta_hero_primary_limit', 10, 2 );
+
+function hovercraft_cta_hero_primary_limit($items, $args) {
+    if ( $args->theme_location == 'cta-hero-primary' ) {
+        $toplinks = 0;
+        foreach ( $items as $k => $v ) {
+            if ( $v->menu_item_parent == 0 ) {
+                // count how many top-level links we have so far...
+                $toplinks++;
+            }
+            // if we've passed our max # ...
+            if ( $toplinks > 1 ) {
+                unset($items[$k]);
+            }
+        }
+    }
+    return $items;
+}
+
+// limit cta-hero-secondary to one link
+add_filter( 'wp_nav_menu_objects', 'hovercraft_cta_hero_secondary_limit', 10, 2 );
+
+function hovercraft_cta_hero_secondary_limit($items, $args) {
+    if ( $args->theme_location == 'cta-hero-secondary' ) {
+        $toplinks = 0;
+        foreach ( $items as $k => $v ) {
+            if ( $v->menu_item_parent == 0 ) {
+                // count how many top-level links we have so far...
+                $toplinks++;
+            }
+            // if we've passed our max # ...
+            if ( $toplinks > 1 ) {
+                unset($items[$k]);
+            }
+        }
+    }
+    return $items;
+}
+
+// https://hollypryce.com/custom-menu-wordpress/
+// https://www.isitwp.com/limit-amount-of-menu-items/
