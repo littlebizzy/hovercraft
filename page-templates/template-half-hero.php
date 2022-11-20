6989dae246b3485bde88f7baa75f605efeb5cda7
Jesse Nickles <jesse@littlebizzy.com>
2022-11-20 14:26:45 +0700

    Update and rename template-page-half-splash.php to template-half-hero.php

--- page-templates/template-half-hero.php
+++ page-templates/template-half-hero.php
@@ -1,5 +1,5 @@
-<?php /* Template Name: Half Splash */ ?>
+<?php /* Template Name: Half Hero */ ?>
 <?php get_template_part('header'); ?>
-<?php get_template_part('template-parts/header/header-half-splash'); ?>
+<?php get_template_part('template-parts/header/header-half-hero'); ?>
 <?php get_template_part('template-parts/content/main-splash'); ?>
 <?php get_template_part('footer'); ?>
