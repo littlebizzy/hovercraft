7e24bfadaf3c7fcdcd7a1c4ec0af606d8f6a8c68
Jesse Nickles <jesse@littlebizzy.com>
2022-11-20 16:03:54 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,14 +1,18 @@
 <?php if ( is_active_sidebar( 'hovercraft_prefooter_top' ) ) { ?>
 <div id="prefooter-top">
+<div class="inner">
 	<?php dynamic_sidebar( 'hovercraft_prefooter_top' ); ?>
-<div class="clear"></div>
+	<div class="clear"></div>
+</div><!--inner -->
 </div><!-- prefooter-top -->
 <?php } ?>
 
 <?php if ( is_active_sidebar( 'hovercraft_prefooter_bottom' ) ) { ?>
 <div id="prefooter-bottom">
+<div class="inner">
 	<?php dynamic_sidebar( 'hovercraft_prefooter_bottom' ); ?>
-<div class="clear"></div>
+	<div class="clear"></div>
+</div><!--inner -->
 </div><!-- prefooter-bottom -->
 <?php } ?>
 
@@ -35,7 +39,7 @@
 	<div class="clear"></div>
 	</div><!-- footer-section-left -->
 	
-<div class="clear"></div>
+	<div class="clear"></div>
 </div><!--inner -->
 </div><!-- footer -->
 
@@ -55,7 +59,7 @@
 	</div><!-- copyright-right -->
 	<?php } ?>
 
-<div class="clear"></div>
+	<div class="clear"></div>
 </div><!--inner -->
 </div><!-- copyright -->
 
