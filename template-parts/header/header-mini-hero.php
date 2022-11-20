0677d24e25dbf2bf577551547061f8ad9bffab13
Jesse Nickles <jesse@littlebizzy.com>
2022-11-20 15:17:58 +0700

    Update header-mini-hero.php

--- template-parts/header/header-mini-hero.php
+++ template-parts/header/header-mini-hero.php
@@ -4,11 +4,10 @@
     rgba(255, 255, 255, 0) 100%), url(<?php 
 	$url_header_image = esc_url( get_header_image() );
 	$url_featured_image = wp_get_attachment_url( get_post_thumbnail_id($post->ID), 'large' ); 
-	$url_random_unsplash = "https://source.unsplash.com/featured/1600x900?mountains,people,city";
-	if (!empty($url_featured_image)){ echo $url_featured_image; } else { echo $url_random_unsplash; } ?>);		
-background-position: center center;
-background-size:cover;
-background-repeat:no-repeat;
+	if (!empty($url_featured_image)){ echo $url_featured_image; } else { echo $url_header_image; } ?>);		
+	background-position: center center;
+	background-size: cover;
+	background-repeat: no-repeat;
 ">
 
 <div id="header-mini-hero">
