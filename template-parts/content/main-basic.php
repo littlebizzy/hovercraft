6d8b28765f7acbe572e767aaf17483b1cb92bb24
Jesse Nickles <jesse@littlebizzy.com>
2022-11-22 17:50:19 +0700

    Update main-basic.php

--- template-parts/content/main-basic.php
+++ template-parts/content/main-basic.php
@@ -1,8 +1,8 @@
 <div id="main">
 <div class="inner">
-<?php get_template_part('template-parts/content/primary-basic'); ?>
-<?php get_template_part('sidebar'); ?>
-<?php get_template_part('template-parts/pagination'); ?>
-<div class="clear"></div>
+    <?php get_template_part( 'template-parts/content/primary' ); ?>
+    <?php get_template_part( 'sidebar' ); ?>
+    <?php get_template_part( 'template-parts/pagination' ); ?>
+    <div class="clear"></div>
 </div><!-- inner -->
 </div><!-- main -->
