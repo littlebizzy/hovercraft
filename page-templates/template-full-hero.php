983c6b39d5088dfcda26dee88eb72f2c275f4d0c
Jesse Nickles <jesse@littlebizzy.com>
2022-11-22 17:55:00 +0700

    Update template-full-hero.php

--- page-templates/template-full-hero.php
+++ page-templates/template-full-hero.php
@@ -1,5 +1,5 @@
 <?php /* Template Name: Full Hero */ ?>
-<?php get_template_part('header'); ?>
-<?php get_template_part('template-parts/header/header-full-hero'); ?>
-<?php get_template_part('template-parts/content/main-splash'); ?>
-<?php get_template_part('footer'); ?>
+<?php get_template_part( 'header' ); ?>
+<?php get_template_part( 'template-parts/header/header-full-hero' ); ?>
+<?php get_template_part( 'template-parts/content/main' ); ?>
+<?php get_template_part( 'footer' ); ?>
