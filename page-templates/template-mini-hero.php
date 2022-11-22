a41c818ba4e70587c414119c3a1202333755e003
Jesse Nickles <jesse@littlebizzy.com>
2022-11-22 17:56:23 +0700

    Update template-mini-hero.php

--- page-templates/template-mini-hero.php
+++ page-templates/template-mini-hero.php
@@ -1,5 +1,5 @@
 <?php /* Template Name: Mini Hero */ ?>
-<?php get_template_part('header'); ?>
-<?php get_template_part('template-parts/header/header-mini-hero'); ?>
-<?php get_template_part('template-parts/content/main-splash'); ?>
-<?php get_template_part('footer'); ?>
+<?php get_template_part( 'header' ); ?>
+<?php get_template_part( 'template-parts/header/header-mini-hero' ); ?>
+<?php get_template_part( 'template-parts/content/main' ); ?>
+<?php get_template_part( 'footer' ); ?>
