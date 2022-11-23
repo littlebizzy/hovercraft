78ee160cd280e27deb4fe61536614aa29b06aadc
Jesse Nickles <jesse@littlebizzy.com>
2022-11-23 21:56:16 +0700

    Update template-full-hero-tiles.php

--- page-templates/template-full-hero-tiles.php
+++ page-templates/template-full-hero-tiles.php
@@ -1,5 +1,5 @@
 <?php /* Template Name: Full Hero Tiles */ ?>
 <?php get_template_part( 'header' ); ?>
 <?php get_template_part( 'template-parts/header/header-full-hero' ); ?>
-<?php get_template_part( 'template-parts/content/main' ); ?>
+<?php get_template_part( 'template-parts/content/tiles' ); ?>
 <?php get_template_part( 'footer' ); ?>
