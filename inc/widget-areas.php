a2e4abe4a64393f0683a44659c8a284fadfd0335
Jesse Nickles <jesse@littlebizzy.com>
2022-11-23 23:26:35 +0700

    Update widget-areas.php

--- inc/widget-areas.php
+++ inc/widget-areas.php
@@ -6,7 +6,7 @@ function hovercraft_topbar_primary() {
 	register_sidebar( array(
 		'name'          => 'Topbar Primary',
 		'id'            => 'hovercraft_topbar_primary',
-		'before_widget' => '<div class="widgets-topbar-primary-wrapper">',
+		'before_widget' => '<div class="widget-topbar-primary widget-wrapper">',
 		'after_widget'  => '</div>',
 		'before_title'  => '<h3 class="widget-title">',
 		'after_title'   => '</h3>',
@@ -22,7 +22,7 @@ function hovercraft_topbar_secondary() {
 	register_sidebar( array(
 		'name'          => 'Topbar Secondary',
 		'id'            => 'hovercraft_topbar_secondary',
-		'before_widget' => '<div class="widgets-topbar-secondary-wrapper">',
+		'before_widget' => '<div class="widget-topbar-secondary widget-wrapper">',
 		'after_widget'  => '</div>',
 		'before_title'  => '<h3 class="widget-title">',
 		'after_title'   => '</h3>',
@@ -38,7 +38,7 @@ function hovercraft_sidebar() {
 	register_sidebar( array(
 		'name'          => 'Sidebar',
 		'id'            => 'hovercraft_sidebar',
-		'before_widget' => '<div class="widgets-sidebar-wrapper">',
+		'before_widget' => '<div class="widget-sidebar widget-wrapper">',
 		'after_widget'  => '</div>',
 		'before_title'  => '<h3 class="widget-title">',
 		'after_title'   => '</h3>',
@@ -54,7 +54,7 @@ function hovercraft_prefooter_top() {
 	register_sidebar( array(
 		'name'          => 'Prefooter Top',
 		'id'            => 'hovercraft_prefooter_top',
-		'before_widget' => '<div class="widgets-prefooter-top-wrapper">',
+		'before_widget' => '<div class="widget-prefooter-top widget-wrapper">',
 		'after_widget'  => '</div>',
 		'before_title'  => '<h3 class="widget-title">',
 		'after_title'   => '</h3>',
@@ -70,7 +70,7 @@ function hovercraft_prefooter_bottom() {
 	register_sidebar( array(
 		'name'          => 'Prefooter Bottom',
 		'id'            => 'hovercraft_prefooter_bottom',
-		'before_widget' => '<div class="widgets-prefooter-bottom-wrapper">',
+		'before_widget' => '<div class="widget-prefooter-bottom widget-wrapper">',
 		'after_widget'  => '</div>',
 		'before_title'  => '<h3 class="widget-title">',
 		'after_title'   => '</h3>',
@@ -86,7 +86,7 @@ function hovercraft_footer_one() {
 	register_sidebar( array(
 		'name'          => 'Footer #1',
 		'id'            => 'hovercraft_footer_one',
-		'before_widget' => '<div class="widgets-footer-one-wrapper">',
+		'before_widget' => '<div class="widget-footer-one widget-wrapper">',
 		'after_widget'  => '</div>',
 		'before_title'  => '<h3 class="widget-title">',
 		'after_title'   => '</h3>',
@@ -102,7 +102,7 @@ function hovercraft_footer_two() {
 	register_sidebar( array(
 		'name'          => 'Footer #2',
 		'id'            => 'hovercraft_footer_two',
-		'before_widget' => '<div class="widgets-footer-two-wrapper">',
+		'before_widget' => '<div class="widget-footer-two widget-wrapper">',
 		'after_widget'  => '</div>',
 		'before_title'  => '<h3 class="widget-title">',
 		'after_title'   => '</h3>',
@@ -118,7 +118,7 @@ function hovercraft_footer_three() {
 	register_sidebar( array(
 		'name'          => 'Footer #3',
 		'id'            => 'hovercraft_footer_three',
-		'before_widget' => '<div class="widgets-footer-three-wrapper">',
+		'before_widget' => '<div class="widget-footer-three widget-wrapper">',
 		'after_widget'  => '</div>',
 		'before_title'  => '<h3 class="widget-title">',
 		'after_title'   => '</h3>',
@@ -134,7 +134,7 @@ function hovercraft_footer_four() {
 	register_sidebar( array(
 		'name'          => 'Footer #4',
 		'id'            => 'hovercraft_footer_four',
-		'before_widget' => '<div class="widgets-footer-four-wrapper">',
+		'before_widget' => '<div class="widget-footer-four widget-wrapper">',
 		'after_widget'  => '</div>',
 		'before_title'  => '<h3 class="widget-title">',
 		'after_title'   => '</h3>',
@@ -150,7 +150,7 @@ function hovercraft_copyright() {
 	register_sidebar( array(
 		'name'          => 'Copyright',
 		'id'            => 'hovercraft_copyright',
-		'before_widget' => '<div class="widgets-copyright-wrapper">',
+		'before_widget' => '<div class="widget-copyright widget-wrapper">',
 		'after_widget'  => '</div>',
 		'before_title'  => '<h3 class="widget-title">',
 		'after_title'   => '</h3>',
@@ -166,7 +166,7 @@ function hovercraft_tile_one() {
 	register_sidebar( array(
 		'name'          => 'Tile #1',
 		'id'            => 'hovercraft_tile_one',
-		'before_widget' => '<div class="widgets-tile-one-wrapper">',
+		'before_widget' => '<div class="widget-tile-one widget-wrapper">',
 		'after_widget'  => '</div>',
 		'before_title'  => '<h3 class="widget-title">',
 		'after_title'   => '</h3>',
@@ -182,7 +182,7 @@ function hovercraft_tile_two() {
 	register_sidebar( array(
 		'name'          => 'Tile #2',
 		'id'            => 'hovercraft_tile_two',
-		'before_widget' => '<div class="widgets-tile-two-wrapper">',
+		'before_widget' => '<div class="widget-tile-two widget-wrapper">',
 		'after_widget'  => '</div>',
 		'before_title'  => '<h3 class="widget-title">',
 		'after_title'   => '</h3>',
@@ -198,7 +198,7 @@ function hovercraft_tile_three() {
 	register_sidebar( array(
 		'name'          => 'Tile #3',
 		'id'            => 'hovercraft_tile_three',
-		'before_widget' => '<div class="widgets-tile-three-wrapper">',
+		'before_widget' => '<div class="widget-tile-three widget-wrapper">',
 		'after_widget'  => '</div>',
 		'before_title'  => '<h3 class="widget-title">',
 		'after_title'   => '</h3>',
@@ -214,7 +214,7 @@ function hovercraft_tile_four() {
 	register_sidebar( array(
 		'name'          => 'Tile #4',
 		'id'            => 'hovercraft_tile_four',
-		'before_widget' => '<div class="widgets-tile-four-wrapper">',
+		'before_widget' => '<div class="widget-tile-four widget-wrapper">',
 		'after_widget'  => '</div>',
 		'before_title'  => '<h3 class="widget-title">',
 		'after_title'   => '</h3>',
@@ -230,7 +230,7 @@ function hovercraft_tile_five() {
 	register_sidebar( array(
 		'name'          => 'Tile #5',
 		'id'            => 'hovercraft_tile_five',
-		'before_widget' => '<div class="widgets-tile-five-wrapper">',
+		'before_widget' => '<div class="widget-tile-five widget-wrapper">',
 		'after_widget'  => '</div>',
 		'before_title'  => '<h3 class="widget-title">',
 		'after_title'   => '</h3>',
@@ -246,7 +246,7 @@ function hovercraft_tile_six() {
 	register_sidebar( array(
 		'name'          => 'Tile #6',
 		'id'            => 'hovercraft_tile_six',
-		'before_widget' => '<div class="widgets-tile-six-wrapper">',
+		'before_widget' => '<div class="widget-tile-six widget-wrapper">',
 		'after_widget'  => '</div>',
 		'before_title'  => '<h3 class="widget-title">',
 		'after_title'   => '</h3>',
@@ -262,7 +262,7 @@ function hovercraft_tile_seven() {
 	register_sidebar( array(
 		'name'          => 'Tile #7',
 		'id'            => 'hovercraft_tile_seven',
-		'before_widget' => '<div class="widgets-tile-seven-wrapper">',
+		'before_widget' => '<div class="widget-tile-seven widget-wrapper">',
 		'after_widget'  => '</div>',
 		'before_title'  => '<h3 class="widget-title">',
 		'after_title'   => '</h3>',
@@ -278,7 +278,7 @@ function hovercraft_tile_eight() {
 	register_sidebar( array(
 		'name'          => 'Tile #8',
 		'id'            => 'hovercraft_tile_eight',
-		'before_widget' => '<div class="widgets-tile-eight-wrapper">',
+		'before_widget' => '<div class="widget-tile-eight widget-wrapper">',
 		'after_widget'  => '</div>',
 		'before_title'  => '<h3 class="widget-title">',
 		'after_title'   => '</h3>',
@@ -294,7 +294,7 @@ function hovercraft_tile_nine() {
 	register_sidebar( array(
 		'name'          => 'Tile #9',
 		'id'            => 'hovercraft_tile_nine',
-		'before_widget' => '<div class="widgets-tile-nine-wrapper">',
+		'before_widget' => '<div class="widget-tile-nine widget-wrapper">',
 		'after_widget'  => '</div>',
 		'before_title'  => '<h3 class="widget-title">',
 		'after_title'   => '</h3>',
@@ -310,7 +310,7 @@ function hovercraft_tile_ten() {
 	register_sidebar( array(
 		'name'          => 'Tile #10',
 		'id'            => 'hovercraft_tile_ten',
-		'before_widget' => '<div class="widgets-tile-ten-wrapper">',
+		'before_widget' => '<div class="widget-tile-ten widget-wrapper">',
 		'after_widget'  => '</div>',
 		'before_title'  => '<h3 class="widget-title">',
 		'after_title'   => '</h3>',
@@ -326,7 +326,7 @@ function hovercraft_tile_eleven() {
 	register_sidebar( array(
 		'name'          => 'Tile #11',
 		'id'            => 'hovercraft_tile_eleven',
-		'before_widget' => '<div class="widgets-tile-eleven-wrapper">',
+		'before_widget' => '<div class="widget-tile-eleven widget-wrapper">',
 		'after_widget'  => '</div>',
 		'before_title'  => '<h3 class="widget-title">',
 		'after_title'   => '</h3>',
@@ -342,7 +342,7 @@ function hovercraft_tile_twelve() {
 	register_sidebar( array(
 		'name'          => 'Tile #12',
 		'id'            => 'hovercraft_tile_twelve',
-		'before_widget' => '<div class="widgets-tile-twelve-wrapper">',
+		'before_widget' => '<div class="widget-tile-twelve widget-wrapper">',
 		'after_widget'  => '</div>',
 		'before_title'  => '<h3 class="widget-title">',
 		'after_title'   => '</h3>',
