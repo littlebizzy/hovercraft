b47c9a94bf2fe573d2607c711fb0c2ff205a218d
Jesse Nickles <jesse@littlebizzy.com>
2022-11-23 22:02:31 +0700

    Update tiles.php

--- template-parts/content/tiles.php
+++ template-parts/content/tiles.php
@@ -1,1 +1,16 @@
-
+<div id="main">
+<div class="inner">
+    <?php get_template_part( 'template-parts/content/primary' ); ?>
+	
+	<?php if ( is_active_sidebar( 'hovercraft_tile_one' ) ) { ?>
+	<div class="tile">
+		<?php dynamic_sidebar( 'hovercraft_tile_one' ); ?>
+		<div class="clear"></div>
+	</div><!-- tile -->
+	<?php } ?>
+    
+    
+    <?php get_template_part( 'template-parts/pagination' ); ?>
+    <div class="clear"></div>
+</div><!-- inner -->
+</div><!-- main -->
