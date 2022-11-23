d17c5ace4b4b635a91badea73417e7f10a5bb2e2
Jesse Nickles <jesse@littlebizzy.com>
2022-11-23 22:28:33 +0700

    Update widget-areas.php

--- inc/widget-areas.php
+++ inc/widget-areas.php
@@ -174,3 +174,179 @@ function hovercraft_tile_one() {
 
 }
 add_action( 'widgets_init', 'hovercraft_tile_one' );
+
+
+// tile two widgets
+function hovercraft_tile_two() {
+
+	register_sidebar( array(
+		'name'          => 'Tile #2',
+		'id'            => 'hovercraft_tile_two',
+		'before_widget' => '<div class="widgets-tile-two-wrapper">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
+	) );
+
+}
+add_action( 'widgets_init', 'hovercraft_tile_two' );
+
+
+// tile three widgets
+function hovercraft_tile_three() {
+
+	register_sidebar( array(
+		'name'          => 'Tile #3',
+		'id'            => 'hovercraft_tile_three',
+		'before_widget' => '<div class="widgets-tile-three-wrapper">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
+	) );
+
+}
+add_action( 'widgets_init', 'hovercraft_tile_three' );
+
+
+// tile four widgets
+function hovercraft_tile_four() {
+
+	register_sidebar( array(
+		'name'          => 'Tile #4',
+		'id'            => 'hovercraft_tile_four',
+		'before_widget' => '<div class="widgets-tile-four-wrapper">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
+	) );
+
+}
+add_action( 'widgets_init', 'hovercraft_tile_four' );
+
+
+// tile five widgets
+function hovercraft_tile_five() {
+
+	register_sidebar( array(
+		'name'          => 'Tile #5',
+		'id'            => 'hovercraft_tile_five',
+		'before_widget' => '<div class="widgets-tile-five-wrapper">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
+	) );
+
+}
+add_action( 'widgets_init', 'hovercraft_tile_five' );
+
+
+// tile six widgets
+function hovercraft_tile_six() {
+
+	register_sidebar( array(
+		'name'          => 'Tile #6',
+		'id'            => 'hovercraft_tile_six',
+		'before_widget' => '<div class="widgets-tile-six-wrapper">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
+	) );
+
+}
+add_action( 'widgets_init', 'hovercraft_tile_six' );
+
+
+// tile seven widgets
+function hovercraft_tile_seven() {
+
+	register_sidebar( array(
+		'name'          => 'Tile #7',
+		'id'            => 'hovercraft_tile_seven',
+		'before_widget' => '<div class="widgets-tile-seven-wrapper">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
+	) );
+
+}
+add_action( 'widgets_init', 'hovercraft_tile_seven' );
+
+
+// tile eight widgets
+function hovercraft_tile_eight() {
+
+	register_sidebar( array(
+		'name'          => 'Tile #8',
+		'id'            => 'hovercraft_tile_eight',
+		'before_widget' => '<div class="widgets-tile-eight-wrapper">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
+	) );
+
+}
+add_action( 'widgets_init', 'hovercraft_tile_eight' );
+
+
+// tile nine widgets
+function hovercraft_tile_nine() {
+
+	register_sidebar( array(
+		'name'          => 'Tile #9',
+		'id'            => 'hovercraft_tile_nine',
+		'before_widget' => '<div class="widgets-tile-nine-wrapper">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
+	) );
+
+}
+add_action( 'widgets_init', 'hovercraft_tile_nine' );
+
+
+// tile ten widgets
+function hovercraft_tile_ten() {
+
+	register_sidebar( array(
+		'name'          => 'Tile #10',
+		'id'            => 'hovercraft_tile_ten',
+		'before_widget' => '<div class="widgets-tile-ten-wrapper">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
+	) );
+
+}
+add_action( 'widgets_init', 'hovercraft_tile_ten' );
+
+
+// tile eleven widgets
+function hovercraft_tile_eleven() {
+
+	register_sidebar( array(
+		'name'          => 'Tile #11',
+		'id'            => 'hovercraft_tile_eleven',
+		'before_widget' => '<div class="widgets-tile-eleven-wrapper">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
+	) );
+
+}
+add_action( 'widgets_init', 'hovercraft_tile_eleven' );
+
+
+// tile twelve widgets
+function hovercraft_tile_twelve() {
+
+	register_sidebar( array(
+		'name'          => 'Tile #12',
+		'id'            => 'hovercraft_tile_twelve',
+		'before_widget' => '<div class="widgets-tile-twelve-wrapper">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
+	) );
+
+}
+add_action( 'widgets_init', 'hovercraft_tile_twelve' );
