8a19f984cee9f62c66a468ed1acf7e67fe3a4064
Jesse Nickles <jesse@littlebizzy.com>
2022-11-24 15:18:36 +0700

    Update customizer-settings.php

--- inc/customizer-settings.php
+++ inc/customizer-settings.php
@@ -4,11 +4,12 @@ function hovercraft_customizer($wp_customize) {
 	
 $wp_customize->get_section('header_image')->title = __( 'Header Media' );
 
-// hovercraft general options
-$wp_customize->add_section( 'hovercraft_general' , array(
+// general options
+$wp_customize->add_section( 'hovercraft_general', array(
     'title'      => 'General Options',
     'priority'   => 30,
 ) );
+
 
 // back to top setting
 $wp_customize->add_setting('hovercraft_back_to_top', array(
@@ -48,6 +49,42 @@ $wp_customize->add_control(
     )
 );
 	
+
+// footer section
+$wp_customize->add_section( 'hovercraft_footer', array(
+	'title'      => 'Footer',
+    'priority'    => 120,
+    'description' => 'Allows you to customize how many footer columns',
+ 	) 
+);
+
+// footer columns setting
+$wp_customize->add_setting( 'hovercraft_footer_columns', array(
+    'default'    => 'four_weighted',
+    'type'       => 'theme_mod',
+ 	) 
+);
+
+// footer columns control
+$wp_customize->add_control( new WP_Customize_Control(
+        $wp_customize,
+ 		'hovercraft_footer_columns', array(
+    		'label' => 'Footer Columns',
+    		'description' => 'Using this option you can change footer columns',
+    		'settings' => 'hovercraft_footer_columns',
+    		'priority' => 10,
+    		'section' => 'hovercraft_footer',
+    		'type' => 'radio',
+    		'choices' => array(
+        		'four_weighted' => 'Four Weighted',
+        		'four_equal' => 'Four Equal',
+        		'three_weighted' => 'Three Weighted',
+        		'three_equal' => 'Three Equal',
+    			)
+			)
+) );
+
+// end function hovercraft_customizer
 }
 
 add_action('customize_register', 'hovercraft_customizer');
