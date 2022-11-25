81f921fb4da3016007c1193e68df5aa0c1c46e63
Jesse Nickles <jesse@littlebizzy.com>
2022-11-25 18:00:35 +0700

    Update footer-three-columns-equal.php

--- template-parts/footer/footer-three-columns-equal.php
+++ template-parts/footer/footer-three-columns-equal.php
@@ -1,1 +1,19 @@
+<div class="footer-columns-three-equal">
 
+	<div class="footer-column-one-three-equal">
+    	<?php dynamic_sidebar( 'hovercraft_footer_one' ); ?>
+		<div class="clear"></div>
+	</div><!-- footer-column-one-three-equal -->
+	
+	<div class="footer-column-two-three-equal">
+    	<?php dynamic_sidebar( 'hovercraft_footer_two' ); ?>
+		<div class="clear"></div>
+	</div><!-- footer-column-two-three-equal -->
+	
+	<div class="footer-column-three-three-equal">
+    	<?php dynamic_sidebar( 'hovercraft_footer_three' ); ?>
+		<div class="clear"></div>
+	</div><!-- footer-column-three-three-equal -->
+	
+	<div class="clear"></div>
+</div><!-- footer-columns-three-equal -->
