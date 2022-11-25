b4b992e8ed6fdc9b84968186afb4cae7a83c7f07
Jesse Nickles <jesse@littlebizzy.com>
2022-11-25 17:18:39 +0700

    Update footer-three-columns-unequal.php

--- template-parts/footer/footer-three-columns-unequal.php
+++ template-parts/footer/footer-three-columns-unequal.php
@@ -1,1 +1,14 @@
-
+<div class="footer-column-one-three-unequal">
+    <?php dynamic_sidebar( 'hovercraft_footer_one' ); ?>
+	<div class="clear"></div>
+</div><!-- footer-column-one-three-unequal -->
+	
+<div class="footer-column-two-three-unequal">
+    <?php dynamic_sidebar( 'hovercraft_footer_two' ); ?>
+	<div class="clear"></div>
+</div><!-- footer-column-two-three-unequal -->
+	
+<div class="footer-column-three-three-unequal">
+    <?php dynamic_sidebar( 'hovercraft_footer_three' ); ?>
+	<div class="clear"></div>
+</div><!-- footer-column-three-three-unequal -->
