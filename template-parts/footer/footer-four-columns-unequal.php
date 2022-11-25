5669aa70a05f0878480ef03825702fcbc29d8330
Jesse Nickles <jesse@littlebizzy.com>
2022-11-25 17:16:31 +0700

    Update footer-four-columns-unequal.php

--- template-parts/footer/footer-four-columns-unequal.php
+++ template-parts/footer/footer-four-columns-unequal.php
@@ -1,1 +1,19 @@
+<div class="footer-column-one-four-unequal">
+    <?php dynamic_sidebar( 'hovercraft_footer_one' ); ?>
+	<div class="clear"></div>
+</div><!-- footer-column-one-four-unequal -->
+	
+<div class="footer-column-two-four-unequal">
+    <?php dynamic_sidebar( 'hovercraft_footer_two' ); ?>
+	<div class="clear"></div>
+</div><!-- footer-column-two-four-unequal -->
+	
+<div class="footer-column-three-four-unequal">
+    <?php dynamic_sidebar( 'hovercraft_footer_three' ); ?>
+	<div class="clear"></div>
+</div><!-- footer-column-three-four-unequal -->
 
+<div class="footer-column-four-four-unequal">
+    <?php dynamic_sidebar( 'hovercraft_footer_four' ); ?>
+	<div class="clear"></div>
+</div><!-- footer-column-four-four-unequal -->
