3688d3fd89b249a5f8207e78c37162b2f455a412
Jesse Nickles <jesse@littlebizzy.com>
2022-11-25 17:17:14 +0700

    Update footer-four-columns-equal.php

--- template-parts/footer/footer-four-columns-equal.php
+++ template-parts/footer/footer-four-columns-equal.php
@@ -1,1 +1,19 @@
+<div class="footer-column-one-four-equal">
+    <?php dynamic_sidebar( 'hovercraft_footer_one' ); ?>
+	<div class="clear"></div>
+</div><!-- footer-column-one-four-equal -->
+	
+<div class="footer-column-two-four-equal">
+    <?php dynamic_sidebar( 'hovercraft_footer_two' ); ?>
+	<div class="clear"></div>
+</div><!-- footer-column-two-four-equal -->
+	
+<div class="footer-column-three-four-equal">
+    <?php dynamic_sidebar( 'hovercraft_footer_three' ); ?>
+	<div class="clear"></div>
+</div><!-- footer-column-three-four-equal -->
 
+<div class="footer-column-four-four-equal">
+    <?php dynamic_sidebar( 'hovercraft_footer_four' ); ?>
+	<div class="clear"></div>
+</div><!-- footer-column-four-four-equal -->
