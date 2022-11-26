706f3bd4b77ceb85d4a006f83b841777518ea8c5
Jesse Nickles <jesse@littlebizzy.com>
2022-11-26 17:43:03 +0700

    Update template-full-hero.php

--- page-templates/template-full-hero.php
+++ page-templates/template-full-hero.php
@@ -1,5 +1,16 @@
 <?php /* Template Name: Full Hero */ ?>
 <?php get_template_part( 'header' ); ?>
 <?php get_template_part( 'template-parts/header/header-full-hero' ); ?>
+
+	<?php if ( is_active_sidebar( 'hovercraft_main_begin' ) ) { ?>
+	<div class="main-begin">
+	<div class="inner">
+		<?php add_filter( 'widget_title' , 'my_widget_title', 10, 1 ); //we use the default priority and 3 arguments in the callback function
+		dynamic_sidebar( 'hovercraft_main_begin' ); remove_filter( 'widget_title', 'my_widget_title' ); ?>
+		<div class="clear"></div>
+	</div><!-- inner -->
+	</div><!-- main-begin -->
+	<?php } ?>
+
 <?php get_template_part( 'template-parts/content/main' ); ?>
 <?php get_template_part( 'footer' ); ?>
