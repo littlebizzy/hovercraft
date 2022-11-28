439d2c5ab2b36ce4bb8518271034ff1d48c55498
Jesse Nickles <jesse@littlebizzy.com>
2022-11-28 23:46:09 +0700

    Update primary.php

--- template-parts/content/primary.php
+++ template-parts/content/primary.php
@@ -9,25 +9,22 @@
 		<?php if ( is_singular() ) { ?>
 				<?php if ( is_page_template( 'page-templates/template-basic.php' ) || !is_page_template() ) { ?>
 					<h1><?php the_title(); ?></h1>
-					<?php if ( 'post' == get_post_type() ) { ?>
-						<?php $url_featured_image = wp_get_attachment_url( get_post_thumbnail_id($post->ID), 'large' ); 
-						if (!empty( $url_featured_image ) ) { ?>
-						<img width="800" height="450" src="<?php echo $url_featured_image; ?>" />
-						<?php } ?>
-						<?php the_time(get_option('date_format')); ?>
-						<?php } ?>
+					<?php $url_featured_image = wp_get_attachment_url( get_post_thumbnail_id($post->ID), 'large' ); 
+					if (!empty( $url_featured_image ) ) { ?>
+					<img width="800" height="450" src="<?php echo $url_featured_image; ?>" />
+					<?php } ?>
+					<?php if ( 'post' == get_post_type() ) { 
+						the_time(get_option('date_format')); 
+					} ?>
 				<?php } ?>
-				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-					<?php the_content(); ?>
-				<?php endwhile; endif; ?>
-		<?php } else { ?>
-			<?php if ( is_page_template( 'page-templates/template-basic.php' ) || !is_page_template() ) { ?>
-					<h1><?php the_title(); ?></h1>
-			<?php } ?>
+		<?php } ?><!-- singular -->
+		
 		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-					<?php the_content(); ?>
-				<?php endwhile; endif; ?>
-		<?php } ?>
+		
+			<?php if (is_archive()) { ?><h4><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h4><?php } ?>
+		
+			<?php the_content(); ?>
+		<?php endwhile; endif; ?><!-- the loop -->
 	
 	<div class="clear"></div>
 	</div><!-- content -->
