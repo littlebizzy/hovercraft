6f4f63ec5ac4abb86a605bc84c11285431a1c6d1
Jesse Nickles <jesse@littlebizzy.com>
2022-11-28 15:55:44 +0700

    Update primary.php

--- template-parts/content/primary.php
+++ template-parts/content/primary.php
@@ -6,58 +6,31 @@
 	
 	<div id="content">
 	
-	<!-- posts -->	
-	<?php if ( is_single() && 'post' == get_post_type() ) { ?>
-	<?php 
-	$url_featured_image = wp_get_attachment_url( get_post_thumbnail_id($post->ID), 'large' ); 
-	if (!empty( $url_featured_image ) ) { ?>
-	<img width="800" height="450" src="<?php echo $url_featured_image; ?>" />
-	<?php } ?>
-	<h1><?php single_post_title(); ?></h1>
-	<?php the_time(get_option('date_format')); ?><br><br>
-	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-		<?php the_content(); ?>
-	<?php endwhile; endif; ?>
-	<?php } ?><!-- posts -->
-		
-	<!-- products -->	
-	<?php if ( is_single() && 'product' == get_post_type() ) { ?>
-	<h1><?php single_post_title(); ?></h1>
-	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-		<?php the_content(); ?>
-	<?php endwhile; endif; ?>
-	<?php } ?><!-- products -->
-	
-	<!-- home page -->
-	<?php if (is_page('home')) { ?>
-	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-		<?php the_content(); ?>
-		<div class="clear"></div>
-	<?php endwhile; endif; ?>	
-	<?php } ?><!-- home page -->
-
-	<!-- page -->
-	<?php if ( is_singular() && 'page' == get_post_type() ) { ?>
-		<?php if ( is_page_template( 'page-templates/template-basic.php' ) || !is_page_template() ) { ?>
-		<h1><?php the_title(); ?></h1>
-		<?php } ?>
+		<?php if ( is_singular() ) { ?>
+				<?php if ( is_page_template( 'page-templates/template-basic.php' ) || !is_page_template() ) { ?>
+					<h1><?php the_title(); ?></h1>
+					<?php if ( 'post' == get_post_type() ) { ?>
+						<?php $url_featured_image = wp_get_attachment_url( get_post_thumbnail_id($post->ID), 'large' ); 
+						if (!empty( $url_featured_image ) ) { ?>
+						<img width="800" height="450" src="<?php echo $url_featured_image; ?>" />
+						<?php } ?>
+						<?php the_time(get_option('date_format')); ?>
+						<?php } ?>
+				<?php } ?>
+				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+					<?php the_content(); ?>
+				<?php endwhile; endif; ?>
+		<?php } else { ?>
+			<?php if ( is_page_template( 'page-templates/template-basic.php' ) || !is_page_template() ) { ?>
+					<h1><?php the_title(); ?></h1>
+			<?php } ?>
 		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-			<?php the_content(); ?>
-		<?php endwhile; endif; ?>
-		<div class="clear"></div>
-	<?php } ?><!-- page -->
-	
-	<!-- archive -->
-	<?php if (is_archive()) { ?>
-	<h1><?php single_cat_title(); ?></h1>
-	<?php if (have_posts()): while (have_posts()): the_post(); ?>
-		<h4><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h4>
-    <?php endwhile; endif; ?>
-	<?php } ?><!-- archive -->
+					<?php the_content(); ?>
+				<?php endwhile; endif; ?>
+		<?php } ?>
 	
 	<div class="clear"></div>
 	</div><!-- content -->
-
 
 	<div class="last-modified">
 		<?php echo "<span>Last modified on "; the_modified_time('F jS, Y'); echo "</span>"; ?>
