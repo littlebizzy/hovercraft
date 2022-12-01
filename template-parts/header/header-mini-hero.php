1cce39ace182685c5d74dd4c915160d21ca86245
Jesse Nickles <jesse@littlebizzy.com>
2022-12-01 15:46:06 +0700

    Update header-mini-hero.php

--- template-parts/header/header-mini-hero.php
+++ template-parts/header/header-mini-hero.php
@@ -19,8 +19,8 @@
 		<div class="title-wrapper">
 		<h1 class="mini-hero-title"><?php single_post_title(); ?></h1>
 		
+		<?php if (is_front_page()) : ?>
 		<div class="welcome-text">
-			
 			<?php if ( has_excerpt() ) { ?>
 					<div class="full-hero-snippet">
 			 			<?php the_excerpt(); ?>
@@ -31,8 +31,8 @@
 							dynamic_sidebar( 'hovercraft_hero_snippet' ); remove_filter( 'widget_title', 'my_widget_title' ); ?>
 				</div><!--full-hero-snippet -->
 				<?php } ?>
-			
 		</div><!-- welcome-text -->
+		<?php endif; ?><!-- is_front_page -->
 	
 		<?php if (is_front_page()) : ?>
 		<div class="cta-hero-wrapper">
