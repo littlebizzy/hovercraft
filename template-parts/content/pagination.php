904a7bacd81394f07fe059ad4e4ed5585f292d69
Jesse Nickles <jesse@littlebizzy.com>
2022-12-09 01:33:29 +0700

    Delete pagination.php

--- template-parts/content/pagination.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// https://njengah.com/pagination-code-in-wordpress/
-
-?>
-
-<div class="nav-previous alignleft"><?php previous_posts_link( 'Older posts' ); ?></div>
-<div class="nav-next alignright"><?php next_posts_link( 'Newer posts' ); ?></div>
