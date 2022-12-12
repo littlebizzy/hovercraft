4027cbaf077629d7edff717a3dbe23cb24ca470d
Jesse Nickles <jesse@littlebizzy.com>
2022-12-13 02:54:27 +0700

    Update git-updater.php

--- inc/git-updater.php
+++ inc/git-updater.php
@@ -1,15 +1,33 @@
 <?php
 
 function hovercraft_suggest_git_updater_plugin() {
+	
+	$git_updater = WP_PLUGIN_DIR . '/git-updater/git-updater.php';
+	// $installurl = wp_nonce_url( self_admin_url( 'update.php?action=install-plugin&plugin=git-updater' ), 'install-plugin_git-updater' );
 
-    if ( ! in_array( 'git-updater/git-updater.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) { ?>
+    if ( file_exists( $git_updater ) && ! in_array( 'git-updater/git-updater.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) { ?>
 
         <div class="notice notice-warning">
-        <p><strong>Git Updater</strong></p>
-        <p>HoverCraft requires the free Git Updater plugin to receive automatic updates via GitHub.</p>
-        </div>
+        <p><strong>Activate Git Updater plugin to receive updates</strong></p>
+        <p>The HoverCraft theme requires the free Git Updater plugin to receive automatic updates via GitHub.</p>
+		<p><?php
+			$git_updater_path = 'git-updater/git-updater.php';
+			$activate_link = wp_nonce_url(admin_url('plugins.php?action=activate&plugin='.$git_updater_path), 'activate-plugin_'.$git_updater_path);
+			echo '<a href="' . $activate_link . '">Activate Now</a>'; 
+			?></p>
+		</div>
 
-    <?php }
+    <?php } elseif ( ! file_exists( $git_updater ) ) { ?>
+
+        <div class="notice notice-warning">
+        <p><strong>Install Git Updater plugin to receive updates</strong></p>
+        <p>The HoverCraft theme requires the free Git Updater plugin to receive automatic updates via GitHub.</p>
+		<p><?php
+			echo '<a href="https://git-updater.com/wp-content/uploads/2022/12/git-updater-11.1.10.6.zip">Download Now</a>'; 
+			?></p>
+		</div>
+
+	<?php }
 
 }
 
@@ -18,3 +36,6 @@ add_action('admin_notices', 'hovercraft_suggest_git_updater_plugin');
 // https://wordpress.stackexchange.com/questions/39408/how-to-make-plugin-required-in-a-wp-theme-without-using-php-conditional-statemen
 // https://gist.github.com/mlbd/b96f62e685ca3d58c17031fcc9e73c07
 // https://www.aurodigo.com/how-to-solve-fatal-error-call-to-undefined-function-is-plugin-active-problem-on-wordpress
+// https://theaveragedev.com/generating-a-wordpress-plugin-activation-link-url/
+// https://katz.co/4769/
+
