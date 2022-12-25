43536effc7be7358f6f84b826ea8020ce1894f11
Jesse Nickles <jesse@littlebizzy.com>
2022-12-25 17:08:52 +0700

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -1,6 +1,4 @@
-<?php if ( get_theme_mod( 'hovercraft_sidebar_status' ) == 1 ) { ?>
 <div id="sidebar">
 	<?php dynamic_sidebar( 'hovercraft_sidebar' ); ?>
 	<div class="clear"></div>
 </div><!-- sidebar -->
-<?php } ?>
