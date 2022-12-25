938768c770db528a67f7a354ff9e19f500abfddf
Jesse Nickles <jesse@littlebizzy.com>
2022-12-25 17:07:30 +0700

    Update main.php

--- template-parts/content/main.php
+++ template-parts/content/main.php
@@ -1,7 +1,9 @@
 <div id="main">
 <div class="inner">
     <?php get_template_part( 'template-parts/content/primary' ); ?>
-    <?php get_template_part( 'sidebar' ); ?>
+    <?php if ( get_theme_mod( 'hovercraft_sidebar_status' ) == 1 ) { 
+		get_template_part( 'sidebar' ); 
+	} ?>
     <?php get_template_part( 'template-parts/content/pagination' ); ?>
     <div class="clear"></div>
 </div><!-- inner -->
