b6ef957b38500d84350ce8f51f8d24e2eea487d1
Jesse Nickles <jesse@littlebizzy.com>
2022-12-27 01:35:35 +0700

    Update primary.php

--- template-parts/content/primary.php
+++ template-parts/content/primary.php
@@ -10,7 +10,7 @@
 	
 		<?php if ( is_singular() ) { ?>
 				<?php if ( is_page_template( 'page-templates/template-basic.php' ) || !is_page_template() ) { ?>
-					<?php if(!is_front_page() ) { ?><h1><?php the_title(); ?></h1><?php } ?>
+					<h1><?php the_title(); ?></h1>
 					<?php $url_featured_image = wp_get_attachment_url( get_post_thumbnail_id($post->ID), 'large' ); 
 					if ( !empty( $url_featured_image ) && ( !is_singular('product') ) ) { ?>
 					<img width="800" height="450" src="<?php echo $url_featured_image; ?>" />
@@ -23,9 +23,13 @@
 		
 		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 		
-			<?php if (is_archive()) { ?><h4><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h4><?php } ?>
+			<?php if (is_home()) { ?>
+				<h4><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h4>
+				<?php the_excerpt(); ?>
+			<?php } else {
+				the_content();
+			} ?>
 		
-			<?php the_content(); ?>
 		<?php endwhile; endif; ?><!-- the loop -->
 	
 	<div class="clear"></div>
