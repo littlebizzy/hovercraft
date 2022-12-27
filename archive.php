ccfda44adc4e2cc724e004baafb0770d7255e36d
Jesse Nickles <jesse@littlebizzy.com>
2022-12-27 14:38:00 +0700

    Delete archive.php

--- archive.php
+++
@@ -1,4 +0,0 @@
-<?php get_template_part( 'header' ); ?>
-<?php get_template_part( 'template-parts/header/header-basic' ); ?>
-<?php get_template_part( 'template-parts/content/main' ); ?>
-<?php get_template_part( 'footer' ); ?>
