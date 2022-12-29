d63e3a0f2305f136daf35a09236c30751095d343
Jesse Nickles <jesse@littlebizzy.com>
2022-12-29 18:42:21 +0700

    Update customizer-settings.php

--- inc/customizer-settings.php
+++ inc/customizer-settings.php
@@ -210,7 +210,7 @@ $wp_customize->add_control( new WP_Customize_Control(
 // hover effects section
 $wp_customize->add_section( 'hovercraft_effects', array(
     'title'      => 'Hover Effects',
-    'priority'   => 140,
+    'priority'   => 107,
 ) );
 
 // hover effects main menu links setting
