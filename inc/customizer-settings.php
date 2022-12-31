7f852e77a57c1dd72f32b316f6059df67bbbd48b
Jesse Nickles <jesse@littlebizzy.com>
2023-01-01 04:20:15 +0700

    Update customizer-settings.php

--- inc/customizer-settings.php
+++ inc/customizer-settings.php
@@ -288,6 +288,31 @@ $wp_customize->add_section( 'hovercraft_effects', array(
     'priority'   => 107,
 ) );
 
+// hover effects logo setting
+$wp_customize->add_setting( 'hovercraft_logo_effect', array(
+    'default'    => 'default',
+	) 
+);
+
+// hover effects logo control
+$wp_customize->add_control( new WP_Customize_Control(
+        $wp_customize,
+        'hovercraft_logo_effect',
+        array(
+            'label'     => __('Logo effect', 'hovercraft'),
+			'description' => __( 'CSS hover effect for logos in the header' ),
+            'section'   => 'hovercraft_effects',
+            'settings'  => 'hovercraft_logo_effect',
+            'type'      => 'select',
+			'choices' => array(
+        		'default' => 'Default',
+        		'cerulean' => 'Cerulean',
+        		'cosmo' => 'Cosmo',
+        		'cyborg' => 'cyborg',
+    			)
+        )
+) );
+
 // hover effects main menu links setting
 $wp_customize->add_setting( 'hovercraft_main_menu_links_effect', array(
     'default'    => 'default',
