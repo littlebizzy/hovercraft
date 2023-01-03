3061ad2cf8a051a8bef368d0e167ef2cf28fc6d3
Jesse Nickles <jesse@littlebizzy.com>
2023-01-04 00:45:58 +0700

    Update css-generator.php

--- inc/css-generator.php
+++ inc/css-generator.php
@@ -12,16 +12,16 @@ function hovercraft_generate_css(){
 		$hero_image = $url_header_image; 
 	}
     
-	$hero_gradient_angle = get_theme_mod( 'hovercraft_hero_gradient_angle' );
-	$hero_gradient_start_color_transparency = get_theme_mod( 'hovercraft_hero_gradient_start_color_transparency' );
-	$hero_gradient_stop_color_transparency = get_theme_mod( 'hovercraft_hero_gradient_stop_color_transparency' );
-	$hero_gradient_start_color_length = get_theme_mod( 'hovercraft_hero_gradient_start_color_length' );
-	$hero_gradient_stop_color_length = get_theme_mod( 'hovercraft_hero_gradient_stop_color_length' );
-	$topbar_background_color = get_theme_mod( 'hovercraft_topbar_background_color' );
-	$topbar_font_color = get_theme_mod( 'hovercraft_topbar_font_color' );
-	$topbar_link_color = get_theme_mod( 'hovercraft_topbar_link_color' );
-	$hero_gradient_start_color = get_theme_mod( 'hovercraft_hero_gradient_start_color' );
-	$hero_gradient_stop_color = get_theme_mod( 'hovercraft_hero_gradient_stop_color' );
+	$hero_gradient_angle = get_theme_mod( 'hovercraft_hero_gradient_angle', '60deg' );
+	$hero_gradient_start_color_transparency = get_theme_mod( 'hovercraft_hero_gradient_start_color_transparency', '0.50' );
+	$hero_gradient_stop_color_transparency = get_theme_mod( 'hovercraft_hero_gradient_stop_color_transparency', '0.50' );
+	$hero_gradient_start_color_length = get_theme_mod( 'hovercraft_hero_gradient_start_color_length', '30%' );
+	$hero_gradient_stop_color_length = get_theme_mod( 'hovercraft_hero_gradient_stop_color_length', '100%' );
+	$topbar_background_color = get_theme_mod( 'hovercraft_topbar_background_color', '#263238' );
+	$topbar_font_color = get_theme_mod( 'hovercraft_topbar_font_color', '#ffffff' );
+	$topbar_link_color = get_theme_mod( 'hovercraft_topbar_link_color', '#ffffff' );
+	$hero_gradient_start_color = get_theme_mod( 'hovercraft_hero_gradient_start_color', '#37474f' );
+	$hero_gradient_stop_color = get_theme_mod( 'hovercraft_hero_gradient_stop_color', '#ffffff' );
     $header_half_hero_background_color = get_theme_mod( 'hovercraft_header_half_hero_background_color', '#ffffff' ); 
     $header_mini_hero_background_color = get_theme_mod( 'hovercraft_header_mini_hero_background_color', '#ffffff' );
     $header_basic_background_color = get_theme_mod( 'hovercraft_header_basic_background_color', '#eceff1' );
@@ -33,7 +33,6 @@ function hovercraft_generate_css(){
     ?>
 
 <style type="text/css" id="hovercraft_custom_css">
-<?php if( !empty($topbar_background_color) && !empty($topbar_font_color) && !empty($topbar_link_color) ) { ?>
 #topbar {
 	background: <?php echo $topbar_background_color; ?>;
 	color: <?php echo $topbar_font_color; ?>;
@@ -42,9 +41,7 @@ function hovercraft_generate_css(){
 #topbar a {
 	color: <?php echo $topbar_link_color; ?>;
 }
-<?php } ?>
 
-<?php if( !empty($hero_image) && !empty($hero_gradient_start_color) ) { ?>
 .splash-wide {
 	background: linear-gradient(<?php echo $hero_gradient_angle; ?>, <?php 
 		list($r1, $g1, $b1) = sscanf($hero_gradient_start_color, "#%02x%02x%02x");
@@ -70,47 +67,6 @@ function hovercraft_generate_css(){
 	background-size: cover;
 	background-repeat: no-repeat;
 }
-<?php } elseif( !empty($hero_image) && empty($hero_gradient_start_color) ) { ?>
-.splash-wide {
-	background: linear-gradient(<?php echo $hero_gradient_angle; ?>, rgba(55, 71, 79, <?php echo $hero_gradient_start_color_transparency; ?>) <?php echo $hero_gradient_start_color_length; ?>, rgba(255, 255, 255, <?php echo $hero_gradient_stop_color_transparency; ?>) <?php echo $hero_gradient_stop_color_length; ?>), url(<?php echo $hero_image; ?>);
-	background-position: center center;
-	background-size: cover;
-	background-repeat: no-repeat;
-}
-
-.splash-mini {
-	background: linear-gradient(<?php echo $hero_gradient_angle; ?>, rgba(55, 71, 79, <?php echo $hero_gradient_start_color_transparency; ?>) <?php echo $hero_gradient_start_color_length; ?>, rgba(255, 255, 255, <?php echo $hero_gradient_stop_color_transparency; ?>) <?php echo $hero_gradient_stop_color_length; ?>), url(<?php echo $hero_image; ?>);
-	background-position: center center;
-	background-size: cover;
-	background-repeat: no-repeat;
-}
-<?php } elseif( empty($hero_image) && !empty($hero_gradient_start_color) ) { ?>
-.splash-wide {
-	background: linear-gradient(<?php echo $hero_gradient_angle; ?>, <?php 
-		list($r1, $g1, $b1) = sscanf($hero_gradient_start_color, "#%02x%02x%02x");
-		echo "rgba($r1, $g1, $b1, $hero_gradient_start_color_transparency)"; 
-		?> <?php echo $hero_gradient_start_color_length; ?>, <?php 
-		list($r2, $g2, $b2) = sscanf($hero_gradient_stop_color, "#%02x%02x%02x");
-		echo "rgba($r2, $g2, $b2, $hero_gradient_stop_color_transparency)"; 
-		?> <?php echo $hero_gradient_stop_color_length; ?>);
-	background-position: center center;
-	background-size: cover;
-	background-repeat: no-repeat;
-}
-
-.splash-mini {
-	background: linear-gradient(<?php echo $hero_gradient_angle; ?>, <?php 
-		list($r1, $g1, $b1) = sscanf($hero_gradient_start_color, "#%02x%02x%02x");
-		echo "rgba($r1, $g1, $b1, $hero_gradient_start_color_transparency)"; 
-		?> <?php echo $hero_gradient_start_color_length; ?>, <?php 
-		list($r2, $g2, $b2) = sscanf($hero_gradient_stop_color, "#%02x%02x%02x");
-		echo "rgba($r2, $g2, $b2, $hero_gradient_stop_color_transparency)"; 
-		?> <?php echo $hero_gradient_stop_color_length; ?>);
-	background-position: center center;
-	background-size: cover;
-	background-repeat: no-repeat;
-}
-<?php } ?>
 
 #header-half-hero {
 	background: <?php echo $header_half_hero_background_color; ?>;
@@ -124,30 +80,21 @@ function hovercraft_generate_css(){
 	background: <?php echo $header_basic_background_color; ?>;
 }
 
-<?php if( !empty($prefooter_top_background_color) ) { ?>
 #prefooter-top {
 	background: <?php echo $prefooter_top_background_color; ?>;
 }
-<?php } ?>
 
-<?php if( !empty($prefooter_bottom_background_color) ) { ?>
 #prefooter-bottom {
 	background: <?php echo $prefooter_bottom_background_color; ?>;
 }
-<?php } ?>
 
-<?php if( !empty($footer_background_color) ) { ?>
 #footer {
 	background: <?php echo $footer_background_color; ?>;
 }
-<?php } ?>
 
-<?php if( !empty($copyright_background_color) ) { ?>
 #copyright {
 	background: <?php echo $copyright_background_color; ?>;
 }
-<?php } ?>
-
 </style>
 
 <?php }
