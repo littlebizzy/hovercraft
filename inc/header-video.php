cb6ab65b92ecb289e1918ad35a513ae65137ca74
Jesse Nickles <jesse@littlebizzy.com>
2023-01-05 02:35:48 +0700

    Update header-video.php

--- inc/header-video.php
+++ inc/header-video.php
@@ -14,18 +14,18 @@ function hovercraft_video($wp_customize) {
 	$wp_customize->add_control(new WP_Customize_Media_Control( $wp_customize, 'video_upload', 
 		array(
         	'label' => 'Background Video',
-			'description' => esc_html__( 'This is the description for the Media Control' ),
+			'description' => esc_html__( 'This is the description for the Media Control', 'hovercraft' ),
         	'section' => 'header_image', //this is the section where the custom-logo from WordPress is
 			'mime_type' => 'video',  // Required. Can be image, audio, video, application, text
         	'settings' => 'hovercraft_video',
 			'button_labels' => array( // Optional
-         	'select' => __( 'Select File' ),
-         	'change' => __( 'Change File' ),
-         	'default' => __( 'Default' ),
-         	'remove' => __( 'Remove' ),
-         	'placeholder' => __( 'No file selected' ),
-         	'frame_title' => __( 'Select File' ),
-         	'frame_button' => __( 'Choose File' ),
+         	'select' => __( 'Select File', 'hovercraft' ),
+         	'change' => __( 'Change File', 'hovercraft' ),
+         	'default' => __( 'Default', 'hovercraft' ),
+         	'remove' => __( 'Remove', 'hovercraft' ),
+         	'placeholder' => __( 'No file selected', 'hovercraft' ),
+         	'frame_title' => __( 'Select File', 'hovercraft' ),
+         	'frame_button' => __( 'Choose File', 'hovercraft' ),
         	'priority' => 8 // show it just below the custom-logo
     		)
 		)
