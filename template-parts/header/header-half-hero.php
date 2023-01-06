7ffcb64ddb23e58a2d13e225673ca7d1b2fc2d37
Jesse Nickles <jesse@littlebizzy.com>
2023-01-07 03:09:15 +0700

    Update header-half-hero.php

--- template-parts/header/header-half-hero.php
+++ template-parts/header/header-half-hero.php
@@ -1,11 +1,11 @@
-<div id="splash-wrapper">
-<div class="splash-wide">
+<div id="hero-wrapper">
+<div class="hero-wide">
 
 <div id="header-half-hero">
 <?php get_template_part('template-parts/header/navigation'); ?>
 </div>
 	
-<div class="splash-main">
+<div class="hero-main">
 <div class="inner">
 	<?php if (is_front_page()) { ?>
 	<div class="welcome-wrapper">
@@ -60,7 +60,7 @@
 		
 <div class="clear"></div>
 </div><!-- inner -->
-</div><!-- splash-main -->
+</div><!-- hero-main -->
 	
-</div><!-- splash-wide -->
-</div><!-- splash-wrapper -->
+</div><!-- hero-wide -->
+</div><!-- hero-wrapper -->
