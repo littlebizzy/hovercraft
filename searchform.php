4dc0cdad13a7b08ccd36c8d704d5e5e2cd39f855
Jesse Nickles <jesse@littlebizzy.com>
2023-01-08 02:42:57 +0700

    Update searchform.php

--- searchform.php
+++ searchform.php
@@ -1,6 +1,5 @@
 <form role="search" method="get" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<div class="searchform-items">
-		<i class="material-icons search">search</i>
 		<input type="search" class="searchinput" value="<?php echo esc_attr( get_search_query() ); ?>" name="s" placeholder="Search" />
 	</div><!-- searchform-items -->
 </form><!-- searchform -->
