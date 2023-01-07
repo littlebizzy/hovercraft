d291580c2fadb4242b3b57b03eef4031ddcf2f6a
Jesse Nickles <jesse@littlebizzy.com>
2023-01-08 02:11:59 +0700

    Update searchform.php

--- searchform.php
+++ searchform.php
@@ -1,9 +1,5 @@
-<form role="search" method="get" id="searchform"
-	class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+<form role="search" method="get" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<div>
-		<label class="screen-reader-text" for="s"><?php _x( 'Search for:', 'label' ); ?></label>
-		<input type="text" value="<?php echo get_search_query(); ?>" name="s" id="s" />
-		<input type="submit" id="searchsubmit"
-			value="<?php echo esc_attr_x( 'Search', 'submit button' ); ?>" />
+		<input type="search" class="searchinput" value="<?php echo esc_attr( get_search_query() ); ?>" name="s" placeholder="Search" />
 	</div>
-</form>
+</form><!-- searchform -->
