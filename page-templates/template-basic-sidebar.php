75e1bfb8fd316917c1a6bbb89b36ab0321c1ecf6
Jesse Nickles <jesse@littlebizzy.com>
2023-01-09 00:49:35 +0700

    Update and rename template-basic.php to template-basic-sidebar.php

---
+++ page-templates/template-basic-sidebar.php
@@ -0,0 +1,14 @@
+<?php /* Template Name: Basic (Sidebar) */ ?>
+<?php get_template_part( 'header' ); ?>
+<?php get_template_part( 'template-parts/header/header-basic' ); ?>
+
+<div id="main">
+<div class="inner">
+    <?php get_template_part( 'template-parts/content/primary' ); ?>
+    <?php get_template_part( 'sidebar' ); ?>
+    <?php get_template_part( 'template-parts/content/pagination' ); ?>
+    <div class="clear"></div>
+</div><!-- inner -->
+</div><!-- main -->
+
+<?php get_template_part( 'footer' ); ?>
