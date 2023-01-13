bd1a4f38f13ce009e86824ceb96c68d3774cbb08
Jesse Nickles <jesse@littlebizzy.com>
2023-01-14 00:27:26 +0700

    Update template-mini-hero-tiles.php

--- page-templates/template-mini-hero-tiles.php
+++ page-templates/template-mini-hero-tiles.php
@@ -1,4 +1,4 @@
-<?php /* Template Name: Mini Hero Tiles */ ?>
+<?php /* Template Name: Mini Hero (Tiles) */ ?>
 <?php get_template_part( 'header' ); ?>
 <?php get_template_part( 'template-parts/header/header-mini-hero' ); ?>
 
