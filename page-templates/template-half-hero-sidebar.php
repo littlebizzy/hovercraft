2729a69843175a0ada86306c49fb21ab9e6c8479
Jesse Nickles <jesse@littlebizzy.com>
2023-01-13 23:41:31 +0700

    Update and rename template-half-hero.php to template-half-hero-sidebar.php

--- page-templates/template-half-hero-sidebar.php
+++ page-templates/template-half-hero-sidebar.php
@@ -1,8 +1,8 @@
-<?php /* Template Name: Half Hero */ ?>
+<?php /* Template Name: Half Hero (Sidebar) */ ?>
 <?php get_template_part( 'header' ); ?>
 <?php get_template_part( 'template-parts/header/header-half-hero' ); ?>
 
-	<?php if ( is_active_sidebar( 'hovercraft_main_begin' ) ) { ?>
+	<?php if ( is_active_sidebar( 'hovercraft_main_begin' ) && get_theme_mod( 'hovercraft_main_begin_widget_display' ) == 'full_and_half_hero' ) : ?>
 	<div class="main-begin">
 	<div class="inner">
 		<?php add_filter( 'widget_title' , 'my_widget_title', 10, 1 ); //we use the default priority and 3 arguments in the callback function
@@ -10,7 +10,7 @@
 		<div class="clear"></div>
 	</div><!-- inner -->
 	</div><!-- main-begin -->
-	<?php } ?>
+	<?php endif; ?>
 
 <?php get_template_part( 'template-parts/content/main' ); ?>
 <?php get_template_part( 'footer' ); ?>
