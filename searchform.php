ba2760a4a595e694ed5f03fec532db381addd3e9
Jesse Nickles <jesse@littlebizzy.com>
2023-01-15 18:43:52 +0700

    Update searchform.php

--- searchform.php
+++ searchform.php
@@ -1,5 +1,5 @@
 <form role="search" method="get" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<div class="searchform-items">
-		<input type="search" class="searchinput" value="<?php echo esc_attr( get_search_query() ); ?>" name="s" placeholder="Search" />
+		<input type="search" class="searchinput" value="<?php echo esc_attr( get_search_query() ); ?>" name="s" placeholder="<?php if ( class_exists( 'WooCommerce' ) ) { echo "Search entire store..."; } else { echo "Search entire site..."; } ?>" />
 	</div><!-- searchform-items -->
 </form><!-- searchform -->
