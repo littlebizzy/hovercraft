cbbd81fdfdfb5af60cc541f7fdf24244a1ec0517
Jesse Nickles <jesse@littlebizzy.com>
2023-01-18 18:11:28 +0700

    Update google-fonts.php

--- inc/google-fonts.php
+++ inc/google-fonts.php
@@ -1,8 +1,30 @@
 <?php
 
 function hovercraft_google_fonts() {
-	wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:wght@300&family=Open+Sans:ital,wght@1,300&family=Roboto+Condensed:wght@300&family=Roboto:wght@300&display=swap', array(), null );
+	
+	$default_font = get_theme_mod( 'hovercraft_default_font', 'noto_sans' );
+	$default_font_clean = str_replace("_", "+", $default_font);
+	$default_font_almost = ucfirst($default_font_clean);
+	$default_font_final = ucwords($default_font_almost, "+");
+	
+	$site_name_font = get_theme_mod( 'hovercraft_site_name_font', 'noto_sans' );
+	$site_name_font_clean = str_replace("_", "+", $site_name_font);
+	$site_name_font_almost = ucfirst($site_name_font_clean);
+	$site_name_font_final = ucwords($site_name_font_almost, "+");
+	
+	
+	if ( empty( $site_name_font_final ) || $site_name_font_final == $default_font_final ) {
+		$google_fonts_url = "https://fonts.googleapis.com/css2?family={$default_font_final}:wght@400";
+	} else {
+		$google_fonts_url = "https://fonts.googleapis.com/css2?family={$default_font_final}:wght@400&family={$site_name_font_final}:wght@400";
+	}
+	
+
+	wp_enqueue_style( 'google-fonts', $google_fonts_url.'&display=swap', array(), null, 'all' );
 
 }
 
-// add_action( 'wp_enqueue_scripts', 'hovercraft_google_fonts' );
+add_action( 'wp_enqueue_scripts', 'hovercraft_google_fonts' );
+
+// https://wordpress.org/support/topic/new-google-fonts-url-incompatible-with-wp_enqueue_style/
+// https://wordpress.stackexchange.com/questions/77227/enqueue-google-web-fonts-without-messing-up-symbols-in-url
