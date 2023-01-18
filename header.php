678164bccf3d3ce58c9ff40ddf2c2d17d2fb1ab1
Jesse Nickles <jesse@littlebizzy.com>
2023-01-18 18:07:54 +0700

    Update header.php

--- header.php
+++ header.php
@@ -5,8 +5,10 @@
 	<meta name="viewport" content="width=device-width,initial-scale=1">
 	
 	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Icons&family=Material+Icons+Outlined&family=Material+Icons+Two+Tone">
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,400;0,600;0,700;1,400&family=Noto+Sans+Mono&display=swap">
+	
 	<?php 
 	$wp_locale = get_locale();
 	if (strpos($wp_locale, 'en') === false) { ?>
