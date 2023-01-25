692080a65c4b0d23a26b3273b4c9d0ea380b1255
Jesse Nickles <jesse@littlebizzy.com>
2023-01-26 05:33:26 +0700

    Update template-basic-no-sidebar.php

--- page-templates/template-basic-no-sidebar.php
+++ page-templates/template-basic-no-sidebar.php
@@ -13,7 +13,26 @@
 			</div><!-- breadcrumbs -->
 		<?php } ?><!-- if hovercraft_breadcrumbs -->
 	
-		<?php get_template_part( 'template-parts/content/content' ); ?>
+		<div id="content">
+	
+					<h1><?php the_title(); ?></h1>
+					<?php $url_featured_image = wp_get_attachment_url( get_post_thumbnail_id($post->ID), 'large' ); 
+					if ( !empty( $url_featured_image ) && ( !is_singular('product') ) ) { ?>
+					<img width="800" height="450" src="<?php echo $url_featured_image; ?>" />
+					<?php } ?>
+					<?php if ( 'post' == get_post_type() ) { 
+						the_time(get_option('date_format')); 
+					} ?>
+		
+		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+			<?php the_content(); ?>
+		<?php endwhile; endif; ?><!-- the loop -->
+	
+		<div class="clear"></div>
+		<?php hovercraft_pagination_nav(); ?>	
+	
+			<div class="clear"></div>
+		</div><!-- content -->
 
 		<div class="last-modified">
 			<span> <?php _e('Last modified on ', 'hovercraft'); echo the_modified_time('F j, Y'); ?> </span>
