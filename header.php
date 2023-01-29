0a209d8448e42468f2240856432015bf2c31cd4d
Jesse Nickles <jesse@littlebizzy.com>
2023-01-30 02:36:14 +0700

    Update header.php

--- header.php
+++ header.php
@@ -7,7 +7,9 @@
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Icons&family=Material+Icons+Outlined&family=Material+Icons+Two+Tone">
-	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,400;0,600;0,700;1,400&family=Noto+Sans+Mono&display=swap">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=<?php $default_font_family = get_theme_mod( 'hovercraft_default_font', 'noto_sans' );
+		echo ucwords(str_replace("_", "+", $default_font_family), "+"); ?>:ital,wght@0,400;0,600;0,700;1,400&display=swap">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Noto+Sans+Mono&display=swap">
 	
 	<?php 
 	$wp_locale = get_locale();
