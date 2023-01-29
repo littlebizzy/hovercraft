b2d84025c8c77d3fdc0363a3f1f6a737f538e38f
Jesse Nickles <jesse@littlebizzy.com>
2023-01-29 17:31:35 +0700

    Update template-basic-sidebar.php

--- page-templates/template-basic-sidebar.php
+++ page-templates/template-basic-sidebar.php
@@ -15,14 +15,12 @@
 	
 	    <div id="content">
 	
-					<h1><?php the_title(); ?></h1>
-					<?php $url_featured_image = wp_get_attachment_url( get_post_thumbnail_id($post->ID), 'large' ); 
-					if ( !empty( $url_featured_image ) && ( !is_singular('product') ) ) { ?>
-					<img class="featured-image" width="800" height="450" src="<?php echo $url_featured_image; ?>" />
-					<?php } ?>
-					<?php if ( 'post' == get_post_type() ) { 
-						the_time(get_option('date_format')); 
-					} ?>
+			<h1><?php the_title(); ?></h1>
+			
+			<?php $url_featured_image = wp_get_attachment_url( get_post_thumbnail_id($post->ID), 'large' ); 
+			if ( !empty( $url_featured_image ) ) { ?>
+			<img class="featured-image" src="<?php echo $url_featured_image; ?>" />
+			<?php } ?>
 		
 		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 			<?php the_content(); ?>
