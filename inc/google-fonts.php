cc1721b2f75b91fd0ebefc704fdd1a0b972affd4
Jesse Nickles <jesse@littlebizzy.com>
2023-01-30 22:36:47 +0700

    Update google-fonts.php

--- inc/google-fonts.php
+++ inc/google-fonts.php
@@ -14,9 +14,9 @@ function hovercraft_google_fonts() {
 	
 	
 	if ( empty( $site_name_font_final ) || $site_name_font_final == $default_font_final ) {
-		$google_fonts_url = "https://fonts.googleapis.com/css2?family={$default_font_final}:wght@400";
+		$google_fonts_url = "https://fonts.googleapis.com/css2?family={$default_font_final}:ital,wght@0,400;0,600;0,700;1,400";
 	} else {
-		$google_fonts_url = "https://fonts.googleapis.com/css2?family={$default_font_final}:wght@400&family={$site_name_font_final}:wght@400";
+		$google_fonts_url = "https://fonts.googleapis.com/css2?family={$default_font_final}:ital,wght@0,400;0,600;0,700;1,400&family={$site_name_font_final}:wght@400";
 	}
 	
 
@@ -28,3 +28,18 @@ add_action( 'wp_enqueue_scripts', 'hovercraft_google_fonts' );
 
 // https://wordpress.org/support/topic/new-google-fonts-url-incompatible-with-wp_enqueue_style/
 // https://wordpress.stackexchange.com/questions/77227/enqueue-google-web-fonts-without-messing-up-symbols-in-url
+
+/* 
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=<?php $default_font_family = get_theme_mod( 'hovercraft_default_font', 'noto_sans' );
+	echo ucwords(str_replace("_", "+", $default_font_family), "+"); ?>:ital,wght@0,400;0,600;0,700;1,400&display=swap">
+	
+	<?php 
+	$wp_locale = get_locale();
+	if (strpos($wp_locale, 'en') === false) { ?>
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Noto+Sans+<?php
+		$wp_locale = get_locale();
+		$wp_locale_trimmed = strstr($wp_locale, '_');
+		$wp_locale_final = str_replace('_', '', $wp_locale_trimmed);
+		echo rtrim($wp_locale_final); ?>:ital,wght@0,400;0,600;0,700;1,400&display=swap">
+	<?php } ?>
+*/
