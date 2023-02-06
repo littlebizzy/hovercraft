a39a1f055c923e7f84b737daa436d5ee1ed553a0
Jesse Nickles <jesse@littlebizzy.com>
2023-02-07 01:17:47 +0700

    Create breadcrumbs.php

---
+++ template-parts/content/breadcrumbs.php
@@ -0,0 +1,5 @@
+<?php if ( get_theme_mod( 'hovercraft_breadcrumbs' ) == 1 ) { ?>
+<div class="breadcrumbs">
+    <?php ah_breadcrumb(); ?>
+</div><!-- breadcrumbs -->
+<?php } ?><!-- if hovercraft_breadcrumbs -->
