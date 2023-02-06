a795a3783c288448e6dd89725933004a0eee7383
Jesse Nickles <jesse@littlebizzy.com>
2023-02-07 01:11:02 +0700

    Update tags.php

--- template-parts/content/tags.php
+++ template-parts/content/tags.php
@@ -1,1 +1,5 @@
-
+<div class="tags">
+    <?php $tags = get_tags(); foreach ( $tags as $tag ) { ?>
+        <a href="<?php echo get_tag_link( $tag->term_id ); ?>" rel="tag"><?php echo $tag->name; ?></a>
+    <?php } ?>
+</div><!-- tags -->
