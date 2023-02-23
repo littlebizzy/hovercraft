64ace5e1299dc49a00ca88084da3d69739e0411d
Jesse Nickles <jesse@littlebizzy.com>
2023-02-23 18:45:28 +0700

    Update remove-generator.php

--- inc/remove-generator.php
+++ inc/remove-generator.php
@@ -6,3 +6,5 @@ function hovercraft_remove_wp_generator() {
 add_filter( 'the_generator', 'hovercraft_remove_wp_generator' );
 
 // https://www.wpbeginner.com/wp-tutorials/the-right-way-to-remove-wordpress-version-number/
+// https://stackoverflow.com/questions/65613509/hiding-wordpress-version-using-wp-generator
+// https://stackoverflow.com/questions/16335347/wordpress-how-do-i-remove-meta-generator-tags
