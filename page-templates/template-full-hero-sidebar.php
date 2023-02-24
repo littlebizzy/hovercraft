5efa25e2de618cf3b418e0d53ea5e08059b583fb
Jesse Nickles <jesse@littlebizzy.com>
2023-02-25 03:47:43 +0700

    Update template-full-hero-sidebar.php

--- page-templates/template-full-hero-sidebar.php
+++ page-templates/template-full-hero-sidebar.php
@@ -22,12 +22,8 @@
     
     <div id="primary">
 
-	    <?php if ( get_theme_mod( 'hovercraft_breadcrumbs' ) == 1 ) { ?>
-		    <div class="breadcrumbs">
-			    <?php ah_breadcrumb(); ?>
-		    </div><!-- breadcrumbs -->
-	    <?php } ?><!-- if hovercraft_breadcrumbs -->
-	
+	    <?php get_template_part( 'template-parts/content/breadcrumbs' ); ?>
+
 	    <?php get_template_part( 'template-parts/content/content' ); ?>
 
 	    <div class="last-modified">
