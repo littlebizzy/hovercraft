97c4a459243acb8ea7b4a08fd1318cc18011653a
Jesse Nickles <jesse@littlebizzy.com>
2023-02-25 03:46:00 +0700

    Update template-basic-no-sidebar.php

--- page-templates/template-basic-no-sidebar.php
+++ page-templates/template-basic-no-sidebar.php
@@ -7,11 +7,7 @@
     
 	<div id="primary-wide">
 
-		<?php if ( get_theme_mod( 'hovercraft_breadcrumbs' ) == 1 ) { ?>
-			<div class="breadcrumbs">
-				<?php ah_breadcrumb(); ?>
-			</div><!-- breadcrumbs -->
-		<?php } ?><!-- if hovercraft_breadcrumbs -->
+		<?php get_template_part( 'template-parts/content/breadcrumbs' ); ?>
 	
 		<div id="content">
 			
