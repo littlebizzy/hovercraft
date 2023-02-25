268e89ada42e3808699c6108c593fb4d00de0afb
Jesse Nickles <jesse@littlebizzy.com>
2023-02-25 17:27:40 +0700

    Update css-generator.php

--- inc/css-generator.php
+++ inc/css-generator.php
@@ -136,6 +136,10 @@ a:hover {
 	text-decoration: <?php echo $topbar_link_decoration; ?>;
 }
 
+.topbar p {
+	margin-bottom: 0;
+}
+
 #hero-full-container {
 	background: linear-gradient(<?php echo $hero_gradient_angle; ?>, <?php 
 		list($r1, $g1, $b1) = sscanf($hero_gradient_start_color, "#%02x%02x%02x");
