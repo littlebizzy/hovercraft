e0b5c21be1700cce18b432dbd23b5688ccca9f4d
Jesse Nickles <jesse@littlebizzy.com>
2023-03-01 19:28:34 +0700

    Update customizer-settings.php

--- inc/customizer-settings.php
+++ inc/customizer-settings.php
@@ -220,6 +220,26 @@ $wp_customize->add_control( new WP_Customize_Control(
     			)
         )
 ) );
+	
+// homepage hide main setting
+$wp_customize->add_setting('hovercraft_hide_main', array(
+    'default' => 0,
+	'sanitize_callback' => 'hovercraft_sanitize_checkbox',
+));
+
+// homepage hide main control
+$wp_customize->add_control(
+    new WP_Customize_Control(
+        $wp_customize,
+        'hovercraft_hide_main',
+        array(
+            'label'     => __('Hide homepage main section', 'hovercraft'),
+            'section'   => 'hovercraft_homepage',
+            'settings'  => 'hovercraft_hide_main',
+            'type'      => 'checkbox',
+        )
+    )
+);
 
 
 // fonts section
