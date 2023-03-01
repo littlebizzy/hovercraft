a6ae3ffd5b7887c804234418ea7f4517816017c3
Jesse Nickles <jesse@littlebizzy.com>
2023-03-01 22:06:28 +0700

    Update last-modified.php

--- template-parts/content/last-modified.php
+++ template-parts/content/last-modified.php
@@ -1,3 +1,3 @@
 <div class="last-modified">
-    <span><?php _e('Last modified on ', 'hovercraft'); echo the_modified_time('F j, Y'); ?></span>
+    <span><?php _e( 'Last modified on ', 'hovercraft' ); echo the_modified_time('F j, Y'); ?></span>
 </div><!-- last-modified -->
