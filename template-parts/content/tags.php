bbe0a0ff28290ec50ad74e27e3ba4cd59b6cfb78
Jesse Nickles <jesse@littlebizzy.com>
2023-03-19 03:31:13 +0700

    Update tags.php

--- template-parts/content/tags.php
+++ template-parts/content/tags.php
@@ -1,5 +1,5 @@
 <div class="tags">
-    <?php $tags = get_tags(); foreach ( $tags as $tag ) { ?>
+    <span>Tags: <?php $tags = get_tags(); foreach ( $tags as $tag ) { ?>
         <a href="<?php echo get_tag_link( $tag->term_id ); ?>" rel="tag"><?php echo $tag->name; ?></a>
-    <?php } ?>
+    <?php } ?></span>
 </div><!-- tags -->
