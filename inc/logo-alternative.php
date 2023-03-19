bf96855fac06a4f0c73c9bd521310783f043fdaf
Jesse Nickles <jesse@littlebizzy.com>
2023-03-19 18:10:20 +0700

    Update logo-alternative.php

--- inc/logo-alternative.php
+++ inc/logo-alternative.php
@@ -10,7 +10,7 @@ function hovercraft_logo_alternative( $wp_customize ) {
 );
     
     $wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize, 'hovercraft_logo_alternative', array( 
-        'label' => 'Alternative Logo',
+        'label' => 'Full Hero Logo',
         'section' => 'title_tagline',
         'settings' => 'hovercraft_logo_alternative',
         'priority' => 8 // display just below the custom-logo
