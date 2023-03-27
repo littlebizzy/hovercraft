bd0575cd6f1dee84e5a948203f24423fa5130989
Jesse Nickles <jesse@littlebizzy.com>
2023-03-27 16:14:22 +0700

    Update single.php

--- single.php
+++ single.php
@@ -15,7 +15,8 @@
 	
 		<?php if ( is_singular() ) { ?>
 				<?php if ( is_page_template( 'page-templates/template-basic.php' ) || !is_page_template() ) { ?>
-					<?php global $post; $url_featured_image = wp_get_attachment_image_src( get_post_thumbnail_id( $currentID ), 'medium' );
+					<?php global $post; $url_featured_image = wp_get_attachment_image_src( get_post_thumbnail_id( $currentID ), 'medium_large' );
+					// https://rudrastyh.com/wordpress/image-sizes.html
 					if ( !empty( $url_featured_image ) && ( !is_singular('product') ) ) { ?>
 					<img class="featured-image" src="<?php echo $url_featured_image[0]; ?>" />
 					<?php } ?>
