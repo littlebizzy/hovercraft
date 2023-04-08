be24a53552401fde9ceba90c3a3ad3f46ad991dc
Jesse Nickles <jesse@littlebizzy.com>
2023-04-08 15:10:00 +0700

    Update premain.php

--- template-parts/content/premain.php
+++ template-parts/content/premain.php
@@ -1,19 +1,21 @@
-<div class="clear"></div>
+<?php if ( is_front_page() ) { ?>
 
-<?php if ( is_active_sidebar( 'hovercraft_home_premain_top' ) && is_front_page() ) { ?>
-<div id="home-premain-top">
-<div class="inner">
-	<?php dynamic_sidebar( 'hovercraft_home_premain_top' ); ?>
-	<div class="clear"></div>
-</div><!--inner -->
-</div><!-- home-premain-top -->
-<?php } ?>
+		<?php if ( is_active_sidebar( 'hovercraft_home_premain_top' ) ) { ?>
+		<div id="home-premain-top">
+			<div class="inner">
+				<?php dynamic_sidebar( 'hovercraft_home_premain_top' ); ?>
+				<div class="clear"></div>
+			</div><!-- inner -->
+		</div><!-- home-premain-top -->
+		<?php } ?>
 
-<?php if ( is_active_sidebar( 'hovercraft_home_premain_bottom' ) && is_front_page() ) { ?>
-<div id="home-premain-bottom">
-<div class="inner">
-	<?php dynamic_sidebar( 'hovercraft_home_premain_bottom' ); ?>
-	<div class="clear"></div>
-</div><!--inner -->
-</div><!-- home-premain-bottom -->
-<?php } ?>
+		<?php if ( is_active_sidebar( 'hovercraft_home_premain_bottom' ) ) { ?>
+		<div id="home-premain-bottom">
+			<div class="inner">
+				<?php dynamic_sidebar( 'hovercraft_home_premain_bottom' ); ?>
+				<div class="clear"></div>
+			</div><!--inner -->
+		</div><!-- home-premain-bottom -->
+		<?php } ?>
+
+<?php } ?><!-- endif is_homepage -->
