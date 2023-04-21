e9f65a54079df1f18353f7762363b1d3b01913b6
Jesse Nickles <jesse@littlebizzy.com>
2023-04-21 14:46:19 +0700

    Update page.php

--- page.php
+++ page.php
@@ -9,31 +9,24 @@ if ( !is_front_page() || ( is_front_page() && $hovercraft_homepage_hide_main_che
     
 	<?php $hovercraft_sidebar_status_checked = get_theme_mod( 'hovercraft_sidebar_status' ) ? true : false;
 	if ( $hovercraft_sidebar_status_checked == true ) { ?><div id="primary"><?php } else { ?><div id="primary-wide"><?php } ?>
-
-		<?php get_template_part( 'template-parts/content/breadcrumbs' ); ?>
 	
 		<div id="content">
+			
+			<?php get_template_part( 'template-parts/content/breadcrumbs' ); ?>
 						
 			<?php get_template_part( 'template-parts/content/featured-image' ); ?>
 			
 			<div class="content-padded">
 				<h1><?php the_title(); ?></h1>
 		
-		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-			<?php the_content(); ?>
-		<?php endwhile; endif; ?><!-- the loop -->
-	
-		<div class="clear"></div>
-		<?php hovercraft_pagination_nav(); ?>	
-	
-			
+		<?php get_template_part( 'template-parts/content/loop' ); ?>
 
 		<?php get_template_part( 'template-parts/content/last-modified' ); ?>
+					
+		<?php get_template_part( 'template-parts/content/pagination' ); ?>
 	
-		
-	
-		<?php comments_template(); ?> 
-				
+		<?php get_template_part( 'template-parts/content/comments' ); ?>
+					
 				<div class="clear"></div>
 			</div><!-- content-padded -->
 		</div><!-- content -->
@@ -44,9 +37,7 @@ if ( !is_front_page() || ( is_front_page() && $hovercraft_homepage_hide_main_che
     <?php if ( get_theme_mod( 'hovercraft_sidebar_status' ) == 1 ) { 
 		get_template_part( 'sidebar' ); 
 		} ?>
-		
-    	<?php get_template_part( 'template-parts/content/pagination' ); ?>
-		
+				
     	<div class="clear"></div>
 	</div><!-- inner -->
 	</div><!-- main -->
