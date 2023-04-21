3332f32dec5b97d360128619b412578d4cb57a10
Jesse Nickles <jesse@littlebizzy.com>
2023-04-21 14:37:58 +0700

    Update content.php

--- template-parts/content/content.php
+++ template-parts/content/content.php
@@ -4,12 +4,8 @@
 		
 	<?php get_template_part( 'template-parts/content/breadcrumbs' ); ?>
 
-	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-		
-		<?php the_content(); ?>
-		
-	<?php endwhile; endif; ?><!-- the loop -->
-				
+	<?php get_template_part( 'template-parts/content/loop' ); ?>
+	
 	<?php get_template_part( 'template-parts/content/last-modified' ); ?>
 					
 	<?php get_template_part( 'template-parts/content/pagination' ); ?>
