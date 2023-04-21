bc33d2c8a74a60f67a783d278c60c98f91d2fd59
Jesse Nickles <jesse@littlebizzy.com>
2023-04-21 14:28:34 +0700

    Update content.php

--- template-parts/content/content.php
+++ template-parts/content/content.php
@@ -1,18 +1,20 @@
 <div id="content">
-			
-			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-			
-				<div class="content-padded">
-						
-			<?php the_content(); ?>
+	
+	<div class="content-padded">
 		
-			<?php endwhile; endif; ?><!-- the loop -->
+	<?php get_template_part( 'template-parts/content/breadcrumbs' ); ?>
+
+	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+		
+		<?php the_content(); ?>
+		
+	<?php endwhile; endif; ?><!-- the loop -->
 				
-			<?php get_template_part( 'template-parts/content/last-modified' ); ?>
+	<?php get_template_part( 'template-parts/content/last-modified' ); ?>
 					
-			<?php get_template_part( 'template-parts/content/pagination' ); ?>
+	<?php get_template_part( 'template-parts/content/pagination' ); ?>
 			
-		</div><!-- content-padded -->
+	</div><!-- content-padded -->
 	
 <div class="clear"></div>
 </div><!-- content -->
