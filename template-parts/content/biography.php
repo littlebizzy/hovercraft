53bbe22a59ead27ca383013fc9bc5730979a9a59
Jesse Nickles <jesse@littlebizzy.com>
2023-04-22 16:48:13 +0700

    Update biography.php

--- template-parts/content/biography.php
+++ template-parts/content/biography.php
@@ -3,7 +3,9 @@ $hovercraft_biography = get_theme_mod( 'hovercraft_biography', 'native_posts_onl
 
 if ( ( $hovercraft_biography == 'native_posts_only' ) || ( $hovercraft_biography == 'all_post_types' ) ) {
 	
-	if ( ( $hovercraft_biography == 'native_posts_only' ) && ( get_post_type() === 'post' ) ) { ?>
+	if ( $hovercraft_biography == 'native_posts_only' ) { 
+
+		if ( get_post_type() === 'post' ) { ?>
 
         <div class="author-biography">
             <div class="biography-photo"><?php echo get_avatar( get_the_author_meta('user_email'), '150' ); ?></div>
@@ -11,9 +13,25 @@ if ( ( $hovercraft_biography == 'native_posts_only' ) || ( $hovercraft_biography
             <p class="biography-description"><?php echo nl2br( get_the_author_meta('description') ); ?></p>
         </div>
 
-	<?php }
+		<?php } // if native post
+
+	} elseif ( $hovercraft_biography == 'all_post_types' ) { 
+
+		if ( ( is_custom_post_type() ) || ( get_post_type() === 'post' ) ) { ?>
+
+		<div class="author-biography">
+            <div class="biography-photo"><?php echo get_avatar( get_the_author_meta('user_email'), '150' ); ?></div>
+            <h3 class="biography-name"><?php the_author_meta('display_name'); ?></h3>
+            <p class="biography-description"><?php echo nl2br( get_the_author_meta('description') ); ?></p>
+        </div>
+
+		<?php } // if custom post or native post
 	
+	} // if all_post_types
+			
 } // if biography either native_posts_only or all_post_types
 
 // https://shihabiiuc.com/how-to-display-author-bio-in-wordpress-single-post/
+// https://stackoverflow.com/questions/4290420/wordpress-how-to-check-whether-it-is-post-or-page
+// https://wordpress.stackexchange.com/questions/6731/how-do-test-if-a-post-is-a-custom-post-type
 // https://stackoverflow.com/questions/4290420/wordpress-how-to-check-whether-it-is-post-or-page
