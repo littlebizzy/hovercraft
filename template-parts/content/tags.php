7155d32af55f069af9d4bf8b1b8ecd60ac75d912
Jesse Nickles <jesse@littlebizzy.com>
2023-04-22 15:48:25 +0700

    Update tags.php

--- template-parts/content/tags.php
+++ template-parts/content/tags.php
@@ -1,5 +1,3 @@
 <div class="tags">
-    <span>Tags: <?php $tags = get_tags(); foreach ( $tags as $tag ) { ?>
-        <a href="<?php echo get_tag_link( $tag->term_id ); ?>" rel="tag"><?php echo $tag->name; ?></a>
-    <?php } ?></span>
+    <span>Tags: <?php echo wpdocs_show_tags(); ?></span>
 </div><!-- tags -->
