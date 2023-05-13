5bbe3f60c30774ea25bd471f7d797010894f09e1
Jesse Nickles <jesse@littlebizzy.com>
2023-05-13 20:10:47 +0700

    Update biography.php

--- template-parts/content/biography.php
+++ template-parts/content/biography.php
@@ -8,6 +8,7 @@ if ( ( $hovercraft_biography == 'native_posts_only' ) || ( $hovercraft_biography
 		if ( get_post_type() === 'post' ) { ?>
 
         <div class="author-biography">
+			<h5 class="author-biography-intro"><?php _e( 'About the Author', 'hovercraft' ); ?></h5>
             <div class="biography-photo"><?php echo get_avatar( get_the_author_meta('user_email'), '150' ); ?></div>
             <h3 class="biography-name"><?php the_author_meta('display_name'); ?></h3>
             <p class="biography-description"><?php echo nl2br( get_the_author_meta('description') ); ?></p>
@@ -20,7 +21,7 @@ if ( ( $hovercraft_biography == 'native_posts_only' ) || ( $hovercraft_biography
 		if ( ( is_custom_post_type() ) || ( get_post_type() === 'post' ) ) { ?>
 
 		<div class="author-biography">
-			<h5 class="author-biography-intro">About the Author</h5>
+			<h5 class="author-biography-intro"><?php _e( 'About the Author', 'hovercraft' ); ?></h5>
             <div class="biography-photo"><?php echo get_avatar( get_the_author_meta('user_email'), '150' ); ?></div>
             <h3 class="biography-name"><?php the_author_meta('display_name'); ?></h3>
             <p class="biography-description"><?php echo nl2br( get_the_author_meta('description') ); ?></p>
