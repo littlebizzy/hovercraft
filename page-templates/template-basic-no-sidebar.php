2e7c5703061aba697a08adc0ade18c8bf536fbae
Jesse Nickles <jesse@littlebizzy.com>
2023-05-14 17:48:20 +0700

    Update template-basic-no-sidebar.php

--- page-templates/template-basic-no-sidebar.php
+++ page-templates/template-basic-no-sidebar.php
@@ -2,8 +2,6 @@
 <?php get_template_part( 'header' ); ?>
 <?php get_template_part( 'template-parts/header/header-basic' ); ?>
 
-<?php get_template_part( 'template-parts/content/premain' ); ?>
-
 <?php $hovercraft_homepage_hide_main_checked = get_theme_mod( 'hovercraft_homepage_hide_main' ) ? true : false;
 if ( !is_front_page() || ( is_front_page() && $hovercraft_homepage_hide_main_checked != true ) ) { ?>
 
@@ -11,41 +9,34 @@ if ( !is_front_page() || ( is_front_page() && $hovercraft_homepage_hide_main_che
 <div class="inner">
     
 	<div id="primary-wide">
-
-		<?php get_template_part( 'template-parts/content/breadcrumbs' ); ?>
 	
 		<div id="content">
 			
-			<h1><?php the_title(); ?></h1>
-			
+			<?php get_template_part( 'template-parts/content/breadcrumbs' ); ?>
+						
 			<?php $url_featured_image = wp_get_attachment_url( get_post_thumbnail_id($post->ID), 'large' ); 
 			if ( !empty( $url_featured_image ) ) { ?>
 			<img class="featured-image" src="<?php echo $url_featured_image; ?>" />
 			<?php } ?>
 		
-		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-			<?php the_content(); ?>
-		<?php endwhile; endif; ?><!-- the loop -->
-	
-		<div class="clear"></div>
-		<?php hovercraft_pagination_nav(); ?>	
-	
+			<div class="content-padded">
+				<h1><?php the_title(); ?></h1>
+				<?php get_template_part( 'template-parts/content/loop' ); ?>
+				<?php get_template_part( 'template-parts/content/last-modified' ); ?>
+				<?php get_template_part( 'template-parts/content/pagination' ); ?>
+				<?php get_template_part( 'template-parts/content/comments' ); ?>
 			<div class="clear"></div>
+			</div><!-- content-padded -->
+	
 		</div><!-- content -->
 
-		<?php get_template_part( 'template-parts/content/last-modified' ); ?>
-	
-		<?php comments_template(); ?> 
-
-		<div class="clear"></div>
+	<div class="clear"></div>
 	</div><!-- primary-wide -->
-    
-    <?php get_template_part( 'template-parts/content/pagination' ); ?>
 	
-    <div class="clear"></div>
+<div class="clear"></div>
 </div><!-- inner -->
 </div><!-- main -->
 
-<?php } ?>
+<?php } //endif is_front_page ?>
 
 <?php get_template_part( 'footer' ); ?>
