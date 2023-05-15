a46e3c9e5acbb826f85e62a19250d21c47ba3a22
Jesse Nickles <jesse@littlebizzy.com>
2023-05-15 22:29:04 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,34 +4,36 @@
 <div id="main">
 <div class="inner">
 
-  <?php if ( get_theme_mod( 'hovercraft_sidebar_status' ) == 1 ) { ?><div id="primary"><?php } else { ?><div id="primary-wide"><?php } ?>
-
-	<?php get_template_part( 'template-parts/content/breadcrumbs' ); ?>
+	<?php $hovercraft_sidebar_status_checked = get_theme_mod( 'hovercraft_sidebar_status' ) ? true : false;
+	if ( $hovercraft_sidebar_status_checked == true ) { ?><div id="primary"><?php } else { ?><div id="primary-wide"><?php } ?>
 	
-	<div id="content">
+		<div id="content">
+			
+			<?php get_template_part( 'template-parts/content/breadcrumbs' ); ?>
+									
+			<div class="content-padded">
 		
-		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 		
 				<h4><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h4>
 				<?php the_excerpt(); ?>
 		
-		<?php endwhile; endif; ?><!-- the loop -->
-	
-	<?php get_template_part( 'template-parts/content/last-modified' ); ?>
-		
-	<?php get_template_part( 'template-parts/content/pagination' ); ?>
-	
-	<div class="clear"></div>
-	</div><!-- content -->
+				<?php endwhile; endif; ?><!-- the loop -->
+					
+				<?php get_template_part( 'template-parts/content/pagination' ); ?>
+						
+			<div class="clear"></div>
+			</div><!-- content-padded -->
+		</div><!-- content -->
 
 	<div class="clear"></div>
-  </div><!-- primary -->
+  	</div><!-- primary -->
   
     <?php if ( get_theme_mod( 'hovercraft_sidebar_status' ) == 1 ) { 
 		get_template_part( 'sidebar' ); 
 	  } ?>
     
-    <div class="clear"></div>
+<div class="clear"></div>
 </div><!-- inner -->
 </div><!-- main -->
 
