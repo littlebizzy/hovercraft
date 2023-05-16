98bb949f8b79682bbee0d020dd649b40b252460d
Jesse Nickles <jesse@littlebizzy.com>
2023-05-17 01:22:48 +0700

    Update template-full-hero-columns.php

--- page-templates/template-full-hero-columns.php
+++ page-templates/template-full-hero-columns.php
@@ -2,5 +2,21 @@
 <?php get_template_part( 'header' ); ?>
 <?php get_template_part( 'template-parts/header/header-full-hero' ); ?>
 <?php get_template_part( 'template-parts/content/posthero' ); ?>
+
+<div id="main">
+<div class="inner">
+	
+<div id="primary-wide">
+	
+<?php get_template_part( 'template-parts/content/breadcrumbs' ); ?>
+<?php get_template_part( 'template-parts/content/loop' ); ?>
 <?php get_template_part( 'template-parts/content/columns' ); ?>
+
+<div class="clear"></div>
+</div><!-- primary -->
+
+<div class="clear"></div>
+</div><!-- inner -->
+</div><!-- main -->
+	
 <?php get_template_part( 'footer' ); ?>
