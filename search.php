0ff3c46ce0f13126ecc1b51a65da6160f99ab746
Jesse Nickles <jesse@littlebizzy.com>
2023-05-16 22:00:34 +0700

    Update search.php

--- search.php
+++ search.php
@@ -4,44 +4,39 @@
 <div id="main">
 <div class="inner">
 	
-<?php if ( get_theme_mod( 'hovercraft_sidebar_status' ) == 1 ) { ?><div id="primary"><?php } else { ?><div id="primary-wide"><?php } ?>
-	
-	<?php get_template_part( 'template-parts/content/breadcrumbs' ); ?>
-	
-	<div id="content">
-	
-    <h1 class="page-title"><?php _e( 'Search results for:', 'nd_dosth' ); ?> <span class="search-query"><?php echo get_search_query(); ?></span></h1> 
-
-            <?php if ( have_posts() ): ?>
-                <?php while( have_posts() ): ?>
-                    <?php the_post(); ?>
-                        <h2><?php the_title(); ?></h2>
-                        <?php the_excerpt(); ?>
-                        <a href="<?php the_permalink(); ?>" class="read-more-link">
-                            <?php _e( 'Read More', 'nd_dosth' );  ?>
-                        </a>
-                <?php endwhile; ?>
-                <?php the_posts_pagination(); ?>
-            <?php else: ?>
-                <p><?php _e( 'No Search Results found', 'nd_dosth' ); ?></p>
-            <?php endif; ?>
+	<?php $hovercraft_sidebar_status_checked = get_theme_mod( 'hovercraft_sidebar_status' ) ? true : false;
+	if ( $hovercraft_sidebar_status_checked == true ) { ?><div id="primary"><?php } else { ?><div id="primary-wide"><?php } ?>
 		
-			<div class="clear"></div>
+	<div id="content">
+		
+		<?php get_template_part( 'template-parts/content/breadcrumbs' ); ?>
+		
+		<div class="content-padded">
+	
+    		<h2 class="page-title"><?php _e( 'Search results for:', 'nd_dosth' ); ?> <span class="search-query"><?php echo get_search_query(); ?></span></h2> 
+			<br>
+			
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+           	<h4><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h4>
+			<?php the_excerpt(); ?><br>
+			<?php endwhile; endif; ?><!-- the loop -->
+			
+			<?php get_template_part( 'template-parts/content/pagination' ); ?>
+			
+		<div class="clear"></div>
+		</div><!-- content-padded -->
+	
 	</div><!-- content -->
 	
 	<div class="clear"></div>
-</div><!-- primary -->
-
+  	</div><!-- primary -->
 	
 	<?php if ( get_theme_mod( 'hovercraft_sidebar_status' ) == 1 ) { 
 		get_template_part( 'sidebar' ); 
 	} ?>
 	
-	<?php get_template_part( 'template-parts/content/pagination' ); ?>
-	
-    <div class="clear"></div>
+<div class="clear"></div>
 </div><!-- inner -->
 </div><!-- main -->
 
 <?php get_template_part( 'footer' ); ?>
-	
