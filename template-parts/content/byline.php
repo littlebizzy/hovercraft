60a81c0688c38f91e6498a8d642ad5cbb9892db6
Jesse Nickles <jesse@littlebizzy.com>
2023-05-19 04:22:08 +0700

    Update byline.php

--- template-parts/content/byline.php
+++ template-parts/content/byline.php
@@ -1,3 +1,5 @@
+<?php if( !is_product() ) { ?>
 <div class="post-byline">
 	<span class="post-author"><?php _e( 'By ', 'hovercraft' ); echo get_the_author_meta('display_name', $author_id); ?></span> | <span class="post-updated"><?php _e( 'Updated on ', 'hovercraft' ); echo the_modified_time('F j, Y'); ?></span>
 </div><!-- post-byline -->
+<?php } ?>
