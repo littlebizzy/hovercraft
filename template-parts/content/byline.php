28bd5767310e73afb297603f02ea80adb88a8113
Jesse Nickles <jesse@littlebizzy.com>
2023-05-22 18:01:14 +0700

    Update byline.php

--- template-parts/content/byline.php
+++ template-parts/content/byline.php
@@ -1,5 +1,10 @@
-<?php if( !is_product() ) { ?>
+<?php if ( post_type_exists( 'product' ) && is_product() ) {
+	echo(null);
+} else { ?>
 <div class="post-byline">
 	<span class="post-author"><?php _e( 'By ', 'hovercraft' ); echo get_the_author_meta('display_name', $author_id); ?></span> | <span class="post-updated"><?php _e( 'Updated on ', 'hovercraft' ); echo the_modified_time('F j, Y'); ?></span>
 </div><!-- post-byline -->
-<?php } ?>
+<?php } 
+
+// https://wordpress.stackexchange.com/questions/264802/how-to-check-if-custom-post-type-exists-in-wordpress
+// https://stackoverflow.com/questions/3634381/php-if-something-is-the-case-do-nothing
