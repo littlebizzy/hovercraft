ed67708620ebb9c2a966e82ac4105c05fbcae209
Jesse Nickles <jesse@littlebizzy.com>
2023-05-24 22:58:04 +0700

    Create template-basic-bullets.php

---
+++ page-templates/template-basic-bullets.php
@@ -0,0 +1,5 @@
+<?php /* Template Name: Basic (Bullets) */ ?>
+<?php get_template_part( 'header' ); ?>
+<?php get_template_part( 'template-parts/header/header-basic' ); ?>
+<?php get_template_part( 'template-parts/content/bullets' ); ?>
+<?php get_template_part( 'footer' ); ?>
