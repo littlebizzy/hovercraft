1ea673ac3594923199412871c61a0e53fe469ca5
Jesse Nickles <jesse@littlebizzy.com>
2023-05-24 23:35:39 +0700

    Create bullets.php

---
+++ template-parts/content/bullets.php
@@ -0,0 +1,11 @@
+<div id="main">
+<div class="inner">
+	
+	<div id="bullets">
+		
+	<div class="clear"></div>
+	</div><!-- bullets -->
+        	
+    <div class="clear"></div>
+</div><!-- inner -->
+</div><!-- main -->
