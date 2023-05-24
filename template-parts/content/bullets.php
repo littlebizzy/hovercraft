0f5be3af49157cf1e9b5bc8e7c7543c75816c311
Jesse Nickles <jesse@littlebizzy.com>
2023-05-25 02:47:57 +0700

    Update bullets.php

--- template-parts/content/bullets.php
+++ template-parts/content/bullets.php
@@ -3,9 +3,34 @@
 	
 	<div id="bullets">
 		
+				<?php
+        // the query
+        $the_query = new WP_Query(array(
+            'category_name' => 'faq',
+            'post_status' => 'publish',
+            'posts_per_page' => 999,
+        ));
+        ?>
+
+        <?php if ($the_query->have_posts()) : ?>
+            <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+                <h5><?php the_title(); ?></h5>
+                <?php the_content(); ?><br><br>
+
+            <?php endwhile; ?>
+            <?php wp_reset_postdata(); ?>
+
+        <?php else : ?>
+            <p><?php __('No posts'); ?></p>
+        <?php endif; ?>
+
+		
 	<div class="clear"></div>
 	</div><!-- bullets -->
         	
     <div class="clear"></div>
 </div><!-- inner -->
 </div><!-- main -->
+
+<!-- https://codepen.io/MSEdgeDev/pen/bpExOB -->
+<!-- https://wordpress.stackexchange.com/questions/145125/display-content-from-a-specific-category -->
