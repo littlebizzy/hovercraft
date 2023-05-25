cb0a6f8e43a680d07ec373b660a007ac1b185aff
Jesse Nickles <jesse@littlebizzy.com>
2023-05-25 22:31:19 +0700

    Update and rename template-basic-bullets.php to template-category-bullets.php

--- page-templates/template-category-bullets.php
+++ page-templates/template-category-bullets.php
@@ -1,5 +1,4 @@
-<?php /* Template Name: Basic (Bullets) */ ?>
 <?php get_template_part( 'header' ); ?>
-<?php get_template_part( 'template-parts/header/header-basic' ); ?>
+<?php get_template_part( 'template-parts/header/header-mini-hero' ); ?>
 <?php get_template_part( 'template-parts/content/bullets' ); ?>
 <?php get_template_part( 'footer' ); ?>
