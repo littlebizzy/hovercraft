e4bb232449a636b2eccc866c0d120ed61f76074a
Jesse Nickles <jesse@littlebizzy.com>
2023-05-31 03:29:44 +0700

    Update category-bullets.php

--- page-templates/category-bullets.php
+++ page-templates/category-bullets.php
@@ -2,3 +2,8 @@
 <?php get_template_part( 'template-parts/header/header-mini-hero' ); ?>
 <?php get_template_part( 'template-parts/content/bullets' ); ?>
 <?php get_template_part( 'footer' ); ?>
+
+<?php
+// https://codepen.io/MSEdgeDev/pen/bpExOB
+// https://wpshout.com/how-to-display-posts-on-a-category-page-divided-into-their-respective-subcategories/
+?>
