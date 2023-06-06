b296f88771d7e020f314dda8b49c0ee8ae6af33a
Jesse Nickles <jesse@littlebizzy.com>
2023-06-06 23:55:42 +0700

    Create template-full-hero-tiles-sidebar.php

---
+++ page-templates/template-full-hero-tiles-sidebar.php
@@ -0,0 +1,7 @@
+<?php /* Template Name: Full Hero (Tiles Sidebar) */ ?>
+<?php get_template_part( 'header' ); ?>
+<?php get_template_part( 'template-parts/header/header-full-hero' ); ?>
+<?php get_template_part( 'template-parts/content/posthero' ); ?>
+<?php get_template_part( 'template-parts/content/tiles' ); ?>
+<?php get_template_part( 'sidebar' ); ?>
+<?php get_template_part( 'footer' ); ?>
