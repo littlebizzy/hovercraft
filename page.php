170967e9fc7799ffca8dec614bffc01cca3dcbe3
Jesse Nickles <jesse@littlebizzy.com>
2023-06-21 23:26:36 +0700

    Update page.php

--- page.php
+++ page.php
@@ -11,12 +11,13 @@ if ( !is_front_page() || ( is_front_page() && $hovercraft_homepage_hide_main_che
 	if ( $hovercraft_sidebar_status_checked == true ) { ?><div id="primary"><?php } else { ?><div id="primary-wide"><?php } ?>
 	
 		<div id="content">
-			
-			<?php get_template_part( 'template-parts/content/breadcrumbs' ); ?>
 						
 			<?php get_template_part( 'template-parts/content/featured-image' ); ?>
 			
 			<div class="content-padded">
+				
+				<?php get_template_part( 'template-parts/content/breadcrumbs' ); ?>
+				
 				<h1><?php the_title(); ?></h1>
 		
 				<?php get_template_part( 'template-parts/content/loop' ); ?>
