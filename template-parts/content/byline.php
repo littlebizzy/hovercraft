f80a30b760955b1d442ddcc88a7d3fbb8f8180a8
Jesse Nickles <jesse@littlebizzy.com>
2023-06-22 16:48:31 +0700

    Update byline.php

--- template-parts/content/byline.php
+++ template-parts/content/byline.php
@@ -2,7 +2,7 @@
 	echo(null);
 } else { ?>
 <div class="post-byline">
-	<span class="post-author"><?php _e( 'By ', 'hovercraft' ); echo get_the_author_meta('display_name', $author_id); ?></span> | <span class="post-updated"><?php _e( 'Updated on ', 'hovercraft' ); echo the_modified_time('F j, Y'); ?></span>
+	<span class="post-author"><?php _e( 'By ', 'hovercraft' ); ?><strong><?php echo get_the_author_meta('display_name', $author_id); ?></strong></span> | <span class="post-updated"><?php _e( 'Updated on ', 'hovercraft' ); echo the_modified_time('F j, Y'); ?></span>
 </div><!-- post-byline -->
 <?php } 
 
