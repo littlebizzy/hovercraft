69f6acbed536201b548780827c9c93a0ddc151a2
Jesse Nickles <jesse@littlebizzy.com>
2023-06-24 16:24:52 +0700

    Update template-basic-gallery-sidebar.php

--- page-templates/template-basic-gallery-sidebar.php
+++ page-templates/template-basic-gallery-sidebar.php
@@ -18,43 +18,7 @@ if ( !is_front_page() || ( is_front_page() && $hovercraft_homepage_hide_main_che
 				<div class="content-padded">
 					<h1><?php the_title(); ?></h1>
 					<?php get_template_part( 'template-parts/content/loop' ); ?>
-					
-				<div class="swiper">
-  				
-				<!-- Additional required wrapper -->
-  				<div class="swiper-wrapper">
-
-     			<?php 
-        		$query_images_args = array(
-            		'post_type'      => 'attachment',
-            		'post_parent'    => $post->ID,
-            		'post_mime_type' => 'image',
-            		'post_status'    => 'inherit',
-            		'orderby'        => 'post_date',
-            		'posts_per_page' =>  30,
-        		);
-																								  
-    			$query_images = new WP_Query( $query_images_args );
-
-        		if($query_images->have_posts()) : 
-            		while($query_images->have_posts()) : 
-                	$query_images->the_post(); ?>
-
-    <!-- Slides -->
-    <div class="swiper-slide">
-    	<div><?php echo wp_get_attachment_image( $query_images->posts->ID, 'medium' ); ?></div>
-    </div>
-  
-            <?php endwhile; ?>
-        <?php else : ?>
-            <p>No media file yet</p>
-        <?php endif;
-
-        /* Restore original Post Data */
-        wp_reset_postdata(); ?>
-		</div>
-		</div>
-					
+					<?php get_template_part( 'template-parts/content/gallery' ); ?>
 					<?php get_template_part( 'template-parts/content/last-modified' ); ?>
 					<?php get_template_part( 'template-parts/content/pagination' ); ?>
 					<?php get_template_part( 'template-parts/content/comments' ); ?>
