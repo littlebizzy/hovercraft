78cb14539d2c38c7c198056d86e728cb43870762
Jesse Nickles <jesse@littlebizzy.com>
2023-06-25 16:05:32 +0700

    Update template-full-hero-tiles-sidebar.php

--- page-templates/template-full-hero-tiles-sidebar.php
+++ page-templates/template-full-hero-tiles-sidebar.php
@@ -12,7 +12,7 @@ if ( !is_front_page() || ( is_front_page() && $hovercraft_homepage_hide_main_che
     <div id="primary">
 
 	    <?php get_template_part( 'template-parts/content/content' ); ?>
-        <?php get_template_part( 'template-parts/content/tiles' ); ?>
+        <?php get_template_part( 'template-parts/content/tiles-narrow' ); ?>
 	
 	    <?php comments_template(); ?> 
 
