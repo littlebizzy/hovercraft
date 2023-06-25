1c198748939eb1d7b62a3127b9a46dbe873b5635
Jesse Nickles <jesse@littlebizzy.com>
2023-06-25 16:05:02 +0700

    Create tiles-narrow.php

---
+++ template-parts/content/tiles-narrow.php
@@ -0,0 +1,88 @@
+<div id="tiles-narrow">
+	
+	<?php if ( is_active_sidebar( 'hovercraft_tile_one' ) ) { ?>
+	<div class="tile">
+		<?php dynamic_sidebar( 'hovercraft_tile_one' ); ?>
+		<div class="clear"></div>
+	</div><!-- tile -->
+	<?php } ?>
+		
+	<?php if ( is_active_sidebar( 'hovercraft_tile_two' ) ) { ?>
+	<div class="tile">
+		<?php dynamic_sidebar( 'hovercraft_tile_two' ); ?>
+		<div class="clear"></div>
+	</div><!-- tile -->
+	<?php } ?>
+		
+	<?php if ( is_active_sidebar( 'hovercraft_tile_three' ) ) { ?>
+	<div class="tile">
+		<?php dynamic_sidebar( 'hovercraft_tile_three' ); ?>
+		<div class="clear"></div>
+	</div><!-- tile -->
+	<?php } ?>
+		
+	<?php if ( is_active_sidebar( 'hovercraft_tile_four' ) ) { ?>
+	<div class="tile">
+		<?php dynamic_sidebar( 'hovercraft_tile_four' ); ?>
+		<div class="clear"></div>
+	</div><!-- tile -->
+	<?php } ?>
+		
+	<?php if ( is_active_sidebar( 'hovercraft_tile_five' ) ) { ?>
+	<div class="tile">
+		<?php dynamic_sidebar( 'hovercraft_tile_five' ); ?>
+		<div class="clear"></div>
+	</div><!-- tile -->
+	<?php } ?>
+		
+	<?php if ( is_active_sidebar( 'hovercraft_tile_six' ) ) { ?>
+	<div class="tile">
+		<?php dynamic_sidebar( 'hovercraft_tile_six' ); ?>
+		<div class="clear"></div>
+	</div><!-- tile -->
+	<?php } ?>
+		
+	<?php if ( is_active_sidebar( 'hovercraft_tile_seven' ) ) { ?>
+	<div class="tile">
+		<?php dynamic_sidebar( 'hovercraft_tile_seven' ); ?>
+		<div class="clear"></div>
+	</div><!-- tile -->
+	<?php } ?>
+		
+	<?php if ( is_active_sidebar( 'hovercraft_tile_eight' ) ) { ?>
+	<div class="tile">
+		<?php dynamic_sidebar( 'hovercraft_tile_eight' ); ?>
+		<div class="clear"></div>
+	</div><!-- tile -->
+	<?php } ?>
+		
+	<?php if ( is_active_sidebar( 'hovercraft_tile_nine' ) ) { ?>
+	<div class="tile">
+		<?php dynamic_sidebar( 'hovercraft_tile_nine' ); ?>
+		<div class="clear"></div>
+	</div><!-- tile -->
+	<?php } ?>
+		
+	<?php if ( is_active_sidebar( 'hovercraft_tile_ten' ) ) { ?>
+	<div class="tile">
+		<?php dynamic_sidebar( 'hovercraft_tile_ten' ); ?>
+		<div class="clear"></div>
+	</div><!-- tile -->
+	<?php } ?>
+		
+	<?php if ( is_active_sidebar( 'hovercraft_tile_eleven' ) ) { ?>
+	<div class="tile">
+		<?php dynamic_sidebar( 'hovercraft_tile_eleven' ); ?>
+		<div class="clear"></div>
+	</div><!-- tile -->
+	<?php } ?>
+		
+	<?php if ( is_active_sidebar( 'hovercraft_tile_twelve' ) ) { ?>
+	<div class="tile">
+		<?php dynamic_sidebar( 'hovercraft_tile_twelve' ); ?>
+		<div class="clear"></div>
+	</div><!-- tile -->
+	<?php } ?>
+        
+<div class="clear"></div>
+</div><!-- tiles-narrow -->
