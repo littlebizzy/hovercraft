5341b2f705bd3bb04d5116e4c39d0f63d7da831c
Jesse Nickles <jesse@littlebizzy.com>
2023-06-25 16:32:10 +0700

    Update and rename template-basic-tiles.php to template-basic-tiles-no-sidebar.php

--- page-templates/template-basic-tiles-no-sidebar.php
+++ page-templates/template-basic-tiles-no-sidebar.php
@@ -1,4 +1,4 @@
-<?php /* Template Name: Basic (Tiles) */ ?>
+<?php /* Template Name: Basic Tiles (No Sidebar) */ ?>
 <?php get_template_part( 'header' ); ?>
 <?php get_template_part( 'template-parts/header/header-basic' ); ?>
 <?php get_template_part( 'template-parts/content/tiles' ); ?>
