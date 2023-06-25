efd09e2813c01898063d5f5045bfe3900d5d9716
Jesse Nickles <jesse@littlebizzy.com>
2023-06-25 16:29:24 +0700

    Update and rename template-mini-hero-tiles.php to template-mini-hero-tiles-no-sidebar.php

--- page-templates/template-mini-hero-tiles-no-sidebar.php
+++ page-templates/template-mini-hero-tiles-no-sidebar.php
@@ -1,4 +1,4 @@
-<?php /* Template Name: Mini Hero (Tiles) */ ?>
+<?php /* Template Name: Mini Hero Tiles (No Sidebar) */ ?>
 <?php get_template_part( 'header' ); ?>
 <?php get_template_part( 'template-parts/header/header-mini-hero' ); ?>
 <?php get_template_part( 'template-parts/content/posthero' ); ?>
