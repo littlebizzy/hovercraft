0953851f7b8b41d82f4e4c3e0a224514fe433873
Jesse Nickles <jesse@littlebizzy.com>
2023-06-25 16:27:38 +0700

    Update and rename template-half-hero-columns.php to template-half-hero-columns-no-sidebar.php

--- page-templates/template-half-hero-columns-no-sidebar.php
+++ page-templates/template-half-hero-columns-no-sidebar.php
@@ -1,4 +1,4 @@
-<?php /* Template Name: Half Hero (Columns) */ ?>
+<?php /* Template Name: Half Hero Columns (No Sidebar) */ ?>
 <?php get_template_part( 'header' ); ?>
 <?php get_template_part( 'template-parts/header/header-half-hero' ); ?>
 <?php get_template_part( 'template-parts/content/posthero' ); ?>
