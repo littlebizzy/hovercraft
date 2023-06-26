b17a6f42cfa92cc418e7cacd35fbe601ad83fb1f
Jesse Nickles <jesse@littlebizzy.com>
2023-06-26 16:09:50 +0700

    Update single.php

--- single.php
+++ single.php
@@ -19,7 +19,7 @@
 
 					<?php get_template_part( 'template-parts/content/breadcrumbs' ); ?>
 				
-					<h1><?php the_title(); ?></h1>
+					<?php get_template_part( 'template-parts/content/title' ); ?>	
 
 					<?php get_template_part( 'template-parts/content/byline' ); ?>					
 		
