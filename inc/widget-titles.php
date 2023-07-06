029b9b362d6b2d5528e8112557609d98d2d4b749
Jesse Nickles <jesse@littlebizzy.com>
2023-07-07 01:28:51 +0700

    Update widget-titles.php

--- inc/widget-titles.php
+++ inc/widget-titles.php
@@ -11,3 +11,4 @@ function my_widget_title($id_base) {
     }
 
 // https://presscustomizr.com/snippet/dynamically-changing-the-widget-title-depending-on-the-context/
+// FIX NEEDED: Undefined variable $title in /var/www/html/wp-content/themes/hovercraft/inc/widget-titles.php on line 9 
