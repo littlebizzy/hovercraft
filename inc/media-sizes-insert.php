63aad796bcf26f53b302c21a5cd14062a01af463
Jesse Nickles <jesse@littlebizzy.com>
2023-07-25 15:25:52 +0700

    Create media-sizes-insert.php

---
+++ inc/media-sizes-insert.php
@@ -0,0 +1,12 @@
+<?php
+
+// make medium_large images available to insert into posts
+add_filter( 'image_size_names_choose', 'hovercraft_medium_large_images_insert' );
+function hovercraft_medium_large_images_insert( $sizes ) {
+   return array_merge( $sizes, array(
+      'medium_large' => __( 'Medium Large' ),
+   ) );
+}
+
+// https://wordpress.stackexchange.com/questions/290259/make-medium-large-images-available-to-insert-into-post
+// https://wpmudev.com/blog/wordpress-image-sizes/
