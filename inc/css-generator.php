19b0bf897e77b14cab8646da051d20056bac1add
Jesse Nickles <jesse@littlebizzy.com>
2023-07-27 17:41:21 +0700

    Update css-generator.php

--- inc/css-generator.php
+++ inc/css-generator.php
@@ -1011,24 +1011,6 @@ img.featured-image {
 }
 
 @media screen and (max-width: 1200px) {
-img.featured-image-archive {
-	width: 100%;
-	height: auto;
-	display: block;
-	margin-bottom: 20px;
-	}
-}
-
-@media screen and (min-width: 1200px) {
-img.featured-image-archive {
-	width: 768px;
-	height: auto;
-	display: block;
-	margin-bottom: 20px;
-	}
-}
-
-@media screen and (max-width: 1200px) {
 .featured-image-wrapper {
 	position: relative;
 	width: 100%;
@@ -1052,7 +1034,37 @@ img.featured-image-archive {
 	padding: 5px 10px;
 	right: 0px;
 }
-	
+
+
+/* archives */
+
+.post-tease-archive {
+	margin-bottom: 40px;
+}
+
+.post-excerpt-archive p {
+	margin: 0;
+}
+
+@media screen and (max-width: 1200px) {
+img.featured-image-archive {
+	width: 100%;
+	height: auto;
+	display: block;
+	margin-bottom: 20px;
+	}
+}
+
+@media screen and (min-width: 1200px) {
+img.featured-image-archive {
+	width: 768px;
+	height: auto;
+	display: block;
+	margin-bottom: 20px;
+	}
+}
+
+
 /* breadcrumbs */
 
 @media screen and (max-width: 1200px) {
