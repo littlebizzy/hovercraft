69e9827a8607a9b303b35f28ef92fdaa7d1f8072
Jesse Nickles <jesse@littlebizzy.com>
2023-07-30 15:43:08 +0700

    Create responsive-menu.js

---
+++ assets/js/responsive-menu.js
@@ -0,0 +1,31 @@
+(function($) {
+
+$(function(){
+  btnToggle = $('.btn--toggle');
+  $submenuTrigger = $('.submenu--trigger');
+  $thirdSubmenuTrigger = $('.third-submenu--trigger');
+  
+  $btnToggle.on('click', function(e) {
+    // e.preventDefault();
+    let $target = $( $(e.currentTarget).attr('data-target') );
+    $target.toggleClass('showing');
+  });
+  
+  
+  $submenuTrigger.on('click', function(e) {
+    e.preventDefault();
+    $(this).toggleClass('active');
+  });
+  
+  $thirdSubmenuTrigger.on('click', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    $(this).toggleClass('active');
+  })
+});
+
+})( jQuery );
+
+// https://codepen.io/joshuaaron/pen/QgdLpB
+// alt https://codepen.io/jigneshpanchalmj/pen/Qevqjp
+// alt https://codepen.io/semikola/pen/ggONzz
