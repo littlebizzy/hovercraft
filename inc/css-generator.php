e4aa6ae07f65c2e2445b603394630aef347724fa
Jesse Nickles <jesse@littlebizzy.com>
2023-08-04 21:15:54 +0700

    Update css-generator.php

--- inc/css-generator.php
+++ inc/css-generator.php
@@ -876,20 +876,103 @@ video.hero-background-video {
 	color: <?php echo $basic_hero_header_link_color; ?>;
 }
 
-	
+
+/* header primary cta */
+
+.cta-header-primary {
+	display: inline-block;
+	margin-left: 40px;
+}
+
+.cta-header-primary ul li {
+	list-style-type: none;
+	display: inline-block;
+	margin: 0;
+	padding: 0;
+	vertical-align: middle;
+	text-align: center;
+}
+
 .cta-header-primary ul li a {
-	font-size: 18px;
+	font-family: <?php echo ucwords(str_replace("_", " ", $main_menu_font_family)); ?>;
+	font-size: <?php echo $main_menu_desktop_font_size; ?>px;
 	font-weight: 700;
 	display: inline-block;
 	padding: 10px 20px;
 	background: <?php echo $default_link_color; ?>;
 	text-decoration: none !important;
-	border-radius: 0px;
+	color: #ffffff !important;
 }
 
 .cta-header-primary ul li a:hover {
 	background: <?php echo $default_hover_color; ?>;
+	color: #ffffff !important;
 }
+	
+
+/* header secondary cta */
+
+.cta-header-secondary {
+	display: inline-block;
+	margin-left: 40px;
+}
+
+#header-full-hero .cta-header-secondary ul li {
+	list-style-type: none;
+	display: inline-block;
+	margin: 0;
+	padding: 0;
+	vertical-align: middle;
+	text-align: center;
+}
+
+#header-half-hero .cta-header-secondary ul li, #header-mini-hero .cta-header-secondary ul li, #header-basic .cta-header-secondary ul li {
+	list-style-type: none;
+	display: inline-block;
+	margin: 0;
+	padding: 0;
+	vertical-align: middle;
+	text-align: center;
+}
+
+#header-full-hero .cta-header-secondary ul li a {
+	font-family: <?php echo ucwords(str_replace("_", " ", $main_menu_font_family)); ?>;
+	font-size: <?php echo $main_menu_desktop_font_size; ?>px;
+	font-weight: 400;
+	display: inline-block;
+	padding: 10px 20px;
+	background: transparent;
+	text-decoration: none;
+	color: #ffffff;
+	border-radius: 0px;
+	box-shadow: inset 0px 0px 0px 1px #ffffff;
+}
+
+#header-half-hero .cta-header-secondary ul li a, #header-mini-hero .cta-header-secondary ul li a, #header-basic .cta-header-secondary ul li a {
+	font-family: <?php echo ucwords(str_replace("_", " ", $main_menu_font_family)); ?>;
+	font-size: <?php echo $main_menu_desktop_font_size; ?>px;
+	font-weight: 400;
+	display: inline-block;
+	padding: 10px 20px;
+	background: transparent;
+	text-decoration: none;
+	color: <?php echo $default_text_color; ?>;
+	border-radius: 0px;
+	box-shadow: inset 0px 0px 0px 1px #263238;
+}
+
+#header-full-hero .cta-header-secondary ul li a:hover {
+	background: #fff;
+	color: <?php echo $default_text_color; ?> !important;
+}
+
+#header-half-hero .cta-header-secondary ul li a:hover, #header-mini-hero .cta-header-secondary ul li a:hover, #header-basic .cta-header-secondary ul li a:hover {
+	background: <?php echo $default_text_color; ?>;
+	color: #ffffff !important;
+}
+
+
+
 
 @media screen and (min-width: 1200px) {
 .welcome-wrapper {
