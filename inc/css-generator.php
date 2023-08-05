c0643f60b79e2d8a5e3ebef332f00c2270a9b3b1
Jesse Nickles <jesse@littlebizzy.com>
2023-08-05 23:58:08 +0700

    Update css-generator.php

--- inc/css-generator.php
+++ inc/css-generator.php
@@ -295,6 +295,55 @@ h5.author-biography-intro {
 }
 
 
+/* code styling */
+
+code {
+	font-family: 'Noto Sans Mono', monospace;
+	font-weight: 400;
+	background: #ECEFF1;
+	padding: 0px 5px;
+}
+
+@media screen and (max-width: 1200px) {
+pre {
+	width: calc(100% - 40px) !important;
+	font-family: 'Noto Sans Mono', monospace;
+	font-weight: 400;
+	background: #ECEFF1; /* correct */
+	padding: 10px;
+	margin-bottom: 30px;
+	overflow-x: scroll;
+	white-space: pre;
+	color: <?php echo $default_text_color; ?>;
+	}
+}
+
+@media screen and (min-width: 1200px) {
+pre {
+	width: 100%;
+	font-family: 'Noto Sans Mono', monospace;
+	font-weight: 400;
+	background: #ECEFF1; /* correct */
+	padding: 10px;
+	margin-bottom: 30px;
+	overflow-x: scroll;
+	white-space: pre;
+	color: <?php echo $default_text_color; ?>;
+	}
+}
+
+.pre {
+	width: 100%;
+	font-family: 'Noto Sans Mono', monospace;
+	background: #ECEFF1; /* correct */
+	padding: 10px;
+	margin-bottom: 30px;
+	overflow-x: scroll;
+	white-space: pre;
+	color: <?php echo $default_text_color; ?>;
+}
+
+
 /* buttons */
 
 button {
