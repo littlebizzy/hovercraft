dbeada39bcf77c0120d2d73e4234d203a4228893
Jesse Nickles <jesse@littlebizzy.com>
2023-08-10 03:46:51 +0700

    Update media-sizes-insert.php

--- inc/media-sizes-insert.php
+++ inc/media-sizes-insert.php
@@ -4,7 +4,7 @@
 add_filter( 'image_size_names_choose', 'hovercraft_medium_large_images_insert' );
 function hovercraft_medium_large_images_insert( $sizes ) {
    return array_merge( $sizes, array(
-      'medium_large' => __( 'Medium Large' ),
+      'medium_large' => __( 'Medium Large', 'hovercraft' ),
    ) );
 }
 
