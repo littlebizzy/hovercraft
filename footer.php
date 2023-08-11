accb6708634f3802da9c6f2c3611c77265123eb9
Jesse Nickles <jesse@littlebizzy.com>
2023-08-11 23:55:30 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -72,18 +72,6 @@
 	<div class="clear"></div>
 	</div><!-- copyright-right -->
 	<?php } ?>
-
-	<?php $hovercraft_search_icon = get_theme_mod( 'hovercraft_search_icon', 'none' );
-		if ( $hovercraft_search_icon != 'none' ) { ?>
-	<div id="full-screen-search">
-		<button type="button" class="close" id="full-screen-search-close"><i class="material-icons close">close</i></button>
-			<form role="search" method="get" action="<?php echo home_url( '/' ); ?>" id="full-screen-search-form">
-				<div id="full-screen-search-container">
-					<input type="text" name="s" placeholder="<?php _e( 'Search' ); ?>" id="full-screen-search-input" />
-				</div>
-			</form>
-	</div><!-- full-screen-search -->
-	<?php } ?>
 	
 	<?php $hovercraft_scroll_to_top = get_theme_mod( 'hovercraft_scroll_to_top', 'mobile_only' );
 	if ( $hovercraft_scroll_to_top != 'none' ) { ?>
@@ -97,6 +85,18 @@
 </div><!-- copyright -->
 
 </div><!-- container -->
+
+<?php $hovercraft_search_icon = get_theme_mod( 'hovercraft_search_icon', 'none' );
+	if ( $hovercraft_search_icon != 'none' ) { ?>
+	<div id="full-screen-search">
+		<button type="button" class="close" id="full-screen-search-close"><i class="material-icons close">close</i></button>
+			<form role="search" method="get" action="<?php echo home_url( '/' ); ?>" id="full-screen-search-form">
+				<div id="full-screen-search-container">
+					<input type="text" name="s" placeholder="<?php _e( 'Search entire site...' ); ?>" id="full-screen-search-input" />
+				</div>
+			</form>
+	</div><!-- full-screen-search -->
+<?php } ?>
 
 <?php wp_footer(); ?>
 
