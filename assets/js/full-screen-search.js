11fd02e1030d85200a4e28a2d0fb2832b9111e30
Jesse Nickles <jesse@littlebizzy.com>
2023-08-11 17:09:34 +0700

    Create full-screen-search.js

---
+++ assets/js/full-screen-search.js
@@ -0,0 +1,30 @@
+// When the document is ready...
+jQuery( document ).ready( function( $ ) {
+
+    // ... display the Full Screen search when:
+    // 1. The user focuses on a search field, or
+    // 2. The user clicks the Search button
+    //$( 'form[role=search] input, form[role=search] button' ).on( 'focus, click', function( event ) {
+	$( ".search-icon-wrapper" ).click(function(){
+        // Prevent the default action
+        event.preventDefault();
+
+        // Display the Full Screen Search
+        $( '#full-screen-search' ).addClass( 'open' );
+
+        // Focus on the Full Screen Search Input Field
+        $( '#full-screen-search input' ).focus();
+    } );
+
+    // Hide the Full Screen search when the user clicks the close button
+    $( '#full-screen-search button.close' ).on( 'click', function( event ) {
+        // Prevent the default event
+        event.preventDefault();
+
+        // Hide the Full Screen Search
+        $( '#full-screen-search' ).removeClass( 'open' );
+    } );
+
+} );
+
+// https://wordpress.org/plugins/full-screen-search-overlay/
