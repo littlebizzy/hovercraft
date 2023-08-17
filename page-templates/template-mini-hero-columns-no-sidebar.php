7b122368a79532e249ffffd6fbfd55fbf9422ce8
Jesse Nickles <jesse@littlebizzy.com>
2023-08-17 17:30:23 +0700

    Update template-mini-hero-columns-no-sidebar.php

--- page-templates/template-mini-hero-columns-no-sidebar.php
+++ page-templates/template-mini-hero-columns-no-sidebar.php
@@ -1,7 +1,12 @@
 <?php /* Template Name: Mini Hero Columns (No Sidebar) */ ?>
 <?php get_template_part( 'header' ); ?>
 <?php get_template_part( 'template-parts/header/header-mini-hero' ); ?>
-<?php get_template_part( 'template-parts/content/posthero' ); ?>
+
+	<?php if ( is_active_sidebar( 'hovercraft_posthero' ) && in_array( get_theme_mod( 'hovercraft_posthero_widget_display' ), array(
+		'full_and_half_and_mini_hero'
+		) )) : ?>
+		<?php get_template_part( 'template-parts/content/posthero' ); ?>
+	<?php endif; ?>
 
 <div id="main">
 <div class="inner">
