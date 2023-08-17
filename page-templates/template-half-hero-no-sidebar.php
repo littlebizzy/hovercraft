e6b7f2e197caafbe6d7fd178591366b9c6bd6b7e
Jesse Nickles <jesse@littlebizzy.com>
2023-08-17 18:41:00 +0700

    Update template-half-hero-no-sidebar.php

--- page-templates/template-half-hero-no-sidebar.php
+++ page-templates/template-half-hero-no-sidebar.php
@@ -1,8 +1,13 @@
 <?php /* Template Name: Half Hero (No Sidebar) */ ?>
 <?php get_template_part( 'header' ); ?>
 <?php get_template_part( 'template-parts/header/header-half-hero' ); ?>
-<?php get_template_part( 'template-parts/content/posthero' ); ?>
-<?php get_template_part( 'template-parts/content/premain' ); ?>
+
+	<?php if ( is_active_sidebar( 'hovercraft_posthero' ) && in_array( get_theme_mod( 'hovercraft_posthero_widget_display' ), array(
+		'full_and_half_hero',
+		'full_and_half_and_mini_hero'
+		) )) : ?>
+		<?php get_template_part( 'template-parts/content/posthero' ); ?>
+	<?php endif; ?>
 
 <?php $hovercraft_homepage_hide_main_checked = get_theme_mod( 'hovercraft_homepage_hide_main' ) ? true : false;
 if ( !is_front_page() || ( is_front_page() && $hovercraft_homepage_hide_main_checked != true ) ) { ?>
