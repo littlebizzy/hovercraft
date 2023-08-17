b5980790d17a11c9a801ae188dcebcd65ebab55b
Jesse Nickles <jesse@littlebizzy.com>
2023-08-17 18:48:14 +0700

    Update template-full-hero-tiles-sidebar.php

--- page-templates/template-full-hero-tiles-sidebar.php
+++ page-templates/template-full-hero-tiles-sidebar.php
@@ -1,7 +1,14 @@
 <?php /* Template Name: Full Hero Tiles (Sidebar) */ ?>
 <?php get_template_part( 'header' ); ?>
 <?php get_template_part( 'template-parts/header/header-full-hero' ); ?>
-<?php get_template_part( 'template-parts/content/posthero' ); ?>
+
+	<?php if ( is_active_sidebar( 'hovercraft_posthero' ) && in_array( get_theme_mod( 'hovercraft_posthero_widget_display' ), array(
+		'full_hero_only',
+		'full_and_half_hero',
+		'full_and_half_and_mini_hero'
+		) )) : ?>
+		<?php get_template_part( 'template-parts/content/posthero' ); ?>
+	<?php endif; ?>
 
 <?php $hovercraft_homepage_hide_main_checked = get_theme_mod( 'hovercraft_homepage_hide_main' ) ? true : false;
 if ( !is_front_page() || ( is_front_page() && $hovercraft_homepage_hide_main_checked != true ) ) { ?>
