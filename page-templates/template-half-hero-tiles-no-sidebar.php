104aebff167fd4e37cba4d2c90ffd51359393812
Jesse Nickles <jesse@littlebizzy.com>
2023-08-17 18:44:54 +0700

    Update template-half-hero-tiles-no-sidebar.php

--- page-templates/template-half-hero-tiles-no-sidebar.php
+++ page-templates/template-half-hero-tiles-no-sidebar.php
@@ -1,6 +1,13 @@
 <?php /* Template Name: Half Hero Tiles (No Sidebar) */ ?>
 <?php get_template_part( 'header' ); ?>
 <?php get_template_part( 'template-parts/header/header-half-hero' ); ?>
-<?php get_template_part( 'template-parts/content/posthero' ); ?>
+
+	<?php if ( is_active_sidebar( 'hovercraft_posthero' ) && in_array( get_theme_mod( 'hovercraft_posthero_widget_display' ), array(
+		'full_and_half_hero',
+		'full_and_half_and_mini_hero'
+		) )) : ?>
+		<?php get_template_part( 'template-parts/content/posthero' ); ?>
+	<?php endif; ?>
+
 <?php get_template_part( 'template-parts/content/tiles' ); ?>
 <?php get_template_part( 'footer' ); ?>
