c0bb5e7e461eac245029de24080608de26ae243f
Jesse Nickles <jesse@littlebizzy.com>
2023-08-20 23:55:31 +0700

    Update bullets-category.php

--- inc/bullets-category.php
+++ inc/bullets-category.php
@@ -5,7 +5,7 @@ add_filter( 'template_include', 'bullets_category_template', 99 );
 function bullets_category_template( $template ) {
 	$bullets_category = get_theme_mod( 'hovercraft_bullets_category', 'none' ); // depends on inc/category-bullets-assign.php
     if ( is_category( $bullets_category )  ) {
-        $new_template = locate_template( array( 'page-templates/category-bullets.php' ) );
+        $new_template = locate_template( array( 'page-templates/categories/category-bullets.php' ) );
 	if ( '' != $new_template ) {
 	    return $new_template ;
 	}
