9cc3ab58d09d3eb6f0b3f4c9b081712658b340b5
Jesse Nickles <jesse@littlebizzy.com>
2023-08-21 01:06:41 +0700

    Update comments.php

--- comments.php
+++ comments.php
@@ -4,14 +4,14 @@
  * If the current post is protected by a password and the visitor has not yet
  * entered the password we will return early without loading the comments.
  */
+
 if ( post_password_required() ) {
 	return;
 }
 ?>
 
-<div id="comments" class="comments-area">
-
 	<?php if ( have_comments() ) : ?>
+		<div id="comments" class="comments-area">
 		<h2 class="comments-title">
 			<?php
 			printf(
@@ -51,8 +51,7 @@ if ( post_password_required() ) {
 			<p class="no-comments"><?php _e( 'Comments are closed.', 'hovercraft' ); ?></p>
 		<?php endif; ?>
 
+	</div><!-- comments -->
 	<?php endif; // have_comments() ?>
 
 	<?php comment_form(); ?>
-
-</div><!-- #comments -->
