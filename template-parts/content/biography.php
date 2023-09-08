9174f0402d1f565caf66597fae03cea004124b01
Jesse Nickles <jesse@littlebizzy.com>
2023-09-09 04:31:33 +0700

    Update biography.php

--- template-parts/content/biography.php
+++ template-parts/content/biography.php
@@ -9,11 +9,11 @@ if ( ( $hovercraft_biography == 'native_posts_only' ) || ( $hovercraft_biography
 
         <div class="author-biography">
 			<h5 class="author-biography-intro"><?php _e( 'About the Author', 'hovercraft' ); ?></h5>
-            <div class="biography-photo"><?php echo get_avatar( get_the_author_meta('user_email'), '150' ); ?></div>
+			<div class="biography-photo"><?php if (get_the_author_meta('user_email')) { echo get_avatar(get_the_author_meta('user_email'), '150'); } ?></div>
             <h3 class="biography-name"><?php the_author_meta('display_name'); ?></h3>
             <p class="biography-description"><?php echo nl2br( get_the_author_meta('description') ); ?></p>
 			<div class="clear"></div>
-        </div>
+        </div><!-- author-biography -->
 
 		<?php } // if native post
 
@@ -23,11 +23,11 @@ if ( ( $hovercraft_biography == 'native_posts_only' ) || ( $hovercraft_biography
 
 		<div class="author-biography">
 			<h5 class="author-biography-intro"><?php _e( 'About the Author', 'hovercraft' ); ?></h5>
-            <div class="biography-photo"><?php echo get_avatar( get_the_author_meta('user_email'), '150' ); ?></div>
+			<div class="biography-photo"><?php if (get_the_author_meta('user_email')) { echo get_avatar(get_the_author_meta('user_email'), '150'); } ?></div>
             <h3 class="biography-name"><?php the_author_meta('display_name'); ?></h3>
             <p class="biography-description"><?php echo nl2br( get_the_author_meta('description') ); ?></p>
 			<div class="clear"></div>
-        </div>
+        </div><!-- author-biography -->
 
 		<?php } // if custom post or native post
 	
@@ -39,3 +39,4 @@ if ( ( $hovercraft_biography == 'native_posts_only' ) || ( $hovercraft_biography
 // https://stackoverflow.com/questions/4290420/wordpress-how-to-check-whether-it-is-post-or-page
 // https://wordpress.stackexchange.com/questions/6731/how-do-test-if-a-post-is-a-custom-post-type
 // https://stackoverflow.com/questions/4290420/wordpress-how-to-check-whether-it-is-post-or-page
+// https://wordpress.stackexchange.com/questions/193369/how-to-get-authors-avatar
