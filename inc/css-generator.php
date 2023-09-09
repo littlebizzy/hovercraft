42d7be2c5e9511a482f5fc898f080a1bc86c3c94
Jesse Nickles <jesse@littlebizzy.com>
2023-09-10 06:31:48 +0700

    Update css-generator.php

--- inc/css-generator.php
+++ inc/css-generator.php
@@ -2886,6 +2886,41 @@ ul.product_list_widget li a:before {
 ul.product_list_widget li a:hover:before {
 	background: rgba(0, 0, 0, 0.0);
 }
+
+
+/* pagination */
+
+.navigation {
+	padding-top: 30px;
+}
+
+.navigation ul {
+	margin: 0 !important;
+}
+
+.navigation li {
+    display: inline-block;
+}
+  
+.navigation li a,
+.navigation li a:hover,
+.navigation li.active a,
+.navigation li.disabled {
+    background-color: #ECEFF1;
+	color: #263238;
+    cursor: pointer;
+    padding: 5px 10px;
+	text-decoration: none;
+	font-size: 14px;
+	line-height: 1;
+}
+  
+.navigation li a:hover,
+.navigation li.active a {
+	background-color: <?php echo $default_text_color; ?>;
+	color: #ffffff;
+}
+
 </style>
 
 <?php }
