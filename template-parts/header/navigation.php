ca2a1f9ea16279d20dc98f62510a0bec0fcaf1c8
Jesse Nickles <jesse@littlebizzy.com>
2023-09-14 06:56:28 +0700

    Update navigation.php

--- template-parts/header/navigation.php
+++ template-parts/header/navigation.php
@@ -65,18 +65,3 @@
 		
 <div class="clear"></div>
 </div><!-- inner -->
-
-<nav class="mobile-nav">
-	<?php if ( has_nav_menu( 'main-menu' ) ) {
-		wp_nav_menu(array(
-			'theme_location' => 'main-menu', 
-			'menu_class' => 'menu', 
-			'container_class' => 'main-menu'
-			));
-	} else {
-		echo '<div class="main-menu"><ul class="menu">';
-		wp_list_pages( array( 'title_li' => '' ) );
-		echo '</ul></div>';
-	} ?>
-
-</nav>
