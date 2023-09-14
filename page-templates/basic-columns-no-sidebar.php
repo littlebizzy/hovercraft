511129536902d909a711dd17ec218f3c7354e158
Jesse Nickles <jesse@littlebizzy.com>
2023-09-14 07:00:09 +0700

    Update basic-columns-no-sidebar.php

--- page-templates/basic-columns-no-sidebar.php
+++ page-templates/basic-columns-no-sidebar.php
@@ -1,5 +1,7 @@
 <?php /* Template Name: Basic Columns (No Sidebar) */ ?>
 <?php get_template_part( 'header' ); ?>
 <?php get_template_part( 'template-parts/header/header-basic' ); ?>
+<?php get_template_part( 'template-parts/header/mobile-menu' ); ?>
+
 <?php get_template_part( 'template-parts/content/columns' ); ?>
 <?php get_template_part( 'footer' ); ?>
