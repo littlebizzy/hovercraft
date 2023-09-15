01a65fe28fbfbbd67e0ac46bfeb2ecce89e96b28
Jesse Nickles <jesse@littlebizzy.com>
2023-09-15 07:08:32 +0700

    Update responsive-menu.js

--- assets/js/responsive-menu.js
+++ assets/js/responsive-menu.js
@@ -1,29 +1,13 @@
-(function($) {
+function openNav() {
+ 	  document.getElementById("myNav").style.height = "100%";
+}
+/* Close overlay mobile menu */
+function closeNav() {
+  	document.getElementById("myNav").style.height = "0%";
+}
 
-$(document).ready(function()
-{
- /* Function 1: Toggle menu. Confirmed working. */
- $('.nav-icon').click(function()
-  { $('.mobile-nav').toggle(0); });
-
- /* Function 3: Toggle submenus. */
- $('.menu-item-has-children').click(function()
- { $(this).children('ul').toggle(0);
-   $(this).siblings('.menu-item-has-children').children('ul').hide(0);
- });
- /* Function 4: Prevent default events from clicking the parent menu. */
- $('.menu-item-has-children').children('a').click(function(event)
- { event.preventDefault(); });
- /* Functions 5 and 6: Handle menu roll on and roll off */
- $('.menu-item-has-children').mouseenter(function()
- { if( $(window).width() > 650 ){ $(this).children('ul').show(0); } });
- $('.menu-item-has-children').mouseleave(function()
- { if( $(window).width() > 650 ){ $(this).children('ul').hide(0); } });
-});
-
-})( jQuery );
-
-// https://codepen.io/nitronova/pen/ONYQbZ
+// https://codepen.io/voala/pen/eXBZeM
+// alt https://codepen.io/nitronova/pen/ONYQbZ
 // alt https://codepen.io/joshuaaron/pen/QgdLpB
 // alt https://codepen.io/jigneshpanchalmj/pen/Qevqjp
 // alt https://codepen.io/kpree/pen/Oeaqxa
