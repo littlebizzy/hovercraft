22ef1d4ffcad1900e42149824b4c01d1b844af69
Jesse Nickles <jesse@littlebizzy.com>
2023-09-16 03:51:09 +0700

    Update header.php

--- header.php
+++ header.php
@@ -28,18 +28,7 @@
 		
 		<?php wp_body_open(); ?>
 
-<!-- ADD IMEDIATLY AFTER OPENING BODY -->
-<!-- start mobile menu overlay -->
-<div id="myNav" class="overlay">
-  	<!-- Button to close the overlay navigation -->
-  	<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-
-  	<!-- Overlay content -->
-  	<div class="overlay-content">
-    	<?php wp_nav_menu( array('menu' => 'Main Menu' )); ?>
-  	</div>
-</div>
-<!-- end mobile menu overlay -->
+<?php get_template_part( 'template-parts/header/mobile-menu' ); ?>
 
 <div id="container">
 	
