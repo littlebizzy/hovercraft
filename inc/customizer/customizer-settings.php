80cf229501cd830e9af080d04b0459ec70dedf0d
Jesse Nickles <jesse@littlebizzy.com>
2023-09-17 05:44:51 +0700

    Update customizer-settings.php

--- inc/customizer/customizer-settings.php
+++ inc/customizer/customizer-settings.php
@@ -1926,6 +1926,31 @@ $wp_customize->add_section( 'hovercraft_icons', array(
     'priority'   => 102,
 ) );
 
+// layout icons setting
+$wp_customize->add_setting( 'hovercraft_layout_icons', array(
+    'default'    => 'material_icons',
+	'sanitize_callback' => 'hovercraft_sanitize_select',
+	) 
+);
+
+// layout icons control
+$wp_customize->add_control( new WP_Customize_Control(
+        $wp_customize,
+        'hovercraft_layout_icons',
+        array(
+            'label'     => __( 'Layout Icons', 'hovercraft' ),
+			'description' => __( 'Which icons should be used for critical site layout and UI features?', 'hovercraft' ),
+            'section'   => 'hovercraft_icons',
+            'settings'  => 'hovercraft_layout_icons',
+            'type'      => 'select',
+			'choices' => array(
+        		'none' => 'None (Disabled)',
+        		'material_icons_classic' => 'Material Icons (Classic)',
+				'font_awesome_version_6' => 'Font Awesome (Version 6)'
+    			)
+        )
+) );
+
 // material icons setting
 $wp_customize->add_setting( 'hovercraft_material_icons', array(
     'default'    => 'classic_only',
@@ -1938,8 +1963,8 @@ $wp_customize->add_control( new WP_Customize_Control(
         $wp_customize,
         'hovercraft_material_icons',
         array(
-            'label'     => __( 'Material Icons', 'hovercraft' ),
-			'description' => __( 'Which variations of the Material Icons should be loaded? Note: Disabling this might break your search and navigation menus.', 'hovercraft' ),
+            'label'     => __( 'Material Icons Library', 'hovercraft' ),
+			'description' => __( 'Which variations of the Material Icons should be loaded? Note: Disabling this might break your search, navigation, and shopping layout.', 'hovercraft' ),
             'section'   => 'hovercraft_icons',
             'settings'  => 'hovercraft_material_icons',
             'type'      => 'select',
@@ -1964,8 +1989,8 @@ $wp_customize->add_control( new WP_Customize_Control(
         $wp_customize,
         'hovercraft_font_awesome',
         array(
-            'label'     => __( 'Font Awesome', 'hovercraft' ),
-			'description' => __( 'Which variations of Font Awesome icons should be loaded?', 'hovercraft' ),
+            'label'     => __( 'Font Awesome Library', 'hovercraft' ),
+			'description' => __( 'Which variations of Font Awesome icons should be loaded? Note: Disabling this might break your search, navigation, and shopping layout.', 'hovercraft' ),
             'section'   => 'hovercraft_icons',
             'settings'  => 'hovercraft_font_awesome',
             'type'      => 'select',
