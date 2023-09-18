eb6ce61f4b02b6775c4f641b37e1ed7aad0ab90e
Jesse Nickles <jesse@littlebizzy.com>
2023-09-18 07:00:16 +0700

    Update byline.php

--- template-parts/content/byline.php
+++ template-parts/content/byline.php
@@ -2,7 +2,11 @@
 	echo(null);
 } else { ?>
 <div class="post-byline">
-	<span class="post-author"><?php _e( 'By ', 'hovercraft' ); ?><strong><?php echo get_the_author_meta('display_name', $author_id); ?></strong></span> | <span class="post-updated"><?php /* _e( 'Updated on ', 'hovercraft' ); */ echo the_modified_time('M j, Y'); ?></span>
+	<span class="post-author"><?php /* _e( 'By ', 'hovercraft' ); */ ?><?php $hovercraft_byline_photo = get_theme_mod( 'hovercraft_byline_photo', 'none' );
+	if ( ($hovercraft_byline_photo == 'byline_only') || ($hovercraft_byline_photo == 'byline_and_biography') ) { ?>
+		<span class="byline-photo"><?php if (get_the_author_meta('user_email')) { echo get_avatar(get_the_author_meta('user_email'), '150'); } ?></span>
+		<?php } ?>
+		<strong><?php echo get_the_author_meta('display_name', $author_id); ?></strong></span> | <span class="post-updated"><?php /* _e( 'Updated on ', 'hovercraft' ); */ echo the_modified_time('M j, Y'); ?></span>
 </div><!-- post-byline -->
 <?php } 
 
