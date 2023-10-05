8183185eb1870619218a54a4534b6c3c0e8376fe
Jesse Nickles <jesse@littlebizzy.com>
2023-10-06 05:11:13 +0700

    Create disable-responsive-images.php

---
+++ inc/disable-responsive-images.php
@@ -0,0 +1,6 @@
+<?php
+
+add_filter( 'wp_calculate_image_srcset', '__return_false' );
+
+// https://perishablepress.com/disable-wordpress-responsive-images/
+// https://wordpress.stackexchange.com/questions/211375/how-do-i-disable-responsive-images-in-wp-4-4
