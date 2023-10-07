505a709ea754681104dabcd773bbd3341a4e72d3
Jesse Nickles <jesse@littlebizzy.com>
2023-10-08 04:29:37 +0700

    Update gallery.php

--- template-parts/content/gallery.php
+++ template-parts/content/gallery.php
@@ -1,5 +1,5 @@
+<div class="gallery-narrow-wrapper">
 <?php echo do_shortcode( '[gallery size="medium" columns="0" link="none"]' ); 
+</div><!-- gallery-narrow-wrapper -->
 
 // https://codex.wordpress.org/Gallery_Shortcode
-
-?>
