736327d509bc9d12882d5152b59dc5e83585f395
Jesse Nickles <jesse@littlebizzy.com>
2023-10-08 04:29:00 +0700

    Create gallery-wide.php

---
+++ template-parts/content/gallery-wide.php
@@ -0,0 +1,5 @@
+<div class="gallery-wide-wrapper">
+<?php echo do_shortcode( '[gallery size="medium" columns="0" link="none"]' ); 
+</div><!-- gallery-wide-wrapper -->
+
+// https://codex.wordpress.org/Gallery_Shortcode
