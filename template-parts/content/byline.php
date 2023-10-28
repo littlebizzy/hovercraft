3eb4b58372b05edce6a21a5a96076683a3571031
Jesse Nickles <jesse@littlebizzy.com>
2023-10-28 13:43:35 +0700

    Update byline.php

--- template-parts/content/byline.php
+++ template-parts/content/byline.php
@@ -4,7 +4,12 @@
 <div class="post-byline">
 	<span class="post-author"><?php /* _e( 'By ', 'hovercraft' ); */ ?><?php $hovercraft_byline_photo = get_theme_mod( 'hovercraft_byline_photo', 'none' );
 	if ( ($hovercraft_byline_photo == 'byline_only') || ($hovercraft_byline_photo == 'byline_and_biography') ) { ?>
-		<span class="byline-photo"><?php if (get_the_author_meta('user_email')) { echo get_avatar(get_the_author_meta('user_email'), '150'); } ?></span>
+		<span class="byline-photo">
+			<img alt="author avatar" src="<?php 
+			$avatar_url = get_avatar_url(get_the_author_meta( 'ID' ), array('size' => 150));
+			echo $avatar_url; ?>" class="avatar photo">
+			<?php //if (get_the_author_meta('user_email')) { echo get_avatar(get_the_author_meta('user_email') ); } ?>
+		</span>
 		<?php } ?>
 		<strong><?php echo get_the_author_meta('display_name', $author_id); ?></strong></span> | <span class="post-updated"><?php /* _e( 'Updated on ', 'hovercraft' ); */ echo the_modified_time('M j, Y'); ?></span>
 </div><!-- post-byline -->
@@ -12,3 +17,5 @@
 
 // https://wordpress.stackexchange.com/questions/264802/how-to-check-if-custom-post-type-exists-in-wordpress
 // https://stackoverflow.com/questions/3634381/php-if-something-is-the-case-do-nothing
+// https://stackoverflow.com/questions/45529150/how-to-get-the-url-of-the-get-avatar-url-function-in-wordpress
+// https://www.wpexplorer.com/remove-async-decoding-wordpress-images/
