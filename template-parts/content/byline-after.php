a946b5066065da9993675f8309ae32a284f965a2
Jesse Nickles <jesse@littlebizzy.com>
2023-11-25 19:06:53 +0700

    Update byline-after.php

--- template-parts/content/byline-after.php
+++ template-parts/content/byline-after.php
@@ -1,7 +1,8 @@
 <?php if ( is_active_sidebar('hovercraft_after_byline')) { ?>
     <div class="after-byline">
-        <?php add_filter ( 'widget_title' , 'my_widget_title', 10, 1); //we use the default priority and 3 arguments in the callback function
-            dynamic_sidebar( 'hovercraft_after_byline' ); remove_filter('widget_title', 'my_widget_title'); ?>
+        <?php add_filter('widget_title', '__return_false');
+		dynamic_sidebar( 'hovercraft_after_byline' ); // https://stackoverflow.com/questions/13903918/apply-widget-title-filter-only-to-wordpress-widgets-from-a-certain-sidebar
+		remove_filter('widget_title', '__return_false'); ?>
     <div class="clear"></div>
     </div><!-- after-byline -->
 <?php } ?>
