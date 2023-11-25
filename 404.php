c3391afe3de921c788795979278a0f73dd17b389
Jesse Nickles <jesse@littlebizzy.com>
2023-11-25 16:49:02 +0700

    Update 404.php

--- 404.php
+++ 404.php
@@ -1,4 +1,34 @@
 <?php get_template_part( 'header' ); ?>
 <?php get_template_part( 'template-parts/header/header-basic' ); ?>
-<?php get_template_part( 'template-parts/content/main' ); ?>
+
+<div id="main">
+<div class="inner">
+
+<?php if ( get_theme_mod( 'hovercraft_sidebar_status' ) == 1 ) { ?><div id="primary"><?php } else { ?><div id="primary-wide"><?php } ?>
+
+	<?php get_template_part( 'template-parts/content/breadcrumbs' ); ?>
+	
+	<div id="content">
+	
+		<h1>404 Error (Not Found)</h1>
+		
+	<?php get_template_part( 'template-parts/content/loop' ); ?>
+	
+	<div class="clear"></div>
+	</div><!-- content -->
+	
+	<div class="clear"></div>
+</div><!-- primary -->
+
+
+<?php if ( get_theme_mod( 'hovercraft_sidebar_status' ) == 1 ) { 
+	get_template_part( 'sidebar' ); 
+} ?>
+
+<?php get_template_part( 'template-parts/content/pagination' ); ?>
+<div class="clear"></div>
+</div><!-- inner -->
+</div><!-- main -->
+
+
 <?php get_template_part( 'footer' ); ?>
