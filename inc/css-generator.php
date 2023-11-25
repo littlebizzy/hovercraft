eaba880376d142e1be1797482247fe65e0f3ae5f
Jesse Nickles <jesse@littlebizzy.com>
2023-11-25 16:42:06 +0700

    Update css-generator.php

--- inc/css-generator.php
+++ inc/css-generator.php
@@ -1955,7 +1955,7 @@ video.hero-background-video {
 
 @media screen and (min-width: 1200px) {
 #main {
-	padding: 40px 0px;
+	padding: 30px 0px;
 	background: <?php echo $main_background_color; ?>;
 	<?php if ( $sitewide_layout == 'floating_islands' ) { 
 			echo "background: $main_background_color;\n"; 
