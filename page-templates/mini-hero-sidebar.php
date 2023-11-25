554da97eab393c5c3ff3a610c5166d5e37816f22
Jesse Nickles <jesse@littlebizzy.com>
2023-11-25 15:15:57 +0700

    Update mini-hero-sidebar.php

--- page-templates/mini-hero-sidebar.php
+++ page-templates/mini-hero-sidebar.php
@@ -18,7 +18,7 @@ if ( !is_front_page() || ( is_front_page() && $hovercraft_homepage_hide_main_che
 	
 	    <?php get_template_part( 'template-parts/content/content' ); ?>
 	
-	    <?php comments_template(); ?> 
+	    <?php get_template_part( 'template-parts/content/comments' ); ?> 
 
 	    <div class="clear"></div>
     </div><!-- primary -->
