0405ec0f63f2c22cc379385f4c6c91a8a000920b
Jesse Nickles <jesse@littlebizzy.com>
2023-11-27 17:35:45 +0700

    Update columns.php

--- template-parts/content/columns.php
+++ template-parts/content/columns.php
@@ -1,12 +1,5 @@
+<div id="columns">
 	
-	<?php if ( is_active_sidebar( 'hovercraft_column_five' ) ) { ?>
-			<div id="columns-five">
-		<?php } elseif ( ! is_active_sidebar( 'hovercraft_column_five' ) && is_active_sidebar( 'hovercraft_column_four' ) ) { ?>
-			<div id="columns-four">
-		<?php } elseif ( ! is_active_sidebar( 'hovercraft_column_five' ) && ! is_active_sidebar( 'hovercraft_column_four' ) ) { ?>
-				<div id="columns-three">
-		<?php } ?>
-		
 	<?php if ( is_active_sidebar( 'hovercraft_column_one' ) ) { ?>
 	<div class="column">
 		<?php dynamic_sidebar( 'hovercraft_column_one' ); ?>
@@ -34,7 +27,7 @@
 		<div class="clear"></div>
 	</div><!-- column -->
 	<?php } ?>
-	
+		
 	<?php if ( is_active_sidebar( 'hovercraft_column_five' ) ) { ?>
 	<div class="column">
 		<?php dynamic_sidebar( 'hovercraft_column_five' ); ?>
@@ -42,23 +35,54 @@
 	</div><!-- column -->
 	<?php } ?>
 		
-	<div class="clear"></div>
-	</div><!-- columns -->
-
-<?php if ( is_active_sidebar( 'hovercraft_postcolumns_top' ) ) { ?>
-<div id="postcolumns-top">
-<div class="inner">
-	<?php dynamic_sidebar( 'hovercraft_postcolumns_top' ); ?>
-	<div class="clear"></div>
-</div><!--inner -->
-</div><!-- postcolumns-top -->
-<?php } ?>
-
-<?php if ( is_active_sidebar( 'hovercraft_postcolumns_bottom' ) ) { ?>
-<div id="postcolumns-bottom">
-<div class="inner">
-	<?php dynamic_sidebar( 'hovercraft_postcolumns_bottom' ); ?>
-	<div class="clear"></div>
-</div><!--inner -->
-</div><!-- postcolumns-bottom -->
-<?php } ?>
+	<?php if ( is_active_sidebar( 'hovercraft_column_six' ) ) { ?>
+	<div class="column">
+		<?php dynamic_sidebar( 'hovercraft_column_six' ); ?>
+		<div class="clear"></div>
+	</div><!-- column -->
+	<?php } ?>
+		
+	<?php if ( is_active_sidebar( 'hovercraft_column_seven' ) ) { ?>
+	<div class="column">
+		<?php dynamic_sidebar( 'hovercraft_column_seven' ); ?>
+		<div class="clear"></div>
+	</div><!-- column -->
+	<?php } ?>
+		
+	<?php if ( is_active_sidebar( 'hovercraft_column_eight' ) ) { ?>
+	<div class="column">
+		<?php dynamic_sidebar( 'hovercraft_column_eight' ); ?>
+		<div class="clear"></div>
+	</div><!-- column -->
+	<?php } ?>
+		
+	<?php if ( is_active_sidebar( 'hovercraft_column_nine' ) ) { ?>
+	<div class="column">
+		<?php dynamic_sidebar( 'hovercraft_column_nine' ); ?>
+		<div class="clear"></div>
+	</div><!-- column -->
+	<?php } ?>
+		
+	<?php if ( is_active_sidebar( 'hovercraft_column_ten' ) ) { ?>
+	<div class="column">
+		<?php dynamic_sidebar( 'hovercraft_column_ten' ); ?>
+		<div class="clear"></div>
+	</div><!-- column -->
+	<?php } ?>
+		
+	<?php if ( is_active_sidebar( 'hovercraft_column_eleven' ) ) { ?>
+	<div class="column">
+		<?php dynamic_sidebar( 'hovercraft_column_eleven' ); ?>
+		<div class="clear"></div>
+	</div><!-- column -->
+	<?php } ?>
+		
+	<?php if ( is_active_sidebar( 'hovercraft_column_twelve' ) ) { ?>
+	<div class="column">
+		<?php dynamic_sidebar( 'hovercraft_column_twelve' ); ?>
+		<div class="clear"></div>
+	</div><!-- column -->
+	<?php } ?>
+		
+<div class="clear"></div>
+</div><!-- columns -->
