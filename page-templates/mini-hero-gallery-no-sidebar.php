4f4345ea679edaf76bd708829a6d1e6d68e667e3
Jesse Nickles <jesse@littlebizzy.com>
2023-12-18 17:34:48 +0700

    Update mini-hero-gallery-no-sidebar.php

--- page-templates/mini-hero-gallery-no-sidebar.php
+++ page-templates/mini-hero-gallery-no-sidebar.php
@@ -20,7 +20,6 @@ if ( !is_front_page() || ( is_front_page() && $hovercraft_homepage_hide_main_che
 							
 				<div id="content-padded">
 					<?php get_template_part( 'template-parts/content/breadcrumbs' ); ?>
-					<h1><?php the_title(); ?></h1>
 					<?php get_template_part( 'template-parts/content/loop' ); ?>
 					<?php get_template_part( 'template-parts/content/gallery-wide' ); ?>
 					<?php get_template_part( 'template-parts/content/pagination' ); ?>
