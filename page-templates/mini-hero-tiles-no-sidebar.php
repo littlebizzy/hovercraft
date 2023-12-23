01146e3bcc56a9982a68d3c5fe8b8c69893db29d
Jesse Nickles <jesse@littlebizzy.com>
2023-12-23 17:43:48 +0700

    Update mini-hero-tiles-no-sidebar.php

--- page-templates/mini-hero-tiles-no-sidebar.php
+++ page-templates/mini-hero-tiles-no-sidebar.php
@@ -8,5 +8,42 @@
 		<?php get_template_part( 'template-parts/content/posthero' ); ?>
 	<?php endif; ?>
 
-<?php get_template_part( 'template-parts/content/tiles' ); ?>
+<?php $hovercraft_homepage_hide_main_checked = get_theme_mod( 'hovercraft_homepage_hide_main' ) ? true : false;
+if ( !is_front_page() || ( is_front_page() && $hovercraft_homepage_hide_main_checked != true ) ) { ?>
+
+<div id="main">
+<div class="inner">
+    
+    <div id="primary-wide">
+
+	<div id="content-wrapper">
+
+	<div id="content-wide">
+			
+	<?php get_template_part( 'template-parts/content/breadcrumbs' ); ?>
+		
+	<h1><?php the_title(); ?></h1>
+
+	<?php get_template_part( 'template-parts/content/loop' ); ?>
+		
+	<?php get_template_part( 'template-parts/content/tiles-wide' ); ?>
+						
+	<?php get_template_part( 'template-parts/content/pagination' ); ?>
+				
+	<div class="clear"></div>
+	</div><!-- content-padded -->
+		
+	</div><!-- content-wrapper -->   
+	
+	    <?php get_template_part( 'template-parts/content/comments' ); ?>
+
+	    <div class="clear"></div>
+    </div><!-- primary -->
+        
+    <div class="clear"></div>
+</div><!-- inner -->
+</div><!-- main -->
+
+<?php } // if is_front_page ?>
+
 <?php get_template_part( 'footer' ); ?>
