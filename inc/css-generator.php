6b3e1a09488cfe882ab84b7c854cd546112032d9
Jesse Nickles <jesse@littlebizzy.com>
2023-12-23 16:38:31 +0700

    Update css-generator.php

--- inc/css-generator.php
+++ inc/css-generator.php
@@ -2243,9 +2243,9 @@ img.featured-image-archive {
 @media screen and (min-width: 1200px) {
 #content-wide {
 	<?php if ( $sitewide_layout == 'floating_islands' ) { 
-			echo "padding: 0px 0px 0px 0px;\n"; 
+			echo "padding: 40px 40px 40px 40px;\n"; 
 		} else { 
-			echo "padding: 0px 0px 0px 0px;\n"; 
+			echo "padding: 40px 40px 40px 0px;\n"; 
 		}  ?>
 	display: block;
 	overflow: hidden;
