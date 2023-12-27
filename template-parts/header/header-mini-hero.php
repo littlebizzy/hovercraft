5a9bf7e2af8216272ba001493d382c12ed172c81
Jesse Nickles <jesse@littlebizzy.com>
2023-12-27 15:47:04 +0700

    Update header-mini-hero.php

--- template-parts/header/header-mini-hero.php
+++ template-parts/header/header-mini-hero.php
@@ -41,8 +41,6 @@
 <div class="inner">
 		<div class="title-wrapper">
 
-		<!-- <h1 class="mini-hero-title"><?php if ( is_singular() ) { single_post_title(); } elseif ( is_category() ) { single_cat_title(); } ?></h1> -->
-
 		<!--Start the hide title-->
     	<?php 
         $hide_title_status = get_post_meta( get_the_ID(), '_mysite_meta_hide_title', true);
