7a46ceae7a2e5e4b165eba2f27f444d60c5ec048
Jesse Nickles <jesse@littlebizzy.com>
2024-01-17 12:37:17 +0700

    Update css-generator.php

--- inc/css-generator.php
+++ inc/css-generator.php
@@ -2305,16 +2305,18 @@ img.featured-image-archive {
 }
 
 .wp-caption {
+	margin-bottom: 30px;
 	max-width: 100%;
 	height: auto;
 	display: block;
 }
 	
 .wp-caption img {
-	margin-bottom: 0;
+	margin-bottom: 0 !important;
 }
 
 .wp-caption-text {
+	margin-bottom: 0 !important;
     text-align: left;
     font-size: 12px;
 	line-height: 2;
