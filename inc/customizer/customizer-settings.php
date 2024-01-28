b276294b9c68a1c5c88a054d65ac87a86b03ccd5
Jesse Nickles <jesse@littlebizzy.com>
2024-01-28 16:24:49 +0700

    Update customizer-settings.php

--- inc/customizer/customizer-settings.php
+++ inc/customizer/customizer-settings.php
@@ -1124,7 +1124,7 @@ $wp_customize->add_control( new WP_Customize_Control(
 
 // footer font size (desktop) setting
 $wp_customize->add_setting( 'hovercraft_footer_desktop_font_size', array(
-    'default'    => '12',
+    'default'    => '14',
 	'sanitize_callback' => 'hovercraft_sanitize_float',
 	) 
 );
@@ -1144,7 +1144,7 @@ $wp_customize->add_control( new WP_Customize_Control(
 
 // footer font size (mobile) setting
 $wp_customize->add_setting( 'hovercraft_footer_mobile_font_size', array(
-    'default'    => '12',
+    'default'    => '14',
 	'sanitize_callback' => 'hovercraft_sanitize_float',
 	) 
 );
@@ -1164,7 +1164,7 @@ $wp_customize->add_control( new WP_Customize_Control(
 
 // copyright font size (desktop) setting
 $wp_customize->add_setting( 'hovercraft_copyright_desktop_font_size', array(
-    'default'    => '12',
+    'default'    => '14',
 	'sanitize_callback' => 'hovercraft_sanitize_float',
 	) 
 );
@@ -1184,7 +1184,7 @@ $wp_customize->add_control( new WP_Customize_Control(
 
 // copyright font size (mobile) setting
 $wp_customize->add_setting( 'hovercraft_copyright_mobile_font_size', array(
-    'default'    => '12',
+    'default'    => '14',
 	'sanitize_callback' => 'hovercraft_sanitize_float',
 	) 
 );
