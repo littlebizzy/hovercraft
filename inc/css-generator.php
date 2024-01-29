03546922fa3b3c706b77499eaa4fd1be69ca297c
Jesse Nickles <jesse@littlebizzy.com>
2024-01-29 16:05:53 +0700

    Update css-generator.php

--- inc/css-generator.php
+++ inc/css-generator.php
@@ -159,17 +159,23 @@ function hovercraft_generate_css(){
 <style type="text/css" id="hovercraft_custom_css">
 @media screen and (max-width: 1200px) {
 html, body {
+	height: 100%;
 	font-family: <?php echo ucwords(str_replace("_", " ", $default_font_family)); ?>;
 	font-size: <?php echo $default_mobile_font_size; ?>px;
+	line-height: 1.5;
 	color: <?php echo $default_text_color; ?>;
+	scroll-behavior: smooth;
 	}
 }
 
 @media screen and (min-width: 1200px) {
 html, body {
+	height: 100%;
 	font-family: <?php echo ucwords(str_replace("_", " ", $default_font_family)); ?>;
 	font-size: <?php echo $default_desktop_font_size; ?>px;
+	line-height: 1.5;
 	color: <?php echo $default_text_color; ?>;
+	scroll-behavior: smooth;
 	}
 }
 
@@ -2873,6 +2879,8 @@ p.biography-description {
 	
 @media screen and (max-width: 1200px) {
 #copyright {
+	width: 100%;
+	padding: 20px 20px;
 	font-size: <?php echo $copyright_mobile_font_size; ?>px;
 	background: <?php echo $copyright_background_color; ?>;
 	color: <?php echo $copyright_text_color; ?>;
@@ -2881,6 +2889,9 @@ p.biography-description {
 
 @media screen and (min-width: 1200px) {
 #copyright {
+	display: table;
+	width: 100%;
+	padding: 20px 0px;
 	font-size: <?php echo $copyright_desktop_font_size; ?>px;
 	background: <?php echo $copyright_background_color; ?>;
 	color: <?php echo $copyright_text_color; ?>;
@@ -2890,6 +2901,58 @@ p.biography-description {
 #copyright a {
 	color: <?php echo $copyright_link_color; ?>;
 	text-decoration: <?php echo $copyright_link_decoration; ?>;
+}
+
+@media screen and (max-width: 1200px) {
+.copyright-left {
+	width: 100%;
+	display: block;
+    vertical-align: top;
+	text-align: center;
+	}
+}
+
+@media screen and (min-width: 1200px) {
+.copyright-left {
+	display: table-cell;
+    vertical-align: middle;
+	height: inherit;
+	min-width: auto;
+	white-space: nowrap;
+	}
+}
+
+@media screen and (max-width: 1200px) {
+.copyright-right {
+	width: 100%;
+	margin-top: 30px;
+	display: block;
+    vertical-align: top;
+	text-align: center;
+	}
+}
+
+@media screen and (min-width: 1200px) {
+.copyright-right {
+	width: 100%;
+	display: table-cell;
+    vertical-align: middle;
+	height: inherit;
+	text-align: right;
+	}
+}
+
+
+/* copyright ul */
+
+#copyright ul {
+	list-style-type: none;
+}
+
+#copyright ul li {
+	list-style-type: none;
+	display: inline-block;
+	margin-left: 10px;
 }
 
 
