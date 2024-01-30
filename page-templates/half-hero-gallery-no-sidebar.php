9cba1dae2f962702cf5bd555695f29a28421dfdf
Jesse Nickles <jesse@littlebizzy.com>
2024-01-30 15:42:58 +0700

    Create half-hero-gallery-no-sidebar.php

---
+++ page-templates/half-hero-gallery-no-sidebar.php
@@ -0,0 +1,43 @@
+<?php /* Template Name: Half Hero Gallery (No Sidebar) */ ?>
+<?php get_template_part( 'header' ); ?>
+<?php get_template_part( 'template-parts/header/header-half-hero' ); ?>
+
+	<?php if ( is_active_sidebar( 'hovercraft_posthero' ) && in_array( get_theme_mod( 'hovercraft_posthero_widget_display' ), array(
+		'full_and_half_hero',
+		'full_and_half_and_mini_hero'
+		) )) : ?>
+		<?php get_template_part( 'template-parts/content/posthero' ); ?>
+	<?php endif; ?>
+
+<?php $hovercraft_homepage_hide_main_checked = get_theme_mod( 'hovercraft_homepage_hide_main' ) ? true : false;
+if ( !is_front_page() || ( is_front_page() && $hovercraft_homepage_hide_main_checked != true ) ) { ?>
+
+<div id="main">
+<div class="inner">
+    
+    <div id="primary-wide">
+
+	<div id="content-wrapper">
+
+	<div id="content-wide">
+					<?php get_template_part( 'template-parts/content/breadcrumbs' ); ?>
+					<h1><?php the_title(); ?></h1>
+					<?php get_template_part( 'template-parts/content/loop' ); ?>
+					<?php get_template_part( 'template-parts/content/gallery' ); ?>
+					<?php get_template_part( 'template-parts/content/pagination' ); ?>
+					<?php get_template_part( 'template-parts/content/comments' ); ?>
+				<div class="clear"></div>
+				</div><!-- content-padded -->
+		
+	</div><!-- content-wrapper -->   
+	
+	    <div class="clear"></div>
+    </div><!-- primary -->
+        
+    <div class="clear"></div>
+</div><!-- inner -->
+</div><!-- main -->
+
+<?php } // if is_front_page ?>
+
+<?php get_template_part( 'footer' ); ?>
