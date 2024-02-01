826df1f11f7c41c88b2b053a65447a71eb2df96a
Jesse Nickles <jesse@littlebizzy.com>
2024-02-01 18:15:21 +0700

    Update social-sharing.php

--- template-parts/content/social-sharing.php
+++ template-parts/content/social-sharing.php
@@ -5,12 +5,12 @@ $social_sharing = get_theme_mod( 'hovercraft_social_sharing', 'none' );
 if ( $social_sharing == 'top_of_post' ) { ?>
 
 <div id="social-sharing">
-<i class="fa-brands fa-pinterest"></i>
-<i class="fa-brands fa-facebook"></i>
-<i class="fa-brands fa-twitter"></i>
-<i class="fa-brands fa-whatsapp"></i>
-<i class="fa-brands fa-telegram"></i>
-<i class="fa-brands fa-viber"></i>
+    <i class="fa-brands fa-x-twitter"></i>
+    <i class="fa-brands fa-facebook"></i>
+    <i class="fa-brands fa-pinterest"></i>
+    <i class="fa-brands fa-telegram"></i>
+    <i class="fa-brands fa-viber"></i>
+    <i class="fa-brands fa-signal-messenger"></i>
 <div class="clear"></div>
 </div><!-- social-sharing -->
 
