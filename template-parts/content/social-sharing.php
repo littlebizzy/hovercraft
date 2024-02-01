8a54b654cc678f1320624e7de9edc19f29c41cb4
Jesse Nickles <jesse@littlebizzy.com>
2024-02-01 15:51:09 +0700

    Create social-sharing.php

---
+++ template-parts/content/social-sharing.php
@@ -0,0 +1,8 @@
+<div id="social-sharing">
+<i class="fa-brands fa-pinterest"></i>
+<i class="fa-brands fa-facebook"></i>
+<i class="fa-brands fa-twitter"></i>
+<i class="fa-brands fa-whatsapp"></i>
+<i class="fa-brands fa-telegram"></i>
+<i class="fa-brands fa-viber"></i>
+</div>
