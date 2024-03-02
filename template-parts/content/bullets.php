7de7311afab877473fa354a09570353c341fd962
Jesse Nickles <jesse@littlebizzy.com>
2024-03-02 16:01:58 +0700

    Update bullets.php

--- template-parts/content/bullets.php
+++ template-parts/content/bullets.php
@@ -25,8 +25,11 @@
 				if ( $hovercraft_bullets_html_content == 'include_html') { 
 					the_content();
 				} else {
+					$hovercraft_faq_character_count = get_theme_mod( 'hovercraft_faq_character_count', '300' );
 					$content = get_the_content();
-					echo wp_strip_all_tags( $content );
+					//echo wp_strip_all_tags( $content );
+					echo wp_trim_words( $content, $hovercraft_faq_character_count, '...' );
+					
 					// https://stackoverflow.com/questions/65033340/how-can-i-get-wordpress-page-content-without-html-tags
 					// https://wordpress.stackexchange.com/questions/94848/how-can-i-remove-all-html-tags-from-get-the-content
 					// https://stackoverflow.com/questions/17387313/custom-wp-strip-all-tags-wordpress-function
