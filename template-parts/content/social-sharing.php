78de22cc7e5623e39840db7b96ed805823064975
Jesse Nickles <jesse@littlebizzy.com>
2024-03-12 13:46:01 +0700

    Update social-sharing.php

--- template-parts/content/social-sharing.php
+++ template-parts/content/social-sharing.php
@@ -12,11 +12,10 @@ if ( $social_sharing == 'top_of_post' ) { ?>
     <a rel="noopener noreferrer nofollow" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $urlnow; ?>"><i class="fa-brands fa-facebook"></i></a>
     <a rel="noopener noreferrer nofollow" target="_blank" href="https://pinterest.com/pin/create/link/?url=<?php echo $urlnow; ?>"><i class="fa-brands fa-pinterest"></i></a>
     <a rel="noopener noreferrer nofollow" target="_blank" href="https://telegram.me/share/url?url=<?php echo $urlnow; ?>"><i class="fa-brands fa-telegram"></i></a>
-    <a rel="noopener noreferrer nofollow" target="_blank" href="https://pinterest.com/pin/create/link/?url=<?php echo $urlnow; ?>"><i class="fa-brands fa-viber"></i></a>
-    <a rel="noopener noreferrer nofollow" target="_blank" href="https://pinterest.com/pin/create/link/?url=<?php echo $urlnow; ?>"><i class="fa-brands fa-signal-messenger"></i></a>
-    <a rel="noopener noreferrer nofollow" target="_blank" href="https://pinterest.com/pin/create/link/?url=<?php echo $urlnow; ?>"><i class="fa-brands fa-whatsapp"></i></a>
-    <a rel="noopener noreferrer nofollow" target="_blank" href="https://pinterest.com/pin/create/link/?url=<?php echo $urlnow; ?>"><i class="fa-brands fa-snapchat"></i></a>
-    <a rel="noopener noreferrer nofollow" target="_blank" href="https://pinterest.com/pin/create/link/?url=<?php echo $urlnow; ?>"><i class="fa-brands fa-line"></i></a>
+    <a rel="noopener noreferrer nofollow" target="_blank" href="viber://forward?text=<?php echo $urlnow; ?>"><i class="fa-brands fa-viber"></i></a>
+    <a rel="noopener noreferrer nofollow" target="_blank" href="https://signal.me/?url=<?php echo $urlnow; ?>"><i class="fa-brands fa-signal-messenger"></i></a>
+    <a rel="noopener noreferrer nofollow" target="_blank" href="https://wa.me/?text=<?php echo $urlnow; ?>"><i class="fa-brands fa-whatsapp"></i></a>
+    <a rel="noopener noreferrer nofollow" target="_blank" href="https://social-plugins.line.me/lineit/share?url=<?php echo $urlnow; ?>"><i class="fa-brands fa-line"></i></a>
 <div class="clear"></div>
 </div><!-- social-sharing -->
 
