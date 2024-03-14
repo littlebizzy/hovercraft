1aefda86e6cbf9c0b680e87fe24fa1fb823bf48e
Jesse Nickles <jesse@littlebizzy.com>
2024-03-14 18:14:31 +0700

    Update css-generator.php

--- inc/css-generator.php
+++ inc/css-generator.php
@@ -2870,14 +2870,21 @@ p.biography-description {
 	width: 100%;
 	display: grid;
 	<?php if ( $prefooter_bottom_columns == '1' ) { 
-			echo "grid-template-columns: repeat(1, 1fr);\n"; 
-		} elseif ( $prefooter_bottom_columns == '2' ) { 
-			echo "grid-template-columns: repeat(2, 1fr);\n"; 
+		echo "grid-template-columns: repeat(1, 1fr);\n"; 
+	} elseif ( $prefooter_bottom_columns == '2' ) { 
+		echo "grid-template-columns: repeat(2, 1fr);\n"; 
 	} elseif ( $prefooter_bottom_columns == '3' ) { 
-			echo "grid-template-columns: repeat(3, 1fr);\n"; 
+		echo "grid-template-columns: repeat(3, 1fr);\n"; 
 	} elseif ( $prefooter_bottom_columns == '4' ) { 
-			echo "grid-template-columns: repeat(4, 1fr);\n"; 
-	} ?>
+		echo "grid-template-columns: repeat(4, 1fr);\n"; 
+	elseif ( $prefooter_bottom_columns == '5' ) { 
+		echo "grid-template-columns: repeat(5, 1fr);\n"; 
+	elseif ( $prefooter_bottom_columns == '6' ) { 
+		echo "grid-template-columns: repeat(6, 1fr);\n"; 
+	elseif ( $prefooter_bottom_columns == '7' ) { 
+		echo "grid-template-columns: repeat(7, 1fr);\n"; 
+	elseif ( $prefooter_bottom_columns == '8' ) { 
+		echo "grid-template-columns: repeat(8, 1fr);\n"; } ?>
   	gap: 30px;
 	}
 }
@@ -2936,14 +2943,21 @@ p.biography-description {
 	width: 100%;
 	display: grid;
 	<?php if ( $prefooter_bottom_columns == '1' ) { 
-			echo "grid-template-columns: repeat(1, 1fr);\n"; 
-		} elseif ( $prefooter_bottom_columns == '2' ) { 
-			echo "grid-template-columns: repeat(2, 1fr);\n"; 
+		echo "grid-template-columns: repeat(1, 1fr);\n"; 
+	} elseif ( $prefooter_bottom_columns == '2' ) { 
+		echo "grid-template-columns: repeat(2, 1fr);\n"; 
 	} elseif ( $prefooter_bottom_columns == '3' ) { 
-			echo "grid-template-columns: repeat(3, 1fr);\n"; 
+		echo "grid-template-columns: repeat(3, 1fr);\n"; 
 	} elseif ( $prefooter_bottom_columns == '4' ) { 
-			echo "grid-template-columns: repeat(4, 1fr);\n"; 
-	} ?>
+		echo "grid-template-columns: repeat(4, 1fr);\n"; 
+	elseif ( $prefooter_bottom_columns == '5' ) { 
+		echo "grid-template-columns: repeat(5, 1fr);\n"; 
+	elseif ( $prefooter_bottom_columns == '6' ) { 
+		echo "grid-template-columns: repeat(6, 1fr);\n"; 
+	elseif ( $prefooter_bottom_columns == '7' ) { 
+		echo "grid-template-columns: repeat(7, 1fr);\n"; 
+	elseif ( $prefooter_bottom_columns == '8' ) { 
+		echo "grid-template-columns: repeat(8, 1fr);\n"; } ?>
   	gap: 30px;
 	}
 }
