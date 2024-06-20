9ca050dd43eb3ae692f02f758ab23e528d774d48
Jesse Nickles <jesse@littlebizzy.com>
2024-06-20 17:33:30 +0700

    Create offcanvas-menu.php

---
+++ inc/offcanvas-menu.php
@@ -0,0 +1,15 @@
+<?php
+
+function hovercraft_offcanvas_menu() {
+    wp_enqueue_script(
+        'offcanvas_menu', // unique name 
+        get_template_directory_uri().'/assets/js/offcanvas-menu.js', // location
+        array('jquery'), // dependencies
+        '', 
+        true
+    );
+}
+
+add_action( 'wp_enqueue_scripts', 'hovercraft_offcanvas_menu' );
+
+// https://wpmudev.com/blog/adding-jquery-scripts-wordpress/
