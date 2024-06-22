2a3746deff867a2faefd97de0bd86943f59f5f07
Jesse Nickles <jesse@littlebizzy.com>
2024-06-22 17:17:51 +0700

    Update byline-archive.php

--- template-parts/content/byline-archive.php
+++ template-parts/content/byline-archive.php
@@ -3,7 +3,21 @@
 } else { ?>
 <div class="post-byline-archive">
 	<span class="post-author"><?php /* _e( 'By ', 'hovercraft' ); */ ?>
-		<strong><?php echo get_the_author_meta('display_name', $author_id); ?></strong></span> | <span class="post-updated"><?php /* _e( 'Updated on ', 'hovercraft' ); */ echo the_modified_time('M j, Y'); ?></span>
+		<strong><?php echo get_the_author_meta('display_name', $author_id); ?></strong></span>
+	
+		<?php $hovercraft_byline_date = get_theme_mod( 'hovercraft_byline_date', 'published_date_only' );
+		if ( $hovercraft_byline_date == 'published_date_only') { ?>
+			<span class="byline-separator"><?php echo "|"; ?></span>
+			<span class="post-published"><?php echo get_the_date('M j, Y'); ?></span>
+		<?php } elseif ( $hovercraft_byline_date == 'updated_date_only') { ?>
+			<span class="byline-separator"><?php echo "|"; ?></span>
+			<span class="post-updated"><?php echo the_modified_time('M j, Y'); ?></span>
+		<?php } elseif ( $hovercraft_byline_date == 'updated_and_published_dates') { ?>
+			<span class="byline-separator"><?php echo "|"; ?></span>
+			<span class="post-published"><?php echo get_the_date('M j, Y'); ?></span>
+			<span class="post-updated"><?php echo the_modified_time('M j, Y'); ?></span>
+		<?php } ?>
+	
 </div><!-- post-byline-archive -->
 <?php } 
 
