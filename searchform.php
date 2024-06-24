660d50dfc33d565eaa2508be8737c6f60455791c
Jesse Nickles <jesse@littlebizzy.com>
2024-06-24 16:55:54 +0700

    Update searchform.php

--- searchform.php
+++ searchform.php
@@ -1,5 +1,8 @@
 <form role="search" method="get" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<div class="searchform-items">
-		<input type="search" class="searchinput" value="<?php echo esc_attr( get_search_query() ); ?>" name="s" placeholder="<?php if ( class_exists( 'WooCommerce' ) ) { echo "Search entire store..."; } else { echo "Search entire site..."; } ?>" />
+		<input type="search" class="search-field" value="<?php echo esc_attr( get_search_query() ); ?>" name="s" placeholder="<?php if ( class_exists( 'WooCommerce' ) ) { echo "Search entire store..."; } else { echo "Search entire site..."; } ?>" />
+		<input type="submit" class="search-submit" value="&#xf002" />
 	</div><!-- searchform-items -->
 </form><!-- searchform -->
+
+<!-- https://codepen.io/jessuppi/pen/pomVZrr -->
