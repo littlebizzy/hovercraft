559b2b3c5bd8bacf41f03611ac3c40c35ef54ea1
Jesse Nickles <jesse@littlebizzy.com>
2024-06-27 00:04:03 +0700

    Update accordion-menu.php

--- inc/menus/accordion-menu.php
+++ inc/menus/accordion-menu.php
@@ -14,3 +14,16 @@ add_action( 'wp_enqueue_scripts', 'hovercraft_accordion_menu' );
 
 // https://wpmudev.com/blog/adding-jquery-scripts-wordpress/
 // https://codepen.io/keithchis/pen/GpdbLm
+
+
+function hovercraft_accordion_menu_toggle($item_output, $item, $depth, $args) {
+    if (in_array('menu-item-has-children', $item->classes)) {
+        $arrow = '<a href="#" class="menu-toggle"></a>'; // Change the class to your font icon
+        $item_output = str_replace('</a>', '</a>'. $arrow .'', $item_output);
+    }
+    return $item_output;
+}
+
+add_filter('walker_nav_menu_start_el', 'hovercraft_accordion_menu_toggle', 10, 4);
+
+// https://wordpress.stackexchange.com/questions/345410/adding-toggle-able-element-after-menu-item
