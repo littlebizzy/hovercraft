3956241951ddee395da90e7cddfae6d90d647d05
Jesse Nickles <jesse@littlebizzy.com>
2024-06-26 22:04:52 +0700

    Update mobile-menu.php

--- template-parts/header/mobile-menu.php
+++ template-parts/header/mobile-menu.php
@@ -29,25 +29,23 @@
 </div><!-- myNav -->
 
 <div id="offcanvas"> 
-		<?php 
-			if ( has_nav_menu( 'mobile-menu' ) ) {
-				wp_nav_menu(array(
-                'theme_location' => 'mobile-menu', 
-				'menu_class' => 'menu', 
-        		'container_class' => 'mobile-menu'
-                ));
-			} elseif ( has_nav_menu( 'main-menu' ) ) {
-            	wp_nav_menu(array(
-                'theme_location' => 'main-menu', 
-				'menu_class' => 'menu', 
-        		'container_class' => 'main-menu'
-                ));
-        	} else {
-            echo '<div class="main-menu"><ul class="menu">';
-            wp_list_pages( array( 'title_li' => '' ) );
-            echo '</ul></div>';
-        	}
-        ?>
+	<?php if ( has_nav_menu( 'mobile-menu' ) ) {
+		wp_nav_menu(array(
+        'theme_location' => 'mobile-menu', 
+		'menu_class' => 'menu', 
+        'container_class' => 'mobile-menu'
+        ));
+	} elseif ( has_nav_menu( 'main-menu' ) ) {
+        wp_nav_menu(array(
+        'theme_location' => 'main-menu', 
+		'menu_class' => 'menu', 
+        'container_class' => 'main-menu'
+        ));
+    } else {
+        echo '<div class="main-menu"><ul class="menu">';
+        wp_list_pages( array( 'title_li' => '' ) );
+        echo '</ul></div>';
+    } ?>
 </div>
 
 <div class="overlay-main"></div>
