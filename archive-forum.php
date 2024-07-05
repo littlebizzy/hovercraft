cd98fa816c2e59bb0ebbdd9efcac10453543e6c7
Jesse Nickles <jesse@littlebizzy.com>
2024-07-05 20:40:28 +0700

    Update and rename bbpress.php to archive-forum.php

--- archive-forum.php
+++ archive-forum.php
@@ -10,8 +10,12 @@
 				
 			<div id="content-wide">
 				<?php get_template_part( 'template-parts/content/breadcrumbs' ); ?>
-				<h1><?php the_title(); ?></h1>
-				<?php get_template_part( 'template-parts/content/loop' ); ?>
+				<h1><?php $forum_page = get_page_by_path( 'forum' ); echo get_the_title( $forum_page ); ?></h1>
+			                
+				<?php $forum_page = get_page_by_path( 'forum' );
+				$forum_page_content = apply_filters('the_content', $forum_page->post_content);
+				echo $forum_page_content; ?>	
+				
 				<?php get_template_part( 'template-parts/content/pagination' ); ?>
 			
             <div class="clear"></div>
@@ -28,5 +32,7 @@
 
 <?php get_template_part( 'footer' ); ?>
 
+<!-- https://bbpress.org/forums/topic/page-php-as-template/ -->
 <!-- https://bbpress.org/forums/topic/how-do-i-get-bbpress-pages-to-use-a-specific-page-template-in-wordpress/ -->
 <!-- https://bbpress.org/forums/topic/force-bbpress-to-use-different-template-then-page-php/ -->
+<!-- https://wordpress.stackexchange.com/questions/102841/how-to-get-page-id-of-a-page-using-page-slug -->
