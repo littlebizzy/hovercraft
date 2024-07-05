3c7c2476f8a59e799788ccd1c26b9c8f40aa0e2f
Jesse Nickles <jesse@littlebizzy.com>
2024-07-05 15:22:09 +0700

    Update related-posts.php

--- template-parts/content/related-posts.php
+++ template-parts/content/related-posts.php
@@ -13,17 +13,13 @@ $args = [
     ]
 ];
 $my_query = new wp_query( $args );
-if( $my_query->have_posts() ) {
-    echo '<div id="related-posts"><h4><?php _e( 'Related Posts: ', 'hovercraft' ); ?></h4><ul>';
-        while( $my_query->have_posts() ) {
-            $my_query->the_post(); ?>
-            <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
-        <?php }
-        wp_reset_postdata();
-    echo '</ul></div><!-- related-posts -->';
-}
+if( $my_query->have_posts() ) { ?>
+    <div id="related-posts">
+        <h4><?php _e( 'Related Posts: ', 'hovercraft' ); ?></h4>
+        <ul><?php while( $my_query->have_posts() ) { $my_query->the_post(); ?><li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li><?php } wp_reset_postdata(); ?></ul>
+    </div><!-- related-posts -->
+<?php }
 
 // https://wordpress.stackexchange.com/a/183498/152624
-// alt: https://stackoverflow.com/a/58809628/1718491
-// alt: https://wordpress.stackexchange.com/a/413722/152624
-?> 
+// https://stackoverflow.com/a/58809628/1718491
+// https://wordpress.stackexchange.com/a/413722/152624
