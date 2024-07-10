dcf76bbf22cd4eaa6340ea08eb9b27c54c52c3a9
Jesse Nickles <jesse@littlebizzy.com>
2024-07-10 13:52:47 +0700

    Update searchform.php

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <form role="search" method="get" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<div class="input-container">
-		<input type="search" class="search-input" value="<?php echo esc_attr( get_search_query() ); ?>" name="s" placeholder="<?php echo "Search entire site..."; ?>" />
+		<input type="search" class="search-input" value="<?php echo esc_attr( get_search_query() ); ?>" name="s" placeholder="<?php _e( 'Search entire website... ', 'hovercraft' ); ?>" />
 		<button type="submit" class="search-submit" name="submit"><?php $hovercraft_layout_icons = get_theme_mod( 'hovercraft_layout_icons', 'material_icons_classic' );
 			if ( $hovercraft_layout_icons == 'material_icons_classic' ) { ?><i class="material-icons search">search</i><?php } 
 			elseif ( $hovercraft_layout_icons == 'font_awesome_version_6' ) { ?><i class="fas fa-search"></i><?php } ?></button>
