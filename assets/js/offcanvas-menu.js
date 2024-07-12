24c83c64bddf39aa5b57d7d98217018c22109324
Jesse Nickles <jesse@littlebizzy.com>
2024-07-12 14:15:19 +0700

    Update offcanvas-menu.js

--- assets/js/offcanvas-menu.js
+++ assets/js/offcanvas-menu.js
@@ -1,12 +1,12 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(function($){
 
-    $(document).ready(function () {
-        $(".burger").on("click", function(){
+    $(document).ready(function(){
+        $('.burger').on('click', function(){
         	$('#offcanvas').toggleClass('active');
 			$('body').toggleClass('frozen');
 			$('.overlay-main').toggleClass('active');
         });
-		$(".overlay-main").on("click", function(){
+		$('.overlay-main').on('click', function(){
 			$('#offcanvas').removeClass('active');
 			$('body').removeClass('frozen');
 			$('.overlay-main').removeClass('active');
