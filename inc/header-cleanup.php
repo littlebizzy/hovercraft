85bb43ed5e7efd884311f61a6ec57e63ae90c349
Jesse Nickles <jesse@littlebizzy.com>
2024-07-21 16:15:47 +0700

    Update header-cleanup.php

--- inc/header-cleanup.php
+++ inc/header-cleanup.php
@@ -7,3 +7,7 @@ function hovercraft_remove_feed_links() {
 add_action( 'after_setup_theme', 'hovercraft_remove_feed_links', 99 );
 
 // https://ronangelo.com/disable-feeds-on-wordpress-or-bbpress/
+
+// https://stackoverflow.com/questions/34750148/how-to-delete-remove-wordpress-feed-urls-in-header
+// https://github.com/littlebizzy/header-cleanup/blob/1.2.0/core/cleaner.php
+// https://slickstack.io/forum/topic/how-to-disable-some-or-all-of-rss-feeds-in-wordpress-website
