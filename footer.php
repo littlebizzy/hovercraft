11194b5bcb2b3c811bffb86f3af2487098127643
Jesse Nickles <jesse@littlebizzy.com>
2024-07-28 17:33:25 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -91,11 +91,16 @@
 	if ( $hovercraft_search_icon != 'none' ) { ?>
 	<div id="full-screen-search">
 		<button type="button" class="close"><i class="material-icons close">close</i></button>
-			<form role="search" method="get" action="<?php echo home_url( '/' ); ?>">
-				<div id="full-screen-search-container">
-					<input type="text" name="s" placeholder="<?php _e( 'Search entire site...' ); ?>" />
-				</div>
-			</form>
+
+			<form role="search" method="get" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+				<div class="input-container">
+					<input type="search" class="search-input" value="<?php echo esc_attr( get_search_query() ); ?>" name="s" placeholder="<?php _e( 'Search entire website... ', 'hovercraft' ); ?>" />
+					<button type="submit" class="search-submit" name="submit"><?php $hovercraft_layout_icons = get_theme_mod( 'hovercraft_layout_icons', 'material_icons_classic' );
+			if ( $hovercraft_layout_icons == 'material_icons_classic' ) { ?><i class="material-icons search">search</i><?php } 
+			elseif ( $hovercraft_layout_icons == 'font_awesome_version_6' ) { ?><i class="fas fa-search"></i><?php } ?></button>
+				</div><!-- input-container -->
+			</form><!-- searchform -->
+				
 	</div><!-- full-screen-search -->
 <?php } ?>
 
