73c4cd03f778aca7be97f045df07af6948a2a4b4
Jesse Nickles <jesse@littlebizzy.com>
2024-08-04 16:57:47 +0700

    Update header-full-hero.php

--- template-parts/header/header-full-hero.php
+++ template-parts/header/header-full-hero.php
@@ -60,7 +60,8 @@
         $hide_title_status = get_post_meta( get_the_ID(), '_mysite_meta_hide_title', true);
         $post_id = get_the_ID();
         	if($hide_title_status == "off" || !metadata_exists( 'post', $post_id, '_mysite_meta_hide_title' ) ) : ?>
-				<h1 class="full-hero-title <?php if ( $h1_divider_display == 'everywhere_possible' ) { echo "divide"; } ?>"><?php single_post_title(); ?></h1>
+				<h1 class="full-hero-title <?php $h1_divider_display = get_theme_mod( 'hovercraft_h1_divider_display', 'none' );
+				if ( $h1_divider_display == 'everywhere_possible' ) { echo "divide"; } ?>"><?php single_post_title(); ?></h1>
 			<?php endif;
 		} ?>
 			
@@ -72,7 +73,8 @@
         				$hide_title_status = get_post_meta( get_the_ID(), '_mysite_meta_hide_title', true);
         				$post_id = get_the_ID();
         				if($hide_title_status == "off" || !metadata_exists( 'post', $post_id, '_mysite_meta_hide_title' ) ) : ?>
-							<h1 class="full-hero-title <?php if ( $h1_divider_display == 'everywhere_possible' ) { echo "divide"; } ?>"><?php single_post_title(); ?></h1>
+							<h1 class="full-hero-title <?php $h1_divider_display = get_theme_mod( 'hovercraft_h1_divider_display', 'none' );
+							if ( $h1_divider_display == 'everywhere_possible' ) { echo "divide"; } ?>"><?php single_post_title(); ?></h1>
 						<?php endif;
 					} ?>
 					<?php if ( is_active_sidebar( 'hovercraft_hero_snippet' ) ) {
