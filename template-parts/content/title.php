cd6a4df6943aa7d9f7dc5cb105f4b8149a95c519
Jesse Nickles <jesse@littlebizzy.com>
2024-08-05 14:38:54 +0700

    Update title.php

--- template-parts/content/title.php
+++ template-parts/content/title.php
@@ -1,1 +1,2 @@
-<h1><?php the_title(); ?></h1>
+<h1 class="<?php $h1_divider_display = get_theme_mod( 'hovercraft_h1_divider_display', 'none' );
+			if ( $h1_divider_display == 'everywhere_possible' || $h1_divider_display == 'everywhere_except_heros' ) { echo "divide"; } ?>"><?php the_title(); ?></h1>
