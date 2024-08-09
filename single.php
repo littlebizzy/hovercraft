8657f80b24ebea6937b1cd2d4b835c63fa3654b7
Jesse Nickles <jesse@littlebizzy.com>
2024-08-09 12:48:22 +0700

    Update single.php

--- single.php
+++ single.php
@@ -76,10 +76,7 @@
 					
 					<?php get_template_part( 'template-parts/content/biography' ); ?>
 
-					<?php $hovercraft_post_tags = get_theme_mod( 'hovercraft_post_tags', 'native_posts_only' );
-					if ( $hovercraft_post_tags != 'none' && is_single() ) { ?>
-						<?php get_template_part( 'template-parts/content/tags' ); ?>
-					<?php } ?>
+					<?php get_template_part( 'template-parts/content/tags' ); ?>
 					
 					<?php get_template_part( 'template-parts/content/link-pages' ); ?>
 					
