42655fa766d247c4b0fa50cf70bc5a667dd8369b
Jesse Nickles <jesse@littlebizzy.com>
2024-08-09 12:44:02 +0700

    Update tags.php

--- template-parts/content/tags.php
+++ template-parts/content/tags.php
@@ -4,24 +4,28 @@ if ( $hovercraft_post_tags == 'native_posts_only' ) {
     if ( is_single() && 'post' == get_post_type() ) { ?>
         <div class="tags"><span><?php _e( 'Tags: ', 'hovercraft' ); echo wpdocs_show_tags(); ?></span></div><!-- tags -->
     <?php }
+}
 elseif ( $hovercraft_post_tags == 'native_posts_and_custom_posts' ) {
     if ( is_single() ) {
         if ( 'post' == get_post_type() || is_custom_post_type() ) { ?>
             <div class="tags"><span><?php _e( 'Tags: ', 'hovercraft' ); echo wpdocs_show_tags(); ?></span></div><!-- tags -->
         <?php }
     }
+}
 elseif ( $hovercraft_post_tags == 'native_posts_and_pages' ) {
     if ( is_single() ) {
         if ( 'post' == get_post_type() || is_page() ) { ?>
             <div class="tags"><span><?php _e( 'Tags: ', 'hovercraft' ); echo wpdocs_show_tags(); ?></span></div><!-- tags -->
         <?php }
     }
+}
 elseif ( $hovercraft_post_tags == 'native_posts_and_pages_and_custom_posts' ) {   
     if ( is_single() ) {
         if ( 'post' == get_post_type() || is_page() || is_custom_post_type() ) { ?>
             <div class="tags"><span><?php _e( 'Tags: ', 'hovercraft' ); echo wpdocs_show_tags(); ?></span></div><!-- tags -->
         <?php }
     }
+}
 else {
     // do nothing
 }
