d5f96ee33538250d0b102700e2ddf1768bf6478d
Jesse Nickles <jesse@littlebizzy.com>
2024-08-16 01:43:44 +0700

    Update customizer-settings.php

--- inc/customizer/customizer-settings.php
+++ inc/customizer/customizer-settings.php
@@ -580,6 +580,74 @@ $wp_customize->add_section( 'hovercraft_widget_layouts', array(
     'priority'   => 35,
 ) );
 
+// home postmain top widget columns setting
+$wp_customize->add_setting( 'hovercraft_home_postmain_top_columns', array(
+    'default'    => '1',
+	'sanitize_callback' => 'hovercraft_sanitize_float',
+	) 
+);
+
+// home postmain top widget columns control
+$wp_customize->add_control( new WP_Customize_Control(
+        $wp_customize,
+        'hovercraft_home_postmain_top_columns',
+        array(
+            'label'     => __('Home Postmain Top Widgets Across (Desktop)', 'hovercraft'),
+			'description' => __( 'How many widgets across should display in the Home Postmain Top widget area? (How many columns)', 'hovercraft' ),
+            'section'   => 'hovercraft_widget_layouts',
+            'settings'  => 'hovercraft_home_postmain_top_columns',
+            'type'      => 'select',
+			'choices' => array(
+				'1' => '1 Column',
+        		'2' => '2 Columns',
+        		'3' => '3 Columns',
+				'4' => '4 Columns',
+				'5' => '5 Columns',
+        		'6' => '6 Columns',
+				'7' => '7 Columns',
+				'8' => '8 Columns',
+				'9' => '9 Columns',
+				'10' => '10 Columns',
+        		'11' => '11 Columns',
+				'12' => '12 Columns'
+    			)
+        )
+) );
+
+// home postmain bottom widget columns setting
+$wp_customize->add_setting( 'hovercraft_home_postmain_bottom_columns', array(
+    'default'    => '1',
+	'sanitize_callback' => 'hovercraft_sanitize_float',
+	) 
+);
+
+// home postmain bottom widget columns control
+$wp_customize->add_control( new WP_Customize_Control(
+        $wp_customize,
+        'hovercraft_home_postmain_bottom_columns',
+        array(
+            'label'     => __('Home Postmain Bottom Widgets Across (Desktop)', 'hovercraft'),
+			'description' => __( 'How many widgets across should display in the Home Postmain Bottom widget area? (How many columns)', 'hovercraft' ),
+            'section'   => 'hovercraft_widget_layouts',
+            'settings'  => 'hovercraft_home_postmain_bottom_columns',
+            'type'      => 'select',
+			'choices' => array(
+				'1' => '1 Column',
+        		'2' => '2 Columns',
+        		'3' => '3 Columns',
+				'4' => '4 Columns',
+				'5' => '5 Columns',
+        		'6' => '6 Columns',
+				'7' => '7 Columns',
+				'8' => '8 Columns',
+				'9' => '9 Columns',
+				'10' => '10 Columns',
+        		'11' => '11 Columns',
+				'12' => '12 Columns'
+    			)
+        )
+) );
+	
 // prefooter top widget columns setting
 $wp_customize->add_setting( 'hovercraft_prefooter_top_columns', array(
     'default'    => '1',
@@ -592,7 +660,7 @@ $wp_customize->add_control( new WP_Customize_Control(
         $wp_customize,
         'hovercraft_prefooter_top_columns',
         array(
-            'label'     => __('Widgets Across (Desktop)', 'hovercraft'),
+            'label'     => __('Prefooter Top Widgets Across (Desktop)', 'hovercraft'),
 			'description' => __( 'How many widgets across should display in the Prefooter Top widget area? (How many columns)', 'hovercraft' ),
             'section'   => 'hovercraft_widget_layouts',
             'settings'  => 'hovercraft_prefooter_top_columns',
@@ -626,7 +694,7 @@ $wp_customize->add_control( new WP_Customize_Control(
         $wp_customize,
         'hovercraft_prefooter_bottom_columns',
         array(
-            'label'     => __('Widgets Across (Desktop)', 'hovercraft'),
+            'label'     => __('Prefooter Bottom Widgets Across (Desktop)', 'hovercraft'),
 			'description' => __( 'How many widgets across should display in the Prefooter Bottom widget area? (How many columns)', 'hovercraft' ),
             'section'   => 'hovercraft_widget_layouts',
             'settings'  => 'hovercraft_prefooter_bottom_columns',
