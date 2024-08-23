fc59284e760ff3580302418ca2db1929ea611c61
Jesse Nickles <jesse@littlebizzy.com>
2024-08-23 15:54:16 +0700

    Update css-generator.php

--- inc/css-generator.php
+++ inc/css-generator.php
@@ -3163,22 +3163,20 @@ p.biography-description {
 #home-postmain-top .inner {
 	width: 100%;
 	display: grid;
-	<?php if ( $home_postmain_top_columns == '1' ) { 
-		echo "grid-template-columns: repeat(1, 1fr);\n"; 
-	} elseif ( $home_postmain_top_columns == '2' ) { 
-		echo "grid-template-columns: repeat(2, 1fr);\n"; 
-	} elseif ( $home_postmain_top_columns == '3' ) { 
-		echo "grid-template-columns: repeat(3, 1fr);\n"; 
-	} elseif ( $home_postmain_top_columns == '4' ) { 
-		echo "grid-template-columns: repeat(4, 1fr);\n"; 
-	} elseif ( $home_postmain_top_columns == '5' ) { 
-		echo "grid-template-columns: repeat(5, 1fr);\n"; 
-	} elseif ( $home_postmain_top_columns == '6' ) { 
-		echo "grid-template-columns: repeat(6, 1fr);\n"; 
-	} elseif ( $home_postmain_top_columns == '7' ) { 
-		echo "grid-template-columns: repeat(7, 1fr);\n"; 
-	} elseif ( $home_postmain_top_columns == '8' ) { 
-		echo "grid-template-columns: repeat(8, 1fr);\n"; } ?>
+	<?php
+	if ( $home_postmain_top_columns == '1' ) { echo "grid-template-columns: repeat(1, 1fr);\n"; } 
+	elseif ( $home_postmain_top_columns == '2' ) { echo "grid-template-columns: repeat(2, 1fr);\n"; } 
+	elseif ( $home_postmain_top_columns == '3' ) { echo "grid-template-columns: repeat(3, 1fr);\n"; } 
+	elseif ( $home_postmain_top_columns == '4' ) { echo "grid-template-columns: repeat(4, 1fr);\n"; } 
+	elseif ( $home_postmain_top_columns == '5' ) { echo "grid-template-columns: repeat(5, 1fr);\n"; } 
+	elseif ( $home_postmain_top_columns == '6' ) { echo "grid-template-columns: repeat(6, 1fr);\n"; } 
+	elseif ( $home_postmain_top_columns == '7' ) { echo "grid-template-columns: repeat(7, 1fr);\n"; } 
+	elseif ( $home_postmain_top_columns == '8' ) { echo "grid-template-columns: repeat(8, 1fr);\n"; } 
+	elseif ( $home_postmain_top_columns == '9' ) { echo "grid-template-columns: repeat(9, 1fr);\n"; } 
+	elseif ( $home_postmain_top_columns == '10' ) { echo "grid-template-columns: repeat(10, 1fr);\n"; } 
+	elseif ( $home_postmain_top_columns == '11' ) { echo "grid-template-columns: repeat(11, 1fr);\n"; } 
+	elseif ( $home_postmain_top_columns == '12' ) { echo "grid-template-columns: repeat(12, 1fr);\n"; } 
+	?>
   	gap: 30px;
 	}
 }
@@ -3207,6 +3205,35 @@ p.biography-description {
 	background: <?php echo $postmain_bottom_background_color; ?>;
 	color: <?php echo $postmain_bottom_text_color; ?>;
 	padding: 30px 0px;
+	}
+}
+
+@media screen and (max-width: 1200px) {
+#home-postmain-bottom .inner {
+	width: 100%;
+	padding: 0px 0px;
+	}
+}
+
+@media screen and (min-width: 1200px) {
+#home-postmain-bottom .inner {
+	width: 100%;
+	display: grid;
+	<?php
+	if ( $home_postmain_bottom_columns == '1' ) { echo "grid-template-columns: repeat(1, 1fr);\n"; } 
+	elseif ( $home_postmain_bottom_columns == '2' ) { echo "grid-template-columns: repeat(2, 1fr);\n"; } 
+	elseif ( $home_postmain_bottom_columns == '3' ) { echo "grid-template-columns: repeat(3, 1fr);\n"; } 
+	elseif ( $home_postmain_bottom_columns == '4' ) { echo "grid-template-columns: repeat(4, 1fr);\n"; } 
+	elseif ( $home_postmain_bottom_columns == '5' ) { echo "grid-template-columns: repeat(5, 1fr);\n"; } 
+	elseif ( $home_postmain_bottom_columns == '6' ) { echo "grid-template-columns: repeat(6, 1fr);\n"; } 
+	elseif ( $home_postmain_bottom_columns == '7' ) { echo "grid-template-columns: repeat(7, 1fr);\n"; } 
+	elseif ( $home_postmain_bottom_columns == '8' ) { echo "grid-template-columns: repeat(8, 1fr);\n"; } 
+	elseif ( $home_postmain_bottom_columns == '9' ) { echo "grid-template-columns: repeat(9, 1fr);\n"; } 
+	elseif ( $home_postmain_bottom_columns == '10' ) { echo "grid-template-columns: repeat(10, 1fr);\n"; } 
+	elseif ( $home_postmain_bottom_columns == '11' ) { echo "grid-template-columns: repeat(11, 1fr);\n"; } 
+	elseif ( $home_postmain_bottom_columns == '12' ) { echo "grid-template-columns: repeat(12, 1fr);\n"; } 
+	?>
+  	gap: 30px;
 	}
 }
 
