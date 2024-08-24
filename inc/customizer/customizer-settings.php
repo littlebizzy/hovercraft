d82b9aaafc515dcacc51c9326d526621243163ce
Jesse Nickles <jesse@littlebizzy.com>
2024-08-24 16:05:05 +0700

    Update customizer-settings.php

--- inc/customizer/customizer-settings.php
+++ inc/customizer/customizer-settings.php
@@ -580,6 +580,74 @@ $wp_customize->add_section( 'hovercraft_widget_layouts', array(
     'priority'   => 35,
 ) );
 
+// home premain top widget columns setting
+$wp_customize->add_setting( 'hovercraft_home_premain_top_columns', array(
+    'default'    => '1',
+	'sanitize_callback' => 'hovercraft_sanitize_float',
+	) 
+);
+
+// home premain top widget columns control
+$wp_customize->add_control( new WP_Customize_Control(
+        $wp_customize,
+        'hovercraft_home_premain_top_columns',
+        array(
+            'label'     => __('Home Premain Top Columns (Desktop)', 'hovercraft'),
+			'description' => __( 'How many widgets across should display in the Home Premain Top widget area?', 'hovercraft' ),
+            'section'   => 'hovercraft_widget_layouts',
+            'settings'  => 'hovercraft_home_premain_top_columns',
+            'type'      => 'select',
+			'choices' => array(
+				'1' => '1 Column',
+        		'2' => '2 Columns',
+        		'3' => '3 Columns',
+				'4' => '4 Columns',
+				'5' => '5 Columns',
+        		'6' => '6 Columns',
+				'7' => '7 Columns',
+				'8' => '8 Columns',
+				'9' => '9 Columns',
+				'10' => '10 Columns',
+        		'11' => '11 Columns',
+				'12' => '12 Columns'
+    			)
+        )
+) );
+
+// home premain bottom widget columns setting
+$wp_customize->add_setting( 'hovercraft_home_premain_bottom_columns', array(
+    'default'    => '1',
+	'sanitize_callback' => 'hovercraft_sanitize_float',
+	) 
+);
+
+// home premain bottom widget columns control
+$wp_customize->add_control( new WP_Customize_Control(
+        $wp_customize,
+        'hovercraft_home_premain_bottom_columns',
+        array(
+            'label'     => __('Home Premain Bottom Columns (Desktop)', 'hovercraft'),
+			'description' => __( 'How many widgets across should display in the Home Premain Bottom widget area?', 'hovercraft' ),
+            'section'   => 'hovercraft_widget_layouts',
+            'settings'  => 'hovercraft_home_premain_bottom_columns',
+            'type'      => 'select',
+			'choices' => array(
+				'1' => '1 Column',
+        		'2' => '2 Columns',
+        		'3' => '3 Columns',
+				'4' => '4 Columns',
+				'5' => '5 Columns',
+        		'6' => '6 Columns',
+				'7' => '7 Columns',
+				'8' => '8 Columns',
+				'9' => '9 Columns',
+				'10' => '10 Columns',
+        		'11' => '11 Columns',
+				'12' => '12 Columns'
+    			)
+        )
+) );
+
 // home postmain top widget columns setting
 $wp_customize->add_setting( 'hovercraft_home_postmain_top_columns', array(
     'default'    => '1',
