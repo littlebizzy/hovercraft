e64433edeb298357a336bd8a3bee31af1895c0e9
Jesse Nickles <jesse@littlebizzy.com>
2024-08-26 14:29:11 +0700

    Update title.php

--- template-parts/content/title.php
+++ template-parts/content/title.php
@@ -1,2 +1,7 @@
-<h1 class="<?php $h1_divider_display = get_theme_mod( 'hovercraft_h1_divider_display', 'none' );
-			if ( $h1_divider_display == 'everywhere_possible' || $h1_divider_display == 'everywhere_except_heros' ) { echo "divide"; } ?>"><?php the_title(); ?></h1>
+<?php 
+$hide_title_status = get_post_meta( get_the_ID(), '_mysite_meta_hide_title', true);
+$post_id = get_the_ID();
+if($hide_title_status == "off" || !metadata_exists( 'post', $post_id, '_mysite_meta_hide_title' ) ) : ?>
+	<h1 class="<?php $h1_divider_display = get_theme_mod( 'hovercraft_h1_divider_display', 'none' );
+	if ( $h1_divider_display == 'everywhere_possible' || $h1_divider_display == 'everywhere_except_heros' ) { echo "divide"; } ?>"><?php the_title(); ?></h1>
+<?php endif; ?>
