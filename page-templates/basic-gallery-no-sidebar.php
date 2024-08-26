772b7f76294a29f24fa4cadc70fa850798ad1c2f
Jesse Nickles <jesse@littlebizzy.com>
2024-08-26 14:32:27 +0700

    Update basic-gallery-no-sidebar.php

--- page-templates/basic-gallery-no-sidebar.php
+++ page-templates/basic-gallery-no-sidebar.php
@@ -16,7 +16,7 @@ if ( !is_front_page() || ( is_front_page() && $hovercraft_homepage_hide_main_che
 				
 				<div id="content-padded">
 					<?php get_template_part( 'template-parts/content/breadcrumbs' ); ?>
-					<h1><?php the_title(); ?></h1>
+					<?php get_template_part( 'template-parts/content/title' ); ?>
 					<?php get_template_part( 'template-parts/content/loop' ); ?>
 					<?php get_template_part( 'template-parts/content/gallery' ); ?>
 					<?php get_template_part( 'template-parts/content/pagination' ); ?>
