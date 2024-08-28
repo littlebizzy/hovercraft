360cec313905224a1e9358dae74ccff861036785
Jesse Nickles <jesse@littlebizzy.com>
2024-08-28 16:48:16 +0700

    Update git-updater.php

--- inc/git-updater.php
+++ inc/git-updater.php
@@ -1,61 +1,93 @@
 <?php
+add_action('admin_notices', 'hovercraft_check_git_updater');
+add_action('admin_init', 'hovercraft_handle_git_updater_actions');
 
-function hovercraft_suggest_git_updater_plugin() {
-
-    $git_updater = WP_PLUGIN_DIR . '/git-updater/git-updater.php';
-
-    if ( ! is_multisite() && file_exists( $git_updater ) && ! is_plugin_active( 'git-updater/git-updater.php' )  ) { ?>
-
-        <div class="notice notice-warning">
-        <p><strong>Activate Git Updater plugin to receive updates</strong></p>
-        <p>The HoverCraft theme requires the free Git Updater plugin to receive automatic updates via GitHub.</p>
-		<p><?php
-			$git_updater_path = 'git-updater/git-updater.php';
-			$activate_link = wp_nonce_url(admin_url('plugins.php?action=activate&plugin='.$git_updater_path), 'activate-plugin_'.$git_updater_path);
-			echo '<a href="' . $activate_link . '">Activate Now</a>'; 
-			?></p>
-		</div>
-
-	<?php } elseif ( is_multisite() && file_exists( $git_updater ) && ! is_plugin_active_for_network( 'git-updater/git-updater.php' ) ) { ?>
-
-		<div class="notice notice-warning">
-        <p><strong>Activate Git Updater plugin to receive updates</strong></p>
-        <p>The HoverCraft theme requires the free Git Updater plugin to receive automatic updates via GitHub.</p>
-		<p><?php
-			$git_updater_path = 'git-updater/git-updater.php';
-			$activate_link = wp_nonce_url(admin_url('/network/plugins.php?action=activate&plugin='.$git_updater_path), 'activate-plugin_'.$git_updater_path);
-			echo '<a href="' . $activate_link . '">Activate Now</a>'; 
-			?></p>
-		</div>
-
-    <?php } elseif ( ! is_multisite() && ! file_exists( $git_updater ) ) { ?>
-
-        <div class="notice notice-warning">
-        <p><strong>Install Git Updater plugin to receive updates</strong></p>
-        <p>The HoverCraft theme requires the free Git Updater plugin to receive automatic updates via GitHub.</p>
-		<p><?php
-			// $installurl = wp_nonce_url( self_admin_url( 'update.php?action=install-plugin&plugin=git-updater' ), 'install-plugin_git-updater' );
-			echo '<a href="https://git-updater.com/wp-content/uploads/2024/03/git-updater-12.4.0.zip">Download Now</a>'; 
-			?></p>
-		</div>
-
-	<?php } elseif ( is_multisite() && ! file_exists( $git_updater ) ) { ?>
-
-		<div class="notice notice-warning">
-        <p><strong>Install Git Updater plugin to receive updates</strong></p>
-        <p>The HoverCraft theme requires the free Git Updater plugin to receive automatic updates via GitHub.</p>
-		<p><?php
-			// $installurl = wp_nonce_url( self_admin_url( 'update.php?action=install-plugin&plugin=git-updater' ), 'install-plugin_git-updater' );
-			echo '<a href="https://git-updater.com/wp-content/uploads/2024/03/git-updater-12.4.0.zip">Download Now</a>'; 
-			?></p>
-		</div>
-
-	<?php }
-
+function hovercraft_check_git_updater() {
+    if (!file_exists(WP_PLUGIN_DIR . '/git-updater/git-updater.php')) {
+        // Plugin is not installed
+        echo '<div class="notice notice-warning"><p>';
+        echo 'Git Updater plugin is not installed. <form method="post" style="display:inline;">
+                <input type="hidden" name="hovercraft_action" value="install_git_updater">
+                <input type="submit" class="button button-primary" value="Click here to install it automatically">
+              </form>';
+        echo '</p></div>';
+    } elseif (is_plugin_inactive('git-updater/git-updater.php')) {
+        // Plugin is installed but not activated
+        echo '<div class="notice notice-info"><p>';
+        echo 'Git Updater plugin is installed but not activated. <form method="post" style="display:inline;">
+                <input type="hidden" name="hovercraft_action" value="activate_git_updater">
+                <input type="submit" class="button button-primary" value="Click here to activate it">
+              </form>';
+        echo '</p></div>';
+    }
 }
 
-add_action('admin_notices', 'hovercraft_suggest_git_updater_plugin');
+function hovercraft_handle_git_updater_actions() {
+    if (!isset($_POST['hovercraft_action'])) {
+        return;
+    }
 
+    if ($_POST['hovercraft_action'] === 'install_git_updater') {
+        if (!current_user_can('install_plugins')) {
+            wp_die('You do not have permission to install plugins.');
+        }
+
+        include_once ABSPATH . 'wp-admin/includes/file.php';
+        include_once ABSPATH . 'wp-admin/includes/plugin.php';
+        include_once ABSPATH . 'wp-admin/includes/class-wp-upgrader.php';
+
+        // Ensure the filesystem is accessible
+        if (!WP_Filesystem()) {
+            wp_die('Filesystem error: could not initialize WP_Filesystem.');
+        }
+
+        $plugin_url = 'https://github.com/afragen/git-updater/archive/master.zip';
+        $tmp_file = download_url($plugin_url);
+
+        if (is_wp_error($tmp_file)) {
+            wp_die('Failed to download the plugin: ' . $tmp_file->get_error_message());
+        }
+
+        // Unzip the plugin into the plugins directory
+        $result = unzip_file($tmp_file, WP_PLUGIN_DIR);
+
+        if (is_wp_error($result)) {
+            @unlink($tmp_file);
+            wp_die('Failed to install the plugin: ' . $result->get_error_message());
+        }
+
+        @unlink($tmp_file);
+
+        // Find and rename the folder if it starts with 'git-updater-'
+        $folders = glob(WP_PLUGIN_DIR . '/git-updater-*');
+        foreach ($folders as $folder) {
+            if (is_dir($folder)) {
+                rename($folder, WP_PLUGIN_DIR . '/git-updater');
+                break;
+            }
+        }
+
+        wp_redirect(admin_url('plugins.php')); // Redirect to Plugins page
+        exit;
+    }
+
+    if ($_POST['hovercraft_action'] === 'activate_git_updater') {
+        if (!current_user_can('activate_plugins')) {
+            wp_die('You do not have permission to activate plugins.');
+        }
+
+        $result = activate_plugin('git-updater/git-updater.php');
+
+        if (is_wp_error($result)) {
+            wp_die('Failed to activate the plugin: ' . $result->get_error_message());
+        }
+
+        wp_redirect(admin_url('plugins.php')); // Redirect to Plugins page
+        exit;
+    }
+}
+
+// ChatGPT
 // https://wordpress.stackexchange.com/questions/39408/how-to-make-plugin-required-in-a-wp-theme-without-using-php-conditional-statemen
 // https://gist.github.com/mlbd/b96f62e685ca3d58c17031fcc9e73c07
 // https://www.aurodigo.com/how-to-solve-fatal-error-call-to-undefined-function-is-plugin-active-problem-on-wordpress
