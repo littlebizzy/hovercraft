089d93750e5021b937e37545a82ead253c6095cd
Jesse Nickles <jesse@littlebizzy.com>
2024-09-02 16:10:06 +0700

    Update zigzag-wide.php

--- template-parts/content/zigzag-wide.php
+++ template-parts/content/zigzag-wide.php
@@ -1,87 +1,73 @@
 <div class="zigzag-wide-wrapper">
 
-  <?php if ( is_active_sidebar( 'hovercraft_zigzag_one' ) ) { ?>
-	<div class="zigzag-section">
-		<?php dynamic_sidebar( 'hovercraft_zigzag_one' ); ?>
-		<div class="clear"></div>
-	</div><!-- zigzag-section -->
-	<?php } ?>
+    <?php if ( is_active_sidebar( 'hovercraft_zigzag_one' ) ) { ?>
+        <div class="zigzag-section">
+            <?php dynamic_sidebar( 'hovercraft_zigzag_one' ); ?>
+            <div class="clear"></div>
+        </div><!-- zigzag-section -->
+    <?php } ?>
 
     <?php if ( is_active_sidebar( 'hovercraft_zigzag_two' ) ) { ?>
-	<div class="zigzag-section-reverse">
-		<?php dynamic_sidebar( 'hovercraft_zigzag_two' ); ?>
-		<div class="clear"></div>
-	</div><!-- zigzag-section -->
-	<?php } ?>
+        <div class="zigzag-section-reverse">
+            <?php dynamic_sidebar( 'hovercraft_zigzag_two' ); ?>
+            <div class="clear"></div>
+        </div><!-- zigzag-section-reverse -->
+    <?php } ?>
 
     <?php if ( is_active_sidebar( 'hovercraft_zigzag_three' ) ) { ?>
-	<div class="zigzag-section">
-		<?php dynamic_sidebar( 'hovercraft_zigzag_three' ); ?>
-		<div class="clear"></div>
-	</div><!-- zigzag-section -->
-	<?php } ?>
+        <div class="zigzag-section">
+            <?php dynamic_sidebar( 'hovercraft_zigzag_three' ); ?>
+            <div class="clear"></div>
+        </div><!-- zigzag-section -->
+    <?php } ?>
 
     <?php if ( is_active_sidebar( 'hovercraft_zigzag_four' ) ) { ?>
-	<div class="zigzag-section-reverse">
-		<?php dynamic_sidebar( 'hovercraft_zigzag_four' ); ?>
-		<div class="clear"></div>
-	</div><!-- zigzag-section -->
-	<?php } ?>
+        <div class="zigzag-section-reverse">
+            <?php dynamic_sidebar( 'hovercraft_zigzag_four' ); ?>
+            <div class="clear"></div>
+        </div><!-- zigzag-section-reverse -->
+    <?php } ?>
 
     <?php if ( is_active_sidebar( 'hovercraft_zigzag_five' ) ) { ?>
-	<div class="zigzag-section">
-		<?php dynamic_sidebar( 'hovercraft_zigzag_five' ); ?>
-		<div class="clear"></div>
-	</div><!-- zigzag-section -->
-	<?php } ?>
+        <div class="zigzag-section">
+            <?php dynamic_sidebar( 'hovercraft_zigzag_five' ); ?>
+            <div class="clear"></div>
+        </div><!-- zigzag-section -->
+    <?php } ?>
 
     <?php if ( is_active_sidebar( 'hovercraft_zigzag_six' ) ) { ?>
-	<div class="zigzag-section-reverse">
-		<?php dynamic_sidebar( 'hovercraft_zigzag_six' ); ?>
-		<div class="clear"></div>
-	</div><!-- zigzag-section -->
-	<?php } ?>
+        <div class="zigzag-section-reverse">
+            <?php dynamic_sidebar( 'hovercraft_zigzag_six' ); ?>
+            <div class="clear"></div>
+        </div><!-- zigzag-section-reverse -->
+    <?php } ?>
 
-	<?php if ( is_active_sidebar( 'hovercraft_zigzag_seven' ) ) { ?>
-	<div class="zigzag-section">
-		<?php dynamic_sidebar( 'hovercraft_zigzag_seven' ); ?>
-		<div class="clear"></div>
-	</div><!-- zigzag-section -->
-	<?php } ?>
+    <?php if ( is_active_sidebar( 'hovercraft_zigzag_seven' ) ) { ?>
+        <div class="zigzag-section">
+            <?php dynamic_sidebar( 'hovercraft_zigzag_seven' ); ?>
+            <div class="clear"></div>
+        </div><!-- zigzag-section -->
+    <?php } ?>
 
     <?php if ( is_active_sidebar( 'hovercraft_zigzag_eight' ) ) { ?>
-	<div class="zigzag-section-reverse">
-		<?php dynamic_sidebar( 'hovercraft_zigzag_eight' ); ?>
-		<div class="clear"></div>
-	</div><!-- zigzag-section -->
-	<?php } ?>
+        <div class="zigzag-section-reverse">
+            <?php dynamic_sidebar( 'hovercraft_zigzag_eight' ); ?>
+            <div class="clear"></div>
+        </div><!-- zigzag-section-reverse -->
+    <?php } ?>
 
-	<?php if ( is_active_sidebar( 'hovercraft_zigzag_nine' ) ) { ?>
-	<div class="zigzag-section">
-		<?php dynamic_sidebar( 'hovercraft_zigzag_nine' ); ?>
-		<div class="clear"></div>
-	</div><!-- zigzag-section -->
-	<?php } ?>
+    <?php if ( is_active_sidebar( 'hovercraft_zigzag_nine' ) ) { ?>
+        <div class="zigzag-section">
+            <?php dynamic_sidebar( 'hovercraft_zigzag_nine' ); ?>
+            <div class="clear"></div>
+        </div><!-- zigzag-section -->
+    <?php } ?>
 
     <?php if ( is_active_sidebar( 'hovercraft_zigzag_ten' ) ) { ?>
-	<div class="zigzag-section-reverse">
-		<?php dynamic_sidebar( 'hovercraft_zigzag_ten' ); ?>
-		<div class="clear"></div>
-	</div><!-- zigzag-section -->
-	<?php } ?>
-
-	<?php if ( is_active_sidebar( 'hovercraft_zigzag_eleven' ) ) { ?>
-	<div class="zigzag-section">
-		<?php dynamic_sidebar( 'hovercraft_zigzag_eleven' ); ?>
-		<div class="clear"></div>
-	</div><!-- zigzag-section -->
-	<?php } ?>
-
-    <?php if ( is_active_sidebar( 'hovercraft_zigzag_twelve' ) ) { ?>
-	<div class="zigzag-section-reverse">
-		<?php dynamic_sidebar( 'hovercraft_zigzag_twelve' ); ?>
-		<div class="clear"></div>
-	</div><!-- zigzag-section -->
-	<?php } ?>
+        <div class="zigzag-section-reverse">
+            <?php dynamic_sidebar( 'hovercraft_zigzag_ten' ); ?>
+            <div class="clear"></div>
+        </div><!-- zigzag-section-reverse -->
+    <?php } ?>
 
 </div><!-- zigzag-wide-wrapper -->
