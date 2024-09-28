f4c16b1902a4954b970b4b55ec350a63d676a7dc
Jesse Nickles <jesse@littlebizzy.com>
2024-09-28 15:08:56 +0700

    Update bbpress-disable-reply-links.php

--- inc/bbpress-disable-reply-links.php
+++ inc/bbpress-disable-reply-links.php
@@ -11,10 +11,17 @@ add_filter( 'bbp_topic_admin_links', 'hovercraft_remove_all_reply_links' );
 add_filter( 'bbp_reply_admin_links', 'hovercraft_remove_all_reply_links' );
 
 // Disable replying to individual replies, allow only replying to the main topic
-function hovercraft_disable_reply_to_individual_replies( $can_publish, $reply_id, $topic_id ) {
+function hovercraft_disable_reply_to_individual_replies( $can_publish, $reply_id = 0, $topic_id = 0 ) {
+    // Ensure that reply and topic IDs are valid
+    if ( ! $reply_id || ! $topic_id ) {
+        return $can_publish; // Return original capability if IDs are not set
+    }
+
+    // Disable replies to individual replies
     if ( bbp_is_reply( $reply_id ) ) {
         return false; // Disable replies to individual replies
     }
+
     return $can_publish; // Allow replies to the main topic
 }
 add_filter( 'bbp_current_user_can_publish_replies', 'hovercraft_disable_reply_to_individual_replies', 10, 3 );
