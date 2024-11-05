9b78ed3812578210ef60965301507e7ca8afe394
Jesse Nickles <jesse@littlebizzy.com>
2024-11-05 19:53:59 +0700

    Update register-blog-category.php

--- inc/customizer/register-blog-category.php
+++ inc/customizer/register-blog-category.php
@@ -1,43 +1,41 @@
 <?php
 
 function hovercraft_blog_category_register( $wp_customize ) {
- 
-    //Get an array with the category list
-	$rjs_categories_full_list = get_categories(array( 'orderby' => 'name', ));
- 
-    //Create an empty array
-	$rjs_choices_list = [ 'none' => 'None (Disabled)', ];
- 
-    //Loop through the array and add the correct values every time
-	foreach( $rjs_categories_full_list as $rjs_single_cat ) {
-    	$rjs_choices_list[$rjs_single_cat->slug] = __( $rjs_single_cat->name );
-	}
-	
-	// blog category setting
-	$wp_customize->add_setting( 'hovercraft_blog_category', array(
-    'default'    => 'none',
-	'sanitize_callback' => 'hovercraft_sanitize_select',
-	) 
-	);
-	
-	// blog category control
-		$wp_customize->add_control( new WP_Customize_Control(
+
+    // get an array of categories
+    $categories_list = get_categories( array( 'orderby' => 'name' ) );
+
+    // initialize choices list with default value
+    $choices_list = [ 'none' => 'None (Disabled)' ];
+
+    // populate choices list with categories
+    foreach ( $categories_list as $category ) {
+        $choices_list[ $category->slug ] = __( $category->name );
+    }
+
+    // blog category setting
+    $wp_customize->add_setting( 'hovercraft_blog_category', array(
+        'default'           => 'none',
+        'sanitize_callback' => 'hovercraft_sanitize_select',
+    ) );
+
+    // blog category control
+    $wp_customize->add_control( new WP_Customize_Control(
         $wp_customize,
         'hovercraft_blog_category',
         array(
-            'label'     => __( 'Feed Category', 'hovercraft' ),
-			'description' => __( 'Which post category should be displayed on the default feed and Posts page?', 'hovercraft' ),
-            'section'   => 'hovercraft_category_layouts',
-            'settings'  => 'hovercraft_blog_category',
-            'type'      => 'select',
-			'choices' =>  $rjs_choices_list,
+            'label'       => __( 'Feed Category', 'hovercraft' ),
+            'description' => __( 'Which post category should be displayed on the default feed and Posts page?', 'hovercraft' ),
+            'section'     => 'hovercraft_category_layouts',
+            'settings'    => 'hovercraft_blog_category',
+            'type'        => 'select',
+            'choices'     => $choices_list,
         )
-) );
- 
+    ) );
 }
- 
+
 add_action( 'customize_register', 'hovercraft_blog_category_register' );
 
-// https://ralphjsmit.com/create-a-category-dropdown-in-wordpress-customizer
-// $category = get_theme_mod( 'rjs_category_dropdown', 'Uncategorized');
-// https://stackoverflow.com/questions/43715567/how-to-add-extra-values-to-a-existing-array
+// Ref: ChatGPT
+// Ref: https://ralphjsmit.com/create-a-category-dropdown-in-wordpress-customizer
+// Ref: https://stackoverflow.com/questions/43715567/how-to-add-extra-values-to-a-existing-array
