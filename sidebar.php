60a188e973859f9ceb2519a0e0f84a74022f666f
Jesse Nickles <jesse@littlebizzy.com>
2025-02-08 03:15:16 +0700

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,4 @@
 <div id="sidebar">
-	<?php dynamic_sidebar( 'hovercraft_callout' ); ?>
-	<?php dynamic_sidebar( 'hovercraft_sidebar' ); ?>
+    <?php dynamic_sidebar( 'hovercraft_callout' ); ?>
+    <?php dynamic_sidebar( 'hovercraft_sidebar' ); ?>
 </div><!-- sidebar -->
