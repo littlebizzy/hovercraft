2e134b4516520d4450e3491505c34fef82f14212
Jesse Nickles <jesse@littlebizzy.com>
2025-02-09 01:34:22 +0700

    Update comments.php

--- comments.php
+++ comments.php
@@ -1,27 +1,29 @@
 <?php
 
 // skip if post is password-protected
-if ( post_password_required() ) return; ?>
+if ( post_password_required() ) return;
+?>
 
 <div id="comments" class="comments-area">
     <?php 
     // check if there are comments before displaying them
     if ( get_comments_number() > 0 && have_comments() ) : ?>
         <h3 class="comments-title">
-            <?php printf( _nx( 'One thought on "%2$s"', '%1$s thoughts on "%2$s"', get_comments_number(), 'comments title', 'hovercraft' ),
+            <?php 
+            // display comment count with post title
+            printf( esc_html( _nx( 'One thought on "%2$s"', '%1$s thoughts on "%2$s"', get_comments_number(), 'comments title', 'hovercraft' ) ), 
                 number_format_i18n( get_comments_number() ), '<span>' . esc_html( get_the_title() ) . '</span>' ); ?>
         </h3>
 
         <ol class="comment-list">
             <?php 
-            // display comments 
-            wp_list_comments( array( 'style' => 'ol', 'short_ping' => true, 'avatar_size' => 74 ) ); 
-            ?>
+            // display comments
+            wp_list_comments( array( 'style' => 'ol', 'short_ping' => true, 'avatar_size' => 74 ) ); ?>
         </ol>
 
         <?php 
         // check for multi-page comments
-        if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : ?>
+        if ( get_comment_pages_count() > 1 ) : ?>
             <nav class="navigation comment-navigation">
                 <p class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'hovercraft' ); ?></p>
                 <div class="nav-previous"><?php previous_comments_link( esc_html__( '&larr; Older Comments', 'hovercraft' ) ); ?></div>
@@ -37,37 +39,38 @@ if ( post_password_required() ) return; ?>
     <?php endif; ?>
 
     <?php 
-    // display comment form 
-    comment_form(); 
-    ?>
+    // display comment form
+    comment_form(); ?>
 </div>
 
 <?php
 
 // prevent duplicate filter registration
-if ( ! has_filter( 'comment_form_default_fields' ) ) {
-    add_filter( 'comment_form_default_fields', function( $fields ) {
-        $commenter = wp_get_current_commenter();
-        $req = get_option( 'require_name_email' );
-        $aria_req = $req ? " aria-required='true'" : '';
-        $required = $req ? '<span class="required">*</span>' : '';
+remove_filter( 'comment_form_default_fields', 'custom_comment_fields' );
+add_filter( 'comment_form_default_fields', 'custom_comment_fields', 10, 1 );
 
-        // set author field
-        $fields['author'] = sprintf( '<p class="comment-form-author"><label for="comment-author">%s</label> %s<input id="comment-author" name="author" type="text" value="%s" size="30"%s /></p>',
-            esc_html__( 'Name', 'hovercraft' ), $required, esc_attr( $commenter['comment_author'] ), $aria_req );
+function custom_comment_fields( $fields ) {
+    $commenter = wp_get_current_commenter();
+    $req = get_option( 'require_name_email' );
+    $aria_req = $req ? " aria-required='true'" : '';
+    $required = $req ? '<span class="required">*</span>' : '';
 
-        // set email field
-        $fields['email'] = sprintf( '<p class="comment-form-email"><label for="comment-email">%s</label> %s<input id="comment-email" name="email" type="email" value="%s" size="30"%s /></p>',
-            esc_html__( 'Email', 'hovercraft' ), $required, esc_attr( $commenter['comment_author_email'] ), $aria_req );
+    // set author field
+    $fields['author'] = sprintf( '<p class="comment-form-author"><label for="comment-author">%s</label> %s<input id="comment-author" name="author" type="text" value="%s" size="30"%s /></p>',
+        esc_html__( 'Name', 'hovercraft' ), $required, esc_attr( $commenter['comment_author'] ), $aria_req );
 
-        // set url field
-        $fields['url'] = sprintf( '<p class="comment-form-url"><label for="comment-url">%s</label><input id="comment-url" name="url" type="url" value="%s" size="30" /></p>',
-            esc_html__( 'Website', 'hovercraft' ), esc_attr( $commenter['comment_author_url'] ) );
+    // set email field
+    $fields['email'] = sprintf( '<p class="comment-form-email"><label for="comment-email">%s</label> %s<input id="comment-email" name="email" type="email" value="%s" size="30"%s /></p>',
+        esc_html__( 'Email', 'hovercraft' ), $required, esc_attr( $commenter['comment_author_email'] ), $aria_req );
 
-        return $fields;
-    }, 10, 1 );
+    // set url field
+    $fields['url'] = sprintf( '<p class="comment-form-url"><label for="comment-url">%s</label><input id="comment-url" name="url" type="url" value="%s" size="30" /></p>',
+        esc_html__( 'Website', 'hovercraft' ), esc_attr( $commenter['comment_author_url'] ) );
+
+    return $fields;
 }
 
+// Ref: ChatGPT
 // Ref: https://developer.wordpress.org/themes/template-files-section/partial-and-miscellaneous-template-files/comment-template/
 // Ref: https://stackoverflow.com/questions/20751219/undefined-variable-on-comments-wordpress
 // Ref: https://stackoverflow.com/questions/55567423/error-please-fill-the-required-fields-name-email-in-wordpress-comment
