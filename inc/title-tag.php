dad3678350f9981007d7652a1a7c6a779c939d50
Jesse Nickles <jesse@littlebizzy.com>
2025-02-21 14:09:15 +0700

    Update title-tag.php

--- inc/title-tag.php
+++ inc/title-tag.php
@@ -6,5 +6,12 @@ function hovercraft_title_tag() {
 
 add_action( 'after_setup_theme', 'hovercraft_title_tag' );
 
-// https://wordpress.stackexchange.com/questions/176940/add-theme-support-title-tag-in-conflict-with-custom-titles-function
-// https://www.usablewp.com/learn-wordpress/home-page/how-to-add-theme-support-for-the-title-tag/
+// remove bbpress title modification if active
+if ( class_exists( 'bbPress' ) ) {
+    remove_filter( 'wp_title', 'bbp_title', 10, 3 );
+    remove_filter( 'document_title_parts', 'bbp_filter_document_title', 10, 1 );
+}
+
+// Ref: ChatGPT
+// Ref: https://wordpress.stackexchange.com/questions/176940/add-theme-support-title-tag-in-conflict-with-custom-titles-function
+// Ref: https://www.usablewp.com/learn-wordpress/home-page/how-to-add-theme-support-for-the-title-tag/
