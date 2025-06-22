fd3038b08f4fb326f1148dcbaf042400c59c8a10
Jesse Nickles <jesse@littlebizzy.com>
2025-06-22 21:29:22 +0700

    Update featured-image.php

--- template-parts/content/featured-image.php
+++ template-parts/content/featured-image.php
@@ -1,25 +1,50 @@
-<?php global $post; 
-$image_id = get_post_thumbnail_id();
-$image_alt = get_post_meta($image_id, '_wp_attachment_image_alt', true);
-$image_title = get_the_title($image_id);
-$url_featured_image = wp_get_attachment_image_src( get_post_thumbnail_id( $currentID ), 'medium_large' );
-$image_width = $url_featured_image[1];
-$image_height = $url_featured_image[2];
-if ( !empty( $url_featured_image ) && ( !is_singular('product') ) ) { ?>
+<?php
+// get current post id safely outside the loop
+$post_id = get_queried_object_id();
+
+// skip featured image for products or if none exists
+if ( is_singular( 'product' ) || ! has_post_thumbnail( $post_id ) ) {
+    return;
+}
+
+// get featured image id
+$image_id = get_post_thumbnail_id( $post_id );
+if ( ! $image_id ) {
+    return;
+}
+
+// get alt text and image source
+$image_alt = get_post_meta( $image_id, '_wp_attachment_image_alt', true );
+$image_src = wp_get_attachment_image_src( $image_id, 'medium_large' );
+
+// force alt to blank if not a string
+if ( ! is_string( $image_alt ) ) {
+    $image_alt = '';
+}
+
+// exit if no valid image source
+if ( ! is_array( $image_src ) ) {
+    return;
+}
+?>
+
 <div class="featured-image-wrapper">
-<img width="<?php echo $image_width; ?>" height="<?php echo $image_height; ?>" class="featured-image" src="<?php echo $url_featured_image[0]; ?>" alt="<?php echo $image_alt; ?>" />
-<div class="featured-image-caption"><?php echo the_post_thumbnail_caption(); ?></div>
-<div class="clear"></div>
+    <img class="featured-image" src="<?php echo esc_url( $image_src[0] ); ?>" width="<?php echo (int) $image_src[1]; ?>" height="<?php echo (int) $image_src[2]; ?>" alt="<?php echo esc_attr( $image_alt ); ?>">
+    <?php if ( get_the_post_thumbnail_caption( $post_id ) ) : ?>
+        <div class="featured-image-caption"><?php echo esc_html( get_the_post_thumbnail_caption( $post_id ) ); ?></div>
+    <?php endif; ?>
+    <div class="clear"></div>
 </div><!-- featured-image-wrapper -->
 <div class="clear"></div>
-<?php }
 
-// https://rudrastyh.com/wordpress/image-sizes.html
-// https://wordpress.stackexchange.com/questions/18337/how-to-get-featured-images-width-and-use-elsewhere-in-template
-// https://wordpress.stackexchange.com/questions/27385/whats-the-best-way-to-use-the-featured-image-for-responsive-web-design
-// https://www.smashingmagazine.com/2012/06/responsive-images-with-wordress-featured-images/
-// https://wordpress.stackexchange.com/questions/111937/how-to-show-featured-image-caption-only-if-it-exist
-// https://developer.wordpress.org/reference/functions/wp_get_attachment_image_src/
-// https://wordpress.stackexchange.com/questions/312283/getting-alt-text-of-featured-image
-// https://wordpress.stackexchange.com/questions/193196/how-to-get-image-title-alt-attribute
-// FIX NEEDED: Undefined variable $currentID in /var/www/html/wp-content/themes/hovercraft/template-parts/content/featured-image.php on line 5
+<?php
+// Ref: ChatGPT
+// Ref: https://rudrastyh.com/wordpress/image-sizes.html
+// Ref: https://wordpress.stackexchange.com/questions/18337/how-to-get-featured-images-width-and-use-elsewhere-in-template
+// Ref: https://wordpress.stackexchange.com/questions/27385/whats-the-best-way-to-use-the-featured-image-for-responsive-web-design
+// Ref: https://www.smashingmagazine.com/2012/06/responsive-images-with-wordress-featured-images/
+// Ref: https://wordpress.stackexchange.com/questions/111937/how-to-show-featured-image-caption-only-if-it-exist
+// Ref: https://developer.wordpress.org/reference/functions/wp_get_attachment_image_src/
+// Ref: https://wordpress.stackexchange.com/questions/312283/getting-alt-text-of-featured-image
+// Ref: https://wordpress.stackexchange.com/questions/193196/how-to-get-image-title-alt-attribute
+?>
