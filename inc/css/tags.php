ae3b116bb4de3d71708486804f32f3de53079780
Jesse Nickles <jesse@littlebizzy.com>
2025-06-22 20:18:37 +0700

    Create tags.php

---
+++ inc/css/tags.php
@@ -0,0 +1,19 @@
+<?php
+// styles for tags
+?>
+
+@media screen and (max-width: 1200px) {
+.tags {
+	margin-top: 20px;
+	font-size: 14px;
+	color: <?php echo $breadcrumbs_text_color; ?>;
+	}
+}
+
+@media screen and (min-width: 1200px) {
+.tags {
+	margin-top: 30px;
+	font-size: 14px;
+	color: <?php echo $breadcrumbs_text_color; ?>;
+	}
+}
