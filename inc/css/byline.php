a10973cae95d8a691377e857d0c4584f79844d1d
Jesse Nickles <jesse@littlebizzy.com>
2025-06-23 01:31:16 +0700

    Create byline.php

---
+++ inc/css/byline.php
@@ -0,0 +1,45 @@
+<?php
+// styles for byline
+?>
+
+.post-byline {
+	margin-bottom: 30px;
+	line-height: 16px;
+}
+
+.post-byline-archive {
+	margin-bottom: 20px;
+	line-height: 1;
+}
+
+.byline-photo {
+	display: inline-block;
+	margin-right: 5px;
+	vertical-align: middle;
+	clip-path: circle();
+	border-radius: 50%;
+	width: 25px;
+	height: 25px;
+}
+
+.post-author {
+	display: inline;
+}
+
+.post-updated {
+	display: inline;
+}
+
+.date-published {
+	display: inline;
+}
+
+.author-name {
+	font-weight: bold;
+	display: inline;
+}
+
+.byline-separator {
+	margin: 0 6px;
+	color: #888;
+}
