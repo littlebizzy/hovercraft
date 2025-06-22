8e1cb204f01274972876defa45759818966e9b4b
Jesse Nickles <jesse@littlebizzy.com>
2025-06-22 20:42:12 +0700

    Create branding.php

---
+++ inc/css/branding.php
@@ -0,0 +1,30 @@
+<?php
+// styles for branding
+?>
+
+#branding {
+	display: table;
+}
+
+#branding a {
+	text-decoration: none !important;
+}
+
+.branding-media {
+	display: table-cell;
+    vertical-align: middle;
+	height: inherit;
+	min-width: auto;
+	white-space: nowrap;
+}
+
+
+/* branding text */
+
+.branding-text {
+	display: table-cell;
+    vertical-align: middle;
+	height: inherit;
+	text-align: left;
+	width: 100%;
+}
