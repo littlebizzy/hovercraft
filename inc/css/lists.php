2a13a2b2b2e81ae2238e3472567cb254da7f551b
Jesse Nickles <jesse@littlebizzy.com>
2025-06-22 18:04:36 +0700

    Create lists.php

---
+++ inc/css/lists.php
@@ -0,0 +1,20 @@
+<?php
+// styles for unordered and ordered lists
+?>
+
+ul {
+	list-style-position: inside;
+}
+
+ol {
+	list-style-position: inside;
+}
+
+li {
+	margin-bottom: 5px;
+	line-height: 2;
+}
+
+ol > li::marker {
+  font-weight: 700;
+}
