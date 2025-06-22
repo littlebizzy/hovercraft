53167f69ec10a14a9587aea548ee27cef1bcd045
Jesse Nickles <jesse@littlebizzy.com>
2025-06-22 20:21:14 +0700

    Create tables.php

---
+++ inc/css/tables.php
@@ -0,0 +1,29 @@
+<?php
+// styles for tables
+?>
+
+@media screen and (max-width: 1200px) {
+table {
+	margin-bottom: 20px;
+	border: 1px solid grey;
+	border-collapse: collapse;
+	}
+}
+
+@media screen and (min-width: 1200px) {
+table {
+	margin-bottom: 30px;
+	border: 1px solid grey;
+	border-collapse: collapse;
+	}
+}
+	
+table tr:first-child {
+  background: #eee !important;
+}
+
+table th,
+table td {
+	padding: 5px;
+  border: 1px solid lightgrey;
+}
