0dc64f87d42ec258332fe7cd8a2e1bdb50eb3b9c
Jesse Nickles <jesse@littlebizzy.com>
2025-06-24 15:56:00 +0700

    Create woocommerce-add-to-cart.php

---
+++ inc/css/woocommerce-add-to-cart.php
@@ -0,0 +1,47 @@
+<?php
+// styles for woocommerce add to cart
+?>
+
+form.variations_form {
+	display: block !important;
+}
+
+form.cart {
+	display: flex;
+}
+	
+div.single_variation_wrap {
+	clear: both;
+	display: flex;
+}
+
+table.variations {
+	display: block;
+	width: 100%;
+	margin-bottom: 30px !important;
+	border: 0;
+}
+
+form.cart input {
+	vertical-align: middle;
+	font-size: 16px;
+	font-weight: 400;
+	line-height: 2;
+	text-align: center;
+	margin-right: 5px;
+	padding: 5px 0px !important;
+	border: 0;
+	outline: none;
+	box-shadow: inset 0px 0px 0px 1px #BDBDBD;
+}
+
+form.cart button {
+	vertical-align: middle;
+	font-size: 16px !important;
+	font-weight: 700 !important;
+	padding: 0 20px;
+	border: 0;
+	outline: none;
+	cursor: pointer;
+	border-radius: 0 !important;
+}
