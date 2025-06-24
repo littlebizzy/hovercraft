3619fa5400121c3c5d1d7f375982b52433d758b2
Jesse Nickles <jesse@littlebizzy.com>
2025-06-24 14:28:30 +0700

    Update css-generator.php

--- inc/css-generator.php
+++ inc/css-generator.php
@@ -1729,107 +1729,12 @@ button {
 
 
 /* topbar */
-
-@media screen and (max-width: 1200px) {
-#topbar {
-	width: 100%;
-	padding: 10px 20px;
-	font-size: <?php echo $topbar_mobile_font_size; ?>px;
-	font-weight: <?php echo $topbar_font_weight; ?>;
-	text-transform: <?php echo $topbar_text_transform; ?>;
-	line-height: 1.5;
-	background: <?php echo $topbar_background_color; ?>;
-	color: <?php echo $topbar_text_color; ?>;
-	}
+<?php
+$hovercraft_topbar_css = get_template_directory() . '/inc/css/topbar.php';
+if ( file_exists( $hovercraft_topbar_css ) ) {
+    include $hovercraft_topbar_css;
 }
-
-@media screen and (min-width: 1200px) {
-#topbar {
-	width: 100%;
-	padding: 10px 0px;
-	font-size: <?php echo $topbar_desktop_font_size; ?>px;
-	font-weight: <?php echo $topbar_font_weight; ?>;
-	text-transform: <?php echo $topbar_text_transform; ?>;
-	line-height: 1.5;
-	display: table;
-	background: <?php echo $topbar_background_color; ?>;
-	color: <?php echo $topbar_text_color; ?>;
-	}
-}
-
-#topbar a {
-	color: <?php echo $topbar_link_color; ?>;
-	text-decoration: <?php echo $topbar_link_decoration; ?>;
-}
-
-#topbar p {
-	margin-bottom: 0;
-}
-	
-@media screen and (max-width: 1200px) {
-.topbar-left {
-    vertical-align: middle;
-	text-align: center;
-	<?php if ( $mobile_topbar_widget == 'topbar_right') { echo "display: none;"; } ?>
-	}
-}
-
-@media screen and (min-width: 1200px) {
-.topbar-left {
-	display: table-cell;
-    vertical-align: middle;
-	height: inherit;
-	white-space: nowrap;
-	width: auto;
-	text-align: left;
-	}
-}
-
-@media screen and (max-width: 1200px) {
-.topbar-right {
-	vertical-align: middle;
-	text-align: center;
-	<?php if ( $mobile_topbar_widget == 'topbar_left') { echo "display: none;"; } ?>
-	}
-}
-
-@media screen and (min-width: 1200px) {
-.topbar-right {
-	display: table-cell;
-    vertical-align: middle;
-	height: inherit;
-	text-align: right;
-	width: 100%;
-	}
-}
-	
-@media screen and (max-width: 1200px) {
-.topbar-center {
-	text-align: center;
-	}
-}
-
-@media screen and (min-width: 1200px) {
-.topbar-center {
-    vertical-align: middle;
-	height: inherit;
-	text-align: center;
-	}
-}
-
-
-/* topbar ul */
-
-#topbar ul {
-	margin: 0;
-	list-style-type: none;
-}
-
-#topbar ul li {
-	list-style-type: none;
-	display: inline-block;
-	margin: 0 0 0 10px;
-}
+?>
 
 
 /* preheader */
