83672460732bad3ad0ca117cf0fc22ecf4d3106f
Jesse Nickles <jesse@littlebizzy.com>
2025-06-24 15:36:37 +0700

    Create woocommerce-product-details.php

---
+++ inc/css/woocommerce-product-details.php
@@ -0,0 +1,47 @@
+<?php
+// styles for woocommerce product details
+?>
+
+.price {
+	font-size: 24px !important;
+	font-weight: 600 !important;
+	color: <?php echo $woocommerce_price_text_color; ?> !important;
+}
+
+p.stock {
+	font-size: 12px !important;
+	font-weight: 400 !important;
+	color: <?php echo $woocommerce_price_text_color; ?> !important;
+}
+
+.woocommerce .summary ul {
+	margin-left: 0 !important;
+}
+
+@media screen and (max-width: 1200px) {
+.product_meta {
+	display: block;
+	font-size: 14px;
+	line-height: 2;
+	margin: 20px 0;
+	color: <?php echo $breadcrumbs_text_color; ?>;
+	}
+}
+
+@media screen and (min-width: 1200px) {
+.product_meta {
+	display: block;
+	font-size: 14px;
+	line-height: 2;
+	margin: 30px 0;
+	color: <?php echo $breadcrumbs_text_color; ?>;
+	}
+}
+
+.sku_wrapper {
+	display: block;
+}
+
+.posted_in {
+	display: block;
+}
