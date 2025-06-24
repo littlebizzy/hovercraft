3e60550e94ad5a4c62f822e006c60b48f7eceedb
Jesse Nickles <jesse@littlebizzy.com>
2025-06-24 18:06:46 +0700

    Create woocommerce-login.php

---
+++ inc/css/woocommerce-login.php
@@ -0,0 +1,20 @@
+<?php
+// styles for woocommerce login
+?>
+
+.show-password-input {
+	position: absolute !important;
+	top: .4em !important;
+}
+
+.woocommerce form .form-row input.input-text, .woocommerce form .form-row textarea {
+    border: 1px solid #d3ced2 !important;
+	padding: 5px 10px !important;
+	box-shadow: inset 0px 0px 0px 0px #bdbdbd !important;
+	font-size: 18px !important;
+}
+
+.woocommerce-form__input-checkbox {
+	width: auto;
+	box-shadow: inset 0px 0px 0px 0px #bdbdbd !important;
+}
