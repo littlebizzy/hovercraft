b44b87278476017eeba6db53bcdc58ef1e67ad94
Jesse Nickles <jesse@littlebizzy.com>
2025-07-01 17:57:05 +0700

    Create header-basic.php

---
+++ inc/css/header-basic.php
@@ -0,0 +1,34 @@
+<?php
+// styles for basic header
+?>
+
+@media screen and (max-width: 1200px) {
+#header-basic {
+	width: 100%;
+	display: table;
+	padding: 10px 20px;
+	background: <?php echo $header_basic_hero_background_color; ?>;
+	color: <?php echo $header_basic_hero_text_color; ?>;
+	}
+}
+
+@media screen and (min-width: 1200px) {
+#header-basic {
+	width: 100%;
+	display: table;
+	margin: 0px auto;
+	padding: 20px 0px;
+	background: <?php echo $header_basic_hero_background_color; ?>;
+	color: <?php echo $header_basic_hero_text_color; ?>;
+	}
+}
+
+#header-basic .menu-mobile-wrapper i {
+	margin-left: 20px;
+	font-size: 36px;
+	color: <?php echo $header_basic_hero_text_color; ?>;
+}
+
+#header-basic a {
+	color: <?php echo $header_basic_hero_link_color; ?>;
+}
