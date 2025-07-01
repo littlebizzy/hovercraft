e26d9d6d90a69cc32e8dab0d67b1a69f2689d438
Jesse Nickles <jesse@littlebizzy.com>
2025-07-01 17:59:12 +0700

    Create posthero.php

---
+++ inc/css/posthero.php
@@ -0,0 +1,44 @@
+<?php
+// styles for posthero
+?>
+
+@media screen and (max-width: 1200px) {
+#posthero {
+	width: 100%;
+	padding: 30px 20px;
+	text-align: center;
+	background: <?php echo $posthero_background_color; ?>;
+	color: <?php echo $posthero_text_color; ?>;
+	}
+}
+
+@media screen and (min-width: 1200px) {
+#posthero {
+	width: 100%;
+	padding: 30px 0px;
+	text-align: center;
+	background: <?php echo $posthero_background_color; ?>;
+	color: <?php echo $posthero_text_color; ?>;
+	}
+}
+
+#posthero .widget-wrapper {
+	display: inline-block;
+}
+
+#posthero .widget_text {
+	display: block;
+}
+	
+#posthero img {
+	margin-right: 30px;
+}
+	
+#posthero img:last-of-type {
+	margin-right: 0;
+}
+
+#posthero a {
+	color: <?php echo $posthero_link_color; ?>;
+	text-decoration: <?php echo $posthero_link_decoration; ?>;
+}
