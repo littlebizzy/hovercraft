f9e4ac232f700464134da17850a1c903219f0618
Jesse Nickles <jesse@littlebizzy.com>
2025-07-01 17:57:20 +0700

    Update css-generator.php

--- inc/css-generator.php
+++ inc/css-generator.php
@@ -1631,37 +1631,12 @@ video.hero-background-video {
 }
 
 /* basic header */
-
-@media screen and (max-width: 1200px) {
-#header-basic {
-	width: 100%;
-	display: table;
-	padding: 10px 20px;
-	background: <?php echo $header_basic_hero_background_color; ?>;
-	color: <?php echo $header_basic_hero_text_color; ?>;
-	}
+<?php
+$hovercraft_header_basic_css = get_template_directory() . '/inc/css/header-basic.php';
+if ( file_exists( $hovercraft_header_basic_css ) ) {
+	include $hovercraft_header_basic_css;
 }
-
-@media screen and (min-width: 1200px) {
-#header-basic {
-	width: 100%;
-	display: table;
-	margin: 0px auto;
-	padding: 20px 0px;
-	background: <?php echo $header_basic_hero_background_color; ?>;
-	color: <?php echo $header_basic_hero_text_color; ?>;
-	}
-}
-
-#header-basic .menu-mobile-wrapper i {
-	margin-left: 20px;
-	font-size: 36px;
-	color: <?php echo $header_basic_hero_text_color; ?>;
-}
-
-#header-basic a {
-	color: <?php echo $header_basic_hero_link_color; ?>;
-}
+?>
 
 
 /* header primary cta */
