b1a61c356203d13ee913fbea7f89c436ac23790e
Jesse Nickles <jesse@littlebizzy.com>
2025-07-01 16:21:43 +0700

    Create hero-mini.php

---
+++ inc/css/hero-mini.php
@@ -0,0 +1,35 @@
+<?php
+// styles for mini hero
+?>
+
+@media screen and (max-width: 1200px) {
+#hero-mini {
+	width:100%; /* correct */
+	padding:0px; /* correct */
+	display:table; /* correct */
+	}
+}
+
+@media screen and (min-width: 1200px) {
+#hero-mini {
+	width:100%; /* correct */
+	padding:0px; /* correct */
+	display:table; /* correct */
+	}
+}
+
+@media screen and (max-width: 1200px) {
+h1.mini-hero-title {
+	font-weight: 700;
+	margin-bottom: 0;
+	color: #ffffff;
+	}
+}
+
+@media screen and (min-width: 1200px) {
+h1.mini-hero-title {
+	font-weight: 700;
+	margin-bottom: 0;
+	color: #ffffff;
+	}
+}
