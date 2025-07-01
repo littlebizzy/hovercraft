679db9bba43c24380d771b4dba551a0db9c1d8d4
Jesse Nickles <jesse@littlebizzy.com>
2025-07-01 16:24:38 +0700

    Create hero-half.php

---
+++ inc/css/hero-half.php
@@ -0,0 +1,40 @@
+<?php
+// styles for half hero
+?>
+
+@media screen and (max-width: 1200px) {
+#header-half-hero {
+	width: 100%;
+	display: table;
+	padding: 10px 20px;
+	background: #ffffff;
+	color: <?php echo $half_hero_text_color; ?>;
+	}
+}
+
+@media screen and (min-width: 1200px) {
+#header-half-hero {
+	width: 100%;
+	display: table;
+	margin: 0px auto;
+	padding: 20px 0px;
+	background: #ffffff;
+	color: <?php echo $half_hero_text_color; ?>;
+	}
+}
+
+@media screen and (max-width: 1200px) {
+h1.half-hero-title {
+	font-weight: 700;
+	margin-bottom: 30px;
+	color: #ffffff;
+	}
+}
+
+@media screen and (min-width: 1200px) {
+h1.half-hero-title {
+	font-weight: 700;
+	margin-bottom: 30px;
+	color: #ffffff;
+	}
+}
