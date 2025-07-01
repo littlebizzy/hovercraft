c831d6e0ba7ae7103e50ca3a8497f1f5ecf577db
Jesse Nickles <jesse@littlebizzy.com>
2025-07-01 17:52:39 +0700

    Create buttons.php

---
+++ inc/css/buttons.php
@@ -0,0 +1,165 @@
+<?php
+// styles for buttons
+?>
+
+button {
+	font-size: 16px;
+	font-weight: 400;
+	padding: 10px 20px;
+	text-decoration: none;
+	background: #ECEFF1;
+	border: 0 !important;
+	border-radius: 0 !important;
+	cursor: pointer;
+}
+
+.button {
+	border: 0 !important;
+	border-radius: 0 !important;
+}
+
+@media screen and (max-width: 1200px) {
+.button-link {
+	width: 100%;
+	text-align: center;
+	font-size: 20px;
+	font-weight: 700;
+	color: #ffffff;
+	padding: 10px 0;
+	text-decoration: none;
+	display: block;
+	background: <?php echo $default_link_color; ?>;
+	}
+}
+
+@media screen and (min-width: 1200px) {
+.button-link {
+	width: 100%;
+	text-align: center;
+	font-size: 20px;
+	font-weight: 700;
+	color: #ffffff;
+	padding: 10px 0;
+	text-decoration: none;
+	display: block;
+	background: <?php echo $default_link_color; ?>;
+	}
+}
+
+@media screen and (max-width: 1200px) {
+.button-outline {
+	width: 100%;
+	text-align: center;
+	font-size: 20px;
+	font-weight: 700;
+	color: <?php echo $default_text_color; ?>;
+	padding: 10px 0;
+	text-decoration: none;
+	display: block;
+	background: transparent;
+	box-shadow: inset 0px 0px 0px 1px <?php echo $default_text_color; ?>;
+	}
+}
+
+@media screen and (min-width: 1200px) {
+.button-outline {
+	width: 100%;
+	text-align: center;
+	font-size: 20px;
+	font-weight: 700;
+	color: <?php echo $default_text_color; ?>;
+	padding: 10px 0;
+	text-decoration: none;
+	display: block;
+	background: transparent;
+	box-shadow: inset 0px 0px 0px 1px <?php echo $default_text_color; ?>;
+	}
+}
+
+@media screen and (max-width: 1200px) {
+.button-outline-white {
+	width: 100%;
+	text-align: center;
+	font-size: 20px;
+	font-weight: 700;
+	color: #ffffff;
+	padding: 10px 0;
+	text-decoration: none;
+	display: block;
+	background: transparent;
+	box-shadow: inset 0px 0px 0px 1px #ffffff;
+	}
+}
+
+@media screen and (min-width: 1200px) {
+.button-outline-white {
+	width: 100%;
+	text-align: center;
+	font-size: 20px;
+	font-weight: 700;
+	color: #ffffff;
+	padding: 10px 0;
+	text-decoration: none;
+	display: block;
+	background: transparent;
+	box-shadow: inset 0px 0px 0px 1px #ffffff;
+	}
+}
+
+@media screen and (max-width: 1200px) {
+.button-wide-dark {
+	width: 100%;
+	text-align: center;
+	font-size: 20px;
+	font-weight: 700;
+	color: #ffffff;
+	padding: 10px 0;
+	text-decoration: none;
+	display: block;
+	background: <?php echo $default_text_color; ?>;
+	}
+}
+
+@media screen and (min-width: 1200px) {
+.button-wide-dark {
+	width: 100%;
+	text-align: center;
+	font-size: 20px;
+	font-weight: 700;
+	color: #ffffff;
+	padding: 10px 0;
+	text-decoration: none;
+	display: block;
+	background: <?php echo $default_text_color; ?>;
+	}
+}
+
+@media screen and (max-width: 1200px) {
+.button-wide-white {
+	width: 100%;
+	text-align: center;
+	font-size: 20px;
+	font-weight: 700;
+	color: <?php echo $default_text_color; ?>;
+	padding: 10px 0;
+	text-decoration: none;
+	display: block;
+	background: #ffffff;
+	box-shadow: inset 0px 0px 0px 1px <?php echo $default_text_color; ?>;
+	}
+}
+
+@media screen and (min-width: 1200px) {
+.button-wide-white {
+	width: 100%;
+	text-align: center;
+	font-size: 20px;
+	font-weight: 700;
+	color: <?php echo $default_text_color; ?>;
+	padding: 10px 0;
+	text-decoration: none;
+	display: block;
+	background: #ffffff;
+	box-shadow: inset 0px 0px 0px 1px <?php echo $default_text_color; ?>;
+	}
+}
