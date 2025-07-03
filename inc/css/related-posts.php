06a9ab60584a64637ba25d66eee6ecb1d2fc8afe
Jesse Nickles <jesse@littlebizzy.com>
2025-07-03 16:08:10 +0700

    Create related-posts.php

---
+++ inc/css/related-posts.php
@@ -0,0 +1,15 @@
+<?php
+// styles for related posts
+?>
+
+@media screen and (max-width: 1200px) {
+#related {
+	margin-top: 20px;
+	}
+}
+
+@media screen and (min-width: 1200px) {
+#related {
+	margin-top: 30px;
+	}
+}
