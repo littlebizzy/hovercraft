b427807f58608d8e0e3fb8130f833279bcfa075a
Jesse Nickles <jesse@littlebizzy.com>
2025-07-04 01:43:55 +0700

    Update main-menu.php

--- inc/css/main-menu.php
+++ inc/css/main-menu.php
@@ -3,8 +3,8 @@
 ?>
 
 @media screen and (max-width: 1200px) {
-.menu-desktop {
-	display: none;	
+	.menu-desktop {
+		display: none;
 	}
 }
 
@@ -13,9 +13,9 @@
 }
 
 .menu-desktop > .main-menu:after {
-	content:"";
-	display:table;
-	clear:both;
+	content: "";
+	display: table;
+	clear: both;
 }
 
 .menu-desktop > .main-menu ul {
@@ -35,25 +35,31 @@
 	vertical-align: middle;
 }
 
-.menu-desktop > .main-menu ul ul {
+.menu-desktop > .main-menu ul ul,
+.menu-desktop > .main-menu ul ul ul {
 	display: none;
-	position: absolute; 
-	top: 36px; 
+	position: absolute;
 }
-	
-.menu-desktop > .main-menu ul li:hover > ul {
+
+.menu-desktop > .main-menu ul ul {
+	top: 36px;
+}
+
+.menu-desktop > .main-menu ul li:hover > ul,
+.menu-desktop > .main-menu li:hover ul,
+.menu-desktop > .main-menu li ul li {
 	display: block;
 	z-index: 100;
-}	
+}
 
 .menu-desktop > .main-menu a {
 	font-family: <?php 
-        echo !empty($main_menu_font_family) 
-            ? ucwords(str_replace("_", " ", $main_menu_font_family)) 
-            : (!empty($default_font_family) 
-                ? ucwords(str_replace("_", " ", $default_font_family)) 
-                : 'sans-serif'); 
-    ?>;
+		echo !empty($main_menu_font_family) 
+			? ucwords(str_replace("_", " ", $main_menu_font_family)) 
+			: (!empty($default_font_family) 
+				? ucwords(str_replace("_", " ", $default_font_family)) 
+				: 'sans-serif'); 
+	?>;
 	font-size: <?php echo $main_menu_desktop_font_size; ?>px;
 	text-transform: <?php echo $main_menu_text_transform; ?>;
 	font-weight: <?php echo $main_menu_font_weight; ?>;
@@ -63,50 +69,55 @@
 }
 
 
-/* Fisrt Tier Dropdown */
+/* first tier dropdown */
 .menu-desktop > .main-menu ul ul li {
-	width:250px;
+	width: 250px;
 	display: list-item;
 	position: relative;
 	text-align: left;
 }
 
-/* do we need it */
-.menu-desktop > .main-menu li ul { position: absolute; display: none;  }
-.menu-desktop > .main-menu li:hover ul, .menu-desktop > .main-menu li ul li { display: block; z-index:999; }
-.menu-desktop > .main-menu ul ul ul { position: absolute; display: none;  }
-	
+/* optional older dropdown rules */
+.menu-desktop > .main-menu li ul {
+	position: absolute;
+	display: none;
+}
 
-/* force display block and line height on desktop drop-down links only (not parent links) */
-.menu-desktop > .main-menu ul ul a, .main-menu ul ul ul a { display:block !important; line-height: 60px; }
+/* force block + line height only on dropdown links */
+.menu-desktop > .main-menu ul ul a,
+.main-menu ul ul ul a {
+	display: block !important;
+	line-height: 60px;
+}
 
-/* Second, Third and more Tiers	*/
-
+/* second, third, and deeper tiers */
 .menu-desktop > .main-menu ul ul ul li {
 	position: relative;
-	top: -56px; 
+	top: -56px;
 	left: 250px;
 }
-	
-/* dropdown symbol should only appear in .main-menu context in header (not e.g. menu widgets in footer or sidebar) 
-.main-menu .menu-item-has-children > a:after { 
+
+/*
+dropdown symbol should only appear in .main-menu context in header (not e.g. menu widgets in footer or sidebar)
+
+.main-menu .menu-item-has-children > a:after {
 	font-family: "Material Icons";
 	font-size: 24px;
-	content:  ' \e5cf'; 
+	content: '\e5cf';
 	vertical-align: middle;
 }
 
-.main-menu li > a:only-child:after { 
-	content: ''; 
+.main-menu li > a:only-child:after {
+	content: '';
 }
 */
-	
+
 .main-menu .menu-item-has-children > a.menu-toggle {
 	position: relative;
 	vertical-align: top;
 	padding-left: 10px;
 }
-	
+
 .main-menu .menu-item-has-children > a.menu-toggle:after {
 	position: relative;
 	font-family: FontAwesome;
