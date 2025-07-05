e4e1f7ece623b399b175a04be0e4b62fa4d1f343
Jesse Nickles <jesse@littlebizzy.com>
2025-07-05 21:55:30 +0700

    Update main-menu-toggle.php

--- inc/menus/main-menu-toggle.php
+++ inc/menus/main-menu-toggle.php
@@ -5,9 +5,12 @@ function hovercraft_main_menu_toggle($item_output, $item, $depth, $args) {
 		$args->theme_location === 'main-menu' &&
 		in_array('menu-item-has-children', $item->classes, true)
 	) {
-		$item_output .= '<a href="#" class="menu-toggle" aria-hidden="true"></a>';
+		$toggle = '<span class="toggle" aria-hidden="true">&#xf078;</span>';
+		$item_output = str_replace('</a>', $toggle . '</a>', $item_output);
 	}
 	return $item_output;
 }
 
 add_filter('walker_nav_menu_start_el', 'hovercraft_main_menu_toggle', 10, 4);
+
+// Ref: ChatGPT
