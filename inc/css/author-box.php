59390df6b00ac5e314d4f9449007578df5472433
Jesse Nickles <jesse@littlebizzy.com>
2025-07-07 18:02:48 +0700

    Create author-box.php

---
+++ inc/css/author-box.php
@@ -0,0 +1,26 @@
+<?php
+// styles for author box
+?>
+
+/* author biography box */
+
+#author {
+	margin-top: 30px;
+	padding: 40px;
+	box-shadow: inset 0px 0px 0px 1px #cccccc;
+	border-radius: 0px;
+}
+
+#author p {
+	margin-bottom: 0;
+}
+
+.biography-photo img {
+	float: left; 
+	margin: 0 20px 0 0; 
+}
+
+p.biography-description {
+	font-size: 14px;
+}
+	
