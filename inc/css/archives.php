336b22b5c77393e8fda2a4214adaa16e66fd541f
Jesse Nickles <jesse@littlebizzy.com>
2025-07-07 17:54:25 +0700

    Create archives.php

---
+++ inc/css/archives.php
@@ -0,0 +1,31 @@
+<?php
+// styles for archives
+?>
+
+/* archives */
+
+.post-tease-archive {
+	margin-bottom: 40px;
+}
+
+.post-excerpt-archive p {
+	margin: 0;
+}
+
+@media screen and (max-width: 1200px) {
+img.featured-image-archive {
+	width: 100%;
+	height: auto;
+	display: block;
+	margin-bottom: 20px;
+	}
+}
+
+@media screen and (min-width: 1200px) {
+img.featured-image-archive {
+	width: 768px;
+	height: auto;
+	display: block;
+	margin-bottom: 20px;
+	}
+}
