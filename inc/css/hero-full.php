36c807c630ef4865e6e178351a06428b801cdd11
Jesse Nickles <jesse@littlebizzy.com>
2025-07-07 18:55:56 +0700

    Update hero-full.php

--- inc/css/hero-full.php
+++ inc/css/hero-full.php
@@ -4,31 +4,45 @@
 
 <?php
 // get gradient background
-function get_hero_gradient_background() {
-    global $hero_gradient_tones, $hero_gradient_angle, $hero_gradient_start_color, $hero_gradient_start_color_transparency, $hero_gradient_start_color_length, $hero_gradient_mid_color, $hero_gradient_mid_color_transparency, $hero_gradient_mid_color_length, $hero_gradient_stop_color, $hero_gradient_stop_color_transparency, $hero_gradient_stop_color_length, $hero_image;
+function hovercraft_get_hero_gradient_background() {
+    global $hero_gradient_tones,
+           $hero_gradient_angle,
+           $hero_gradient_start_color,
+           $hero_gradient_start_opacity,
+           $hero_gradient_start_position,
+           $hero_gradient_mid_color,
+           $hero_gradient_mid_opacity,
+           $hero_gradient_mid_position,
+           $hero_gradient_stop_color,
+           $hero_gradient_stop_opacity,
+           $hero_gradient_stop_position,
+           $hero_background_image;
 
     if ($hero_gradient_tones === 'two_tones') {
         list($r1, $g1, $b1) = sscanf($hero_gradient_start_color, "#%02x%02x%02x");
-        $start = "rgba({$r1}, {$g1}, {$b1}, {$hero_gradient_start_color_transparency}) {$hero_gradient_start_color_length}%";
+        $start = "rgba({$r1}, {$g1}, {$b1}, {$hero_gradient_start_opacity}) {$hero_gradient_start_position}%";
 
         list($r2, $g2, $b2) = sscanf($hero_gradient_stop_color, "#%02x%02x%02x");
-        $stop = "rgba({$r2}, {$g2}, {$b2}, {$hero_gradient_stop_color_transparency}) {$hero_gradient_stop_color_length}%";
+        $stop = "rgba({$r2}, {$g2}, {$b2}, {$hero_gradient_stop_opacity}) {$hero_gradient_stop_position}%";
+
+        $gradient = "linear-gradient({$hero_gradient_angle}, {$start}, {$stop})";
     } elseif ($hero_gradient_tones === 'three_tones') {
         list($r1, $g1, $b1) = sscanf($hero_gradient_start_color, "#%02x%02x%02x");
-        $start = "rgba({$r1}, {$g1}, {$b1}, {$hero_gradient_start_color_transparency}) {$hero_gradient_start_color_length}%";
+        $start = "rgba({$r1}, {$g1}, {$b1}, {$hero_gradient_start_opacity}) {$hero_gradient_start_position}%";
 
         list($r2, $g2, $b2) = sscanf($hero_gradient_mid_color, "#%02x%02x%02x");
-        $mid = "rgba({$r2}, {$g2}, {$b2}, {$hero_gradient_mid_color_transparency}) {$hero_gradient_mid_color_length}%";
+        $mid = "rgba({$r2}, {$g2}, {$b2}, {$hero_gradient_mid_opacity}) {$hero_gradient_mid_position}%";
 
         list($r3, $g3, $b3) = sscanf($hero_gradient_stop_color, "#%02x%02x%02x");
-        $stop = "rgba({$r3}, {$g3}, {$b3}, {$hero_gradient_stop_color_transparency}) {$hero_gradient_stop_color_length}%";
+        $stop = "rgba({$r3}, {$g3}, {$b3}, {$hero_gradient_stop_opacity}) {$hero_gradient_stop_position}%";
+
+        $gradient = "linear-gradient({$hero_gradient_angle}, {$start}, {$mid}, {$stop})";
+    } else {
+        return '';
     }
 
-    $gradient = "linear-gradient({$hero_gradient_angle}, ";
-    $gradient .= ($hero_gradient_tones === 'two_tones') ? "{$start}, {$stop}" : "{$start}, {$mid}, {$stop}";
-    $gradient .= ")";
-    if (!empty($hero_image)) {
-        $gradient .= ", url({$hero_image})";
+    if (!empty($hero_background_image)) {
+        $gradient .= ", url({$hero_background_image})";
     }
 
     return $gradient;
@@ -38,9 +52,9 @@ function get_hero_gradient_background() {
 #hero-full-container,
 .hero-background-video-overlay {
 <?php if ($hero_gradient_tones === 'two_tones' || $hero_gradient_tones === 'three_tones') { ?>
-    background: <?php echo get_hero_gradient_background(); ?>;
+    background: <?php echo hovercraft_get_hero_gradient_background(); ?>;
 <?php } ?>
-    background-position: <?php echo str_replace('_',' ', $full_hero_background_position); ?>;
+    background-position: <?php echo str_replace('_', ' ', $hero_background_position); ?>;
     background-size: cover;
     background-repeat: no-repeat;
 }
@@ -92,7 +106,7 @@ video.hero-background-video {
         width: 100%;
         display: table;
         padding: 10px 20px;
-        background: <?php list($r1, $g1, $b1) = sscanf($full_hero_header_background_color, "#%02x%02x%02x"); echo "rgba({$r1}, {$g1}, {$b1}, {$full_hero_header_background_transparency})"; ?>;
+        background: <?php list($r1, $g1, $b1) = sscanf($hero_header_background_color, "#%02x%02x%02x"); echo "rgba({$r1}, {$g1}, {$b1}, {$hero_header_background_opacity})"; ?>;
         position: relative;
         color: #ffffff;
     }
@@ -104,7 +118,7 @@ video.hero-background-video {
         display: table;
         margin: 0px auto;
         padding: 20px 0px;
-        background: <?php list($r1, $g1, $b1) = sscanf($full_hero_header_background_color, "#%02x%02x%02x"); echo "rgba({$r1}, {$g1}, {$b1}, {$full_hero_header_background_transparency})"; ?>;
+        background: <?php list($r1, $g1, $b1) = sscanf($hero_header_background_color, "#%02x%02x%02x"); echo "rgba({$r1}, {$g1}, {$b1}, {$hero_header_background_opacity})"; ?>;
         position: relative;
         color: #ffffff;
     }
