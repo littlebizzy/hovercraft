102ea9fc16fb9b6bc11cb5f1a0ba0fa5e8591803
Jesse Nickles <jesse@littlebizzy.com>
2025-07-08 02:12:49 +0700

    Update breadcrumbs.php

--- inc/breadcrumbs.php
+++ inc/breadcrumbs.php
@@ -5,6 +5,7 @@ add_filter( 'bbp_no_breadcrumb', '__return_true' );
 
 function ah_breadcrumb() {
     echo '<ul id="ah-breadcrumb" class="ah-breadcrumb breadcrumb-list" itemscope itemtype="https://schema.org/BreadcrumbList">';
+
     ah_breadcrumb_home();
 
     if ( function_exists( 'is_bbpress' ) && is_bbpress() ) {
@@ -34,7 +35,9 @@ function ah_breadcrumb_item( $url, $name, $position, $is_current = false ) {
     }
 
     $class = $is_current ? 'item-current item' : 'item';
+
     echo '<li class="' . esc_attr( $class ) . '" itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">';
+
     if ( $url && ! $is_current ) {
         echo '<a href="' . esc_url( $url ) . '" itemprop="item">';
         echo '<span itemprop="name">' . esc_html( $name ) . '</span>';
@@ -42,6 +45,7 @@ function ah_breadcrumb_item( $url, $name, $position, $is_current = false ) {
     } else {
         echo '<span itemprop="name">' . esc_html( $name ) . '</span>';
     }
+
     echo '<meta itemprop="position" content="' . intval( $position ) . '" />';
     echo '</li>';
 }
@@ -58,12 +62,14 @@ function ah_breadcrumb_home() {
 function ah_breadcrumb_bbpress() {
     $pos =& $GLOBALS['ah_position'];
 
-    ah_breadcrumb_item( bbp_get_forums_url(), __( 'Forum', 'hovercraft' ), $pos++ );
+    if ( function_exists( 'bbp_get_forums_url' ) ) {
+        ah_breadcrumb_item( bbp_get_forums_url(), __( 'Forum', 'hovercraft' ), $pos++ );
+    }
 
-    if ( bbp_is_forum_archive() ) {
+    if ( function_exists( 'bbp_is_forum_archive' ) && bbp_is_forum_archive() ) {
         ah_breadcrumb_item( '', __( 'All Forums', 'hovercraft' ), $pos++, true );
 
-    } elseif ( bbp_is_single_reply() ) {
+    } elseif ( function_exists( 'bbp_is_single_reply' ) && bbp_is_single_reply() ) {
         $reply_id = bbp_get_reply_id();
         $topic_id = bbp_get_reply_topic_id( $reply_id );
         $forum_id = bbp_get_topic_forum_id( $topic_id );
@@ -72,82 +78,91 @@ function ah_breadcrumb_bbpress() {
         ah_breadcrumb_item( get_permalink( $topic_id ), get_the_title( $topic_id ), $pos++ );
         ah_breadcrumb_item( '', get_the_title( $reply_id ), $pos++, true );
 
-    } elseif ( bbp_is_single_topic() ) {
+    } elseif ( function_exists( 'bbp_is_single_topic' ) && bbp_is_single_topic() ) {
         $forum_id = bbp_get_topic_forum_id();
 
         ah_breadcrumb_bbpress_hierarchy( $forum_id, $pos );
         ah_breadcrumb_item( '', get_the_title(), $pos++, true );
 
-    } elseif ( bbp_is_single_forum() ) {
+    } elseif ( function_exists( 'bbp_is_single_forum' ) && bbp_is_single_forum() ) {
         ah_breadcrumb_bbpress_hierarchy( get_the_ID(), $pos );
         ah_breadcrumb_item( '', get_the_title(), $pos++, true );
 
-    } elseif ( bbp_is_topic_tag() || bbp_is_single_topic_tag() ) {
+    } elseif (
+        ( function_exists( 'bbp_is_topic_tag' ) && bbp_is_topic_tag() ) ||
+        ( function_exists( 'bbp_is_single_topic_tag' ) && bbp_is_single_topic_tag() )
+    ) {
         ah_breadcrumb_item( '', bbp_get_topic_tag_name(), $pos++, true );
 
-    } elseif ( bbp_is_view() ) {
+    } elseif ( function_exists( 'bbp_is_view' ) && bbp_is_view() ) {
         ah_breadcrumb_item( '', bbp_get_view_title(), $pos++, true );
 
-    } elseif ( bbp_is_search() ) {
+    } elseif ( function_exists( 'bbp_is_search' ) && bbp_is_search() ) {
         ah_breadcrumb_item( '', __( 'Search Results', 'hovercraft' ), $pos++, true );
 
-    } elseif ( bbp_is_user_home() || bbp_is_single_user() ) {
+    } elseif (
+        ( function_exists( 'bbp_is_user_home' ) && bbp_is_user_home() ) ||
+        ( function_exists( 'bbp_is_single_user' ) && bbp_is_single_user() )
+    ) {
         ah_breadcrumb_item( '', __( 'User: ', 'hovercraft' ) . bbp_get_user_display_name( bbp_get_displayed_user_id() ), $pos++, true );
 
-    } elseif ( bbp_is_user_edit() ) {
+    } elseif ( function_exists( 'bbp_is_user_edit' ) && bbp_is_user_edit() ) {
         ah_breadcrumb_item( '', __( 'Edit Profile', 'hovercraft' ), $pos++, true );
 
-    } elseif ( bbp_is_user_subscriptions() ) {
+    } elseif ( function_exists( 'bbp_is_user_subscriptions' ) && bbp_is_user_subscriptions() ) {
         ah_breadcrumb_item( '', __( 'Subscriptions', 'hovercraft' ), $pos++, true );
 
-    } elseif ( bbp_is_user_favorites() ) {
+    } elseif ( function_exists( 'bbp_is_user_favorites' ) && bbp_is_user_favorites() ) {
         ah_breadcrumb_item( '', __( 'Favorites', 'hovercraft' ), $pos++, true );
 
-    } elseif ( bbp_is_user_topics() ) {
+    } elseif ( function_exists( 'bbp_is_user_topics' ) && bbp_is_user_topics() ) {
         ah_breadcrumb_item( '', __( 'User Topics', 'hovercraft' ), $pos++, true );
 
-    } elseif ( bbp_is_user_replies() ) {
+    } elseif ( function_exists( 'bbp_is_user_replies' ) && bbp_is_user_replies() ) {
         ah_breadcrumb_item( '', __( 'User Replies', 'hovercraft' ), $pos++, true );
     }
 }
 
 function ah_breadcrumb_bbpress_hierarchy( $post_id, &$position ) {
     $ancestors = array_reverse( get_post_ancestors( $post_id ) );
+
     foreach ( $ancestors as $ancestor ) {
         ah_breadcrumb_item( get_permalink( $ancestor ), get_the_title( $ancestor ), $position++ );
     }
+
     ah_breadcrumb_item( get_permalink( $post_id ), get_the_title( $post_id ), $position++ );
 }
 
 function ah_breadcrumb_woocommerce() {
     $pos =& $GLOBALS['ah_position'];
 
-    if ( is_shop() ) {
+    if ( function_exists( 'is_shop' ) && is_shop() ) {
         ah_breadcrumb_item( '', woocommerce_page_title( false ), $pos++, true );
         return;
     }
 
-    $shop_page_id = wc_get_page_id( 'shop' );
-    if ( $shop_page_id && ! is_shop() ) {
-        ah_breadcrumb_item( get_permalink( $shop_page_id ), get_the_title( $shop_page_id ), $pos++ );
+    if ( function_exists( 'wc_get_page_id' ) ) {
+        $shop_page_id = wc_get_page_id( 'shop' );
+        if ( $shop_page_id && ! is_shop() ) {
+            ah_breadcrumb_item( get_permalink( $shop_page_id ), get_the_title( $shop_page_id ), $pos++ );
+        }
     }
 
-    if ( is_product_category() || is_product_tag() ) {
+    if ( is_tax( 'product_cat' ) || is_tax( 'product_tag' ) ) {
         $term = get_queried_object();
-        $ancestors = get_ancestors( $term->term_id, $term->taxonomy );
-        $ancestors = array_reverse( $ancestors );
-        foreach ( $ancestors as $ancestor_id ) {
-            $ancestor = get_term( $ancestor_id, $term->taxonomy );
-            ah_breadcrumb_item( get_term_link( $ancestor ), $ancestor->name, $pos++ );
+        if ( $term && isset( $term->term_id, $term->taxonomy ) ) {
+            $ancestors = array_reverse( get_ancestors( $term->term_id, $term->taxonomy ) );
+            foreach ( $ancestors as $ancestor_id ) {
+                $ancestor = get_term( $ancestor_id, $term->taxonomy );
+                ah_breadcrumb_item( get_term_link( $ancestor ), $ancestor->name, $pos++ );
+            }
+            ah_breadcrumb_item( '', $term->name, $pos++, true );
         }
-        ah_breadcrumb_item( '', $term->name, $pos++, true );
-
-    } elseif ( is_singular( 'product' ) ) {
+    } elseif ( is_singular( 'product' ) && function_exists( 'wc_get_product_terms' ) ) {
         $terms = wc_get_product_terms( get_the_ID(), 'product_cat' );
         if ( ! empty( $terms ) && ! is_wp_error( $terms ) ) {
-            $primary = $terms[0];
-            $ancestors = get_ancestors( $primary->term_id, 'product_cat' );
-            $ancestors = array_reverse( $ancestors );
+            $primary   = $terms[0];
+            $ancestors = array_reverse( get_ancestors( $primary->term_id, 'product_cat' ) );
             foreach ( $ancestors as $ancestor_id ) {
                 $ancestor = get_term( $ancestor_id, 'product_cat' );
                 ah_breadcrumb_item( get_term_link( $ancestor ), $ancestor->name, $pos++ );
@@ -163,12 +178,14 @@ function ah_breadcrumb_cpt_fallback( $post_type ) {
         'book' => array( 'url' => '/library/', 'label' => 'Library' ),
         // add more custom types here
     );
+
     return $fallback[ $post_type ] ?? false;
 }
 
 function ah_breadcrumb_home_blog() {
     $pos =& $GLOBALS['ah_position'];
     $page_for_posts = get_option( 'page_for_posts' );
+
     if ( $page_for_posts ) {
         ah_breadcrumb_item( '', get_the_title( $page_for_posts ), $pos++, true );
     } else {
@@ -183,6 +200,7 @@ function ah_breadcrumb_single() {
 
     if ( $post_type !== 'post' ) {
         $obj = get_post_type_object( $post_type );
+
         if ( $obj && ! empty( $obj->has_archive ) ) {
             ah_breadcrumb_item( get_post_type_archive_link( $post_type ), $obj->labels->name, $pos++ );
         } else {
@@ -241,7 +259,8 @@ function ah_breadcrumb_archive() {
 
     } elseif ( is_author() ) {
         $author = get_userdata( get_query_var( 'author' ) );
-        ah_breadcrumb_item( '', __( 'Author: ', 'hovercraft' ) . $author->display_name, $pos++, true );
+        $name = $author && isset( $author->display_name ) ? $author->display_name : '';
+        ah_breadcrumb_item( '', __( 'Author: ', 'hovercraft' ) . $name, $pos++, true );
 
     } elseif ( is_year() ) {
         ah_breadcrumb_item( get_year_link( get_the_time( 'Y' ) ), get_the_time( 'Y' ) . ' Archives', $pos++ );
@@ -264,7 +283,7 @@ function ah_breadcrumb_page() {
     global $post;
     $pos =& $GLOBALS['ah_position'];
 
-    if ( $post->post_parent ) {
+    if ( isset( $post ) && is_object( $post ) && $post->post_parent ) {
         $ancestors = array_reverse( get_post_ancestors( $post->ID ) );
         foreach ( $ancestors as $ancestor ) {
             ah_breadcrumb_item( get_permalink( $ancestor ), get_the_title( $ancestor ), $pos++ );
