ac5948496ae62dcbaa347374deff1423bdcf2486
Jesse Nickles <jesse@littlebizzy.com>
2025-07-08 00:59:00 +0700

    Update featured-image-large.php

--- template-parts/content/featured-image-large.php
+++ template-parts/content/featured-image-large.php
@@ -6,18 +6,27 @@ if ( empty( $currentID ) || ! is_numeric( $currentID ) || $currentID < 1 ) {
 	$currentID = isset( $post ) && is_object( $post ) ? $post->ID : 0;
 }
 
+// skip if single product page
+if ( is_singular( 'product' ) ) {
+	return;
+}
+
 // get featured image data
 $url_featured_image = wp_get_attachment_image_src( get_post_thumbnail_id( $currentID ), 'large' );
 
-// output image if valid and not a single product
-if ( is_array( $url_featured_image ) && ! is_singular( 'product' ) ) {
-	$image_url = $url_featured_image[0];
-	$image_width = $url_featured_image[1];
-	$image_height = $url_featured_image[2];
-	?>
-	<img width="<?php echo esc_attr( $image_width ); ?>" height="<?php echo esc_attr( $image_height ); ?>" class="featured-image" src="<?php echo esc_url( $image_url ); ?>" />
-<?php }
+// skip if no image
+if ( ! is_array( $url_featured_image ) ) {
+	return;
+}
 
+// output image
+$image_url = $url_featured_image[0];
+$image_width = $url_featured_image[1];
+$image_height = $url_featured_image[2];
+?>
+<img width="<?php echo esc_attr( $image_width ); ?>" height="<?php echo esc_attr( $image_height ); ?>" class="featured-image" src="<?php echo esc_url( $image_url ); ?>" />
+
+<?php
 // Ref: ChatGPT
 // Ref: https://wordpress.stackexchange.com/questions/18337/how-to-get-featured-images-width-and-use-elsewhere-in-template
 // Ref: https://wordpress.stackexchange.com/questions/27385/whats-the-best-way-to-use-the-featured-image-for-responsive-web-design
