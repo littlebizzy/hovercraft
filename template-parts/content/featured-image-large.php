876056ee72611fd81102508e77d69a1a21d1fb05
Jesse Nickles <jesse@littlebizzy.com>
2025-07-08 00:28:51 +0700

    Update featured-image-large.php

--- template-parts/content/featured-image-large.php
+++ template-parts/content/featured-image-large.php
@@ -1,12 +1,24 @@
-<?php global $post; 
+<?php
+global $post;
+
+// fallback to global post id if currentID is missing or invalid
+if ( empty( $currentID ) || ! is_numeric( $currentID ) || $currentID < 1 ) {
+	$currentID = isset( $post ) && is_object( $post ) ? $post->ID : 0;
+}
+
+// get featured image data
 $url_featured_image = wp_get_attachment_image_src( get_post_thumbnail_id( $currentID ), 'large' );
-// https://rudrastyh.com/wordpress/image-sizes.html
-$image_width = $url_featured_image[1];
-$image_height = $url_featured_image[2];
-if ( !empty( $url_featured_image ) && ( !is_singular('product') ) ) { ?>
-<img width="<?php echo $image_width; ?>" height="<?php echo $image_height; ?>" class="featured-image" src="<?php echo $url_featured_image[0]; ?>" />
+
+// output image if valid and not a single product
+if ( is_array( $url_featured_image ) && ! is_singular( 'product' ) ) {
+	$image_url = $url_featured_image[0];
+	$image_width = $url_featured_image[1];
+	$image_height = $url_featured_image[2];
+	?>
+	<img width="<?php echo esc_attr( $image_width ); ?>" height="<?php echo esc_attr( $image_height ); ?>" class="featured-image" src="<?php echo esc_url( $image_url ); ?>" />
 <?php }
 
-// https://wordpress.stackexchange.com/questions/18337/how-to-get-featured-images-width-and-use-elsewhere-in-template
-// https://wordpress.stackexchange.com/questions/27385/whats-the-best-way-to-use-the-featured-image-for-responsive-web-design
-// https://www.smashingmagazine.com/2012/06/responsive-images-with-wordress-featured-images/
+// Ref: ChatGPT
+// Ref: https://wordpress.stackexchange.com/questions/18337/how-to-get-featured-images-width-and-use-elsewhere-in-template
+// Ref: https://wordpress.stackexchange.com/questions/27385/whats-the-best-way-to-use-the-featured-image-for-responsive-web-design
+// Ref: https://www.smashingmagazine.com/2012/06/responsive-images-with-wordress-featured-images/
