1c8a812fc8f87cfe9133b644dd37936f2066f875
Jesse Nickles <jesse@littlebizzy.com>
2025-07-07 17:08:54 +0700

    Create hero-title.php

---
+++ inc/css/hero-title.php
@@ -0,0 +1,33 @@
+<?php
+// styles for hero-title.php
+?>
+
+/* hero title */
+
+@media screen and (max-width: 1200px) {
+.welcome-wrapper {
+	width: 100%;
+	padding: 30px 20px;
+	}
+}
+
+@media screen and (min-width: 1200px) {
+.welcome-wrapper {
+	max-width: 900px;
+	}
+}
+
+@media screen and (max-width: 1200px) {
+.title-wrapper {
+	width: 100%;
+	padding: 0px 20px;
+	text-align: center;
+	}
+}
+
+@media screen and (min-width: 1200px) {
+.title-wrapper {
+	width: 100%;
+	text-align: center;
+	}
+}
