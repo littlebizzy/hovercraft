f39cd28dd31e9f48890d1153c9c1636327512d6c
Jesse Nickles <jesse@littlebizzy.com>
2025-07-09 13:47:57 +0700

    Update css-generator.php

--- inc/css-generator.php
+++ inc/css-generator.php
@@ -750,39 +750,6 @@ if ( file_exists( $hovercraft_main_menu_css ) ) {
 #header-basic .menu-desktop > .main-menu ul li ul li { background: <?php echo $header_basic_hero_background_color; ?>; }
 
 
-
-	
-#header-full-hero .main-menu ul li a {
-	text-decoration: none;
-	color: #ffffff; /* correct */
-}
-
-
-
-#header-full-hero a {
-	color: #ffffff; /* correct */
-}
-
-#header-half-hero .main-menu ul li a {
-	text-decoration: none;
-	color: <?php echo $half_hero_link_color; ?>;
-}
-
-#header-half-hero a {
-	color: <?php echo $half_hero_link_color; ?>;
-}
-
-#header-mini-hero .main-menu ul li a {
-	text-decoration: none;
-	color: <?php echo $mini_hero_header_link_color; ?>;
-}
-
-#header-basic .main-menu ul li a {
-	text-decoration: none;
-	color: <?php echo $header_basic_hero_link_color; ?>;
-}
-
-
 /* topbar */
 <?php
 $hovercraft_topbar_css = get_template_directory() . '/inc/css/topbar.php';
@@ -798,42 +765,6 @@ if ( file_exists( $hovercraft_preheader_css ) ) {
 	include $hovercraft_preheader_css;
 }
 ?>
-	
-.hero-half {
-<?php if ( $hero_gradient_tones == 'two_tones' ) { ?>
-	background: linear-gradient(<?php echo $hero_gradient_angle; ?>, <?php 
-		list($r1, $g1, $b1) = sscanf($hero_gradient_start_color, "#%02x%02x%02x");
-		echo "rgba({$r1}, {$g1}, {$b1}, {$hero_gradient_start_color_transparency})"; 
-		?> <?php echo $hero_gradient_start_color_length; ?>%, <?php 
-		list($r2, $g2, $b2) = sscanf($hero_gradient_stop_color, "#%02x%02x%02x");
-		echo "rgba({$r2}, {$g2}, {$b2}, {$hero_gradient_stop_color_transparency})"; 
-		?> <?php echo $hero_gradient_stop_color_length; ?>%)<?php if (!empty( $hero_image )) echo ", url({$hero_image})" ?>;
-<?php } elseif ( $hero_gradient_tones == 'three_tones' ) { ?>
-	background: linear-gradient(<?php echo $hero_gradient_angle; ?>, <?php 
-		list($r1, $g1, $b1) = sscanf($hero_gradient_start_color, "#%02x%02x%02x");
-		echo "rgba({$r1}, {$g1}, {$b1}, {$hero_gradient_start_color_transparency})"; 
-		?> <?php echo $hero_gradient_start_color_length; ?>%, <?php 
-		list($r1, $g1, $b1) = sscanf($hero_gradient_mid_color, "#%02x%02x%02x");
-		echo "rgba({$r1}, {$g1}, {$b1}, {$hero_gradient_mid_color_transparency})"; 
-		?> <?php echo $hero_gradient_mid_color_length; ?>%, <?php 
-		list($r2, $g2, $b2) = sscanf($hero_gradient_stop_color, "#%02x%02x%02x");
-		echo "rgba({$r2}, {$g2}, {$b2}, {$hero_gradient_stop_color_transparency})"; 
-		?> <?php echo $hero_gradient_stop_color_length; ?>%)<?php if (!empty( $hero_image )) echo ", url({$hero_image})" ?>;
-<?php } ?>
-	background-position: <?php $halfpos = str_replace('_',' ',$half_hero_background_position); echo $halfpos; ?>;
-	background-size: cover;
-	background-repeat: no-repeat;
-}
-
-#header-half-hero {
-	background: <?php echo $half_hero_background_color; ?>;
-}
-
-#header-half-hero .menu-mobile-wrapper i {
-	margin-left: 20px;
-	font-size: 36px;
-	color: <?php echo $default_text_color; ?>;
-}
 
 /* basic header */
 <?php
@@ -1002,116 +933,12 @@ if ( file_exists( $hovercraft_author_box_css ) ) {
 ?>
 
 /* columns */
-
-@media screen and (max-width: 1200px) {
-.columns {
-	width: 100%;
-	padding: 30px 20px;
-	}
+<?php
+$hovercraft_columns_css = get_template_directory() . '/inc/css/columns.php';
+if ( file_exists( $hovercraft_columns_css ) ) {
+	include $hovercraft_columns_css;
 }
-
-@media screen and (min-width: 1200px) {
-.columns {
-	width: 100%;
-	display: grid;
-  	<?php if ( $columns_across == '3' ) { 
-			echo "grid-template-columns: repeat(3, 1fr);\n"; 
-		} elseif ( $columns_across == '4' ) { 
-			echo "grid-template-columns: repeat(4, 1fr);\n"; 
-		} elseif ( $columns_across == '5' ) { 
-			echo "grid-template-columns: repeat(5, 1fr);\n"; 
-		} elseif ( $columns_across == '6' ) { 
-			echo "grid-template-columns: repeat(6, 1fr);\n"; 
-		} ?>
-  	gap: 30px;
-	}
-}
-
-@media screen and (max-width: 1200px) {
-.column {
-	width: 100%;
-	margin-bottom: 30px;
-	padding: 30px;
-	box-shadow: inset 0px 0px 0px 1px <?php echo $column_border_color; ?>;
-	background: <?php echo $column_background_color; ?>;
-	}
-}
-
-@media screen and (min-width: 1200px) {
-.column {
-	display: inline-block;
-	max-width: 100%;
-	padding: 30px;
-  	justify-content: center;
-  	align-items: center;
-	text-align: center;
-	box-shadow: inset 0px 0px 0px 1px <?php echo $column_border_color; ?>;
-	background: <?php echo $column_background_color; ?>;
-	}
-}
-	
-.column ul {
-	font-size: 14px;
-}
-
-.column i {
-	margin-bottom: 20px;
-	font-size: 48px;
-	display: block;
-	color: <?php echo $default_link_color; ?>;
-}
-
-.column img {
-	display: block;
-	margin: 0 auto;
-}
-
-.column p:last-child {
-	margin-bottom: 0;
-}
-
-
-/* postcolumns */
-	
-@media screen and (max-width: 1200px) {
-.postcolumns-top {
-	width: 100%;
-	padding: 30px 20px;
-	text-align: <?php echo $postcolumns_top_align; ?>;
-	background: <?php echo $postcolumns_top_background_color; ?>;
-	color: <?php echo $postcolumns_top_text_color; ?>;
-	}
-}
-
-@media screen and (min-width: 1200px) {
-.postcolumns-top {
-	width: 100%;
-	padding: 30px 0px;
-	text-align: <?php echo $postcolumns_top_align; ?>;
-	background: <?php echo $postcolumns_top_background_color; ?>;
-	color: <?php echo $postcolumns_top_text_color; ?>;
-	}
-}
-
-@media screen and (max-width: 1200px) {
-.postcolumns-bottom {
-	width: 100%;
-	padding: 30px 20px;
-	text-align: <?php echo $postcolumns_bottom_align; ?>;
-	background: <?php echo $postcolumns_bottom_background_color; ?>;
-	color: <?php echo $postcolumns_bottom_text_color; ?>;
-	}
-}
-
-@media screen and (min-width: 1200px) {
-.postcolumns-bottom {
-	width: 100%;
-	padding: 30px 0px;
-	text-align: <?php echo $postcolumns_bottom_align; ?>;
-	background: <?php echo $postcolumns_bottom_background_color; ?>;
-	color: <?php echo $postcolumns_bottom_text_color; ?>;
-	}
-}
+?>
 
 
 /* zigzag (page template) */
