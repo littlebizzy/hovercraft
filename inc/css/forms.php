d0c12e1d39b1b9cc44a5636cbae0de324cf9bf0e
Jesse Nickles <jesse@littlebizzy.com>
2025-07-17 01:43:33 +0700

    Update forms.php

--- inc/css/forms.php
+++ inc/css/forms.php
@@ -3,13 +3,13 @@
 ?>
 
 /* forms */
-	
+
 form {
 	margin: 0 !important;
 	padding: 0 !important;
 }
-	
-input, select {
+
+input, select, textarea, button {
 	width: 100%;
 	max-width: 100%;
 	height: auto;
@@ -24,6 +24,19 @@ input, select {
 	background: #ffffff;
 	display: inline-block;
 	box-shadow: inset 0px 0px 0px 1px <?php echo $breadcrumbs_text_color; ?>;
+	font-family: inherit;
+}
+
+select {
+	appearance: none;
+	-webkit-appearance: none;
+	-moz-appearance: none;
+	background-image: url('data:image/svg+xml;utf8,<svg fill="%23666" height="20" viewBox="0 0 24 24" width="20" xmlns="http://www.w3.org/2000/svg"><path d="M7 10l5 5 5-5z"/></svg>');
+	background-repeat: no-repeat;
+	background-position: right 15px center;
+	background-size: 16px;
+	padding-right: 45px !important;
+	border-radius: 0;
 }
 
 option {
@@ -31,47 +44,70 @@ option {
 	padding: 0 !important;
 	display: block;
 	white-space: nowrap;
+	font-family: inherit;
+}
+
+textarea {
+	font-size: 18px;
+	font-weight: 400;
+	padding: 10px 20px !important;
+	border-radius: 0;
+	box-shadow: inset 0px 0px 0px 1px #BDBDBD;
+	resize: vertical;
+}
+
+input[type="checkbox"], input[type="radio"] {
+	width: auto;
+	display: inline-block;
+	margin-right: 10px;
+	box-shadow: none;
+}
+
+input[type="file"] {
+	padding: 10px 0 !important;
+	box-shadow: none;
+	background: transparent;
+}
+
+input[type="submit"], button, button[type="submit"] {
+	font-size: 18px;
+	font-weight: 600;
+	padding: 10px 20px !important;
+	background: <?php echo $default_link_color; ?>;
+	text-decoration: none;
+	color: #ffffff;
+	border-radius: 0;
+	cursor: pointer;
+	box-shadow: none;
+}
+
+input[type="submit"]:hover, button:hover, button[type="submit"]:hover {
+	background: <?php echo $default_hover_color; ?> !important;
 }
 
 #topbar input, .hero-half input {
 	box-shadow: none;
 }
 
-textarea {
-	width: 100%;
-	font-size: 18px;
-	font-weight: 400;
-	display: inline-block;
-	padding: 10px 20px !important;
-	background: #ffffff;
-	border-radius: 0px;
-	border: 0;
-	box-shadow: inset 0px 0px 0px 1px #BDBDBD;
-}
-
-input[type="checkbox"] {
-	width: auto;
-	font-size: 18px;
-	display: inline-block;
-	outline: none;
-	box-shadow: none;
-}
-
-input[type="submit"] {
-	width: 100%;
-	font-size: 18px;
+label {
+	display: block;
+	margin-bottom: 5px;
 	font-weight: 600;
-	display: inline-block;
-	padding: 10px 20px !important;
-	background: <?php echo $default_link_color; ?>;
-	text-decoration: none;
-	color: #ffffff;
-	border-radius: 0px;
-	border: 0;
-	cursor: pointer;
-	box-shadow: none;
+	font-size: 15px;
 }
-	
-button[type="submit"]:hover, input[type="submit"]:hover {
-	background: <?php echo $default_hover_color; ?> !important;
+
+fieldset {
+	border: 1px solid #e0e0e0;
+	padding: 20px;
+	margin-bottom: 20px;
+}
+
+legend {
+	font-weight: 600;
+	font-size: 16px;
+	padding: 0 5px;
+}
+
+input:focus, select:focus, textarea:focus, button:focus {
+	box-shadow: 0 0 0 2px rgba(100, 100, 100, 0.15);
 }
