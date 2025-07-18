fe44af8b6f1d0abcb160b98e149716feb832b645
Jesse Nickles <jesse@littlebizzy.com>
2025-07-18 14:36:59 +0700

    Update woocommerce-add-to-cart.php

--- inc/css/woocommerce-add-to-cart.php
+++ inc/css/woocommerce-add-to-cart.php
@@ -3,45 +3,51 @@
 ?>
 
 form.variations_form {
-	display: block !important;
+display: block !important;
 }
 
 form.cart {
-	display: flex;
+display: flex;
 }
-	
+
 div.single_variation_wrap {
-	clear: both;
-	display: flex;
+clear: both;
+display: flex;
 }
 
 table.variations {
-	display: block;
-	width: 100%;
-	margin-bottom: 30px !important;
-	border: 0;
+display: block;
+width: 100%;
+margin-bottom: 30px !important;
+border: 0;
 }
 
 form.cart input {
-	vertical-align: middle;
-	font-size: 16px;
-	font-weight: 400;
-	line-height: 2;
-	text-align: center;
-	margin-right: 5px;
-	padding: 5px 0px !important;
-	border: 0;
-	outline: none;
-	box-shadow: inset 0px 0px 0px 1px #BDBDBD;
+vertical-align: middle;
+font-size: 16px;
+font-weight: 400;
+line-height: 2;
+text-align: center;
+margin-right: 5px;
+padding: 5px 0px !important;
+border: 0;
+outline: none;
+box-shadow: inset 0px 0px 0px 1px #BDBDBD;
 }
 
 form.cart button {
-	vertical-align: middle;
-	font-size: 16px !important;
-	font-weight: 700 !important;
-	padding: 0 20px;
-	border: 0;
-	outline: none;
-	cursor: pointer;
-	border-radius: 0 !important;
+vertical-align: middle;
+font-size: 16px !important;
+font-weight: 700 !important;
+padding: 0 20px;
+border: 0;
+outline: none;
+cursor: pointer;
+border-radius: 0 !important;
+}
+
+.quantity-button, .quantity .plus, .quantity .minus {
+box-shadow: none !important;
+border: none !important;
+background: transparent;
 }
