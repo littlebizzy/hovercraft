314d963e96665b2165ecd7f9bc4d5f34f5e79efc
Jesse Nickles <jesse@littlebizzy.com>
2025-07-21 17:27:04 +0700

    Update logo-alternative.php

--- inc/logo-alternative.php
+++ inc/logo-alternative.php
@@ -1,38 +1,34 @@
 <?php
 
-/** alternative logo */
-
+// add alternative logo setting to customizer
 function hovercraft_logo_alternative( $wp_customize ) {
-    // $wp_customize->add_setting( 'hovercraft_logo_alternative' );
-    $wp_customize->add_setting('hovercraft_logo_alternative', array(
-	'sanitize_callback' => 'hovercraft_sanitize_image',
-    )
-);
-    
-    $wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize, 'hovercraft_logo_alternative', array( 
-        'label' => 'Full Hero Logo',
+    $wp_customize->add_setting( 'hovercraft_logo_alternative', array(
+        'sanitize_callback' => 'hovercraft_sanitize_image',
+    ) );
+
+    $wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize, 'hovercraft_logo_alternative', array(
+        'label' => 'Alternative Logo',
         'section' => 'title_tagline',
         'settings' => 'hovercraft_logo_alternative',
-        'priority' => 8 // display just below the custom-logo
-        ) ) );
+        'priority' => 8,
+    ) ) );
 }
 add_action( 'customize_register', 'hovercraft_logo_alternative' );
 
-// sanitize image function
+// sanitize uploaded image file
 function hovercraft_sanitize_image( $file, $setting ) {
-    //allowed file types
+    // allowed image mime types
     $mimes = array(
-        'jpg|jpeg|jpe' => 'image/jpeg',
-		'gif'          => 'image/gif',
-		'png'          => 'image/png',
-		'bmp'          => 'image/bmp',
-		'tif|tiff'     => 'image/tiff',
-		'ico'          => 'image/x-icon',
-        );
-              
-    //check file type from file name
+        'jpg|jpeg' => 'image/jpeg',
+        'png' => 'image/png',
+        'gif' => 'image/gif',
+    );
+
+    // check mime type of uploaded file
     $file_ext = wp_check_filetype( $file, $mimes );
-              
-    //if file has a valid mime type return it, otherwise return default
+
+    // return file if valid or fallback to default
     return ( $file_ext['ext'] ? $file : $setting->default );
 }
+
+// Ref: ChatGPT
