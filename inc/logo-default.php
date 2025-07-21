d5fa76598989219d24b5784cdff2b55753b2961e
Jesse Nickles <jesse@littlebizzy.com>
2025-07-21 17:28:30 +0700

    Update logo-default.php

--- inc/logo-default.php
+++ inc/logo-default.php
@@ -1,14 +1,15 @@
 <?php
 
-/** default logo */
-
+// add default logo support
 function hovercraft_logo() {
     $defaults = array(
         'height' => 100,
         'width' => 250,
         'flex-height' => true,
-        'flex-width' => true
-        );
+        'flex-width' => true,
+    );
     add_theme_support( 'custom-logo', $defaults );
 }
 add_action( 'after_setup_theme', 'hovercraft_logo' );
+
+// Ref: ChatGPT
