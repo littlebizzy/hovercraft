95263d5dbfd1019ec61500d2232b24326cda975f
Jesse Nickles <jesse@littlebizzy.com>
2025-07-22 16:27:26 +0700

    Update branding.php

--- template-parts/header/branding.php
+++ template-parts/header/branding.php
@@ -1,25 +1,29 @@
 <div id="branding">
 
 <?php
-// get logos and location setting
+// get logo urls and settings
 $default_logo = wp_get_attachment_url( get_theme_mod( 'custom_logo' ) );
 $alternative_logo = get_theme_mod( 'hovercraft_logo_alternative' );
-$location = get_theme_mod( 'hovercraft_logo_alternative_location', 'none' );
+$logo_location = get_theme_mod( 'hovercraft_logo_alternative_location', 'none' );
 $template_slug = get_page_template_slug();
 
-// choose which logo to show
+// default to default logo
 $logo_url = $default_logo;
 
-if ( $alternative_logo && $location !== 'none' ) {
-    $map = array(
+if ( $alternative_logo && $logo_location !== 'none' ) {
+    $location_map = array(
         'full_hero_only' => array( 'full-hero' ),
+        'half_hero_only' => array( 'half-hero' ),
+        'mini_hero_only' => array( 'mini-hero' ),
         'full_and_half_hero' => array( 'full-hero', 'half-hero' ),
+        'full_and_mini_hero' => array( 'full-hero', 'mini-hero' ),
+        'half_and_mini_hero' => array( 'half-hero', 'mini-hero' ),
         'full_and_half_and_mini_hero' => array( 'full-hero', 'half-hero', 'mini-hero' ),
     );
 
-    if ( isset( $map[ $location ] ) ) {
-        foreach ( $map[ $location ] as $slug ) {
-            if ( strpos( $template_slug, $slug ) !== false ) {
+    if ( isset( $location_map[ $logo_location ] ) ) {
+        foreach ( $location_map[ $logo_location ] as $match ) {
+            if ( strpos( $template_slug, $match ) !== false ) {
                 $logo_url = $alternative_logo;
                 break;
             }
@@ -27,7 +31,7 @@ if ( $alternative_logo && $location !== 'none' ) {
     }
 }
 
-// bail if no logo set
+// bail if no logo
 if ( ! $logo_url ) {
     return;
 }
@@ -35,15 +39,11 @@ if ( ! $logo_url ) {
 
 <div class="branding-media">
     <a href="<?php echo esc_url( home_url() ); ?>" class="custom-logo-link" rel="home">
-        <img
-            src="<?php echo esc_url( $logo_url ); ?>"
-            class="site-logo custom-logo"
-            alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?> logo"
-        />
+        <img class="site-logo custom-logo" src="<?php echo esc_url( $logo_url ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?> logo" />
     </a>
 </div><!-- branding-media -->
 
-<?php if ( display_header_text() ): ?>
+<?php if ( display_header_text() ) : ?>
 <div class="branding-text">
     <a href="<?php echo esc_url( home_url() ); ?>" class="site-title-link" rel="home">
         <div class="site-title"><?php echo get_bloginfo( 'name' ); ?></div>
