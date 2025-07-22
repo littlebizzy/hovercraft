07a08caca5a4f75270b6b37893b09be650cf87c2
Jesse Nickles <jesse@littlebizzy.com>
2025-07-22 17:38:10 +0700

    Update branding.php

--- template-parts/header/branding.php
+++ template-parts/header/branding.php
@@ -2,35 +2,35 @@
 
 <?php
 // get logo urls and settings
-$default_logo = wp_get_attachment_url( get_theme_mod( 'custom_logo' ) );
-$alternative_logo = get_theme_mod( 'hovercraft_logo_alternative' );
-$logo_location = get_theme_mod( 'hovercraft_logo_alternative_location', 'none' );
-$template_slug = get_page_template_slug();
+$default_logo      = wp_get_attachment_url( get_theme_mod( 'custom_logo' ) );
+$alternative_logo  = get_theme_mod( 'hovercraft_logo_alternative' );
+$logo_location     = get_theme_mod( 'hovercraft_logo_alternative_location', 'none' );
+$template_slug     = get_page_template_slug();
 
 // default to default logo
 $logo_url = $default_logo;
 
 if ( $alternative_logo && $logo_location !== 'none' ) {
-    $location_map = array(
-        'full_only' => array( 'full-hero' ),
-        'half_only' => array( 'half-hero' ),
-        'mini_only' => array( 'mini-hero' ),
-        'basic_only' => array( 'basic-' ),
-        'full_and_half' => array( 'full-hero', 'half-hero' ),
-        'full_and_mini' => array( 'full-hero', 'mini-hero' ),
-        'full_and_basic' => array( 'full-hero', 'basic-' ),
-        'half_and_mini' => array( 'half-hero', 'mini-hero' ),
-        'half_and_basic' => array( 'half-hero', 'basic-' ),
-        'mini_and_basic' => array( 'mini-hero', 'basic-' ),
-        'full_half_mini' => array( 'full-hero', 'half-hero', 'mini-hero' ),
-        'full_half_basic' => array( 'full-hero', 'half-hero', 'basic-' ),
-        'full_mini_basic' => array( 'full-hero', 'mini-hero', 'basic-' ),
-        'half_mini_basic' => array( 'half-hero', 'mini-hero', 'basic-' ),
-        'full_half_mini_basic' => array( 'full-hero', 'half-hero', 'mini-hero', 'basic-' ),
+    $logo_locations = array(
+        'full_hero_only' => array( 'full-hero' ),
+        'half_hero_only' => array( 'half-hero' ),
+        'mini_hero_only' => array( 'mini-hero' ),
+        'basic_header_only' => array( 'basic-' ),
+        'full_and_half_hero' => array( 'full-hero', 'half-hero' ),
+        'full_and_mini_hero' => array( 'full-hero', 'mini-hero' ),
+        'full_hero_and_basic_header' => array( 'full-hero', 'basic-' ),
+        'half_and_mini_hero' => array( 'half-hero', 'mini-hero' ),
+        'half_hero_and_basic_header' => array( 'half-hero', 'basic-' ),
+        'mini_hero_and_basic_header' => array( 'mini-hero', 'basic-' ),
+        'full_and_half_and_mini_hero' => array( 'full-hero', 'half-hero', 'mini-hero' ),
+        'full_and_half_hero_and_basic_header' => array( 'full-hero', 'half-hero', 'basic-' ),
+        'full_and_mini_hero_and_basic_header' => array( 'full-hero', 'mini-hero', 'basic-' ),
+        'half_and_mini_hero_and_basic_header' => array( 'half-hero', 'mini-hero', 'basic-' ),
+        'full_and_half_and_mini_hero_and_basic_header' => array( 'full-hero', 'half-hero', 'mini-hero', 'basic-' ),
     );
 
-    if ( isset( $location_map[ $logo_location ] ) ) {
-        foreach ( $location_map[ $logo_location ] as $match ) {
+    if ( isset( $logo_locations[ $logo_location ] ) ) {
+        foreach ( $logo_locations[ $logo_location ] as $match ) {
             if ( strpos( $template_slug, $match ) !== false ) {
                 $logo_url = $alternative_logo;
                 break;
