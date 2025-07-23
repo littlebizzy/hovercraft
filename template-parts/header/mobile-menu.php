76d4adf86ffefde45ef707eecae39bb7242229be
Jesse Nickles <jesse@littlebizzy.com>
2025-07-23 16:35:12 +0700

    Update mobile-menu.php

--- template-parts/header/mobile-menu.php
+++ template-parts/header/mobile-menu.php
@@ -1,52 +1,61 @@
 <div id="myNav" class="overlay">
-	
-  	<!-- Button to close the overlay navigation -->
-  	<a href="javascript:void(0)" class="closebtn" onclick="closeNav()"><?php $hovercraft_layout_icons = get_theme_mod( 'hovercraft_layout_icons', 'material_icons_classic' ); if ( $hovercraft_layout_icons == 'material_icons_classic' ) { ?><i class="material-icons close">close</i><?php } 
-				elseif ( $hovercraft_layout_icons == 'font_awesome_version_6' ) { ?><i class="fa-solid fa-x"></i><?php } ?></a>
-	
-  	<!-- Overlay content -->
-  	<div class="overlay-content">
-    	<?php 
-			if ( has_nav_menu( 'mobile-menu' ) ) {
-				wp_nav_menu(array(
-                'theme_location' => 'mobile-menu', 
-				'menu_class' => 'menu', 
-        		'container_class' => 'mobile-menu'
-                ));
-			} elseif ( has_nav_menu( 'main-menu' ) ) {
-            	wp_nav_menu(array(
-                'theme_location' => 'main-menu', 
-				'menu_class' => 'menu', 
-        		'container_class' => 'main-menu'
-                ));
-        	} else {
-            echo '<div class="main-menu"><ul class="menu">';
-            wp_list_pages( array( 'title_li' => '' ) );
-            echo '</ul></div>';
-        	}
-        ?>
-  	</div>
+
+	<!-- button to close the overlay navigation -->
+	<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">
+		<?php 
+		$hovercraft_layout_icons = get_theme_mod( 'hovercraft_layout_icons', 'material_icons_classic' ); 
+		if ( $hovercraft_layout_icons == 'material_icons_classic' ) { ?>
+			<i class="material-icons close">close</i>
+		<?php } elseif ( $hovercraft_layout_icons == 'font_awesome_version_6' ) { ?>
+			<i class="fa-solid fa-x"></i>
+		<?php } ?>
+	</a>
+
+	<!-- overlay content -->
+	<div class="overlay-content">
+		<?php 
+		if ( has_nav_menu( 'mobile-menu' ) ) {
+			wp_nav_menu( array(
+				'theme_location'  => 'mobile-menu',
+				'menu_class'      => 'menu',
+				'container_class' => 'mobile-menu',
+			) );
+		} elseif ( has_nav_menu( 'main-menu' ) ) {
+			wp_nav_menu( array(
+				'theme_location'  => 'main-menu',
+				'menu_class'      => 'menu',
+				'container_class' => 'main-menu',
+			) );
+		} else {
+			echo '<div class="main-menu"><ul class="menu">';
+			wp_list_pages( array( 'title_li' => '' ) );
+			echo '</ul></div>';
+		}
+		?>
+	</div>
 </div><!-- myNav -->
 
-<div id="offcanvas"> 
+<div id="offcanvas">
 	<h4 class="sitename-offcanvas"><?php echo get_bloginfo( 'name' ); ?></h4>
-	<?php if ( has_nav_menu( 'mobile-menu' ) ) {
-		wp_nav_menu(array(
-        'theme_location' => 'mobile-menu', 
-		'menu_class' => 'menu', 
-        'container_class' => 'mobile-menu'
-        ));
+	<?php 
+	if ( has_nav_menu( 'mobile-menu' ) ) {
+		wp_nav_menu( array(
+			'theme_location'  => 'mobile-menu',
+			'menu_class'      => 'menu',
+			'container_class' => 'mobile-menu',
+		) );
 	} elseif ( has_nav_menu( 'main-menu' ) ) {
-        wp_nav_menu(array(
-        'theme_location' => 'main-menu', 
-		'menu_class' => 'menu', 
-        'container_class' => 'main-menu'
-        ));
-    } else {
-        echo '<div class="main-menu"><ul class="menu">';
-        wp_list_pages( array( 'title_li' => '' ) );
-        echo '</ul></div>';
-    } ?>
+		wp_nav_menu( array(
+			'theme_location'  => 'main-menu',
+			'menu_class'      => 'menu',
+			'container_class' => 'main-menu',
+		) );
+	} else {
+		echo '<div class="main-menu"><ul class="menu">';
+		wp_list_pages( array( 'title_li' => '' ) );
+		echo '</ul></div>';
+	}
+	?>
 </div>
 
 <div class="overlay-main"></div>
