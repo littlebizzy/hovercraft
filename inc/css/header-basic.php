39ce8d477ac15faa7b31eca8f7f05ce60caec322
Jesse Nickles <jesse@littlebizzy.com>
2025-07-31 16:07:00 +0700

    Update header-basic.php

--- inc/css/header-basic.php
+++ inc/css/header-basic.php
@@ -32,3 +32,18 @@
 #header-basic a {
 	color: <?php echo $header_basic_hero_link_color; ?>;
 }
+
+
+@media screen and (max-width: 1199px) {
+	#header-basic .main-menu ul li a {
+		text-decoration: none;
+		color: <?php echo $header_basic_hero_link_color; ?>;
+	}
+}
+
+@media screen and (min-width: 1200px) {
+	#header-basic .main-menu ul li a {
+		text-decoration: none;
+		color: <?php echo $header_basic_hero_link_color; ?>;
+	}
+}
