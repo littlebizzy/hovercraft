4c88c1e436a8e120f643558c4481b41035765a4f
Jesse Nickles <jesse@littlebizzy.com>
2025-07-31 16:03:18 +0700

    Update hero-mini.php

--- inc/css/hero-mini.php
+++ inc/css/hero-mini.php
@@ -10,7 +10,7 @@
 		?> <?php echo $hero_gradient_start_color_length; ?>%, <?php 
 		list($r2, $g2, $b2) = sscanf($hero_gradient_stop_color, "#%02x%02x%02x");
 		echo "rgba({$r2}, {$g2}, {$b2}, {$hero_gradient_stop_color_transparency})"; 
-		?> <?php echo $hero_gradient_stop_color_length; ?>%)<?php if (!empty( $hero_image )) echo ", url({$hero_image})" ?>;
+		?> <?php echo $hero_gradient_stop_color_length; ?>%)<?php if (!empty($hero_image)) echo ", url({$hero_image})" ?>;
 <?php } elseif ( $hero_gradient_tones == 'three_tones' ) { ?>
 	background: linear-gradient(<?php echo $hero_gradient_angle; ?>, <?php 
 		list($r1, $g1, $b1) = sscanf($hero_gradient_start_color, "#%02x%02x%02x");
@@ -21,9 +21,9 @@
 		?> <?php echo $hero_gradient_mid_color_length; ?>%, <?php 
 		list($r2, $g2, $b2) = sscanf($hero_gradient_stop_color, "#%02x%02x%02x");
 		echo "rgba({$r2}, {$g2}, {$b2}, {$hero_gradient_stop_color_transparency})"; 
-		?> <?php echo $hero_gradient_stop_color_length; ?>%)<?php if (!empty( $hero_image )) echo ", url({$hero_image})" ?>;
+		?> <?php echo $hero_gradient_stop_color_length; ?>%)<?php if (!empty($hero_image)) echo ", url({$hero_image})" ?>;
 <?php } ?>
-	background-position: <?php $minipos = str_replace('_',' ',$mini_hero_background_position); echo $minipos; ?>;
+	background-position: <?php $minipos = str_replace('_', ' ', $mini_hero_background_position); echo $minipos; ?>;
 	background-size: cover;
 	background-repeat: no-repeat;
 }
@@ -36,7 +36,7 @@
 		?> <?php echo $hero_gradient_start_color_length; ?>%, <?php 
 		list($r2, $g2, $b2) = sscanf($hero_gradient_stop_color, "#%02x%02x%02x");
 		echo "rgba({$r2}, {$g2}, {$b2}, {$hero_gradient_stop_color_transparency})"; 
-		?> <?php echo $hero_gradient_stop_color_length; ?>%)<?php if (!empty( $hero_image )) echo ", url({$hero_image})" ?>;
+		?> <?php echo $hero_gradient_stop_color_length; ?>%)<?php if (!empty($hero_image)) echo ", url({$hero_image})" ?>;
 <?php } elseif ( $hero_gradient_tones == 'three_tones' ) { ?>
 	background: linear-gradient(<?php echo $hero_gradient_angle; ?>, <?php 
 		list($r1, $g1, $b1) = sscanf($hero_gradient_start_color, "#%02x%02x%02x");
@@ -47,50 +47,12 @@
 		?> <?php echo $hero_gradient_mid_color_length; ?>%, <?php 
 		list($r2, $g2, $b2) = sscanf($hero_gradient_stop_color, "#%02x%02x%02x");
 		echo "rgba({$r2}, {$g2}, {$b2}, {$hero_gradient_stop_color_transparency})"; 
-		?> <?php echo $hero_gradient_stop_color_length; ?>%)<?php if (!empty( $hero_image )) echo ", url({$hero_image})" ?>;
+		?> <?php echo $hero_gradient_stop_color_length; ?>%)<?php if (!empty($hero_image)) echo ", url({$hero_image})" ?>;
 <?php } ?>
-	background-position: <?php $minipos = str_replace('_',' ',$mini_hero_background_position); echo $minipos; ?>;
+	background-position: <?php $minipos = str_replace('_', ' ', $mini_hero_background_position); echo $minipos; ?>;
 	background-size: cover;
 	background-repeat: no-repeat;
 	position: relative;
-}
-
-.hero-main-mini .search-input {
-	background: #ffffff !important;
-	box-shadow: inset 0px 0px 0px 1px #ffffff !important;
-}
-
-.mini-hero-caption {
-	position: absolute;
-	bottom: 0;
-	right: 0;
-	background: rgba(0, 0, 0, 0.6);
-	color: #ffffff;
-	padding: 6px 10px;
-	font-size: 14px;
-	z-index: 10;
-	max-width: 90%;
-	text-align: right;
-}
-
-#header-mini-hero .menu-mobile-wrapper i {
-	margin-left: 20px;
-	font-size: 36px;
-	color: <?php echo $mini_hero_header_text_color; ?>;
-}
-
-#header-mini-hero a {
-	color: <?php echo $mini_hero_header_link_color; ?>;
-}
-
-#header-mini-hero .main-menu ul li a {
-	text-decoration: none;
-	color: <?php echo $mini_hero_header_link_color; ?>;
-}
-
-#header-basic .main-menu ul li a {
-	text-decoration: none;
-	color: <?php echo $header_basic_hero_link_color; ?>;
 }
 
 @media screen and (max-width: 1199px) {
@@ -155,5 +117,105 @@
 		font-weight: 700;
 		margin-bottom: 0;
 		color: #ffffff;
+	}
+}
+
+@media screen and (max-width: 1199px) {
+	.hero-main-mini .search-input {
+		background: #ffffff !important;
+		box-shadow: inset 0px 0px 0px 1px #ffffff !important;
+	}
+}
+
+@media screen and (min-width: 1200px) {
+	.hero-main-mini .search-input {
+		background: #ffffff !important;
+		box-shadow: inset 0px 0px 0px 1px #ffffff !important;
+	}
+}
+
+@media screen and (max-width: 1199px) {
+	.mini-hero-caption {
+		position: absolute;
+		bottom: 0;
+		right: 0;
+		background: rgba(0, 0, 0, 0.6);
+		color: #ffffff;
+		padding: 6px 10px;
+		font-size: 14px;
+		z-index: 10;
+		max-width: 90%;
+		text-align: right;
+	}
+}
+
+@media screen and (min-width: 1200px) {
+	.mini-hero-caption {
+		position: absolute;
+		bottom: 0;
+		right: 0;
+		background: rgba(0, 0, 0, 0.6);
+		color: #ffffff;
+		padding: 6px 10px;
+		font-size: 14px;
+		z-index: 10;
+		max-width: 90%;
+		text-align: right;
+	}
+}
+
+@media screen and (max-width: 1199px) {
+	#header-mini-hero .menu-mobile-wrapper i {
+		margin-left: 20px;
+		font-size: 36px;
+		color: <?php echo $mini_hero_header_text_color; ?>;
+	}
+}
+
+@media screen and (min-width: 1200px) {
+	#header-mini-hero .menu-mobile-wrapper i {
+		margin-left: 20px;
+		font-size: 36px;
+		color: <?php echo $mini_hero_header_text_color; ?>;
+	}
+}
+
+@media screen and (max-width: 1199px) {
+	#header-mini-hero a {
+		color: <?php echo $mini_hero_header_link_color; ?>;
+	}
+}
+
+@media screen and (min-width: 1200px) {
+	#header-mini-hero a {
+		color: <?php echo $mini_hero_header_link_color; ?>;
+	}
+}
+
+@media screen and (max-width: 1199px) {
+	#header-mini-hero .main-menu ul li a {
+		text-decoration: none;
+		color: <?php echo $mini_hero_header_link_color; ?>;
+	}
+}
+
+@media screen and (min-width: 1200px) {
+	#header-mini-hero .main-menu ul li a {
+		text-decoration: none;
+		color: <?php echo $mini_hero_header_link_color; ?>;
+	}
+}
+
+@media screen and (max-width: 1199px) {
+	#header-basic .main-menu ul li a {
+		text-decoration: none;
+		color: <?php echo $header_basic_hero_link_color; ?>;
+	}
+}
+
+@media screen and (min-width: 1200px) {
+	#header-basic .main-menu ul li a {
+		text-decoration: none;
+		color: <?php echo $header_basic_hero_link_color; ?>;
 	}
 }
