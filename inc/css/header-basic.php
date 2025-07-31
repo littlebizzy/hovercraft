da5c978f6dbfcd320796b3e4fbe2c6db8a83b330
Jesse Nickles <jesse@littlebizzy.com>
2025-07-31 16:11:04 +0700

    Update header-basic.php

--- inc/css/header-basic.php
+++ inc/css/header-basic.php
@@ -2,38 +2,53 @@
 // styles for basic header
 ?>
 
-@media screen and (max-width: 1200px) {
-#header-basic {
-	width: 100%;
-	display: table;
-	padding: 10px 20px;
-	background: <?php echo $header_basic_hero_background_color; ?>;
-	color: <?php echo $header_basic_hero_text_color; ?>;
+/* mobile: header container */
+@media screen and (max-width: 1199px) {
+	#header-basic {
+		width: 100%;
+		display: table;
+		padding: 10px 20px;
+		background: <?php echo $header_basic_hero_background_color; ?>;
+		color: <?php echo $header_basic_hero_text_color; ?>;
 	}
 }
 
+/* desktop: header container */
 @media screen and (min-width: 1200px) {
-#header-basic {
-	width: 100%;
-	display: table;
-	margin: 0px auto;
-	padding: 20px 0px;
-	background: <?php echo $header_basic_hero_background_color; ?>;
-	color: <?php echo $header_basic_hero_text_color; ?>;
+	#header-basic {
+		width: 100%;
+		display: table;
+		margin: 0px auto;
+		padding: 20px 0px;
+		background: <?php echo $header_basic_hero_background_color; ?>;
+		color: <?php echo $header_basic_hero_text_color; ?>;
 	}
 }
 
-#header-basic .menu-mobile-wrapper i {
-	margin-left: 20px;
-	font-size: 36px;
-	color: <?php echo $header_basic_hero_text_color; ?>;
+/* mobile: mobile icon */
+@media screen and (max-width: 1199px) {
+	#header-basic .menu-mobile-wrapper i {
+		margin-left: 20px;
+		font-size: 36px;
+		color: <?php echo $header_basic_hero_text_color; ?>;
+	}
 }
 
-#header-basic a {
-	color: <?php echo $header_basic_hero_link_color; ?>;
+/* mobile: general anchor color */
+@media screen and (max-width: 1199px) {
+	#header-basic a {
+		color: <?php echo $header_basic_hero_link_color; ?>;
+	}
 }
 
+/* desktop: general anchor color */
+@media screen and (min-width: 1200px) {
+	#header-basic a {
+		color: <?php echo $header_basic_hero_link_color; ?>;
+	}
+}
 
+/* mobile: menu link style */
 @media screen and (max-width: 1199px) {
 	#header-basic .main-menu ul li a {
 		text-decoration: none;
@@ -41,6 +56,7 @@
 	}
 }
 
+/* desktop: menu link style */
 @media screen and (min-width: 1200px) {
 	#header-basic .main-menu ul li a {
 		text-decoration: none;
