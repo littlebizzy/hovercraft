fe5d3cb112d9c3968e32b0f6f198764c9cdcf631
Jesse Nickles <jesse@littlebizzy.com>
2025-07-31 23:28:53 +0700

    Update cart-icon.php

--- inc/css/cart-icon.php
+++ inc/css/cart-icon.php
@@ -2,29 +2,30 @@
 // styles for cart icon
 ?>
 
-/* cart (desktop) */
-
+/* desktop: cart icon wrapper */
 .cart-icon-wrapper {
 	display: inline-block;
 	padding-left: 30px;
-	vertical-align: middle;
 	text-align: center;
+	vertical-align: middle;
 }
 
+/* desktop: cart icon */
 .cart-icon-wrapper i {
-	padding: 10px 0px;
-	text-decoration: none !important;
-	font-size: 24px;
 	position: relative;
+	font-size: 24px;
+	padding: 10px 0;
+	text-decoration: none !important;
 }
 
+/* desktop: red notification dot */
 .notification-dot {
-	position: relative;
+	position: absolute;
+	top: -8px;
+	right: -8px;
 	display: inline-block;
 	background-color: #f44336;
-  	border-radius: 50%;
-  	top: -15px;
-  	left: -1px;
+	border-radius: 50%;
 	line-height: 1;
 	font-size: 11px;
 	font-weight: 700;
@@ -32,45 +33,46 @@
 	padding: 2px 4px;
 }
 
-
-/* cart (mobile) */
-
+/* mobile: cart button wrapper */
 .cart-button-offcanvas-wrapper {
 	width: 100%;
 	padding: 0 20px;
 	margin-bottom: 20px;
 }
 
+/* mobile: cart button */
 .cart-button-offcanvas {
 	width: 100%;
+	display: inline-block;
 	text-align: center;
 	font-size: 18px;
 	font-weight: 700;
-	display: inline-block;
 	padding: 10px 0;
-	background: transparent;
-	text-decoration: none;
 	color: #ffffff;
-	border-radius: 0px;
-	box-shadow: inset 0px 0px 0px 1px #ffffff;
+	background: transparent;
+	border-radius: 0;
+	box-shadow: inset 0 0 0 1px #ffffff;
+	text-decoration: none;
 	vertical-align: middle;
 }
 
+/* mobile: cart icon inside button */
 .cart-button-offcanvas i {
 	padding-right: 10px !important;
 	text-decoration: none !important;
 }
 
+/* mobile: red notification dot */
 .notification-dot-offcanvas {
-	position: relative;
+	position: absolute;
+	top: -5px;
+	right: -5px;
 	display: inline-block;
 	background-color: #f44336;
-  	border-radius: 50%;
+	border-radius: 50%;
 	line-height: 1;
 	font-size: 11px;
 	font-weight: 700;
 	color: #ffffff;
 	padding: 2px 4px;
-	left: 5px;
-	top: -5px;
 }
