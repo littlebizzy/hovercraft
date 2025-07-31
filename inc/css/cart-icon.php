0128582409dabda8cac685c940d83c8205c8d7ea
Jesse Nickles <jesse@littlebizzy.com>
2025-07-31 23:27:49 +0700

    Create cart-icon.php

---
+++ inc/css/cart-icon.php
@@ -0,0 +1,76 @@
+<?php
+// styles for cart icon
+?>
+
+/* cart (desktop) */
+
+.cart-icon-wrapper {
+	display: inline-block;
+	padding-left: 30px;
+	vertical-align: middle;
+	text-align: center;
+}
+
+.cart-icon-wrapper i {
+	padding: 10px 0px;
+	text-decoration: none !important;
+	font-size: 24px;
+	position: relative;
+}
+
+.notification-dot {
+	position: relative;
+	display: inline-block;
+	background-color: #f44336;
+  	border-radius: 50%;
+  	top: -15px;
+  	left: -1px;
+	line-height: 1;
+	font-size: 11px;
+	font-weight: 700;
+	color: #ffffff;
+	padding: 2px 4px;
+}
+
+
+/* cart (mobile) */
+
+.cart-button-offcanvas-wrapper {
+	width: 100%;
+	padding: 0 20px;
+	margin-bottom: 20px;
+}
+
+.cart-button-offcanvas {
+	width: 100%;
+	text-align: center;
+	font-size: 18px;
+	font-weight: 700;
+	display: inline-block;
+	padding: 10px 0;
+	background: transparent;
+	text-decoration: none;
+	color: #ffffff;
+	border-radius: 0px;
+	box-shadow: inset 0px 0px 0px 1px #ffffff;
+	vertical-align: middle;
+}
+
+.cart-button-offcanvas i {
+	padding-right: 10px !important;
+	text-decoration: none !important;
+}
+
+.notification-dot-offcanvas {
+	position: relative;
+	display: inline-block;
+	background-color: #f44336;
+  	border-radius: 50%;
+	line-height: 1;
+	font-size: 11px;
+	font-weight: 700;
+	color: #ffffff;
+	padding: 2px 4px;
+	left: 5px;
+	top: -5px;
+}
