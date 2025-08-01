dea871ae4664356a41db008d7a24d1f1d0e649ca
Jesse Nickles <jesse@littlebizzy.com>
2025-08-01 14:50:59 +0700

    Create images-general.php

---
+++ inc/css/images-general.php
@@ -0,0 +1,21 @@
+<?php
+// styles for images (general)
+?>
+
+/* images */
+	
+.alignright { 
+	float: right; 
+	margin: 0 0 20px 20px; 
+}
+
+.alignleft { 
+	float: left; 
+	margin: 0 20px 20px 0; 
+}
+
+.aligncenter { 
+	display: block; 
+	margin-left: auto; 
+	margin-right: auto; 
+}
