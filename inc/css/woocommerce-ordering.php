37e4359d46cdd976b9ff4aeb3aed9294da5ecfe8
Jesse Nickles <jesse@littlebizzy.com>
2025-08-15 12:48:40 +0700

    Update woocommerce-ordering.php

--- inc/css/woocommerce-ordering.php
+++ inc/css/woocommerce-ordering.php
@@ -2,32 +2,6 @@
 // styles for woocommerce ordering
 ?>
 
-form.woocommerce-ordering {
-	margin: 0 !important;
-	padding: 0 !important;
-	position: relative;
-	background-color: #eeeeee;
-}
-
-form.woocommerce-ordering select {
-	appearance: none;
-	max-width: 100%;
-	vertical-align: middle !important;
-	outline: none;
-	border: none;
-	box-shadow: none;
-	line-height: 1.5 !important;
-	font-size: 16px;
-	color: #757575;
-	padding: 5px 40px 5px 10px !important;
-	background-color: transparent;
-}
-
-form.woocommerce-ordering select:active, form.woocommerce-ordering select:focus {
-	outline: none;
-	box-shadow: none;
-}
-
 @media screen and (max-width: 1200px) {
 	form.woocommerce-ordering {
 		float: none !important;
@@ -40,6 +14,24 @@ form.woocommerce-ordering select:active, form.woocommerce-ordering select:focus 
 		float: right !important;
 		width: auto;
 	} 
+}
+
+form.woocommerce-ordering select {
+	vertical-align: middle !important;
+	outline: none !important;
+	border: none !important;
+	line-height: 1.5 !important;
+	font-size: 16px;
+	color: #757575;
+	padding: 5px 40px 5px 10px !important;
+	background-color: transparent;
+	box-shadow: inset 0 0 0 1px #cccccc !important;
+}
+
+form.woocommerce-ordering select:active, form.woocommerce-ordering select:focus {
+	outline: none !important;
+	border: none !important;
+	box-shadow: inset 0 0 0 1px #cccccc !important;
 }
 
 /*
