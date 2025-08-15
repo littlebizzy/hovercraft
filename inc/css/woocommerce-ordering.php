4ca900ff4bd2402533f8e76ac96098ef6470f85f
Jesse Nickles <jesse@littlebizzy.com>
2025-08-15 13:38:10 +0700

    Update woocommerce-ordering.php

--- inc/css/woocommerce-ordering.php
+++ inc/css/woocommerce-ordering.php
@@ -17,24 +17,20 @@
 }
 
 form.woocommerce-ordering select {
-	vertical-align: middle !important;
-	outline: none !important;
-	border: none !important;
 	line-height: 1.5 !important;
-	font-size: 16px;
+	font-size: 16px !important;
 	color: #757575;
 	padding: 5px 40px 5px 10px !important;
-	background-color: transparent;
+	background: transparent !important;
 	box-shadow: inset 0 0 0 1px #cccccc !important;
 }
 
 form.woocommerce-ordering select:active, form.woocommerce-ordering select:focus {
-	outline: none !important;
-	border: none !important;
 	box-shadow: inset 0 0 0 1px #cccccc !important;
 }
 
 /*
-https://codepen.io/jpdanks/pen/KKBarq 
-https://codepen.io/rabakilgur/pen/zyggOe
+Ref: https://github.com/littlebizzy/hovercraft/blob/master/inc/css/forms.php
+Ref: https://codepen.io/jpdanks/pen/KKBarq 
+Ref: https://codepen.io/rabakilgur/pen/zyggOe
 */
