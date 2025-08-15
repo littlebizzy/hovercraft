c75e2a447811cb9bc1a833edecbb9a78fc8955b8
Jesse Nickles <jesse@littlebizzy.com>
2025-08-15 15:01:05 +0700

    Create header-general.php

---
+++ inc/css/header-general.php
@@ -0,0 +1,19 @@
+<?php
+// styles for general header stuff
+?>
+
+.header-left {
+	display: table-cell;
+    vertical-align: middle;
+	height: inherit;
+	min-width: auto; /* https://developer.mozilla.org/en-US/docs/Web/CSS/min-width */
+	white-space: nowrap;
+}
+
+.header-right {
+	display: table-cell;
+    vertical-align: middle;
+	height: inherit;
+	text-align: right;
+	width: 100%;
+}
