a307ea4400a6a6d11ef49c13f9712de5c7282699
Jesse Nickles <jesse@littlebizzy.com>
2025-09-15 20:35:54 +0700

    Update copyright.php

--- inc/css/copyright.php
+++ inc/css/copyright.php
@@ -2,76 +2,88 @@
 // styles for copyright
 ?>
 
-@media screen and (max-width: 1200px) {
+/* base */
 #copyright {
 	width: 100%;
-	padding: 20px 20px;
-	font-size: <?php echo $copyright_mobile_font_size; ?>px;
-	line-height: 1;
-	background: <?php echo $copyright_background_color; ?>;
-	color: <?php echo $copyright_text_color; ?>;
+}
+
+/* mobile ≤1199px */
+@media screen and (max-width: 1199px) {
+
+	#copyright {
+		width: 100%;
+		padding: 20px 20px;
+		font-size: <?php echo $copyright_mobile_font_size; ?>px;
+		line-height: 1;
+		background: <?php echo $copyright_background_color; ?>;
+		color: <?php echo $copyright_text_color; ?>;
+	}
+
+	#copyright .inner {
+		max-width: none;
+	}
+
+	.copyright-left {
+		width: 100%;
+		display: block;
+		vertical-align: top;
+		text-align: center;
+	}
+
+	.copyright-right {
+		width: 100%;
+		margin-top: 30px;
+		display: block;
+		vertical-align: top;
+		text-align: center;
 	}
 }
 
+/* desktop ≥1200px */
 @media screen and (min-width: 1200px) {
-#copyright {
-	display: table;
-	width: 100%;
-	padding: 20px 0px;
-	font-size: <?php echo $copyright_desktop_font_size; ?>px;
-	line-height: 1;
-	background: <?php echo $copyright_background_color; ?>;
-	color: <?php echo $copyright_text_color; ?>;
+
+	#copyright {
+		display: table;
+		width: 100%;
+		padding: 20px 0;
+		font-size: <?php echo $copyright_desktop_font_size; ?>px;
+		line-height: 1;
+		background: <?php echo $copyright_background_color; ?>;
+		color: <?php echo $copyright_text_color; ?>;
+	}
+
+	#copyright.is-fixed .inner {
+		max-width: 1200px;
+	}
+
+	#copyright.is-full .inner {
+		max-width: none;
+	}
+
+	.copyright-left {
+		display: table-cell;
+		vertical-align: middle;
+		height: inherit;
+		min-width: auto;
+		white-space: nowrap;
+	}
+
+	.copyright-right {
+		width: 100%;
+		display: table-cell;
+		vertical-align: middle;
+		height: inherit;
+		text-align: right;
 	}
 }
 
+/* links */
 #copyright a {
 	color: <?php echo $copyright_link_color; ?>;
 	text-decoration: <?php echo $copyright_link_decoration; ?>;
 }
 
-@media screen and (max-width: 1200px) {
-.copyright-left {
-	width: 100%;
-	display: block;
-    vertical-align: top;
-	text-align: center;
-	}
-}
-
-@media screen and (min-width: 1200px) {
-.copyright-left {
-	display: table-cell;
-    vertical-align: middle;
-	height: inherit;
-	min-width: auto;
-	white-space: nowrap;
-	}
-}
-
-@media screen and (max-width: 1200px) {
-.copyright-right {
-	width: 100%;
-	margin-top: 30px;
-	display: block;
-    vertical-align: top;
-	text-align: center;
-	}
-}
-
-@media screen and (min-width: 1200px) {
-.copyright-right {
-	width: 100%;
-	display: table-cell;
-    vertical-align: middle;
-	height: inherit;
-	text-align: right;
-	}
-}
-
-
-/* copyright icons */
-
+/* icons */
 #copyright i {
 	font-size: 20px;
 	margin-left: 20px !important;
@@ -81,9 +93,7 @@
 	margin-left: 0;
 }
 
-
-/* copyright ul */
-
+/* lists */
 #copyright ul {
 	margin: 0;
 	list-style-type: none;
