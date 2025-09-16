dadac10da905c197583e2000d90edc53b163e094
Jesse Nickles <jesse@littlebizzy.com>
2025-09-16 17:53:23 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,60 +1,62 @@
 <div class="clear"></div>
 
 <?php if ( is_active_sidebar( 'hovercraft_home_postmain_top' ) && is_front_page() ) { ?>
-<div id="home-postmain-top">
-<div class="inner">
-	<?php dynamic_sidebar( 'hovercraft_home_postmain_top' ); ?>
-	<div class="clear"></div>
-</div><!--inner -->
-</div><!-- home-postmain-top -->
+	<div id="home-postmain-top">
+		<div class="inner">
+			<?php dynamic_sidebar( 'hovercraft_home_postmain_top' ); ?>
+			<div class="clear"></div>
+		</div><!--inner -->
+	</div><!-- home-postmain-top -->
 <?php } ?>
 
 <?php if ( is_active_sidebar( 'hovercraft_home_postmain_bottom' ) && is_front_page() ) { ?>
-<div id="home-postmain-bottom">
-<div class="inner">
-	<?php dynamic_sidebar( 'hovercraft_home_postmain_bottom' ); ?>
-	<div class="clear"></div>
-</div><!--inner -->
-</div><!-- home-postmain-bottom -->
+	<div id="home-postmain-bottom">
+		<div class="inner">
+			<?php dynamic_sidebar( 'hovercraft_home_postmain_bottom' ); ?>
+			<div class="clear"></div>
+		</div><!--inner -->
+	</div><!-- home-postmain-bottom -->
 <?php } ?>
 
 <?php if ( is_active_sidebar( 'hovercraft_prefooter_top' ) ) { ?>
-<div id="prefooter-top">
-<div class="inner">
-	<?php dynamic_sidebar( 'hovercraft_prefooter_top' ); ?>
-	<div class="clear"></div>
-</div><!--inner -->
-</div><!-- prefooter-top -->
+	<div id="prefooter-top">
+		<div class="inner">
+			<?php dynamic_sidebar( 'hovercraft_prefooter_top' ); ?>
+			<div class="clear"></div>
+		</div><!--inner -->
+	</div><!-- prefooter-top -->
 <?php } ?>
 
 <?php if ( is_active_sidebar( 'hovercraft_prefooter_bottom' ) ) { ?>
-<div id="prefooter-bottom">
-<div class="inner">
-	<?php dynamic_sidebar( 'hovercraft_prefooter_bottom' ); ?>
-	<div class="clear"></div>
-</div><!--inner -->
-</div><!-- prefooter-bottom -->
+	<div id="prefooter-bottom">
+		<div class="inner">
+			<?php dynamic_sidebar( 'hovercraft_prefooter_bottom' ); ?>
+			<div class="clear"></div>
+		</div><!--inner -->
+	</div><!-- prefooter-bottom -->
 <?php } ?>
 
 <?php if ( is_active_sidebar( 'hovercraft_footer_one' ) || is_active_sidebar( 'hovercraft_footer_two' ) || is_active_sidebar( 'hovercraft_footer_three' ) ) { ?>
-<div id="footer">
-<div class="inner">
-	
-	<?php if ( get_theme_mod( 'hovercraft_footer_columns' ) == 'four_equal') { 
-		get_template_part( 'template-parts/footer/footer-four-columns-equal' );
-	} elseif ( get_theme_mod( 'hovercraft_footer_columns' ) == 'four_weighted') {
-		get_template_part( 'template-parts/footer/footer-four-columns-unequal' );
-	} elseif ( get_theme_mod( 'hovercraft_footer_columns' ) == 'three_equal') {
-		get_template_part( 'template-parts/footer/footer-three-columns-equal' );
-	} elseif ( get_theme_mod( 'hovercraft_footer_columns' ) == 'three_weighted') {
-		get_template_part( 'template-parts/footer/footer-three-columns-unequal' );
-	} else {
-		get_template_part( 'template-parts/footer/footer-four-columns-unequal' );
-	} ?>
-	
-	<div class="clear"></div>
-</div><!--inner -->
-</div><!-- footer -->
+	<div id="footer">
+		<div class="inner">
+
+			<?php
+			if ( get_theme_mod( 'hovercraft_footer_columns' ) === 'four_equal' ) {
+				get_template_part( 'template-parts/footer/footer-four-columns-equal' );
+			} elseif ( get_theme_mod( 'hovercraft_footer_columns' ) === 'four_weighted' ) {
+				get_template_part( 'template-parts/footer/footer-four-columns-unequal' );
+			} elseif ( get_theme_mod( 'hovercraft_footer_columns' ) === 'three_equal' ) {
+				get_template_part( 'template-parts/footer/footer-three-columns-equal' );
+			} elseif ( get_theme_mod( 'hovercraft_footer_columns' ) === 'three_weighted' ) {
+				get_template_part( 'template-parts/footer/footer-three-columns-unequal' );
+			} else {
+				get_template_part( 'template-parts/footer/footer-four-columns-unequal' );
+			}
+			?>
+
+			<div class="clear"></div>
+		</div><!--inner -->
+	</div><!-- footer -->
 <?php } ?>
 
 <?php
