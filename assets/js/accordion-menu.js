4066fe62aebc4d3d334262db21a3ea9a16ee8cb3
Jesse Nickles <jesse@littlebizzy.com>
2025-10-13 00:52:29 +0700

    Update accordion-menu.js

--- assets/js/accordion-menu.js
+++ assets/js/accordion-menu.js
@@ -1,24 +1,25 @@
-jQuery(document).ready(function($){
+// accordion menu
+jQuery( function( $ ) {
 
-accordionNav = $(function(){
-	$('.menu-toggle').click(function(e){
-		e.preventDefault();
-		var toggleButton = $(this);
-		if (toggleButton.next().hasClass('active')) {
-			toggleButton.next().removeClass('active');
-			toggleButton.next().slideUp(400);
-			toggleButton.removeClass('rotate');
-		 } else {
-			toggleButton.parent().parent().find('li .sub-menu').removeClass('active');
-			toggleButton.parent().parent().find('li .sub-menu').slideUp(400);
-			toggleButton.parent().parent().find('.menu-toggle').removeClass('rotate');
-			toggleButton.next().toggleClass('active');
-			toggleButton.next().slideToggle(400);
-			toggleButton.toggleClass('rotate');
-		}
-	});
-});
-	
-});
+	// initialize accordion navigation
+	var accordionNav = $( function() {
+		$( '.menu-toggle' ).on( 'click', function( e ) {
+			e.preventDefault();
+			var toggleButton = $( this );
 
-// https://codepen.io/keithchis/pen/GpdbLm
+			if ( toggleButton.next().hasClass( 'active' ) ) {
+				toggleButton.next().removeClass( 'active' ).slideUp( 400 );
+				toggleButton.removeClass( 'rotate' );
+			} else {
+				toggleButton.parent().parent().find( 'li .sub-menu' ).removeClass( 'active' ).slideUp( 400 );
+				toggleButton.parent().parent().find( '.menu-toggle' ).removeClass( 'rotate' );
+				toggleButton.next().toggleClass( 'active' ).slideToggle( 400 );
+				toggleButton.toggleClass( 'rotate' );
+			}
+		} );
+	} );
+
+} );
+
+// Ref: ChatGPT
+// Ref: https://codepen.io/keithchis/pen/GpdbLm
