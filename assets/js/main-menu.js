a42ff7711e79260290f56716c014ab26f9bb4727
Jesse Nickles <jesse@littlebizzy.com>
2025-10-13 00:29:54 +0700

    Update main-menu.js

--- assets/js/main-menu.js
+++ assets/js/main-menu.js
@@ -1,31 +1,41 @@
-(function ($) {
+// hover delays for submenus
+jQuery( function( $ ) {
+
 	'use strict';
 
-	var $menu = $('.menu-desktop');
-	if (!$menu.length) return;
+	var $menu = $( '.menu-desktop' );
+	if ( ! $menu.length ) {
+		return;
+	}
 
 	var delayIn = 100;  // submenu open delay
 	var delayOut = 150; // submenu close delay
 
-	$menu.find('.menu-item-has-children').each(function () {
-		var $li = $(this);
-		var openTimer, closeTimer;
+	$menu.find( '.menu-item-has-children' ).each( function() {
+		var $li = $( this );
+		var openTimer;
+		var closeTimer;
 
-		$li.on('mouseenter', function () {
-			clearTimeout(openTimer);
-			clearTimeout(closeTimer);
-			openTimer = setTimeout(function () {
-				$menu.find('li.open').not($li.parents()).removeClass('open');
-				$li.addClass('open');
-			}, delayIn);
-		});
+		$li.on( 'mouseenter.hovercraftMenu', function() {
+			clearTimeout( openTimer );
+			clearTimeout( closeTimer );
 
-		$li.on('mouseleave', function () {
-			clearTimeout(openTimer);
-			clearTimeout(closeTimer);
-			closeTimer = setTimeout(function () {
-				$li.removeClass('open');
-			}, delayOut);
-		});
-	});
-})(jQuery);
+			openTimer = setTimeout( function() {
+				$menu.find( 'li.open' ).not( $li.parents() ).removeClass( 'open' );
+				$li.addClass( 'open' );
+			}, delayIn );
+		} );
+
+		$li.on( 'mouseleave.hovercraftMenu', function() {
+			clearTimeout( openTimer );
+			clearTimeout( closeTimer );
+
+			closeTimer = setTimeout( function() {
+				$li.removeClass( 'open' );
+			}, delayOut );
+		} );
+	} );
+
+} );
+
+// Ref: ChatGPT
