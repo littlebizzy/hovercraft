f96e2535aff09f6869f653d501dbe3ff7dae628a
Jesse Nickles <jesse@littlebizzy.com>
2025-10-13 00:39:34 +0700

    Update offcanvas-menu.js

--- assets/js/offcanvas-menu.js
+++ assets/js/offcanvas-menu.js
@@ -1,18 +1,21 @@
-jQuery(document).ready(function($){
+// mobile menu
+jQuery( function( $ ) {
 
-    $(document).ready(function(){
-        $('.burger').on('click', function(){
-        	$('#offcanvas').toggleClass('active');
-			$('body').toggleClass('frozen');
-			$('.overlay-main').toggleClass('active');
-        });
-		$('.overlay-main').on('click', function(){
-			$('#offcanvas').removeClass('active');
-			$('body').removeClass('frozen');
-			$('.overlay-main').removeClass('active');
-		});
-    });
-	
-});
+	// toggle menu open and overlay
+	$( '.burger' ).on( 'click', function() {
+		$( '#offcanvas' ).toggleClass( 'active' );
+		$( 'body' ).toggleClass( 'frozen' );
+		$( '.overlay-main' ).toggleClass( 'active' );
+	} );
 
+	// close menu when overlay is clicked
+	$( '.overlay-main' ).on( 'click', function() {
+		$( '#offcanvas' ).removeClass( 'active' );
+		$( 'body' ).removeClass( 'frozen' );
+		$( '.overlay-main' ).removeClass( 'active' );
+	} );
+
+} );
+
+// Ref: ChatGPT
 // https://codepen.io/abhi_pawar/pen/qzpEJL
