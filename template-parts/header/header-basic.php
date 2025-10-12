b3029c1248efdad48faa0aca2789a8357d42b13d
Jesse Nickles <jesse@littlebizzy.com>
2025-10-13 01:16:20 +0700

    Update header-basic.php

--- template-parts/header/header-basic.php
+++ template-parts/header/header-basic.php
@@ -1,36 +1,60 @@
 <div id="header-basic">
-    
-    <?php if ( is_active_sidebar( 'hovercraft_preheader_left' ) || is_active_sidebar( 'hovercraft_preheader_right' ) ) { ?>
+
+	<?php if ( is_active_sidebar( 'hovercraft_preheader_left' ) || is_active_sidebar( 'hovercraft_preheader_right' ) ) { ?>
 	<div id="preheader">
 		<div class="inner">
+
 			<?php if ( is_active_sidebar( 'hovercraft_preheader_left' ) && is_active_sidebar( 'hovercraft_preheader_right' ) ) { ?>
+
+			<!-- preheader-left -->
 			<div class="preheader-left">
-				<?php add_filter('widget_title', '__return_false');
-				dynamic_sidebar( 'hovercraft_preheader_left' ); // https://stackoverflow.com/questions/13903918/apply-widget-title-filter-only-to-wordpress-widgets-from-a-certain-sidebar
-				remove_filter('widget_title', '__return_false'); ?>
-			</div><!-- preheader-left -->
+				<?php
+				// remove widget titles for this sidebar only
+				add_filter( 'widget_title', '__return_false' );
+				dynamic_sidebar( 'hovercraft_preheader_left' );
+				remove_filter( 'widget_title', '__return_false' );
+				?>
+			</div>
+
+			<!-- preheader-right -->
 			<div class="preheader-right">
-				<?php add_filter('widget_title', '__return_false');
-				dynamic_sidebar( 'hovercraft_preheader_right' ); // https://stackoverflow.com/questions/13903918/apply-widget-title-filter-only-to-wordpress-widgets-from-a-certain-sidebar
-				remove_filter('widget_title', '__return_false'); ?>
-			</div><!-- preheader-right -->
+				<?php
+				// remove widget titles for this sidebar only
+				add_filter( 'widget_title', '__return_false' );
+				dynamic_sidebar( 'hovercraft_preheader_right' );
+				remove_filter( 'widget_title', '__return_false' );
+				?>
+			</div>
+
 			<?php } elseif ( is_active_sidebar( 'hovercraft_preheader_left' ) xor is_active_sidebar( 'hovercraft_preheader_right' ) ) { ?>
+
+			<!-- preheader-center -->
 			<div class="preheader-center">
-				<?php if ( is_active_sidebar( 'hovercraft_preheader_left' ) ) {
-					add_filter('widget_title', '__return_false');
-					dynamic_sidebar( 'hovercraft_preheader_left' ); // https://stackoverflow.com/questions/13903918/apply-widget-title-filter-only-to-wordpress-widgets-from-a-certain-sidebar
-					remove_filter('widget_title', '__return_false');
+				<?php
+				if ( is_active_sidebar( 'hovercraft_preheader_left' ) ) {
+					// remove widget titles for this sidebar only
+					add_filter( 'widget_title', '__return_false' );
+					dynamic_sidebar( 'hovercraft_preheader_left' );
+					remove_filter( 'widget_title', '__return_false' );
 				} elseif ( is_active_sidebar( 'hovercraft_preheader_right' ) ) {
-					add_filter('widget_title', '__return_false');
-					dynamic_sidebar( 'hovercraft_preheader_right' ); // https://stackoverflow.com/questions/13903918/apply-widget-title-filter-only-to-wordpress-widgets-from-a-certain-sidebar
-					remove_filter('widget_title', '__return_false');
-				} ?>
-			</div><!-- preheader-center -->
+					// remove widget titles for this sidebar only
+					add_filter( 'widget_title', '__return_false' );
+					dynamic_sidebar( 'hovercraft_preheader_right' );
+					remove_filter( 'widget_title', '__return_false' );
+				}
+				?>
+			</div>
+
 			<?php } ?>
+
 			<div class="clear"></div>
+
 		</div><!-- inner -->
 	</div><!-- preheader -->
 	<?php } ?><!-- if preheader widgets active -->
-    
-    <?php get_template_part('template-parts/header/navigation'); ?>
+
+	<?php get_template_part( 'template-parts/header/navigation' ); ?>
+
 </div><!-- header-basic -->
+
+<!-- Ref: https://stackoverflow.com/questions/13903918/apply-widget-title-filter-only-to-wordpress-widgets-from-a-certain-sidebar -->
