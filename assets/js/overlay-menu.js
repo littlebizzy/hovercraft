acd9fa9d3ab6918b24afbcfafec7e7536f265b89
Jesse Nickles <jesse@littlebizzy.com>
2025-10-13 00:41:08 +0700

    Update overlay-menu.js

--- assets/js/overlay-menu.js
+++ assets/js/overlay-menu.js
@@ -1,13 +1,12 @@
+// open overlay mobile menu
 function openNav() {
- 	  document.getElementById("myNav").style.height = "100%";
-}
-/* Close overlay mobile menu */
-function closeNav() {
-  	document.getElementById("myNav").style.height = "0%";
+	document.getElementById( 'myNav' ).style.height = '100%';
 }
 
-// https://codepen.io/voala/pen/eXBZeM
-// alt https://codepen.io/nitronova/pen/ONYQbZ
-// alt https://codepen.io/joshuaaron/pen/QgdLpB
-// alt https://codepen.io/jigneshpanchalmj/pen/Qevqjp
-// alt https://codepen.io/kpree/pen/Oeaqxa
+// close overlay mobile menu
+function closeNav() {
+	document.getElementById( 'myNav' ).style.height = '0%';
+}
+
+// Ref: ChatGPT
+// Ref: https://codepen.io/voala/pen/eXBZeM
