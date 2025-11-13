f2405738a51d69dcfc4a2bf6120e218171bed0ff
Jesse Nickles <jesse@littlebizzy.com>
2025-11-13 15:30:25 +0700

    Update mini-hero-columns-no-sidebar.php

--- page-templates/mini-hero-columns-no-sidebar.php
+++ page-templates/mini-hero-columns-no-sidebar.php
@@ -13,7 +13,7 @@
 	
 <div id="primary-wide">
 	
-<?php get_template_part( 'template-parts/content/breadcrumbs' ); ?>
+<?php get_template_part( 'template-parts/misc/breadcrumbs' ); ?>
 <?php get_template_part( 'template-parts/content/loop' ); ?>
 <?php get_template_part( 'template-parts/content/columns' ); ?>
 
