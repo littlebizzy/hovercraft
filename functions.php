b5a17e6048c911b92d09b08aa60c6919f1497141
Jesse Nickles <jesse@littlebizzy.com>
2026-05-07 15:49:49 +0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
 if ( ! defined( 'HOVERCRAFT_VERSION' ) ) {
-	define( 'HOVERCRAFT_VERSION', '0.3.24' );
+	define( 'HOVERCRAFT_VERSION', '0.3.23' );
 }
 
 // disable wordpress.org updates for this theme
