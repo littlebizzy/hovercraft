82deae25ea971fbec4c51a40ee0648fa411539e2
Jesse Nickles <jesse@littlebizzy.com>
2026-05-08 04:11:27 +0700

    Restore main layout CSS includes

--- inc/css/main.php
+++ inc/css/main.php
@@ -27,3 +27,19 @@ body.home #main {
 body.home #content-wide {
 	background: <?php echo $main_background_color_homepage; ?>;
 }
+
+/* primary */
+<?php
+$hovercraft_primary_css = get_template_directory() . '/inc/css/primary.php';
+if ( file_exists( $hovercraft_primary_css ) ) {
+	include $hovercraft_primary_css;
+}
+?>
+
+/* content */
+<?php
+$hovercraft_content_css = get_template_directory() . '/inc/css/content.php';
+if ( file_exists( $hovercraft_content_css ) ) {
+	include $hovercraft_content_css;
+}
+?>
