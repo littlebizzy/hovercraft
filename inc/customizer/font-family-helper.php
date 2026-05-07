18da8eaea8e0ee77d1092bb02e21af44be357cbb
Jesse Nickles <jesse@littlebizzy.com>
2026-05-07 19:35:59 +0700

    Clean font family helper

--- inc/customizer/font-family-helper.php
+++ inc/customizer/font-family-helper.php
@@ -1,44 +1,29 @@
 <?php
+function hovercraft_format_font_family_label( $font_family ) {
+    return ucwords( str_replace( '_', ' ', sanitize_key( $font_family ) ) );
+}
+
+function hovercraft_add_available_font( &$font_families, $font_family ) {
+    $font_family = sanitize_key( $font_family );
+
+    if ( empty( $font_family ) || 'none' === $font_family ) {
+        return;
+    }
+
+    $font_families[ $font_family ] = hovercraft_format_font_family_label( $font_family );
+}
+
 function hovercraft_available_fonts() {
-    // get and format the first font family for google fonts
-    $first_font_family = get_theme_mod( 'hovercraft_first_font_family', 'noto_sans' );
-    $first_font_family_final = ucwords( str_replace( '_', ' ', ucfirst( $first_font_family ) ) );
-
-    // get and format the second font family for google fonts
-    $second_font_family = get_theme_mod( 'hovercraft_second_font_family', '' );
-    $second_font_family_final = ucwords( str_replace( '_', ' ', ucfirst( $second_font_family ) ) );
-
-    // get and format the third font family for google fonts
-    $third_font_family = get_theme_mod( 'hovercraft_third_font_family', '' );
-    $third_font_family_final = ucwords( str_replace( '_', ' ', ucfirst( $third_font_family ) ) );
-
-    // get and format the multilingual font family for google fonts
-    $multilingual_font_family = get_theme_mod( 'hovercraft_multilingual_font_family', '' );
-    $multilingual_font_family_final = ucwords( str_replace( '_', ' ', ucfirst( $multilingual_font_family ) ) );
-
-    // initialize an array for font families with the default option included
-    $hovercraft_font_families = array(
-        '' => __( 'Default (unspecified)', 'hovercraft' ), // always include this as the first option
+    $font_families = array(
+        '' => __( 'Default (unspecified)', 'hovercraft' ),
     );
 
-    // add each formatted font family to the array if it's not empty or 'none'
-    if ( ! empty( $first_font_family ) && $first_font_family !== 'none' ) {
-        $hovercraft_font_families[ $first_font_family ] = $first_font_family_final;
-    }
+    hovercraft_add_available_font( $font_families, get_theme_mod( 'hovercraft_first_font_family', 'noto_sans' ) );
+    hovercraft_add_available_font( $font_families, get_theme_mod( 'hovercraft_second_font_family', '' ) );
+    hovercraft_add_available_font( $font_families, get_theme_mod( 'hovercraft_third_font_family', '' ) );
+    hovercraft_add_available_font( $font_families, get_theme_mod( 'hovercraft_multilingual_font_family', '' ) );
 
-    if ( ! empty( $second_font_family ) && $second_font_family !== 'none' ) {
-        $hovercraft_font_families[ $second_font_family ] = $second_font_family_final;
-    }
-
-    if ( ! empty( $third_font_family ) && $third_font_family !== 'none' ) {
-        $hovercraft_font_families[ $third_font_family ] = $third_font_family_final;
-    }
-
-    if ( ! empty( $multilingual_font_family ) && $multilingual_font_family !== 'none' ) {
-        $hovercraft_font_families[ $multilingual_font_family ] = $multilingual_font_family_final;
-    }
-
-    return $hovercraft_font_families;
+    return $font_families;
 }
 
 // Ref: ChatGPT
