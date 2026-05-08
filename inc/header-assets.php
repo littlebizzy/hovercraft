591c4fe843b93e4cb24a81c493724d019b70f6f8
Jesse Nickles <jesse@littlebizzy.com>
2026-05-08 17:11:50 +0700

    Fix Material Icons combined loading

--- inc/header-assets.php
+++ inc/header-assets.php
@@ -28,13 +28,27 @@ function hovercraft_enqueue_header_assets() {
     $material_icons_setting = get_theme_mod( 'hovercraft_material_icons', 'classic_only' );
 
     $material_icons_map = array(
-        'classic_only' => 'Material+Icons',
-        'classic_and_outlined' => 'Material+Icons&family=Material+Icons+Outlined',
-        'classic_and_outlined_and_two_toned' => 'Material+Icons&family=Material+Icons+Outlined&family=Material+Icons+Two+Tone',
+        'classic_only' => array(
+            'Material+Icons',
+        ),
+        'classic_and_outlined' => array(
+            'Material+Icons',
+            'Material+Icons+Outlined',
+        ),
+        'classic_and_outlined_and_two_toned' => array(
+            'Material+Icons',
+            'Material+Icons+Outlined',
+            'Material+Icons+Two+Tone',
+        ),
     );
 
     if ( 'none' !== $material_icons_setting && isset( $material_icons_map[ $material_icons_setting ] ) ) {
-        wp_enqueue_style( 'hovercraft_material_icons', 'https://fonts.googleapis.com/css2?family=' . $material_icons_map[ $material_icons_setting ] . '&display=block', array(), HOVERCRAFT_VERSION );
+        foreach ( $material_icons_map[ $material_icons_setting ] as $material_icons_family ) {
+            $material_icons_handle = 'hovercraft_material_icons_' . sanitize_key( $material_icons_family );
+            $material_icons_url = 'https://fonts.googleapis.com/icon?family=' . $material_icons_family . '&display=block';
+
+            wp_enqueue_style( $material_icons_handle, $material_icons_url, array(), HOVERCRAFT_VERSION );
+        }
     }
 
     $font_awesome_setting = get_theme_mod( 'hovercraft_font_awesome', 'none' );
