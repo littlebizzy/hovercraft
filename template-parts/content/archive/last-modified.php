35505645d0531a4331dc8e56f9bf6a9ae473ee5a
Jesse Nickles <jesse@littlebizzy.com>
2026-05-08 20:05:49 +0700

    Remove unused last modified template part

--- template-parts/content/archive/last-modified.php
+++
@@ -1,3 +0,0 @@
-<div class="last-modified">
-    <span><?php esc_html_e( 'Last modified on ', 'hovercraft' ); echo esc_html( get_the_modified_time( 'F j, Y' ) ); ?></span>
-</div><!-- last-modified -->
