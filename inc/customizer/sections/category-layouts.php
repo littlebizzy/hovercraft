75f99200a92cab1fb88f7c8375cf283b108e9adf
Jesse Nickles <jesse@littlebizzy.com>
2026-05-09 12:56:25 +0700

    Clean up category layouts Customizer section formatting

--- inc/customizer/sections/category-layouts.php
+++ inc/customizer/sections/category-layouts.php
@@ -6,27 +6,24 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 // category layouts section
 $wp_customize->add_section( 'hovercraft_category_layouts', array(
-    'title'      => 'Category Layouts',
-    'priority'   => 34,
+    'title' => 'Category Layouts',
+    'priority' => 34,
 ) );
-	
+
 // homepage hide main setting
-$wp_customize->add_setting('hovercraft_homepage_hide_main', array(
+$wp_customize->add_setting( 'hovercraft_homepage_hide_main', array(
     'default' => 0,
-	'sanitize_callback' => 'hovercraft_sanitize_checkbox',
-));
+    'sanitize_callback' => 'hovercraft_sanitize_checkbox',
+) );
 
 // homepage hide main control
-$wp_customize->add_control(
-    new WP_Customize_Control(
-        $wp_customize,
-        'hovercraft_homepage_hide_main',
-        array(
-            'label'     => __('Hide homepage main section', 'hovercraft'),
-            'section'   => 'static_front_page',
-            'settings'  => 'hovercraft_homepage_hide_main',
-            'type'      => 'checkbox',
-        )
+$wp_customize->add_control( new WP_Customize_Control(
+    $wp_customize,
+    'hovercraft_homepage_hide_main',
+    array(
+        'label' => __( 'Hide homepage main section', 'hovercraft' ),
+        'section' => 'static_front_page',
+        'settings' => 'hovercraft_homepage_hide_main',
+        'type' => 'checkbox',
     )
-);
-
+) );
