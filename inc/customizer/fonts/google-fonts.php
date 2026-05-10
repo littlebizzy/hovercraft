1f0a6c3dee2bb6e3ddb0309c8e25b86e16cc9ca7
Jesse Nickles <jesse@littlebizzy.com>
2026-05-10 17:10:23 +0700

    Add ABSPATH guard to customizer google fonts list

--- inc/customizer/fonts/google-fonts.php
+++ inc/customizer/fonts/google-fonts.php
@@ -1,5 +1,9 @@
 <?php
 
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
 function hovercraft_google_fonts_array() {
     $google_fonts_array = array(
         ''                          => 'None (Disabled)', // blank value for no font
