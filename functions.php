0f3e9025174f3777f03c1036cbf610af8db59b53
Jesse Nickles <jesse@littlebizzy.com>
2026-05-11 00:11:05 +0700

    Bump theme version to 0.3.35

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
 if ( ! defined( 'HOVERCRAFT_VERSION' ) ) {
-	define( 'HOVERCRAFT_VERSION', '0.3.34' );
+	define( 'HOVERCRAFT_VERSION', '0.3.35' );
 }
 
 // disable wordpress.org updates for this theme
