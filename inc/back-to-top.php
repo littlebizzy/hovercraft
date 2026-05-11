ae45ed285276479019832dbec9c0b8dd02cd01fa
Jesse Nickles <jesse@littlebizzy.com>
2026-05-11 16:22:38 +0700

    Clean up back to top script loader

--- inc/back-to-top.php
+++ inc/back-to-top.php
@@ -1,9 +1,13 @@
 <?php
 
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
 function hovercraft_back_to_top() {
 	wp_enqueue_script(
 		'hovercraft_back_to_top',
-		get_template_directory_uri() . '/assets/js/back-to-top.js',
+		esc_url_raw( get_template_directory_uri() . '/assets/js/back-to-top.js' ),
 		array( 'jquery' ),
 		HOVERCRAFT_VERSION,
 		true
