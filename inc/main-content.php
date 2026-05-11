72ed4cd4e7ae8c8474806102fda602f9ac9e3ed7
Jesse Nickles <jesse@littlebizzy.com>
2026-05-12 02:23:25 +0700

    Simplify main content visibility check

--- inc/main-content.php
+++ inc/main-content.php
@@ -5,9 +5,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 function hovercraft_should_show_main_content() {
-	$hide_main_content = (bool) get_theme_mod( 'hovercraft_homepage_hide_main', false );
-
-	if ( is_front_page() && true === $hide_main_content ) {
+	if ( is_front_page() && get_theme_mod( 'hovercraft_homepage_hide_main', false ) ) {
 		return false;
 	}
 
