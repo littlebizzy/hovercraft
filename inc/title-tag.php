c6e36e8662b6c654fb5e849225ab91bb113914f4
Jesse Nickles <jesse@littlebizzy.com>
2026-05-11 16:42:59 +0700

    Clean up title tag helper

--- inc/title-tag.php
+++ inc/title-tag.php
@@ -1,15 +1,19 @@
 <?php
 
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
 function hovercraft_title_tag() {
-    add_theme_support( 'title-tag' );
+	add_theme_support( 'title-tag' );
 }
 
 add_action( 'after_setup_theme', 'hovercraft_title_tag' );
 
 // remove bbpress title modification if active
 if ( class_exists( 'bbPress' ) ) {
-    remove_filter( 'wp_title', 'bbp_title', 10, 3 );
-    remove_filter( 'document_title_parts', 'bbp_filter_document_title', 10, 1 );
+	remove_filter( 'wp_title', 'bbp_title', 10, 3 );
+	remove_filter( 'document_title_parts', 'bbp_filter_document_title', 10, 1 );
 }
 
 // Ref: ChatGPT
