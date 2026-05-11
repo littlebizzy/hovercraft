78385f6b8b0f0b2a89c5fd8cc727da28cdc24761
Jesse Nickles <jesse@littlebizzy.com>
2026-05-11 15:48:51 +0700

    Add guard to header video customizer

--- inc/header-video.php
+++ inc/header-video.php
@@ -1,13 +1,17 @@
 <?php
 
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
 function hovercraft_video( $wp_customize ) {
 	$wp_customize->add_setting(
 		'hovercraft_video',
 		array(
-			'default'           => '',
-			'transport'         => 'refresh',
+			'default' => '',
+			'transport' => 'refresh',
 			'sanitize_callback' => 'absint',
-			'type'              => 'theme_mod',
+			'type' => 'theme_mod',
 		)
 	);
 
@@ -16,19 +20,19 @@ function hovercraft_video( $wp_customize ) {
 			$wp_customize,
 			'video_upload',
 			array(
-				'label'         => esc_html__( 'Background Video', 'hovercraft' ),
-				'description'   => esc_html__( 'This is the description for the Media Control', 'hovercraft' ),
-				'section'       => 'header_image',
-				'mime_type'     => 'video',
-				'settings'      => 'hovercraft_video',
-				'priority'      => 8,
+				'label' => esc_html__( 'Background Video', 'hovercraft' ),
+				'description' => esc_html__( 'This is the description for the Media Control', 'hovercraft' ),
+				'section' => 'header_image',
+				'mime_type' => 'video',
+				'settings' => 'hovercraft_video',
+				'priority' => 8,
 				'button_labels' => array(
-					'select'       => esc_html__( 'Select File', 'hovercraft' ),
-					'change'       => esc_html__( 'Change File', 'hovercraft' ),
-					'default'      => esc_html__( 'Default', 'hovercraft' ),
-					'remove'       => esc_html__( 'Remove', 'hovercraft' ),
-					'placeholder'  => esc_html__( 'No file selected', 'hovercraft' ),
-					'frame_title'  => esc_html__( 'Select File', 'hovercraft' ),
+					'select' => esc_html__( 'Select File', 'hovercraft' ),
+					'change' => esc_html__( 'Change File', 'hovercraft' ),
+					'default' => esc_html__( 'Default', 'hovercraft' ),
+					'remove' => esc_html__( 'Remove', 'hovercraft' ),
+					'placeholder' => esc_html__( 'No file selected', 'hovercraft' ),
+					'frame_title' => esc_html__( 'Select File', 'hovercraft' ),
 					'frame_button' => esc_html__( 'Choose File', 'hovercraft' ),
 				),
 			)
