090252506c19c63cf0ee0cd215c5073e7e98661d
Jesse Nickles <jesse@littlebizzy.com>
2026-05-11 16:29:46 +0700

    Clean up featured image helper

--- inc/featured-images.php
+++ inc/featured-images.php
@@ -1,22 +1,41 @@
 <?php
 
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
 // featured image support
 add_theme_support( 'post-thumbnails' );
 
 // featured image
 function hovercraft_post_image_large() {
-    global $post;
+	global $post;
 
-    if ( ! isset( $post ) || ! is_object( $post ) || empty( $post->ID ) ) {
-        return;
-    }
+	if ( ! isset( $post ) || ! is_object( $post ) || empty( $post->ID ) ) {
+		return;
+	}
 
-    $args = array( 'post_type' => 'attachment', 'post_mime_type' => 'image', 'numberposts' => 1, 'orderby' => 'menu_order', 'order' => 'ASC', 'post_status' => null, 'post_parent' => $post->ID ); 
-    $attachments = get_posts($args);
-    if ($attachments) {
-            foreach ( $attachments as $attachment ) {
-                    $src = wp_get_attachment_image_src( $attachment->ID, 'large');
-                    if ($src) {echo esc_url( $src[0] );}
-            }
-    }
+	$args = array(
+		'post_type' => 'attachment',
+		'post_mime_type' => 'image',
+		'numberposts' => 1,
+		'orderby' => 'menu_order',
+		'order' => 'ASC',
+		'post_status' => null,
+		'post_parent' => $post->ID,
+	);
+
+	$attachments = get_posts( $args );
+
+	if ( empty( $attachments ) ) {
+		return;
+	}
+
+	foreach ( $attachments as $attachment ) {
+		$src = wp_get_attachment_image_src( $attachment->ID, 'large' );
+
+		if ( $src ) {
+			echo esc_url( $src[0] );
+		}
+	}
 }
