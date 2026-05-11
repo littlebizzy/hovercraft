ac1fb1b2dfe3cb00b0efdacc62b2bde011c8877b
Jesse Nickles <jesse@littlebizzy.com>
2026-05-11 16:21:33 +0700

    Clean up full screen search script loader

--- inc/full-screen-search.php
+++ inc/full-screen-search.php
@@ -1,9 +1,13 @@
 <?php
 
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
 function hovercraft_full_screen_search() {
 	wp_enqueue_script(
 		'hovercraft_full_screen_search',
-		get_template_directory_uri() . '/assets/js/full-screen-search.js',
+		esc_url_raw( get_template_directory_uri() . '/assets/js/full-screen-search.js' ),
 		array( 'jquery' ),
 		HOVERCRAFT_VERSION,
 		true
