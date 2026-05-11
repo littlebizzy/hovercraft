057067036065b19e85a739717281e2f1e934d28c
Jesse Nickles <jesse@littlebizzy.com>
2026-05-11 16:36:24 +0700

    Clean up media size insert helper

--- inc/media-sizes-insert.php
+++ inc/media-sizes-insert.php
@@ -1,11 +1,19 @@
 <?php
 
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
 // make medium_large images available to insert into posts
 add_filter( 'image_size_names_choose', 'hovercraft_medium_large_images_insert' );
+
 function hovercraft_medium_large_images_insert( $sizes ) {
-    return array_merge( $sizes, array(
-        'medium_large' => esc_html__( 'Medium Large', 'hovercraft' ),
-    ) );
+	return array_merge(
+		$sizes,
+		array(
+			'medium_large' => esc_html__( 'Medium Large', 'hovercraft' ),
+		)
+	);
 }
 
 // https://wordpress.stackexchange.com/questions/290259/make-medium-large-images-available-to-insert-into-post
