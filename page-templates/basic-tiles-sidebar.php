1d6214d5ffbc4748c2dc065a96d5029599f8c3aa
Jesse Nickles <jesse@littlebizzy.com>
2026-05-12 18:30:51 +0700

    Add basic tiles sidebar page template

---
+++ page-templates/basic-tiles-sidebar.php
@@ -0,0 +1,44 @@
+<?php
+/* Template Name: Basic Tiles (Sidebar) */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+?>
+<?php get_template_part( 'header' ); ?>
+<?php get_template_part( 'template-parts/header/header-basic' ); ?>
+
+<?php if ( hovercraft_should_show_main_content() ) : ?>
+
+<div id="main">
+<div class="inner">
+	
+	<div id="primary">
+		
+		<div id="content-wrapper">
+			
+			<div id="content-padded">
+				<?php get_template_part( 'template-parts/misc/breadcrumbs' ); ?>
+				<?php get_template_part( 'template-parts/content/title' ); ?>
+				<?php get_template_part( 'template-parts/content/loop' ); ?>
+				<?php get_template_part( 'template-parts/content/tiles-narrow' ); ?>
+				<?php get_template_part( 'template-parts/content/pagination' ); ?>
+				<?php get_template_part( 'template-parts/content/comments' ); ?>
+			<div class="clear"></div>
+			</div><!-- content-padded -->
+			
+		</div><!-- content-wrapper -->
+
+	<div class="clear"></div>
+	</div><!-- primary -->
+	
+	<?php get_template_part( 'sidebar' ); ?>
+		
+<div class="clear"></div>
+</div><!-- inner -->
+</div><!-- main -->
+
+<?php endif; // endif is_front_page ?>
+
+<?php get_template_part( 'footer' ); ?>
