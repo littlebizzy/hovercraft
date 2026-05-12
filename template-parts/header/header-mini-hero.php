caa790d6673ef8dfbf10983d1ed3c4ee06ddef95
Jesse Nickles <jesse@littlebizzy.com>
2026-05-12 20:19:56 +0700

    Use shared hero snippet in mini hero

--- template-parts/header/header-mini-hero.php
+++ template-parts/header/header-mini-hero.php
@@ -59,15 +59,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 		<?php if ( is_front_page() ) : ?>
 			<?php if ( is_active_sidebar( 'hovercraft_hero_snippet' ) || has_excerpt() ) : ?>
 				<div class="hero-snippet">
-					<?php if ( is_active_sidebar( 'hovercraft_hero_snippet' ) ) : ?>
-						<?php
-						add_filter( 'widget_title', '__return_false' );
-						dynamic_sidebar( 'hovercraft_hero_snippet' );
-						remove_filter( 'widget_title', '__return_false' );
-						?>
-					<?php else : ?>
-						<?php the_excerpt(); ?>
-					<?php endif; ?>
+					<?php get_template_part( 'template-parts/header/hero-snippet' ); ?>
 				</div>
 			<?php endif; ?>
 		<?php endif; ?>
