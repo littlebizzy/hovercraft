4cc3bbe8713d4e706c7bcb47243ea4c1df6a019c
Jesse Nickles <jesse@littlebizzy.com>
2026-05-12 18:31:28 +0700

    Add mini hero tiles sidebar page template

---
+++ page-templates/mini-hero-tiles-sidebar.php
@@ -0,0 +1,47 @@
+<?php
+/* Template Name: Mini Hero Tiles (Sidebar) */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+?>
+<?php get_template_part( 'header' ); ?>
+<?php get_template_part( 'template-parts/header/header-mini-hero' ); ?>
+
+	<?php if ( hovercraft_should_show_posthero( 'mini' ) ) : ?>
+		<?php get_template_part( 'template-parts/content/posthero' ); ?>
+	<?php endif; ?>
+
+<?php if ( hovercraft_should_show_main_content() ) : ?>
+
+<div id="main">
+<div class="inner">
+	
+	<div id="primary">
+		
+		<div id="content-wrapper">
+			
+			<div id="content-padded">
+				<?php get_template_part( 'template-parts/misc/breadcrumbs' ); ?>
+				<?php get_template_part( 'template-parts/content/loop' ); ?>
+				<?php get_template_part( 'template-parts/content/tiles-narrow' ); ?>
+				<?php get_template_part( 'template-parts/content/pagination' ); ?>
+				<?php get_template_part( 'template-parts/content/comments' ); ?>
+			<div class="clear"></div>
+			</div><!-- content-padded -->
+			
+		</div><!-- content-wrapper -->
+
+	<div class="clear"></div>
+	</div><!-- primary -->
+	
+	<?php get_template_part( 'sidebar' ); ?>
+		
+<div class="clear"></div>
+</div><!-- inner -->
+</div><!-- main -->
+
+<?php endif; ?>
+
+<?php get_template_part( 'footer' ); ?>
