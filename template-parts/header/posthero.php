141b7103c821ca53abb41095ccd28657f5f4dc7c
Jesse Nickles <jesse@littlebizzy.com>
2026-05-12 20:34:17 +0700

    Move posthero template part into header folder

---
+++ template-parts/header/posthero.php
@@ -0,0 +1,17 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+?>
+<div id="posthero">
+	<div class="inner">
+		<?php
+		add_filter( 'widget_title', '__return_false' );
+	dynamic_sidebar( 'hovercraft_posthero' );
+	remove_filter( 'widget_title', '__return_false' );
+		?>
+		<div class="clear"></div>
+	</div><!-- inner -->
+</div><!-- posthero -->
