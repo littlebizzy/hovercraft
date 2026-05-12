9b86af8691c404a02df5f490c6ef6d9622b37981
Jesse Nickles <jesse@littlebizzy.com>
2026-05-13 03:31:16 +0700

    Rename header video helper with prefix

---
+++ inc/header/header-video.php
@@ -0,0 +1,42 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+function hovercraft_video( $wp_customize ) {
+	$wp_customize->add_setting(
+		'hovercraft_video',
+		array(
+			'default' => '',
+			'transport' => 'refresh',
+			'sanitize_callback' => 'absint',
+			'type' => 'theme_mod',
+		)
+	);
+
+	$wp_customize->add_control(
+		new WP_Customize_Media_Control(
+			$wp_customize,
+			'video_upload',
+			array(
+				'label' => esc_html__( 'Background Video', 'hovercraft' ),
+				'description' => esc_html__( 'Upload or select a background video for the site header.', 'hovercraft' ),
+				'section' => 'header_image',
+				'mime_type' => 'video',
+				'settings' => 'hovercraft_video',
+				'priority' => 8,
+				'button_labels' => array(
+					'select' => esc_html__( 'Select File', 'hovercraft' ),
+					'change' => esc_html__( 'Change File', 'hovercraft' ),
+					'default' => esc_html__( 'Default', 'hovercraft' ),
+					'remove' => esc_html__( 'Remove', 'hovercraft' ),
+					'placeholder' => esc_html__( 'No file selected', 'hovercraft' ),
+					'frame_title' => esc_html__( 'Select File', 'hovercraft' ),
+					'frame_button' => esc_html__( 'Choose File', 'hovercraft' ),
+				),
+			)
+		)
+	);
+}
+add_action( 'customize_register', 'hovercraft_video' );
