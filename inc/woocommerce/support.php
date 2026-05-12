bc74153866cf3a0588d11fbdcdc1c6a984b45908
Jesse Nickles <jesse@littlebizzy.com>
2026-05-12 17:12:48 +0700

    Clean WooCommerce support helper

--- inc/woocommerce/support.php
+++ inc/woocommerce/support.php
@@ -4,12 +4,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-// only run if woocommerce is active
 if ( ! class_exists( 'WooCommerce' ) ) {
 	return;
 }
 
-// woocommerce support
 remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10 );
 remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10 );
 add_action( 'woocommerce_before_main_content', 'hovercraft_main_start', 10 );
