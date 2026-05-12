d6dc8a75304badd076172f878999f21a56372c4c
Jesse Nickles <jesse@littlebizzy.com>
2026-05-12 17:25:18 +0700

    Clean title tag helper

--- inc/title-tag.php
+++ inc/title-tag.php
@@ -7,15 +7,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 function hovercraft_title_tag() {
 	add_theme_support( 'title-tag' );
 }
-
 add_action( 'after_setup_theme', 'hovercraft_title_tag' );
 
-// remove bbpress title modification if active
 if ( class_exists( 'bbPress' ) ) {
 	remove_filter( 'wp_title', 'bbp_title', 10, 3 );
 	remove_filter( 'document_title_parts', 'bbp_filter_document_title', 10, 1 );
 }
-
-// Ref: ChatGPT
-// Ref: https://wordpress.stackexchange.com/questions/176940/add-theme-support-title-tag-in-conflict-with-custom-titles-function
-// Ref: https://www.usablewp.com/learn-wordpress/home-page/how-to-add-theme-support-for-the-title-tag/
