61177eebf20dc6c4a854ed8e811ff332f3f49c96
Jesse Nickles <jesse@littlebizzy.com>
2026-05-12 16:46:12 +0700

    Clean bbPress search results helper

--- inc/bbpress/search-results.php
+++ inc/bbpress/search-results.php
@@ -4,7 +4,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-// include bbpress topics in wordpress search results
 function hovercraft_bbp_topic_search( $topic_search ) {
 	$topic_search['exclude_from_search'] = false;
 
@@ -12,13 +11,9 @@ function hovercraft_bbp_topic_search( $topic_search ) {
 }
 add_filter( 'bbp_register_topic_post_type', 'hovercraft_bbp_topic_search' );
 
-// include bbpress replies in wordpress search results
 function hovercraft_bbp_reply_search( $reply_search ) {
 	$reply_search['exclude_from_search'] = false;
 
 	return $reply_search;
 }
 add_filter( 'bbp_register_reply_post_type', 'hovercraft_bbp_reply_search' );
-
-// https://gist.github.com/ntwb/7363a1de1184d459f0c3
-// https://bbpress.org/forums/topic/plugin-snippet-hack-to-include-bbpress-topics-in-wordpress-search/
