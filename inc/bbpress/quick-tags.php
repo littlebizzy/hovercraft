5662c5894ddcb1a99832cef16d2424acd3edb519
Jesse Nickles <jesse@littlebizzy.com>
2026-05-12 16:32:54 +0700

    Clean bbPress quick tags helper

--- inc/bbpress/quick-tags.php
+++ inc/bbpress/quick-tags.php
@@ -10,6 +10,3 @@ function hovercraft_swap_js_body_tags() {
 	remove_action( 'wp_footer', 'hovercraft_swap_js_body_tags' );
 	echo "<script>document.body.className = document.body.className.replace('no-js','js');</script>\n";
 }
-
-// https://bbpress.trac.wordpress.org/ticket/3445
-// https://slickstack.io/forum/topic/quicktags-toolbar-on-this-forum
