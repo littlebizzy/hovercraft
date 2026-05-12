f6ce745bd998fc82110fa910a3f7207c117e8d3b
Jesse Nickles <jesse@littlebizzy.com>
2026-05-12 20:05:03 +0700

    Rename hero preheader template part

---
+++ template-parts/header/preheader.php
@@ -0,0 +1,42 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+?>
+<?php if ( is_active_sidebar( 'hovercraft_preheader_left' ) || is_active_sidebar( 'hovercraft_preheader_right' ) ) : ?>
+<div id="preheader">
+	<div class="inner">
+		<?php if ( is_active_sidebar( 'hovercraft_preheader_left' ) && is_active_sidebar( 'hovercraft_preheader_right' ) ) : ?>
+		<div class="preheader-left">
+			<?php add_filter( 'widget_title', '__return_false' );
+			dynamic_sidebar( 'hovercraft_preheader_left' );
+			remove_filter( 'widget_title', '__return_false' ); ?>
+		</div><!-- preheader-left -->
+		<div class="preheader-right">
+			<?php add_filter( 'widget_title', '__return_false' );
+			dynamic_sidebar( 'hovercraft_preheader_right' );
+			remove_filter( 'widget_title', '__return_false' ); ?>
+		</div><!-- preheader-right -->
+		<?php elseif ( is_active_sidebar( 'hovercraft_preheader_left' ) xor is_active_sidebar( 'hovercraft_preheader_right' ) ) : ?>
+		<div class="preheader-center">
+			<?php if ( is_active_sidebar( 'hovercraft_preheader_left' ) ) : ?>
+				<?php
+				add_filter( 'widget_title', '__return_false' );
+				dynamic_sidebar( 'hovercraft_preheader_left' );
+				remove_filter( 'widget_title', '__return_false' );
+				?>
+			<?php elseif ( is_active_sidebar( 'hovercraft_preheader_right' ) ) : ?>
+				<?php
+				add_filter( 'widget_title', '__return_false' );
+				dynamic_sidebar( 'hovercraft_preheader_right' );
+				remove_filter( 'widget_title', '__return_false' );
+				?>
+			<?php endif; ?>
+		</div><!-- preheader-center -->
+		<?php endif; ?>
+		<div class="clear"></div>
+	</div><!-- inner -->
+</div><!-- preheader -->
+<?php endif; ?>
