a92cc47b8c6da1c4809d0a7e1e34066b6863da42
Jesse Nickles <jesse@littlebizzy.com>
2026-05-12 19:01:30 +0700

    Wire homepage premain widget areas

--- inc/main-content.php
+++ inc/main-content.php
@@ -4,7 +4,23 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+function hovercraft_maybe_load_premain() {
+	static $premain_loaded = false;
+
+	if ( $premain_loaded ) {
+		return;
+	}
+
+	$premain_loaded = true;
+
+	if ( is_front_page() ) {
+		get_template_part( 'template-parts/content/premain' );
+	}
+}
+
 function hovercraft_should_show_main_content() {
+	hovercraft_maybe_load_premain();
+
 	if ( is_front_page() && get_theme_mod( 'hovercraft_homepage_hide_main', false ) ) {
 		return false;
 	}
