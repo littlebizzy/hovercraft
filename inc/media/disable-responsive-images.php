4e1191e4e7b7c8d2959f72890fcbd096c00c0df3
Jesse Nickles <jesse@littlebizzy.com>
2026-05-12 17:01:29 +0700

    Clean responsive image helper

--- inc/media/disable-responsive-images.php
+++ inc/media/disable-responsive-images.php
@@ -5,6 +5,3 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 add_filter( 'wp_calculate_image_srcset', '__return_false' );
-
-// https://perishablepress.com/disable-wordpress-responsive-images/
-// https://wordpress.stackexchange.com/questions/211375/how-do-i-disable-responsive-images-in-wp-4-4
