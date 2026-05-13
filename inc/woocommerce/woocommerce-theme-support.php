f91d69ca4d4899a0e3673873d7460ba3a828a35c
Jesse Nickles <jesse@littlebizzy.com>
2026-05-14 00:25:56 +0700

    Add WooCommerce theme support

---
+++ inc/woocommerce/woocommerce-theme-support.php
@@ -0,0 +1,13 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+function hovercraft_add_woocommerce_theme_support() {
+	add_theme_support( 'woocommerce' );
+	add_theme_support( 'wc-product-gallery-zoom' );
+	add_theme_support( 'wc-product-gallery-lightbox' );
+	add_theme_support( 'wc-product-gallery-slider' );
+}
+add_action( 'after_setup_theme', 'hovercraft_add_woocommerce_theme_support' );
