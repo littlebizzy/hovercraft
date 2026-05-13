0e03d002113ac995d0d6e0f82d62efe8b915bd9a
Jesse Nickles <jesse@littlebizzy.com>
2026-05-14 02:29:08 +0700

    Document bbPress QuickTags body class fix

--- inc/bbpress/quick-tags.php
+++ inc/bbpress/quick-tags.php
@@ -4,9 +4,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+if ( ! function_exists( 'bbpress' ) ) {
+	return;
+}
+
+// force frontend js body class so bbpress quicktags render correctly
 add_action( 'wp_footer', 'hovercraft_swap_js_body_tags' );
 
 function hovercraft_swap_js_body_tags() {
 	remove_action( 'wp_footer', 'hovercraft_swap_js_body_tags' );
-	echo "<script>document.body.className = document.body.className.replace('no-js','js');</script>\n";
+	wp_print_inline_script_tag( "document.body.className = document.body.className.replace('no-js','js');" );
 }
