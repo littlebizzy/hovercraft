51c647a5ffcb0eb78f4e1b12fcb59196939a56b1
Jesse Nickles <jesse@littlebizzy.com>
2026-05-14 16:29:41 +0700

    Check all registered legacy tile widgets in admin

--- inc/widgets/widget-tiles.php
+++ inc/widgets/widget-tiles.php
@@ -4,7 +4,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-// get legacy tile widget areas
+// get rendered legacy tile widget areas
 function hovercraft_get_legacy_tile_sidebars() {
 	return array(
 		'hovercraft_tile_one',
@@ -22,11 +22,37 @@ function hovercraft_get_legacy_tile_sidebars() {
 	);
 }
 
+// get registered legacy tile widget areas
+function hovercraft_get_registered_legacy_tile_sidebars() {
+	return array(
+		'hovercraft_tile_one',
+		'hovercraft_tile_two',
+		'hovercraft_tile_three',
+		'hovercraft_tile_four',
+		'hovercraft_tile_five',
+		'hovercraft_tile_six',
+		'hovercraft_tile_seven',
+		'hovercraft_tile_eight',
+		'hovercraft_tile_nine',
+		'hovercraft_tile_ten',
+		'hovercraft_tile_eleven',
+		'hovercraft_tile_twelve',
+		'hovercraft_tile_thirteen',
+		'hovercraft_tile_fourteen',
+		'hovercraft_tile_fifteen',
+		'hovercraft_tile_sixteen',
+		'hovercraft_tile_seventeen',
+		'hovercraft_tile_eighteen',
+		'hovercraft_tile_nineteen',
+		'hovercraft_tile_twenty',
+	);
+}
+
 // mark legacy tile widget areas in admin
 function hovercraft_label_legacy_tile_sidebars() {
 	global $wp_registered_sidebars;
 
-	$legacy_tile_sidebars = hovercraft_get_legacy_tile_sidebars();
+	$legacy_tile_sidebars = hovercraft_get_registered_legacy_tile_sidebars();
 
 	foreach ( $legacy_tile_sidebars as $legacy_tile_sidebar ) {
 		if ( ! isset( $wp_registered_sidebars[ $legacy_tile_sidebar ]['name'] ) ) {
@@ -42,9 +68,22 @@ function hovercraft_label_legacy_tile_sidebars() {
 }
 add_action( 'widgets_init', 'hovercraft_label_legacy_tile_sidebars', 20 );
 
-// check if legacy tile widget areas have content
+// check if rendered legacy tile widget areas have content
 function hovercraft_has_legacy_tile_sidebars() {
 	$legacy_tile_sidebars = hovercraft_get_legacy_tile_sidebars();
+
+	foreach ( $legacy_tile_sidebars as $legacy_tile_sidebar ) {
+		if ( is_active_sidebar( $legacy_tile_sidebar ) ) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+// check if registered legacy tile widget areas have content
+function hovercraft_has_registered_legacy_tile_sidebars() {
+	$legacy_tile_sidebars = hovercraft_get_registered_legacy_tile_sidebars();
 
 	foreach ( $legacy_tile_sidebars as $legacy_tile_sidebar ) {
 		if ( is_active_sidebar( $legacy_tile_sidebar ) ) {
@@ -61,7 +100,7 @@ function hovercraft_legacy_tile_widget_notice() {
 		return;
 	}
 
-	if ( ! hovercraft_has_legacy_tile_sidebars() ) {
+	if ( ! hovercraft_has_registered_legacy_tile_sidebars() ) {
 		return;
 	}
 	?>
