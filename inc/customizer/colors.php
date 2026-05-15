e668d4b822fc4f643088dc73423b698198ec9bc0
Jesse Nickles <jesse@littlebizzy.com>
2026-05-15 17:04:10 +0700

    Load widget band color controls

--- inc/customizer/colors.php
+++ inc/customizer/colors.php
@@ -8,4 +8,5 @@ if ( ! defined( 'ABSPATH' ) ) {
 require get_template_directory() . '/inc/customizer/colors/color-base.php';
 require get_template_directory() . '/inc/customizer/colors/color-header-hero.php';
 require get_template_directory() . '/inc/customizer/colors/color-content-widgets.php';
+require get_template_directory() . '/inc/customizer/colors/color-widget-bands.php';
 require get_template_directory() . '/inc/customizer/colors/color-footer.php';
