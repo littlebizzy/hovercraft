53d4c90b340b86283fbcc65e50c00f92c7f076f0
Jesse Nickles <jesse@littlebizzy.com>
2026-05-16 00:31:34 +0700

    Remove nested featured image CSS include

--- inc/css/images-general.php
+++ inc/css/images-general.php
@@ -24,11 +24,3 @@ if ( ! defined( 'ABSPATH' ) ) {
 	margin-left: auto; 
 	margin-right: auto; 
 }
-
-/* images featured */
-<?php
-$hovercraft_images_featured_css = get_template_directory() . '/inc/css/images-featured.php';
-if ( file_exists( $hovercraft_images_featured_css ) ) {
-	include $hovercraft_images_featured_css;
-}
-?>
