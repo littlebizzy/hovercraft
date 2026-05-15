a0e7e75c87b10efbf7a0c09616fcd6f2428bd64d
Jesse Nickles <jesse@littlebizzy.com>
2026-05-16 03:53:26 +0700

    Update HOVERCRAFT_VERSION to 0.3.57

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 if ( ! defined( 'HOVERCRAFT_VERSION' ) ) {
-	define( 'HOVERCRAFT_VERSION', '0.3.56' );
+	define( 'HOVERCRAFT_VERSION', '0.3.57' );
 }
 
 // title tag
