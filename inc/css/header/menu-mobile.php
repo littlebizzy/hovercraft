db4f9df519a1458515ddf37a26824eed761ae6e5
Jesse Nickles <jesse@littlebizzy.com>
2026-05-16 04:33:37 +0700

    Format mobile menu CSS

--- inc/css/header/menu-mobile.php
+++ inc/css/header/menu-mobile.php
@@ -9,71 +9,74 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 /* mobile nav container */
 @media screen and (max-width: 1199px) {
-.mobile-nav {
-	display: none;
-	clear: both;
-	width: 100%;
-	list-style: none;
-	background: #212121;
-	padding: 20px;
+	.mobile-nav {
+		display: none;
+		clear: both;
+		width: 100%;
+		list-style: none;
+		background: #212121;
+		padding: 20px;
 	}
 
-/* mobile nav list items */
-.mobile-nav ul li {
-	font-size: 18px;
-	list-style-type: none;
- 	vertical-align: middle;
-	display: block;
-	margin: 0;
-	padding: 0;
-	border-bottom: 1px solid rgba(255,255,255, 0.2);
-}
+	/* mobile nav list items */
+	.mobile-nav ul li {
+		font-size: 18px;
+		list-style-type: none;
+		vertical-align: middle;
+		display: block;
+		margin: 0;
+		padding: 0;
+		border-bottom: 1px solid rgba(255,255,255, 0.2);
+	}
 
-/* remove border on last item */
-.mobile-nav ul li:last-child {
-	border-bottom: 0;
-}
-	
-/* mobile nav links */
-.mobile-nav a {
-	display: block;
-	width: 100%;
-	line-height: 40px;
-	color: #ffffff !important;
-	font-size: 18px;	
-	text-decoration: none;
-}
+	/* remove border on last item */
+	.mobile-nav ul li:last-child {
+		border-bottom: 0;
+	}
 
-/* hide submenus by default */
- .mobile-nav li > ul { display: none; width: 100%; position: relative; }
+	/* mobile nav links */
+	.mobile-nav a {
+		display: block;
+		width: 100%;
+		line-height: 40px;
+		color: #ffffff !important;
+		font-size: 18px;
+		text-decoration: none;
+	}
 
+	/* hide submenus by default */
+	.mobile-nav li > ul {
+		display: none;
+		width: 100%;
+		position: relative;
+	}
 }
 
 /* hide mobile nav on desktop */
 @media screen and (min-width: 1200px) {
-.mobile-nav {
-	display: none !important;
+	.mobile-nav {
+		display: none !important;
 	}
 }
 
 /* hamburger icon wrapper with touch-safe area */
 @media screen and (max-width: 1199px) {
-.menu-mobile-wrapper {
-	display: inline-flex;
-	justify-content: center;
-	align-items: center;
-	padding: 10px;
-	min-width: 44px;
-	min-height: 44px;
-	text-align: right;
-	vertical-align: middle;
+	.menu-mobile-wrapper {
+		display: inline-flex;
+		justify-content: center;
+		align-items: center;
+		padding: 10px;
+		min-width: 44px;
+		min-height: 44px;
+		text-align: right;
+		vertical-align: middle;
 	}
 }
-	
+
 /* hide hamburger wrapper on desktop */
 @media screen and (min-width: 1200px) {
-.menu-mobile-wrapper {
- 	display: none;	
+	.menu-mobile-wrapper {
+		display: none;
 	}
 }
 
@@ -98,26 +101,27 @@ if ( ! defined( 'ABSPATH' ) ) {
 	cursor: pointer;
 }
 
-.overlay {   
-	position: fixed; 
+.overlay {
+	position: fixed;
 	overflow-x: hidden;
-   	overflow-y: scroll;
-   	overscroll-behavior: contain;
-   	top: 0; right: 0; bottom: 0; left: 0; 
-
+	overflow-y: scroll;
+	overscroll-behavior: contain;
+	top: 0;
+	right: 0;
+	bottom: 0;
+	left: 0;
 	height: 0;
-  	z-index: 999999; 
-  	background-color: rgba(0,0,0, 0.9); 
-  	transition: height 0.5s ease; 
+	z-index: 999999;
+	background-color: rgba(0,0,0, 0.9);
+	transition: height 0.5s ease;
 }
 
 .overlay-content {
 	max-width: 768px;
-	width: 100%; 
+	width: 100%;
 	height: 100%;
-	
-  	position: relative;
-  	text-align: center; 
+	position: relative;
+	text-align: center;
 }
 
 .overlay ul.menu {
@@ -128,7 +132,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 .overlay ul a {
 	display: block;
-  	padding: 5px 0;
+	padding: 5px 0;
 	font-size: 18px;
 }
 
@@ -138,21 +142,21 @@ if ( ! defined( 'ABSPATH' ) ) {
 	padding-left: 0;
 	font-style: italic;
 }
-	
+
 .overlay a {
-  	text-decoration: none;
-  	color: #818181;
-  	transition: color 0.3s ease;
+	text-decoration: none;
+	color: #818181;
+	transition: color 0.3s ease;
 }
 
 .overlay li.current-menu-item a {
 	color: #ffffff;
 }
 
-.overlay a:hover, 
+.overlay a:hover,
 .overlay a:focus {
-  	color: #f1f1f1;
-  	text-decoration: none;
+	color: #f1f1f1;
+	text-decoration: none;
 }
 
 .overlay .closebtn {
@@ -163,10 +167,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 .overlay .closebtn i {
-  	position: absolute;
-  	top: 20px;
-  	right: 20px;
-  	font-size: 36px;
+	position: absolute;
+	top: 20px;
+	right: 20px;
+	font-size: 36px;
 	z-index: 999999;
 }
 
@@ -175,19 +179,18 @@ if ( ! defined( 'ABSPATH' ) ) {
 	text-decoration: none;
 }
 
-
-	
 @media screen and (max-width: 800px) {
-	nav#site-navigation{
+	nav#site-navigation {
 		display: none;
 	}
-	.mobile-menu-trig{
+
+	.mobile-menu-trig {
 		display: inline;
 	}
-
 }
+
 /* end mobile menu overlay */
-	
+
 #hovercraft-offcanvas-menu {
 	top: 0;
 	left: -100%;
@@ -304,7 +307,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	transition: opacity 0.3s ease, visibility 0.3s ease;
 	z-index: 99;
 }
-	
+
 .hovercraft-offcanvas-overlay.active {
 	visibility: visible;
 	opacity: 1;
