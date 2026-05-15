4750e1fb276a38d63ab8b2541d17d9c2e8e97ae4
Jesse Nickles <jesse@littlebizzy.com>
2026-05-16 00:06:47 +0700

    Bump version to 0.3.50

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 if ( ! defined( 'HOVERCRAFT_VERSION' ) ) {
-	define( 'HOVERCRAFT_VERSION', '0.3.49' );
+	define( 'HOVERCRAFT_VERSION', '0.3.50' );
 }
 
 // title tag
