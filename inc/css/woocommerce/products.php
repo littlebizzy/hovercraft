56ce82cfa8489fc54e592ab6f8ac25059a3d8a7b
Jesse Nickles <jesse@littlebizzy.com>
2026-05-17 01:03:53 +0700

    Normalize WooCommerce products CSS formatting

--- inc/css/woocommerce/products.php
+++ inc/css/woocommerce/products.php
@@ -8,7 +8,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 
 /* woocommerce products unordered list */
-	
+
 .woocommerce ul.products {
 	margin: 0 !important;
 	padding: 30px 0 0 0 !important;
@@ -16,7 +16,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 .woocommerce ul.products li.product {
 	float: left;
-    list-style: none outside !important;
+	list-style: none outside !important;
 	position: relative;
 	margin-bottom: 30px !important;
 	padding: 0;
