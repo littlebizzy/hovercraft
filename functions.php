a0112848dc8168a020d93aa987c0fa229f59af88
Jesse Nickles <jesse@littlebizzy.com>
2026-05-17 01:25:43 +0700

    Bump runtime version to 0.3.63

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 if ( ! defined( 'HOVERCRAFT_VERSION' ) ) {
-	define( 'HOVERCRAFT_VERSION', '0.3.62' );
+	define( 'HOVERCRAFT_VERSION', '0.3.63' );
 }
 
 // title tag
