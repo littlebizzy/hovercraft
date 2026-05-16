8e9df225c2ebda3da3f362919ae3dea587ae6331
Jesse Nickles <jesse@littlebizzy.com>
2026-05-16 17:13:24 +0700

    Simplify site title CSS

--- inc/css/header/site-title.php
+++ inc/css/header/site-title.php
@@ -9,25 +9,25 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 /* site title (site name) */
 
-@media screen and (max-width: 1199px) {
 .site-title {
 	font-family: <?php echo hovercraft_format_css_font_family( $site_name_font_family, $default_font_family ); ?>;
 	text-transform: <?php echo $site_name_text_transform; ?>;
-	font-size: <?php echo $site_name_mobile_font_size; ?>px;
 	font-weight: <?php echo $site_name_font_weight; ?>;
-	line-height: 1; /* site-tagline is hidden on mobile anyways */
-	display: <?php echo $site_name_display_mobile; ?>;
+}
+
+@media screen and (max-width: 1199px) {
+	.site-title {
+		font-size: <?php echo $site_name_mobile_font_size; ?>px;
+		line-height: 1; /* site-tagline is hidden on mobile anyways */
+		display: <?php echo $site_name_display_mobile; ?>;
 	}
 }
 
 @media screen and (min-width: 1200px) {
-.site-title {
-	font-family: <?php echo hovercraft_format_css_font_family( $site_name_font_family, $default_font_family ); ?>;
-	text-transform: <?php echo $site_name_text_transform; ?>;
-	font-size: <?php echo $site_name_desktop_font_size; ?>px;
-	font-weight: <?php echo $site_name_font_weight; ?>;
-	line-height: 1; /* therefore site-tagline requires padding-top */
-	display: block;
+	.site-title {
+		font-size: <?php echo $site_name_desktop_font_size; ?>px;
+		line-height: 1; /* therefore site-tagline requires padding-top */
+		display: block;
 	}
 }
 
