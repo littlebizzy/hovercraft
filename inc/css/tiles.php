2ac3b02f8f0435ef2ad9ac72f959047e20d6dd17
Jesse Nickles <jesse@littlebizzy.com>
2026-05-16 17:35:29 +0700

    Normalize tiles CSS formatting

--- inc/css/tiles.php
+++ inc/css/tiles.php
@@ -12,52 +12,52 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 @media screen and (max-width: 1199px) {
-#tiles {
-	padding: 30px 20px;
+	#tiles {
+		padding: 30px 20px;
 	}
 }
 
 @media screen and (min-width: 1200px) {
-#tiles {
-	display: grid;
-  	<?php if ( $tiles_across == '3' ) { 
+	#tiles {
+		display: grid;
+		<?php if ( $tiles_across == '3' ) { 
 			echo "grid-template-columns: repeat(3, 1fr);\n"; 
 		} elseif ( $tiles_across == '4' ) { 
 			echo "grid-template-columns: repeat(4, 1fr);\n"; 
-	} ?>
-  	gap: 30px;
+		} ?>
+		gap: 30px;
 	}
 }
 
 @media screen and (max-width: 1199px) {
-#tiles-wide {
-	padding: 0;
+	#tiles-wide {
+		padding: 0;
 	}
 }
 
 @media screen and (min-width: 1200px) {
-#tiles-wide {
-	display: grid;
-  	<?php if ( $tiles_across == '3' ) { 
+	#tiles-wide {
+		display: grid;
+		<?php if ( $tiles_across == '3' ) { 
 			echo "grid-template-columns: repeat(3, minmax(200px, 1fr));\n";
 		} elseif ( $tiles_across == '4' ) { 
 			echo "grid-template-columns: repeat(4, minmax(200px, 1fr));\n";
-	} ?>
-  	gap: 30px;
+		} ?>
+		gap: 30px;
 	}
 }
 
 @media screen and (max-width: 1199px) {
-#tiles-narrow {
-	padding: 0;
+	#tiles-narrow {
+		padding: 0;
 	}
 }
 
 @media screen and (min-width: 1200px) {
-#tiles-narrow {
-	display: grid;
-	grid-template-columns: repeat(2, minmax(200px, 1fr));
-	gap: 30px;
+	#tiles-narrow {
+		display: grid;
+		grid-template-columns: repeat(2, minmax(200px, 1fr));
+		gap: 30px;
 	}
 }
 
@@ -69,18 +69,18 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 @media screen and (max-width: 1199px) {
-.tile {
-	width: 100%;
-	margin-bottom: 30px;
+	.tile {
+		width: 100%;
+		margin-bottom: 30px;
 	}
 }
 
 @media screen and (min-width: 1200px) {
-.tile {
-	max-width: 100%;
-  	justify-content: center;
-  	align-items: center;
-	display: inline-block;
+	.tile {
+		max-width: 100%;
+		justify-content: center;
+		align-items: center;
+		display: inline-block;
 	}
 }
 
@@ -99,7 +99,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 .tile p:last-child {
 	margin-bottom: 0;
 }
-
 
 .tile p.wp-caption-text {
 	<?php if ( $tiles_captions == 'none') { echo "display: none;"; } ?>
