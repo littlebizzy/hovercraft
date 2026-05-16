a68136cabadea4a0f5f62b3c4e7f7aac9a47af8b
Jesse Nickles <jesse@littlebizzy.com>
2026-05-16 13:11:40 +0700

    Format table styles

--- inc/css/tables.php
+++ inc/css/tables.php
@@ -10,20 +10,20 @@ if ( ! defined( 'ABSPATH' ) ) {
 /* tables */
 
 @media screen and (max-width: 1199px) {
-table {
-	width: 100%;
-	margin-bottom: 20px;
-	border: 1px solid #dddddd;
-	border-collapse: collapse;
+	table {
+		width: 100%;
+		margin-bottom: 20px;
+		border: 1px solid #dddddd;
+		border-collapse: collapse;
 	}
 }
 
 @media screen and (min-width: 1200px) {
-table {
-	width: 100%;
-	margin-bottom: 30px;
-	border: 1px solid #dddddd;
-	border-collapse: collapse;
+	table {
+		width: 100%;
+		margin-bottom: 30px;
+		border: 1px solid #dddddd;
+		border-collapse: collapse;
 	}
 }
 
@@ -31,7 +31,8 @@ table tr:first-child {
 	background: #f5f5f5;
 }
 
-table th, table td {
+table th,
+table td {
 	padding: 10px;
 	border: 1px solid #dddddd;
 	vertical-align: top;
