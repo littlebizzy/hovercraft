ed181bec2ade0b5a8c324f87f0f08a4426082268
Jesse Nickles <jesse@littlebizzy.com>
2026-05-16 17:43:44 +0700

    Simplify code snippet CSS

--- inc/css/code-snippets.php
+++ inc/css/code-snippets.php
@@ -14,31 +14,26 @@ code {
 	padding: 0 5px;
 }
 
+pre {
+	font-family: ui-monospace, SFMono-Regular, Consolas, 'Liberation Mono', Menlo, monospace;
+	font-weight: 400;
+	background: #ECEFF1; /* correct */
+	padding: 10px;
+	margin-bottom: 30px;
+	overflow-x: scroll;
+	white-space: pre;
+	color: <?php echo $default_text_color; ?>;
+}
+
 @media screen and (max-width: 1199px) {
 	pre {
 		width: calc(100% - 40px) !important;
-		font-family: ui-monospace, SFMono-Regular, Consolas, 'Liberation Mono', Menlo, monospace;
-		font-weight: 400;
-		background: #ECEFF1; /* correct */
-		padding: 10px;
-		margin-bottom: 30px;
-		overflow-x: scroll;
-		white-space: pre;
-		color: <?php echo $default_text_color; ?>;
 	}
 }
 
 @media screen and (min-width: 1200px) {
 	pre {
 		width: 100%;
-		font-family: ui-monospace, SFMono-Regular, Consolas, 'Liberation Mono', Menlo, monospace;
-		font-weight: 400;
-		background: #ECEFF1; /* correct */
-		padding: 10px;
-		margin-bottom: 30px;
-		overflow-x: scroll;
-		white-space: pre;
-		color: <?php echo $default_text_color; ?>;
 	}
 }
 
