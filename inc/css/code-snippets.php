8b316d68d8a6bbbbd559878659843755c4f97e72
Jesse Nickles <jesse@littlebizzy.com>
2026-05-16 12:53:20 +0700

    Format code snippet styles

--- inc/css/code-snippets.php
+++ inc/css/code-snippets.php
@@ -15,30 +15,30 @@ code {
 }
 
 @media screen and (max-width: 1199px) {
-pre {
-	width: calc(100% - 40px) !important;
-	font-family: ui-monospace, SFMono-Regular, Consolas, 'Liberation Mono', Menlo, monospace;
-	font-weight: 400;
-	background: #ECEFF1; /* correct */
-	padding: 10px;
-	margin-bottom: 30px;
-	overflow-x: scroll;
-	white-space: pre;
-	color: <?php echo $default_text_color; ?>;
+	pre {
+		width: calc(100% - 40px) !important;
+		font-family: ui-monospace, SFMono-Regular, Consolas, 'Liberation Mono', Menlo, monospace;
+		font-weight: 400;
+		background: #ECEFF1; /* correct */
+		padding: 10px;
+		margin-bottom: 30px;
+		overflow-x: scroll;
+		white-space: pre;
+		color: <?php echo $default_text_color; ?>;
 	}
 }
 
 @media screen and (min-width: 1200px) {
-pre {
-	width: 100%;
-	font-family: ui-monospace, SFMono-Regular, Consolas, 'Liberation Mono', Menlo, monospace;
-	font-weight: 400;
-	background: #ECEFF1; /* correct */
-	padding: 10px;
-	margin-bottom: 30px;
-	overflow-x: scroll;
-	white-space: pre;
-	color: <?php echo $default_text_color; ?>;
+	pre {
+		width: 100%;
+		font-family: ui-monospace, SFMono-Regular, Consolas, 'Liberation Mono', Menlo, monospace;
+		font-weight: 400;
+		background: #ECEFF1; /* correct */
+		padding: 10px;
+		margin-bottom: 30px;
+		overflow-x: scroll;
+		white-space: pre;
+		color: <?php echo $default_text_color; ?>;
 	}
 }
 
