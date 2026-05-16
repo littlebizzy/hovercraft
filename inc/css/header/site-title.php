881dcd95ed0f7cde80e5ac6132e49436d20d1aef
Jesse Nickles <jesse@littlebizzy.com>
2026-05-17 01:07:01 +0700

    Simplify site title CSS

--- inc/css/header/site-title.php
+++ inc/css/header/site-title.php
@@ -13,12 +13,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 	font-family: <?php echo hovercraft_format_css_font_family( $site_name_font_family, $default_font_family ); ?>;
 	text-transform: <?php echo $site_name_text_transform; ?>;
 	font-weight: <?php echo $site_name_font_weight; ?>;
+	line-height: 1;
 }
 
 @media screen and (max-width: 1199px) {
 	.site-title {
 		font-size: <?php echo $site_name_mobile_font_size; ?>px;
-		line-height: 1; /* site-tagline is hidden on mobile anyways */
 		display: <?php echo $site_name_display_mobile; ?>;
 	}
 }
@@ -26,15 +26,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 @media screen and (min-width: 1200px) {
 	.site-title {
 		font-size: <?php echo $site_name_desktop_font_size; ?>px;
-		line-height: 1; /* therefore site-tagline requires padding-top */
 		display: block;
 	}
 }
 
 .site-title-link {
-	<?php if ( $tagline_display == 'right_of_site_title' ) { 
-			echo "display: inline-block;\n"; 
-		} else { 
-			echo "display: block;\n"; 
-		}  ?>
+	<?php if ( $tagline_display == 'right_of_site_title' ) {
+		echo "display: inline-block;\n";
+	} else {
+		echo "display: block;\n";
+	} ?>
 }
