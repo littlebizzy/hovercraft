5e8750824eb7514fac341f0a03a158ca8e4209ae
Jesse Nickles <jesse@littlebizzy.com>
2026-05-17 02:29:03 +0700

    Clean up base form styles

--- inc/css/forms.php
+++ inc/css/forms.php
@@ -14,114 +14,100 @@ form {
 	padding: 0 !important;
 }
 
-input,
-select,
-textarea {
+label {
+	display: block;
+	margin-bottom: 6px;
+	font-size: 15px;
+	font-weight: 600;
+	line-height: 1.4;
+}
+
+fieldset {
+	margin: 0 0 20px;
+	padding: 20px;
+	border: 1px solid #e0e0e0;
+}
+
+legend {
+	padding: 0 6px;
+	font-size: 16px;
+	font-weight: 600;
+	line-height: 1.4;
+}
+
+input:not([type="button"]):not([type="checkbox"]):not([type="file"]):not([type="hidden"]):not([type="radio"]):not([type="range"]):not([type="reset"]):not([type="submit"]), select, textarea {
 	appearance: none;
 	-webkit-appearance: none;
 	-moz-appearance: none;
+	display: inline-block;
 	width: 100%;
-	height: auto;
-	vertical-align: middle;
-	font-weight: 400;
-	line-height: 2;
-	padding: 10px 20px !important;
-	border: none !important;
-	font-size: 16px;
+	min-height: 46px;
+	padding: 10px 16px !important;
 	background: #ffffff;
-	display: inline-block;
+	border: 0 !important;
+	border-radius: 0;
 	box-shadow: inset 0 0 0 1px <?php echo $breadcrumbs_text_color; ?>;
+	color: <?php echo $default_text_color; ?>;
 	font-family: inherit;
-	transition: background-color 0.15s ease-in-out, color 0.15s ease-in-out, box-shadow 0.15s ease-in-out, border-color 0.15s ease-in-out;
-}
-
-button {
-	height: auto;
-	vertical-align: middle;
-	font-weight: 400;
-	line-height: 2;
-	padding: 10px 20px !important;
-	border: 0;
 	font-size: 16px;
-	background: #ffffff;
-	display: inline-block;
-	box-shadow: inset 0 0 0 1px <?php echo $breadcrumbs_text_color; ?>;
-	font-family: inherit;
-	transition: background-color 0.15s ease-in-out, color 0.15s ease-in-out, box-shadow 0.15s ease-in-out, border-color 0.15s ease-in-out;
+	font-weight: 400;
+	line-height: 1.5;
+	vertical-align: middle;
+	transition: background-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out, color 0.15s ease-in-out;
 }
 
 select {
+	padding-right: 44px !important;
 	background-image: url('data:image/svg+xml;utf8,<svg fill="%23666" height="20" viewBox="0 0 24 24" width="20" xmlns="http://www.w3.org/2000/svg"><path d="M7 10l5 5 5-5z"/></svg>');
+	background-position: right 14px center;
 	background-repeat: no-repeat;
-	background-position: right 15px center;
 	background-size: 16px;
-	padding-right: 45px !important;
-	border-radius: 0;
 }
 
 option {
-	line-height: 1.5;
 	padding: 0 !important;
-	white-space: nowrap;
 	font-family: inherit;
+	line-height: 1.5;
+	white-space: nowrap;
 }
 
 textarea {
-	font-size: 16px;
-	font-weight: 400;
-	padding: 10px 20px !important;
-	border-radius: 0;
-	box-shadow: inset 0 0 0 1px <?php echo $breadcrumbs_text_color; ?>;
+	min-height: 140px;
 	resize: vertical;
-	font-family: inherit;
 }
 
-input[type="checkbox"],
-input[type="radio"] {
+input[type="checkbox"], input[type="radio"] {
+	appearance: auto;
+	-webkit-appearance: auto;
+	-moz-appearance: auto;
+	display: inline-block;
 	width: auto;
 	height: auto;
-	display: inline-block;
-	margin-right: 10px;
-	margin-top: 2px;
+	margin: 2px 10px 0 0;
 	box-shadow: none;
 	cursor: pointer;
+	vertical-align: middle;
 }
 
 input[type="file"] {
+	width: 100%;
 	padding: 10px 0 !important;
-	box-shadow: none;
 	background: transparent;
+	box-shadow: none;
+	font-family: inherit;
+	font-size: 16px;
+	line-height: 1.5;
 	transition: none;
 }
 
 input[type="range"] {
+	appearance: none;
+	-webkit-appearance: none;
 	width: 100%;
 	padding: 0 !important;
 	background: transparent;
-	cursor: pointer;
 	box-shadow: none;
-	appearance: none;
-	-webkit-appearance: none;
-}
-
-input[type="range"]::-webkit-slider-thumb {
-	appearance: none;
-	height: 16px;
-	width: 16px;
-	border-radius: 50%;
-	background: <?php echo $default_link_color; ?>;
 	cursor: pointer;
-	margin-top: -6px;
-	border: none;
-}
-
-input[type="range"]::-moz-range-thumb {
-	height: 16px;
-	width: 16px;
-	border-radius: 50%;
-	background: <?php echo $default_link_color; ?>;
-	cursor: pointer;
-	border: none;
 }
 
 input[type="range"]::-webkit-slider-runnable-track {
@@ -136,88 +122,72 @@ input[type="range"]::-moz-range-track {
 	border-radius: 2px;
 }
 
-input[type="submit"],
-button[type="submit"] {
-	width: 100%;
-	font-size: 18px;
-	font-weight: 600;
-	line-height: 1;
-	padding: 10px 20px !important;
+input[type="range"]::-webkit-slider-thumb {
+	appearance: none;
+	-webkit-appearance: none;
+	width: 16px;
+	height: 16px;
+	margin-top: -6px;
 	background: <?php echo $default_link_color; ?>;
-	text-decoration: none;
-	color: #ffffff;
-	border-radius: 0;
 	border: 0;
+	border-radius: 50%;
 	cursor: pointer;
+}
+
+input[type="range"]::-moz-range-thumb {
+	width: 16px;
+	height: 16px;
+	background: <?php echo $default_link_color; ?>;
+	border: 0;
+	border-radius: 50%;
+	cursor: pointer;
+}
+
+form button, input[type="button"], input[type="reset"], input[type="submit"] {
+	display: inline-block;
+	height: auto;
+	padding: 12px 20px !important;
+	background: <?php echo $default_link_color; ?>;
+	border: 0;
+	border-radius: 0;
 	box-shadow: none;
+	color: #ffffff;
+	cursor: pointer;
+	font-family: inherit;
+	font-size: 16px;
+	font-weight: 600;
+	line-height: 1.2;
+	text-align: center;
+	text-decoration: none;
+	vertical-align: middle;
 	transition: background-color 0.15s ease-in-out, color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
 }
 
-input[type="submit"]:hover,
-button[type="submit"]:hover {
+form button:hover, input[type="button"]:hover, input[type="reset"]:hover, input[type="submit"]:hover {
 	background: <?php echo $default_hover_color; ?> !important;
+}
+
+input::placeholder, textarea::placeholder {
+	color: #999999;
+	opacity: 1;
+}
+
+input:disabled, select:disabled, textarea:disabled, button:disabled {
+	background: #f5f5f5;
+	box-shadow: inset 0 0 0 1px #dddddd;
+	color: #999999;
+	cursor: not-allowed;
+}
+
+input[readonly], textarea[readonly] {
+	background: #f5f5f5;
+	box-shadow: inset 0 0 0 1px #cccccc;
+	color: #333333;
 }
 
 #topbar input,
 .hero-half input {
 	box-shadow: none;
-}
-
-label {
-	display: block;
-	margin-bottom: 5px;
-	font-weight: 600;
-	font-size: 15px;
-}
-
-fieldset {
-	border: 1px solid #e0e0e0;
-	padding: 20px;
-	margin-bottom: 20px;
-}
-
-legend {
-	font-weight: 600;
-	font-size: 16px;
-	padding: 0 5px;
-}
-
-input:focus,
-select:focus,
-textarea:focus,
-button:focus {
-	box-shadow: 0 0 0 2px rgba(100, 100, 100, 0.15);
-}
-
-input:focus:not(:focus-visible),
-select:focus:not(:focus-visible),
-textarea:focus:not(:focus-visible),
-button:focus:not(:focus-visible) {
-	outline: none;
-}
-
-input::placeholder,
-textarea::placeholder {
-	color: #999999;
-	font-style: italic;
-	opacity: 1;
-}
-
-input:disabled,
-select:disabled,
-textarea:disabled,
-button:disabled {
-	background: #f5f5f5;
-	color: #999999;
-	cursor: not-allowed;
-	box-shadow: inset 0 0 0 1px #dddddd;
-}
-
-input[readonly],
-textarea[readonly] {
-	background: #f5f5f5;
-	color: #333333;
-	box-shadow: inset 0 0 0 1px #cccccc;
 }
 
 [dir="rtl"] input,
@@ -227,32 +197,21 @@ textarea[readonly] {
 }
 
 [dir="rtl"] select {
-	background-position: left 15px center;
-	padding-left: 45px !important;
-	padding-right: 20px !important;
-}
-
-input:invalid,
-select:invalid,
-textarea:invalid {
-	background-color: #fff8f8;
-	box-shadow: inset 0 0 0 1px #e57373;
+	padding-right: 16px !important;
+	padding-left: 44px !important;
+	background-position: left 14px center;
 }
 
 /* responsive button widths */
 
 @media screen and (max-width: 1199px) {
-	button,
-	button.button,
-	input.button {
+	form button, input[type="button"], input[type="reset"], input[type="submit"] {
 		width: 100%;
 	}
 }
 
 @media screen and (min-width: 1200px) {
-	button,
-	button.button,
-	input.button {
+	form button, input[type="button"], input[type="reset"], input[type="submit"] {
 		width: auto;
 	}
 }
