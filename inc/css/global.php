653d36dad98c461d24a7a142d25da9b97baacae9
Jesse Nickles <jesse@littlebizzy.com>
2026-05-16 17:37:51 +0700

    Simplify global CSS

--- inc/css/global.php
+++ inc/css/global.php
@@ -7,35 +7,31 @@ if ( ! defined( 'ABSPATH' ) ) {
 // styles for global styles
 ?>
 
-@media screen and (max-width: 1199px) {
 html, body {
-    height: 100%;
-    font-family: <?php echo hovercraft_format_css_font_family( $default_font_family, $first_font_family ); ?>;
-    font-size: <?php echo $default_mobile_font_size; ?>px;
-    line-height: 1.5;
-    color: <?php echo $default_text_color; ?>;
-    scroll-behavior: smooth;
-    }
+	height: 100%;
+	font-family: <?php echo hovercraft_format_css_font_family( $default_font_family, $first_font_family ); ?>;
+	line-height: 1.5;
+	color: <?php echo $default_text_color; ?>;
+	scroll-behavior: smooth;
+}
+
+@media screen and (max-width: 1199px) {
+	html, body {
+		font-size: <?php echo $default_mobile_font_size; ?>px;
+	}
 }
 
 @media screen and (min-width: 1200px) {
-html, body {
-    height: 100%;
-    font-family: <?php echo hovercraft_format_css_font_family( $default_font_family, $first_font_family ); ?>;
-    font-size: <?php echo $default_desktop_font_size; ?>px;
-    line-height: 1.5;
-    color: <?php echo $default_text_color; ?>;
-    scroll-behavior: smooth;
-    }
+	html, body {
+		font-size: <?php echo $default_desktop_font_size; ?>px;
+	}
 }
-
 
 /* body frozen (triggered by offcanvas menu js) */
 
 body.frozen {
 	overflow: hidden;
 }
-
 
 /* misc */
 
@@ -44,21 +40,20 @@ strong {
 }
 
 em {
-    font-style: italic;
+	font-style: italic;
 }
-
 
 /* paragraphs */
 
 @media screen and (max-width: 1199px) {
-p {
-	margin-bottom: 20px;
+	p {
+		margin-bottom: 20px;
 	}
 }
 
 @media screen and (min-width: 1200px) {
-p {
-	margin-bottom: 30px;
+	p {
+		margin-bottom: 30px;
 	}
 }
 
@@ -66,23 +61,18 @@ p {
 	clear: both;
 }
 
-
 /* general */
 
-@media screen and (max-width: 1199px) {
 #container {
 	width: 100%;
 	height: 100%;
-	position: relative; /* required for mobile menu transform */
-	transform: translate3d(0, 0, 0);
-  	transition: transform 0.5s;
-	}
 }
 
-@media screen and (min-width: 1200px) {
-#container {
-	width: 100%;
-	height: 100%;
+@media screen and (max-width: 1199px) {
+	#container {
+		position: relative; /* required for mobile menu transform */
+		transform: translate3d(0, 0, 0);
+		transition: transform 0.5s;
 	}
 }
 
@@ -92,54 +82,55 @@ p {
 	margin: 0 auto;
 }
 
-@media screen and (max-width: 1199px) {
 .full {
 	max-width: 100%;
-	padding: 0 0;
+}
+
+@media screen and (max-width: 1199px) {
+	.full {
+		padding: 0 0;
 	}
 }
 
 @media screen and (min-width: 1200px) {
-.full {
-	max-width: 100%;
-	padding: 0 40px;
+	.full {
+		padding: 0 40px;
 	}
 }
 
-
 /* wordpress video/media container */
 @media screen and (max-width: 1199px) {
-.mejs-container {
-	margin-bottom: 20px;
+	.mejs-container {
+		margin-bottom: 20px;
 	}
 }
 
 /* wordpress video/media container */
 @media screen and (min-width: 1200px) {
-.mejs-container {
-	margin-bottom: 30px;
+	.mejs-container {
+		margin-bottom: 30px;
 	}
 }
-
 
 iframe {
 	position: relative;
-    display: block;
+	display: block;
 }
 
-@media screen and (max-width: 1199px) {
 hr {
 	background: <?php echo $default_text_color; ?>;
 	height: 1px;
-	margin-bottom: 20px;
+}
+
+@media screen and (max-width: 1199px) {
+	hr {
+		margin-bottom: 20px;
 	}
 }
 
 @media screen and (min-width: 1200px) {
-hr {
-	background: <?php echo $default_text_color; ?>;
-	height: 1px;
-	margin-bottom: 30px;
+	hr {
+		margin-bottom: 30px;
 	}
 }
 
