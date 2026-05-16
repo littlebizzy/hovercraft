39bdff708eccb483597672d88face62dda85f485
Jesse Nickles <jesse@littlebizzy.com>
2026-05-16 13:06:17 +0700

    Format pagination styles

--- inc/css/pagination.php
+++ inc/css/pagination.php
@@ -18,22 +18,22 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 .navigation li {
-    display: inline-block;
+	display: inline-block;
 }
-  
+
 .navigation li a,
 .navigation li a:hover,
 .navigation li.active a,
 .navigation li.disabled {
-    background-color: #ECEFF1;
+	background-color: #ECEFF1;
 	color: #263238;
-    cursor: pointer;
-    padding: 5px 10px;
+	cursor: pointer;
+	padding: 5px 10px;
 	text-decoration: none;
 	font-size: 14px;
 	line-height: 1;
 }
-  
+
 .navigation li a:hover,
 .navigation li.active a {
 	background-color: <?php echo $default_text_color; ?>;
