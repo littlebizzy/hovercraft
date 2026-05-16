c6093e497d8c5ff4da96153e03a6fe344d086f63
Jesse Nickles <jesse@littlebizzy.com>
2026-05-16 17:39:02 +0700

    Simplify heading CSS

--- inc/css/headings.php
+++ inc/css/headings.php
@@ -7,115 +7,119 @@ if ( ! defined( 'ABSPATH' ) ) {
 // styles for headings
 ?>
 
-@media screen and (max-width: 1199px) {
 h1 {
 	font-family: <?php echo hovercraft_format_css_font_family( $h1_font_family, $default_font_family ); ?>;
 	font-weight: <?php echo $h1_font_weight; ?>;
-	font-size: <?php echo $h1_mobile_font_size; ?>px;
-	margin-bottom: 20px;
+}
+
+@media screen and (max-width: 1199px) {
+	h1 {
+		font-size: <?php echo $h1_mobile_font_size; ?>px;
+		margin-bottom: 20px;
 	}
 }
 
 @media screen and (min-width: 1200px) {
-h1 {
-	font-family: <?php echo hovercraft_format_css_font_family( $h1_font_family, $default_font_family ); ?>;
-	font-weight: <?php echo $h1_font_weight; ?>;
-	font-size: <?php echo $h1_desktop_font_size; ?>px;
-	margin-bottom: 30px;
+	h1 {
+		font-size: <?php echo $h1_desktop_font_size; ?>px;
+		margin-bottom: 30px;
 	}
 }
 
-@media screen and (max-width: 1199px) {
 h1.divide:after {
-	margin-top: 10px;
-	margin-bottom: 20px;
 	content: '';
 	display: block;
 	border: 1px solid <?php echo $title_divider_background_color; ?>;
+}
+
+@media screen and (max-width: 1199px) {
+	h1.divide:after {
+		margin-top: 10px;
+		margin-bottom: 20px;
 	}
 }
 	
 @media screen and (min-width: 1200px) {
-h1.divide:after {
-	margin-top: 15px;
-	margin-bottom: 30px;
-	content: '';
-	display: block;
-	border: 1px solid <?php echo $title_divider_background_color; ?>;
+	h1.divide:after {
+		margin-top: 15px;
+		margin-bottom: 30px;
 	}
 }
 
-@media screen and (max-width: 1199px) {
 h2 {
 	font-family: <?php echo hovercraft_format_css_font_family( $h2_font_family, $default_font_family ); ?>;
 	font-weight: <?php echo $h2_font_weight; ?>;
-	font-size: <?php echo $h2_mobile_font_size; ?>px;
-	margin-bottom: 20px;
+}
+
+@media screen and (max-width: 1199px) {
+	h2 {
+		font-size: <?php echo $h2_mobile_font_size; ?>px;
+		margin-bottom: 20px;
 	}
 }
 
 @media screen and (min-width: 1200px) {
-h2 {
-	font-family: <?php echo hovercraft_format_css_font_family( $h2_font_family, $default_font_family ); ?>;
-	font-weight: <?php echo $h2_font_weight; ?>;
-	font-size: <?php echo $h2_desktop_font_size; ?>px;
-	margin-bottom: 30px;
+	h2 {
+		font-size: <?php echo $h2_desktop_font_size; ?>px;
+		margin-bottom: 30px;
 	}
 }
 
-@media screen and (max-width: 1199px) {
 h2:after {
-	<?php if ( $h2_divider_display == 'everywhere_possible') { echo "margin-top: 10px;"; } ?>
-	<?php if ( $h2_divider_display == 'everywhere_possible') { echo "margin-bottom: 20px;"; } ?>
 	content: '';
 	display: block;
 	<?php if ( $h2_divider_display == 'everywhere_possible') { echo "border: 1px solid $title_divider_background_color;"; } ?>
+}
+
+@media screen and (max-width: 1199px) {
+	h2:after {
+		<?php if ( $h2_divider_display == 'everywhere_possible') { echo "margin-top: 10px;"; } ?>
+		<?php if ( $h2_divider_display == 'everywhere_possible') { echo "margin-bottom: 20px;"; } ?>
 	}
 }
 	
 @media screen and (min-width: 1200px) {
-h2:after {
-	<?php if ( $h2_divider_display == 'everywhere_possible') { echo "margin-top: 15px;"; } ?>
-	<?php if ( $h2_divider_display == 'everywhere_possible') { echo "margin-bottom: 30px;"; } ?>
-	content: '';
-	display: block;
-	<?php if ( $h2_divider_display == 'everywhere_possible') { echo "border: 1px solid $title_divider_background_color;"; } ?>
+	h2:after {
+		<?php if ( $h2_divider_display == 'everywhere_possible') { echo "margin-top: 15px;"; } ?>
+		<?php if ( $h2_divider_display == 'everywhere_possible') { echo "margin-bottom: 30px;"; } ?>
 	}
 }
 
-@media screen and (max-width: 1199px) {
 h3 {
 	font-family: <?php echo hovercraft_format_css_font_family( $h3_font_family, $default_font_family ); ?>;
 	font-weight: <?php echo $h3_font_weight; ?>;
-	font-size: <?php echo $h3_mobile_font_size; ?>px;
-	margin-bottom: 20px;
+}
+
+@media screen and (max-width: 1199px) {
+	h3 {
+		font-size: <?php echo $h3_mobile_font_size; ?>px;
+		margin-bottom: 20px;
 	}
 }
 
 @media screen and (min-width: 1200px) {
-h3 {
-	font-family: <?php echo hovercraft_format_css_font_family( $h3_font_family, $default_font_family ); ?>;
-	font-weight: <?php echo $h3_font_weight; ?>;
-	font-size: <?php echo $h3_desktop_font_size; ?>px;
-	margin-bottom: 20px;
+	h3 {
+		font-size: <?php echo $h3_desktop_font_size; ?>px;
+		margin-bottom: 20px;
 	}
 }
 
-@media screen and (max-width: 1199px) {
 h4 {
 	font-family: <?php echo hovercraft_format_css_font_family( $h4_font_family, $default_font_family ); ?>;
 	font-weight: <?php echo $h4_font_weight; ?>;
-	font-size: <?php echo $h4_mobile_font_size; ?>px;
-	margin-bottom: 20px;
+}
+
+@media screen and (max-width: 1199px) {
+	h4 {
+		font-size: <?php echo $h4_mobile_font_size; ?>px;
+		margin-bottom: 20px;
 	}
 }
 
 @media screen and (min-width: 1200px) {
-h4 {
-	font-family: <?php echo hovercraft_format_css_font_family( $h4_font_family, $default_font_family ); ?>;
-	font-weight: <?php echo $h4_font_weight; ?>;
-	font-size: <?php echo $h4_desktop_font_size; ?>px;
-	margin-bottom: 20px;
+	h4 {
+		font-size: <?php echo $h4_desktop_font_size; ?>px;
+		margin-bottom: 20px;
 	}
 }
 
@@ -125,23 +129,24 @@ h4.sitename-offcanvas {
 	line-height: 1;
 	margin-bottom: 20px;
 	padding-left: 20px;
-	}
+}
 
-@media screen and (max-width: 1199px) {
 h5 {
 	font-family: <?php echo hovercraft_format_css_font_family( $h5_font_family, $default_font_family ); ?>;
 	font-weight: <?php echo $h5_font_weight; ?>;
-	font-size: <?php echo $h5_mobile_font_size; ?>px;
-	margin-bottom: 20px;
+}
+
+@media screen and (max-width: 1199px) {
+	h5 {
+		font-size: <?php echo $h5_mobile_font_size; ?>px;
+		margin-bottom: 20px;
 	}
 }
 
 @media screen and (min-width: 1200px) {
-h5 {
-	font-family: <?php echo hovercraft_format_css_font_family( $h5_font_family, $default_font_family ); ?>;
-	font-weight: <?php echo $h5_font_weight; ?>;
-	font-size: <?php echo $h5_desktop_font_size; ?>px;
-	margin-bottom: 20px;
+	h5 {
+		font-size: <?php echo $h5_desktop_font_size; ?>px;
+		margin-bottom: 20px;
 	}
 }
 	
