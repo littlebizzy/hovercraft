f37cedb87a77761188313784ce8e6c872f5b4aff
Jesse Nickles <jesse@littlebizzy.com>
2026-05-16 17:01:50 +0700

    Simplify WooCommerce general CSS

--- inc/css/woocommerce/general.php
+++ inc/css/woocommerce/general.php
@@ -13,33 +13,31 @@ div.coupon {
 	display: flex;
 }
 
-
 .term-description {
 	font-size: 18px;
 	color: #607D8B;
 }
 
-@media screen and (max-width: 1199px) {
 p.woocommerce-result-count {
-	float: none !important;
-	display: block;
-	margin: 0 0 20px 0 !important;
-	padding: 0 !important;
 	font-size: 16px;
 	font-weight: 600;
 	color: <?php echo $breadcrumbs_text_color; ?>;
 	line-height: 1.5 !important;
+}
+
+@media screen and (max-width: 1199px) {
+	p.woocommerce-result-count {
+		float: none !important;
+		display: block;
+		margin: 0 0 20px 0 !important;
+		padding: 0 !important;
 	}
 }
 
 @media screen and (min-width: 1200px) {
-p.woocommerce-result-count {
-	float: left !important;
-	margin: 0 !important;
-	padding: 5px 0 !important;
-	font-size: 16px;
-	font-weight: 600;
-	color: <?php echo $breadcrumbs_text_color; ?>;
-	line-height: 1.5 !important;
+	p.woocommerce-result-count {
+		float: left !important;
+		margin: 0 !important;
+		padding: 5px 0 !important;
 	}
 }
