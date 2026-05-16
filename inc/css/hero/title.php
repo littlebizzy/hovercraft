7b2575d3a63d1bf15e817d70e0b64ba7235cfd65
Jesse Nickles <jesse@littlebizzy.com>
2026-05-16 21:15:20 +0700

    Simplify hero title CSS

--- inc/css/hero/title.php
+++ inc/css/hero/title.php
@@ -10,34 +10,29 @@ if ( ! defined( 'ABSPATH' ) ) {
 /* hero title */
 
 @media screen and (max-width: 1199px) {
-.welcome-wrapper {
-	width: 100%;
-	padding: 30px 20px;
+	.welcome-wrapper {
+		width: 100%;
+		padding: 30px 20px;
 	}
 }
 
 @media screen and (min-width: 1200px) {
-.welcome-wrapper {
-	max-width: 900px;
+	.welcome-wrapper {
+		max-width: 900px;
 	}
 }
 
-@media screen and (max-width: 1199px) {
-.title-wrapper {
-	width: 100%;
-	padding: 0 20px;
-	text-align: center;
-	}
-}
-
-@media screen and (min-width: 1200px) {
 .title-wrapper {
 	width: 100%;
 	text-align: center;
-	}
 }
 
 @media screen and (max-width: 1199px) {
+	.title-wrapper {
+		padding: 0 20px;
+	}
+}
+
 .hero-image-caption {
 	position: absolute;
 	bottom: 0;
@@ -45,24 +40,19 @@ if ( ! defined( 'ABSPATH' ) ) {
 	background: rgba(0, 0, 0, 0.5);
 	color: #ffffff;
 	padding: 5px 10px;
-	font-size: 12px;
 	z-index: 10;
 	text-align: right;
 	max-width: 90%;
+}
+
+@media screen and (max-width: 1199px) {
+	.hero-image-caption {
+		font-size: 12px;
 	}
 }
 
 @media screen and (min-width: 1200px) {
-.hero-image-caption {
-	position: absolute;
-	bottom: 0;
-	right: 0;
-	background: rgba(0, 0, 0, 0.5);
-	color: #ffffff;
-	padding: 5px 10px;
-	font-size: 14px;
-	z-index: 10;
-	text-align: right;
-	max-width: 90%;
+	.hero-image-caption {
+		font-size: 14px;
 	}
 }
