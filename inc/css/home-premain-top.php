463a7904bdb61973c79ee04a23c11de29efd1e92
Jesse Nickles <jesse@littlebizzy.com>
2026-05-16 15:11:32 +0700

    Clean home premain top parent CSS

--- inc/css/home-premain-top.php
+++ inc/css/home-premain-top.php
@@ -7,21 +7,21 @@ if ( ! defined( 'ABSPATH' ) ) {
 // styles for home-premain-top
 ?>
 
+#home-premain-top {
+	width: 100%;
+	background: <?php echo $premain_top_background_color; ?>;
+	color: <?php echo $premain_top_text_color; ?>;
+}
+
 @media screen and (max-width: 1199px) {
 	#home-premain-top {
-		width: 100%;
-		background: <?php echo $premain_top_background_color; ?>;
-		color: <?php echo $premain_top_text_color; ?>;
 		padding: 20px 20px;
 	}
 }
 
 @media screen and (min-width: 1200px) {
 	#home-premain-top {
-		width: 100%;
 		margin: 0 auto;
-		background: <?php echo $premain_top_background_color; ?>;
-		color: <?php echo $premain_top_text_color; ?>;
 		padding: 30px 0;
 	}
 }
