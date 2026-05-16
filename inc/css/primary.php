f34d3096e093cdcbbb1a6eca637a64f8692ec85e
Jesse Nickles <jesse@littlebizzy.com>
2026-05-16 17:17:08 +0700

    Simplify primary layout CSS

--- inc/css/primary.php
+++ inc/css/primary.php
@@ -10,47 +10,46 @@ if ( ! defined( 'ABSPATH' ) ) {
 /* primary layout */
 /* primary is 768px, primary-wide is 1200px, and primary-center is centered 768px */
 
-@media screen and (max-width: 1199px) {
 #primary {
-	width: 100%;
 	padding: 0 0;
+}
+
+@media screen and (max-width: 1199px) {
+	#primary {
+		width: 100%;
 	}
 }
 
 @media screen and (min-width: 1200px) {
-#primary {
-	float: left;
-	width: 768px;
-	padding: 0 0;
+	#primary {
+		float: left;
+		width: 768px;
 	}
 }
 	
 /* primary-wide */
 
-@media screen and (max-width: 1199px) {
 #primary-wide {
 	width: 100%;
-	padding: 0 0;
-	}
 }
 
-@media screen and (min-width: 1200px) {
-#primary-wide {
-	width: 100%;
+@media screen and (max-width: 1199px) {
+	#primary-wide {
+		padding: 0 0;
 	}
 }
 	
 /* primary-center */
 
 @media screen and (max-width: 1199px) {
-#primary-center {
-	width: 100%;
+	#primary-center {
+		width: 100%;
 	}
 }
 
 @media screen and (min-width: 1200px) {
-#primary-center {
-	width: 768px;
-	margin: 0 auto;
+	#primary-center {
+		width: 768px;
+		margin: 0 auto;
 	}
 }
