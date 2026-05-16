724402d4673b5aa3228d700643e083d3b4b0390a
Jesse Nickles <jesse@littlebizzy.com>
2026-05-16 13:37:49 +0700

    Format home premain top styles

--- inc/css/home-premain-top.php
+++ inc/css/home-premain-top.php
@@ -8,50 +8,50 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 
 @media screen and (max-width: 1199px) {
-#home-premain-top {
-	width: 100%;
-	background: <?php echo $premain_top_background_color; ?>;
-	color: <?php echo $premain_top_text_color; ?>;
-	padding: 20px 20px;
+	#home-premain-top {
+		width: 100%;
+		background: <?php echo $premain_top_background_color; ?>;
+		color: <?php echo $premain_top_text_color; ?>;
+		padding: 20px 20px;
 	}
 }
 
 @media screen and (min-width: 1200px) {
-#home-premain-top {
-	width: 100%;
-	margin: 0 auto;
-	background: <?php echo $premain_top_background_color; ?>;
-	color: <?php echo $premain_top_text_color; ?>;
-	padding: 30px 0;
+	#home-premain-top {
+		width: 100%;
+		margin: 0 auto;
+		background: <?php echo $premain_top_background_color; ?>;
+		color: <?php echo $premain_top_text_color; ?>;
+		padding: 30px 0;
 	}
 }
 
 @media screen and (max-width: 1199px) {
-#home-premain-top .inner {
-	width: 100%;
-	padding: 0 0;
+	#home-premain-top .inner {
+		width: 100%;
+		padding: 0 0;
 	}
 }
 
 @media screen and (min-width: 1200px) {
-#home-premain-top .inner {
-	width: 100%;
-	display: grid;
-	<?php
-	if ( $home_premain_top_columns == '1' ) { echo "grid-template-columns: repeat(1, 1fr);\n"; } 
-	elseif ( $home_premain_top_columns == '2' ) { echo "grid-template-columns: repeat(2, 1fr);\n"; } 
-	elseif ( $home_premain_top_columns == '3' ) { echo "grid-template-columns: repeat(3, 1fr);\n"; } 
-	elseif ( $home_premain_top_columns == '4' ) { echo "grid-template-columns: repeat(4, 1fr);\n"; } 
-	elseif ( $home_premain_top_columns == '5' ) { echo "grid-template-columns: repeat(5, 1fr);\n"; } 
-	elseif ( $home_premain_top_columns == '6' ) { echo "grid-template-columns: repeat(6, 1fr);\n"; } 
-	elseif ( $home_premain_top_columns == '7' ) { echo "grid-template-columns: repeat(7, 1fr);\n"; } 
-	elseif ( $home_premain_top_columns == '8' ) { echo "grid-template-columns: repeat(8, 1fr);\n"; } 
-	elseif ( $home_premain_top_columns == '9' ) { echo "grid-template-columns: repeat(9, 1fr);\n"; } 
-	elseif ( $home_premain_top_columns == '10' ) { echo "grid-template-columns: repeat(10, 1fr);\n"; } 
-	elseif ( $home_premain_top_columns == '11' ) { echo "grid-template-columns: repeat(11, 1fr);\n"; } 
-	elseif ( $home_premain_top_columns == '12' ) { echo "grid-template-columns: repeat(12, 1fr);\n"; } 
-	?>
-  	gap: 30px;
+	#home-premain-top .inner {
+		width: 100%;
+		display: grid;
+		<?php
+		if ( $home_premain_top_columns == '1' ) { echo "grid-template-columns: repeat(1, 1fr);\n"; }
+		elseif ( $home_premain_top_columns == '2' ) { echo "grid-template-columns: repeat(2, 1fr);\n"; }
+		elseif ( $home_premain_top_columns == '3' ) { echo "grid-template-columns: repeat(3, 1fr);\n"; }
+		elseif ( $home_premain_top_columns == '4' ) { echo "grid-template-columns: repeat(4, 1fr);\n"; }
+		elseif ( $home_premain_top_columns == '5' ) { echo "grid-template-columns: repeat(5, 1fr);\n"; }
+		elseif ( $home_premain_top_columns == '6' ) { echo "grid-template-columns: repeat(6, 1fr);\n"; }
+		elseif ( $home_premain_top_columns == '7' ) { echo "grid-template-columns: repeat(7, 1fr);\n"; }
+		elseif ( $home_premain_top_columns == '8' ) { echo "grid-template-columns: repeat(8, 1fr);\n"; }
+		elseif ( $home_premain_top_columns == '9' ) { echo "grid-template-columns: repeat(9, 1fr);\n"; }
+		elseif ( $home_premain_top_columns == '10' ) { echo "grid-template-columns: repeat(10, 1fr);\n"; }
+		elseif ( $home_premain_top_columns == '11' ) { echo "grid-template-columns: repeat(11, 1fr);\n"; }
+		elseif ( $home_premain_top_columns == '12' ) { echo "grid-template-columns: repeat(12, 1fr);\n"; }
+		?>
+		gap: 30px;
 	}
 }
 
