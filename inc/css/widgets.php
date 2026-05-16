ede5836ce26d8e638374f814d2cdb032a1e75916
Jesse Nickles <jesse@littlebizzy.com>
2026-05-16 13:14:20 +0700

    Format widget styles

--- inc/css/widgets.php
+++ inc/css/widgets.php
@@ -10,14 +10,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 /* widgets */
 
 .widget-wrapper ul li ul {
-    margin-left: 0 !important;
+	margin-left: 0 !important;
 }
 
 .widget-wrapper ul li ul li {
-    margin-left: 0 !important;
-    list-style-type: circle;
+	margin-left: 0 !important;
+	list-style-type: circle;
 }
-	
- .widget-wrapper iframe {
+
+.widget-wrapper iframe {
 	max-width: 100%;
 }
