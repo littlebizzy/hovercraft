e4323aa57ba403ba07ba719252fc6137a18fa0a9
Jesse Nickles <jesse@littlebizzy.com>
2026-05-16 13:00:14 +0700

    Format featured image styles

--- inc/css/images-featured.php
+++ inc/css/images-featured.php
@@ -10,37 +10,37 @@ if ( ! defined( 'ABSPATH' ) ) {
 /* featured image wrapper */
 
 @media screen and (max-width: 1199px) {
-.featured-image-wrapper {
-	position: relative;
-	margin-bottom: 20px;
-	width: 100%;
-	height: auto;
+	.featured-image-wrapper {
+		position: relative;
+		margin-bottom: 20px;
+		width: 100%;
+		height: auto;
 	}
 }
 
 @media screen and (min-width: 1200px) {
-.featured-image-wrapper {
-	position: relative;
-	margin-bottom: 30px;
-	width: 768px;
-	height: auto;
-	<?php if ( $sitewide_layout == 'floating_islands' ) { 
-		
-		if ( $featured_image_position == 'below_title' ) { 
-			echo "margin-left: -40px;\n"; 
-		} elseif ( $featured_image_position == 'above_title' ) { 
-			echo "margin-left: 0;\n"; 
-		} 
-		
-	} elseif ( $sitewide_layout == 'classic_clean' ) { 
+	.featured-image-wrapper {
+		position: relative;
+		margin-bottom: 30px;
+		width: 768px;
+		height: auto;
+		<?php if ( $sitewide_layout == 'floating_islands' ) {
 
-		if ( $featured_image_position == 'below_title' ) { 
-			echo "margin-left: 0;\n"; 
-		} elseif ( $featured_image_position == 'above_title' ) { 
-			echo "margin-left: 0;\n"; 
-		} 
+			if ( $featured_image_position == 'below_title' ) {
+				echo "margin-left: -40px;\n";
+			} elseif ( $featured_image_position == 'above_title' ) {
+				echo "margin-left: 0;\n";
+			}
 
-	} ?>
+		} elseif ( $sitewide_layout == 'classic_clean' ) {
+
+			if ( $featured_image_position == 'below_title' ) {
+				echo "margin-left: 0;\n";
+			} elseif ( $featured_image_position == 'above_title' ) {
+				echo "margin-left: 0;\n";
+			}
+
+		} ?>
 	}
 }
 
@@ -51,27 +51,27 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 @media screen and (max-width: 1199px) {
-img.featured-image {
-	width: 100%;
-	height: auto;
-	display: block;
-	margin-bottom: 0;
+	img.featured-image {
+		width: 100%;
+		height: auto;
+		display: block;
+		margin-bottom: 0;
 	}
 }
 
 @media screen and (min-width: 1200px) {
-img.featured-image {
-	<?php if ( $sitewide_layout == 'floating_islands' ) { 
-			echo "width: 768px;\n"; 
-		} else { 
-			echo "width: 768px;\n"; 
-		}  ?>
-	height: auto;
-	display: block;
-	margin-bottom: 0;
+	img.featured-image {
+		<?php if ( $sitewide_layout == 'floating_islands' ) {
+			echo "width: 768px;\n";
+		} else {
+			echo "width: 768px;\n";
+		} ?>
+		height: auto;
+		display: block;
+		margin-bottom: 0;
 	}
 }
-	
+
 .featured-image-caption {
 	position: absolute;
 	font-size: 12px;
