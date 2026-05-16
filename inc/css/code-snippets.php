6a0fefb687499b380acaf31f8aa1c772a996d800
Jesse Nickles <jesse@littlebizzy.com>
2026-05-17 03:02:43 +0700

    Clean up code snippet styles

--- inc/css/code-snippets.php
+++ inc/css/code-snippets.php
@@ -7,43 +7,49 @@ if ( ! defined( 'ABSPATH' ) ) {
 // styles for code snippets
 ?>
 
-code {
-	font-family: ui-monospace, SFMono-Regular, Consolas, 'Liberation Mono', Menlo, monospace;
-	font-weight: 400;
-	background: #ECEFF1;
-	padding: 0 5px;
-}
+/* code snippets */
 
-pre {
+code,
+pre,
+.pre {
 	font-family: ui-monospace, SFMono-Regular, Consolas, 'Liberation Mono', Menlo, monospace;
 	font-weight: 400;
-	background: #ECEFF1; /* correct */
-	padding: 10px;
-	margin-bottom: 30px;
-	overflow-x: scroll;
-	white-space: pre;
 	color: <?php echo $default_text_color; ?>;
 }
 
+code {
+	padding: 2px 5px;
+	background: <?php echo $search_bar_background_color; ?>;
+}
+
+pre,
+.pre {
+	width: 100%;
+	padding: 16px 20px;
+	margin-bottom: 30px;
+	background: <?php echo $search_bar_background_color; ?>;
+	box-shadow: inset 0 0 0 1px <?php echo $default_text_color; ?>;
+	overflow-x: auto;
+	white-space: pre;
+}
+
+pre code,
+.pre code {
+	padding: 0;
+	background: transparent;
+	box-shadow: none;
+}
+
 @media screen and (max-width: 1199px) {
-	pre {
-		width: calc(100% - 40px) !important;
+	pre,
+	.pre {
+		max-width: 100%;
 	}
 }
 
 @media screen and (min-width: 1200px) {
-	pre {
-		width: 100%;
+	pre,
+	.pre {
+		max-width: 100%;
 	}
-}
-
-.pre {
-	width: 100%;
-	font-family: ui-monospace, SFMono-Regular, Consolas, 'Liberation Mono', Menlo, monospace;
-	background: #ECEFF1; /* correct */
-	padding: 10px;
-	margin-bottom: 30px;
-	overflow-x: scroll;
-	white-space: pre;
-	color: <?php echo $default_text_color; ?>;
 }
