4f7bd7d84f84b155b6105cce36558f7d06dd6236
Jesse Nickles <jesse@littlebizzy.com>
2026-05-16 17:29:18 +0700

    Simplify home premain bottom CSS

--- inc/css/home-premain-bottom.php
+++ inc/css/home-premain-bottom.php
@@ -26,16 +26,18 @@ if ( ! defined( 'ABSPATH' ) ) {
 	}
 }
 
+#home-premain-bottom .inner {
+	width: 100%;
+}
+
 @media screen and (max-width: 1199px) {
 	#home-premain-bottom .inner {
-		width: 100%;
 		padding: 0 0;
 	}
 }
 
 @media screen and (min-width: 1200px) {
 	#home-premain-bottom .inner {
-		width: 100%;
 		display: grid;
 		<?php
 		if ( $home_premain_bottom_columns == '1' ) { echo "grid-template-columns: repeat(1, 1fr);\n"; }
