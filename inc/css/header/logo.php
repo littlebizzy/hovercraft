51e35e32e2738bf5967a7620d88b9fb29d68cc8e
Jesse Nickles <jesse@littlebizzy.com>
2026-05-16 17:44:39 +0700

    Simplify logo CSS

--- inc/css/header/logo.php
+++ inc/css/header/logo.php
@@ -7,22 +7,23 @@ if ( ! defined( 'ABSPATH' ) ) {
 // styles for logo
 ?>
 
-@media screen and (max-width: 1199px) {
 .site-logo {
-	width: <?php echo $mobile_logo_width; ?>px !important;
-	max-width: <?php echo $mobile_logo_width; ?>px !important;
 	height: auto;
-	margin-right: 10px;
 	display: inline-block;
+}
+
+@media screen and (max-width: 1199px) {
+	.site-logo {
+		width: <?php echo $mobile_logo_width; ?>px !important;
+		max-width: <?php echo $mobile_logo_width; ?>px !important;
+		margin-right: 10px;
 	}
 }
 
 @media screen and (min-width: 1200px) {
-.site-logo {
-	width: <?php echo $desktop_logo_width; ?>px !important;
-	max-width: <?php echo $desktop_logo_width; ?>px !important;
-	height: auto;
-	margin-right: 20px;
-	display: inline-block;
+	.site-logo {
+		width: <?php echo $desktop_logo_width; ?>px !important;
+		max-width: <?php echo $desktop_logo_width; ?>px !important;
+		margin-right: 20px;
 	}
 }
