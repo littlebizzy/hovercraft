e8887078dc78f965447bfee6b45ac45cf493a089
Jesse Nickles <jesse@littlebizzy.com>
2026-05-16 13:23:48 +0700

    Format topbar styles

--- inc/css/header/topbar.php
+++ inc/css/header/topbar.php
@@ -8,29 +8,29 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 
 @media screen and (max-width: 1199px) {
-#topbar {
-	width: 100%;
-	padding: 10px 20px;
-	font-size: <?php echo $topbar_mobile_font_size; ?>px;
-	font-weight: <?php echo $topbar_font_weight; ?>;
-	text-transform: <?php echo $topbar_text_transform; ?>;
-	line-height: 1.5;
-	background: <?php echo $topbar_background_color; ?>;
-	color: <?php echo $topbar_text_color; ?>;
+	#topbar {
+		width: 100%;
+		padding: 10px 20px;
+		font-size: <?php echo $topbar_mobile_font_size; ?>px;
+		font-weight: <?php echo $topbar_font_weight; ?>;
+		text-transform: <?php echo $topbar_text_transform; ?>;
+		line-height: 1.5;
+		background: <?php echo $topbar_background_color; ?>;
+		color: <?php echo $topbar_text_color; ?>;
 	}
 }
 
 @media screen and (min-width: 1200px) {
-#topbar {
-	width: 100%;
-	padding: 10px 0;
-	font-size: <?php echo $topbar_desktop_font_size; ?>px;
-	font-weight: <?php echo $topbar_font_weight; ?>;
-	text-transform: <?php echo $topbar_text_transform; ?>;
-	line-height: 1.5;
-	display: table;
-	background: <?php echo $topbar_background_color; ?>;
-	color: <?php echo $topbar_text_color; ?>;
+	#topbar {
+		width: 100%;
+		padding: 10px 0;
+		font-size: <?php echo $topbar_desktop_font_size; ?>px;
+		font-weight: <?php echo $topbar_font_weight; ?>;
+		text-transform: <?php echo $topbar_text_transform; ?>;
+		line-height: 1.5;
+		display: table;
+		background: <?php echo $topbar_background_color; ?>;
+		color: <?php echo $topbar_text_color; ?>;
 	}
 }
 
@@ -42,58 +42,57 @@ if ( ! defined( 'ABSPATH' ) ) {
 #topbar p {
 	margin-bottom: 0;
 }
-	
+
 @media screen and (max-width: 1199px) {
-.topbar-left {
-    vertical-align: middle;
-	text-align: center;
-	<?php if ( $mobile_topbar_widget == 'topbar_right') { echo "display: none;"; } ?>
+	.topbar-left {
+		vertical-align: middle;
+		text-align: center;
+		<?php if ( $mobile_topbar_widget == 'topbar_right') { echo "display: none;"; } ?>
 	}
 }
 
 @media screen and (min-width: 1200px) {
-.topbar-left {
-	display: table-cell;
-    vertical-align: middle;
-	height: inherit;
-	white-space: nowrap;
-	width: auto;
-	text-align: left;
+	.topbar-left {
+		display: table-cell;
+		vertical-align: middle;
+		height: inherit;
+		white-space: nowrap;
+		width: auto;
+		text-align: left;
 	}
 }
 
 @media screen and (max-width: 1199px) {
-.topbar-right {
-	vertical-align: middle;
-	text-align: center;
-	<?php if ( $mobile_topbar_widget == 'topbar_left') { echo "display: none;"; } ?>
+	.topbar-right {
+		vertical-align: middle;
+		text-align: center;
+		<?php if ( $mobile_topbar_widget == 'topbar_left') { echo "display: none;"; } ?>
 	}
 }
 
 @media screen and (min-width: 1200px) {
-.topbar-right {
-	display: table-cell;
-    vertical-align: middle;
-	height: inherit;
-	text-align: right;
-	width: 100%;
+	.topbar-right {
+		display: table-cell;
+		vertical-align: middle;
+		height: inherit;
+		text-align: right;
+		width: 100%;
 	}
 }
-	
+
 @media screen and (max-width: 1199px) {
-.topbar-center {
-	text-align: center;
+	.topbar-center {
+		text-align: center;
 	}
 }
 
 @media screen and (min-width: 1200px) {
-.topbar-center {
-    vertical-align: middle;
-	height: inherit;
-	text-align: center;
+	.topbar-center {
+		vertical-align: middle;
+		height: inherit;
+		text-align: center;
 	}
 }
-
 
 /* topbar ul */
 
