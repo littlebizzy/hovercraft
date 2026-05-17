5318747dd6fe8fd69795e68b62f884cff01ba91b
Jesse Nickles <jesse@littlebizzy.com>
2026-05-18 05:30:16 +0700

    Refine WooCommerce ordering styles

--- inc/css/woocommerce/ordering.php
+++ inc/css/woocommerce/ordering.php
@@ -7,45 +7,49 @@ if ( ! defined( 'ABSPATH' ) ) {
 // styles for woocommerce ordering
 ?>
 
-form.woocommerce-ordering {
-	margin-bottom: 30px;
+form.woocommerce-ordering, .woocommerce form.woocommerce-ordering {
+	clear: none;
+	margin: 0 0 30px;
 }
 
-@media screen and (max-width: 1199px) {
-	form.woocommerce-ordering {
-		float: none !important;
-		width: 100%;
-	}
-}
-
-@media screen and (min-width: 1200px) {
-	form.woocommerce-ordering {
-		float: right !important;
-		width: auto;
-	}
-}
-
-form.woocommerce-ordering select {
+form.woocommerce-ordering select, .woocommerce form.woocommerce-ordering select {
 	min-width: 220px !important;
 	height: 44px !important;
 	padding: 0 44px 0 16px !important;
-	font-size: 16px !important;
-	font-weight: 400 !important;
-	line-height: 44px !important;
-	vertical-align: middle !important;
 	background-color: #ffffff !important;
 	box-shadow: inset 0 0 0 1px <?php echo $breadcrumbs_text_color; ?> !important;
 	color: <?php echo $default_text_color; ?> !important;
 	cursor: pointer !important;
+	font-size: 16px !important;
+	font-weight: 400 !important;
+	line-height: 44px !important;
+	vertical-align: middle !important;
 }
 
-form.woocommerce-ordering select:active,
-form.woocommerce-ordering select:focus {
+form.woocommerce-ordering select:active, form.woocommerce-ordering select:focus, .woocommerce form.woocommerce-ordering select:active, .woocommerce form.woocommerce-ordering select:focus {
 	box-shadow: inset 0 0 0 1px <?php echo $default_link_color; ?> !important;
 }
 
+.woocommerce .woocommerce-ordering::after {
+	clear: both;
+	content: "";
+	display: table;
+}
+
 @media screen and (max-width: 1199px) {
-	form.woocommerce-ordering select {
+	form.woocommerce-ordering, .woocommerce form.woocommerce-ordering {
+		float: none !important;
+		width: 100%;
+	}
+
+	form.woocommerce-ordering select, .woocommerce form.woocommerce-ordering select {
 		width: 100% !important;
+	}
+}
+
+@media screen and (min-width: 1200px) {
+	form.woocommerce-ordering, .woocommerce form.woocommerce-ordering {
+		float: right !important;
+		width: auto;
 	}
 }
