e183a4fc28c4ef983386bf72b9f534651c36e474
Jesse Nickles <jesse@littlebizzy.com>
2026-05-18 05:25:34 +0700

    Expand WooCommerce message styles

--- inc/css/woocommerce/messages.php
+++ inc/css/woocommerce/messages.php
@@ -7,57 +7,108 @@ if ( ! defined( 'ABSPATH' ) ) {
 // styles for woocommerce messages
 ?>
 
-/* success message wrapper */
-.woocommerce-message {
+.woocommerce-notices-wrapper {
+	clear: both;
+}
+
+.woocommerce-notices-wrapper:empty {
+	display: none;
+}
+
+.woocommerce-message, .woocommerce-info, .woocommerce-error {
 	position: relative;
+	clear: both;
+	box-sizing: border-box;
+	width: 100%;
+	margin: 0 0 30px !important;
+	padding: 16px 18px !important;
+	background: <?php echo $search_bar_background_color; ?>;
+	border: 0 !important;
+	border-radius: 0;
+	box-shadow: none !important;
+	color: <?php echo $default_text_color; ?> !important;
+	font-size: 16px;
+	line-height: 1.5;
+	list-style: none;
+}
+
+.woocommerce-message::before, .woocommerce-message::after, .woocommerce-info::before, .woocommerce-info::after, .woocommerce-error::before, .woocommerce-error::after {
+	content: none !important;
+	display: none !important;
+	border: 0 !important;
+}
+
+.woocommerce-message {
 	display: flex !important;
 	align-items: center;
 	justify-content: space-between;
 	flex-wrap: wrap;
 	gap: 14px;
-	padding: 16px 18px;
-	margin-bottom: 30px !important;
-	background: #f3faf4;
-	color: <?php echo $default_text_color; ?> !important;
-	border: none !important;
-	border-radius: 4px;
 }
 
-/* remove all woo pseudo icons */
-.woocommerce-message::before,
-.woocommerce-message::after,
-.woocommerce-info::before,
-.woocommerce-info::after,
-.woocommerce-error::before,
-.woocommerce-error::after {
-	content: none !important;
-	display: none !important;
-	border: none !important;
+.woocommerce-info {
+	background: <?php echo $search_bar_background_color; ?>;
 }
 
-/* remove all woo borders and shadows */
-.woocommerce-message,
-.woocommerce-info,
 .woocommerce-error {
-	border: none !important;
-	box-shadow: none !important;
+	background: #fff5f5;
 }
 
-/* style any buttons inside success messages */
-.woocommerce-message .button {
-	margin-left: auto !important;
-	order: 2;
-	border-radius: 999px;
+.woocommerce-error li {
+	margin: 0 0 8px;
+	padding: 0;
+	list-style: none;
+}
+
+.woocommerce-error li:last-child {
+	margin-bottom: 0;
+}
+
+.woocommerce-message a, .woocommerce-info a, .woocommerce-error a {
+	color: <?php echo $default_link_color; ?>;
+}
+
+.woocommerce-message a:hover, .woocommerce-info a:hover, .woocommerce-error a:hover {
+	color: <?php echo $default_hover_color; ?>;
+}
+
+.woocommerce-message .button, .woocommerce-info .button, .woocommerce-error .button {
+	display: inline-block;
+	margin: 0 0 0 auto !important;
 	padding: 8px 20px !important;
+	background: <?php echo $default_link_color; ?> !important;
+	border: 0 !important;
+	border-radius: 0;
+	color: #ffffff !important;
 	font-size: 14px !important;
 	font-weight: 600;
-	background: <?php echo $default_link_color; ?> !important;
-	color: #ffffff !important;
-	border: none !important;
+	line-height: 1.5 !important;
+	order: 2;
+	text-decoration: none;
 }
 
-/* hover state for success message buttons */
-.woocommerce-message .button:hover {
+.woocommerce-message .button:hover, .woocommerce-info .button:hover, .woocommerce-error .button:hover {
 	background: <?php echo $default_hover_color; ?> !important;
 	color: #ffffff !important;
+}
+
+.woocommerce-message p:last-child, .woocommerce-info p:last-child, .woocommerce-error p:last-child {
+	margin-bottom: 0;
+}
+
+.woocommerce .woocommerce-form-login-toggle, .woocommerce .woocommerce-form-coupon-toggle {
+	clear: both;
+}
+
+@media screen and (max-width: 1199px) {
+	.woocommerce-message, .woocommerce-info, .woocommerce-error {
+		display: block !important;
+	}
+
+	.woocommerce-message .button, .woocommerce-info .button, .woocommerce-error .button {
+		display: block;
+		width: 100%;
+		margin: 12px 0 0 !important;
+		text-align: center;
+	}
 }
