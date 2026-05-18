0cbee5df7aa260ab9ff9fff5226e734de3dbb029
Jesse Nickles <jesse@littlebizzy.com>
2026-05-18 22:27:33 +0700

    Remove WooCommerce product page upsells

--- inc/woocommerce/woocommerce-theme-support.php
+++ inc/woocommerce/woocommerce-theme-support.php
@@ -13,3 +13,9 @@ function hovercraft_add_woocommerce_theme_support() {
 	add_theme_support( 'wc-product-gallery-slider' );
 }
 add_action( 'after_setup_theme', 'hovercraft_add_woocommerce_theme_support' );
+
+// remove product page upsells
+function hovercraft_remove_woocommerce_upsells() {
+	remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_upsell_display', 15 );
+}
+add_action( 'woocommerce_after_single_product_summary', 'hovercraft_remove_woocommerce_upsells', 1 );
