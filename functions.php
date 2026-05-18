11884169309e9ca15a6e4315375f3e6535086e8d
Jesse Nickles <jesse@littlebizzy.com>
2026-05-18 18:08:44 +0700

    Bump theme constant to 0.3.79

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 if ( ! defined( 'HOVERCRAFT_VERSION' ) ) {
-	define( 'HOVERCRAFT_VERSION', '0.3.78' );
+	define( 'HOVERCRAFT_VERSION', '0.3.79' );
 }
 
 // title tag
