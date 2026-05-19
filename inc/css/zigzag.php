c80fc32a0cf380bdee470fed1a6b20e3d99ba796
Jesse Nickles <jesse@littlebizzy.com>
2026-05-19 22:52:13 +0700

    Apply radius policy to zigzag sections

--- inc/css/zigzag.php
+++ inc/css/zigzag.php
@@ -12,29 +12,33 @@ if ( ! defined( 'ABSPATH' ) ) {
 .zigzag-section {
 	text-align: left;
 	padding: 40px 40px;
+	border-radius: 8px;
 }
 
 .zigzag-section-reverse {
 	text-align: right;
 	padding: 40px;
+	border-radius: 8px;
 }
 
 .zigzag-section h3 {
-text-align: left;
+	text-align: left;
 }
 
 .zigzag-section-reverse h3 {
-text-align: right;
+	text-align: right;
 }
 
 .zigzag-section img {
 	float: left;
 	margin-right: 40px;
 	width: 300px !important;
+	border-radius: 0;
 }
 
 .zigzag-section-reverse img {
 	float: right;
 	margin-left: 40px;
 	width: 300px !important;
+	border-radius: 0;
 }
