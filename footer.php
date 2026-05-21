bb1fb16eae0adc5387cc784937b78d2d22290c1b
Jesse Nickles <jesse@littlebizzy.com>
2026-05-22 02:53:51 +0700

    Use shared search form in modal

--- footer.php
+++ footer.php
@@ -128,20 +128,7 @@ if ( $hovercraft_copyright_width === 'full' ) {
 		<button type="button" class="close" aria-label="<?php esc_attr_e( 'Close search', 'hovercraft' ); ?>"><i class="fa-solid fa-x" aria-hidden="true"></i></button>
 	<?php endif; // end layout icons ?>
 
-	<?php $hovercraft_search_id = wp_unique_id( 'search-' ); ?>
-	<form role="search" method="get" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-		<label class="screen-reader-text" for="<?php echo esc_attr( $hovercraft_search_id ); ?>"><?php esc_html_e( 'Search', 'hovercraft' ); ?></label>
-		<div class="input-container">
-			<input id="<?php echo esc_attr( $hovercraft_search_id ); ?>" type="search" class="search-input" value="<?php echo esc_attr( get_search_query() ); ?>" name="s" placeholder="<?php esc_attr_e( 'Search entire website... ', 'hovercraft' ); ?>" />
-			<button type="submit" class="search-submit" name="submit" aria-label="<?php esc_attr_e( 'Submit search', 'hovercraft' ); ?>">
-				<?php if ( $hovercraft_layout_icons === 'material_icons_classic' ) : ?>
-					<i class="material-icons search" aria-hidden="true">search</i>
-				<?php elseif ( $hovercraft_layout_icons === 'font_awesome_version_6' ) : ?>
-					<i class="fa-solid fa-search" aria-hidden="true"></i>
-				<?php endif; // end layout icons ?>
-			</button>
-		</div><!-- input-container -->
-	</form><!-- searchform -->
+	<?php get_search_form(); ?>
 </div><!-- full-screen-search -->
 
 <?php wp_footer(); ?>
