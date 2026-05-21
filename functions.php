202e2297b942dc87f74ce251cd5400326038313e
Jesse Nickles <jesse@littlebizzy.com>
2026-05-22 04:34:43 +0700

    Bump version after mobile footer list update

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 if ( ! defined( 'HOVERCRAFT_VERSION' ) ) {
-	define( 'HOVERCRAFT_VERSION', '0.3.111' );
+	define( 'HOVERCRAFT_VERSION', '0.3.112' );
 }
 
 // title tag
