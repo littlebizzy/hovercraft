a3f483b5474539a3f51a1fa9274de458fdf6c859
Jesse Nickles <jesse@littlebizzy.com>
2026-05-21 15:44:30 +0700

    Restore icon handling in default search form

--- searchform.php
+++ searchform.php
@@ -5,6 +5,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 $hovercraft_search_id = wp_unique_id( 'search-' );
+$hovercraft_layout_icons = get_theme_mod( 'hovercraft_layout_icons', 'material_icons_classic' );
 ?>
 <form role="search" method="get" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<label class="screen-reader-text" for="<?php echo esc_attr( $hovercraft_search_id ); ?>"><?php esc_html_e( 'Search', 'hovercraft' ); ?></label>
@@ -13,7 +14,11 @@ $hovercraft_search_id = wp_unique_id( 'search-' );
 		<input id="<?php echo esc_attr( $hovercraft_search_id ); ?>" type="search" class="search-input" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" placeholder="<?php esc_attr_e( 'Search entire website...', 'hovercraft' ); ?>" />
 
 		<button type="submit" class="search-submit" aria-label="<?php esc_attr_e( 'Submit search', 'hovercraft' ); ?>">
-			<span aria-hidden="true" class="search-submit-icon">search</span>
+			<?php if ( $hovercraft_layout_icons === 'material_icons_classic' ) : ?>
+				<i class="material-icons search" aria-hidden="true">search</i>
+			<?php elseif ( $hovercraft_layout_icons === 'font_awesome_version_6' ) : ?>
+				<i class="fa-solid fa-search" aria-hidden="true"></i>
+			<?php endif; // end layout icons ?>
 		</button>
 	</div><!-- input-container -->
 </form><!-- searchform -->
