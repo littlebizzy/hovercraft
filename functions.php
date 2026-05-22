cc6da2e5ae85512ed658c9a952a79328308f8573
Jesse Nickles <jesse@littlebizzy.com>
2026-05-23 02:24:23 +0700

    Bump theme version to 0.3.125

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 if ( ! defined( 'HOVERCRAFT_VERSION' ) ) {
-	define( 'HOVERCRAFT_VERSION', '0.3.124' );
+	define( 'HOVERCRAFT_VERSION', '0.3.125' );
 }
 
 // title tag
