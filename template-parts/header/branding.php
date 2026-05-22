c37b9b87003b504259c1c258d85a3e1ad7e7b4df
Jesse Nickles <jesse@littlebizzy.com>
2026-05-22 21:41:46 +0700

    Clean up branding template logic

--- template-parts/header/branding.php
+++ template-parts/header/branding.php
@@ -8,31 +8,27 @@ if ( ! defined( 'ABSPATH' ) ) {
 <div id="branding">
 
 <?php
-// get logo url
+// get branding content
 $logo_url = hovercraft_get_header_logo_url();
-
-// check if a logo should be rendered
-$has_logo = false;
-
-if ( $logo_url ) {
-	$has_logo = true;
-}
+$tagline = get_bloginfo( 'description' );
 ?>
 
-<?php if ( $has_logo ) : ?>
+<?php if ( $logo_url ) : ?>
 	<div class="branding-media">
 		<a href="<?php echo esc_url( home_url() ); ?>" class="custom-logo-link" rel="home">
 			<img class="site-logo custom-logo" src="<?php echo esc_url( $logo_url ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?> logo" />
 		</a>
 	</div><!-- branding-media -->
-<?php endif; // end has-logo ?>
+<?php endif; // end logo-url ?>
 
 <?php if ( display_header_text() ) : ?>
 	<div class="branding-text">
 		<a href="<?php echo esc_url( home_url() ); ?>" class="site-title-link" rel="home">
 			<div class="site-title"><?php echo esc_html( get_bloginfo( 'name' ) ); ?></div>
 		</a>
-		<div class="tagline"><?php echo esc_html( get_bloginfo( 'description' ) ); ?></div>
+		<?php if ( $tagline ) : ?>
+			<div class="tagline"><?php echo esc_html( $tagline ); ?></div>
+		<?php endif; // end tagline ?>
 	</div><!-- branding-text -->
 <?php endif; // end header text ?>
 
