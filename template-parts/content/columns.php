fba8f0a8870cce75515454fe5e0a9097b1cbd20d
Jesse Nickles <jesse@littlebizzy.com>
2026-05-23 02:08:26 +0700

    Clean up postcolumns template markup

--- template-parts/content/columns.php
+++ template-parts/content/columns.php
@@ -7,17 +7,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 hovercraft_render_columns();
 
 ?>
-
 <?php if ( is_active_sidebar( 'hovercraft_postcolumns_top' ) ) : ?>
-<div class="postcolumns-top">
-	<?php dynamic_sidebar( 'hovercraft_postcolumns_top' ); ?>
-<div class="clear"></div>
-</div><!-- postcolumns-top -->
+	<div class="postcolumns-top">
+		<?php dynamic_sidebar( 'hovercraft_postcolumns_top' ); ?>
+	</div><!-- postcolumns-top -->
 <?php endif; // end hovercraft-postcolumns-top sidebar ?>
 
 <?php if ( is_active_sidebar( 'hovercraft_postcolumns_bottom' ) ) : ?>
-<div class="postcolumns-bottom">
-	<?php dynamic_sidebar( 'hovercraft_postcolumns_bottom' ); ?>
-<div class="clear"></div>
-</div><!-- postcolumns-bottom -->
+	<div class="postcolumns-bottom">
+		<?php dynamic_sidebar( 'hovercraft_postcolumns_bottom' ); ?>
+	</div><!-- postcolumns-bottom -->
 <?php endif; // end hovercraft-postcolumns-bottom sidebar ?>
