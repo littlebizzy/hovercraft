405fdd9ec0c9bb6c4ecd525a3785291499466706
Jesse Nickles <jesse@littlebizzy.com>
2026-05-22 18:01:17 +0700

    Fix preheader mobile disable setting

--- inc/css/header/preheader.php
+++ inc/css/header/preheader.php
@@ -45,6 +45,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 	text-align: center;
 }
 
+<?php if ( $mobile_preheader_widget == 'none' ) : ?>
+@media screen and (max-width: 1199px) {
+	#preheader {
+		display: none;
+	}
+}
+<?php endif; ?>
+
 @media screen and (max-width: 1199px) {
 	#preheader .inner {
 		flex-direction: column;
@@ -87,4 +95,21 @@ if ( ! defined( 'ABSPATH' ) ) {
 		width: 100%;
 		text-align: right;
 	}
+}
+
+/* preheader ul */
+
+#preheader ul {
+	margin: 0;
+	list-style-type: none;
+}
+
+#preheader ul li {
+	list-style-type: none;
+	display: inline-block;
+	margin: 0 0 0 10px;
+}
+
+#preheader ul li:first-child {
+	margin-left: 0;
 }
