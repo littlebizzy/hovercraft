35d74c8f1fda3ae0b60a5dbf033a1254a528fdae
Jesse Nickles <jesse@littlebizzy.com>
2026-05-22 22:58:39 +0700

    Clean up prefooter template markup

--- template-parts/footer/prefooter.php
+++ template-parts/footer/prefooter.php
@@ -4,21 +4,19 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-?>
-<?php if ( is_active_sidebar( 'hovercraft_prefooter_top' ) ) : ?>
-	<div id="prefooter-top">
+// render prefooter widget area
+function hovercraft_render_prefooter_widget_area( $sidebar_id, $wrapper_id ) {
+	if ( ! is_active_sidebar( $sidebar_id ) ) {
+		return;
+	}
+	?>
+	<div id="<?php echo esc_attr( $wrapper_id ); ?>">
 		<div class="inner">
-			<?php dynamic_sidebar( 'hovercraft_prefooter_top' ); ?>
-			<div class="clear"></div>
+			<?php dynamic_sidebar( $sidebar_id ); ?>
 		</div><!-- inner -->
-	</div><!-- prefooter-top -->
-<?php endif; // end hovercraft-prefooter-top sidebar ?>
+	</div><!-- <?php echo esc_html( $wrapper_id ); ?> -->
+	<?php
+}
 
-<?php if ( is_active_sidebar( 'hovercraft_prefooter_bottom' ) ) : ?>
-	<div id="prefooter-bottom">
-		<div class="inner">
-			<?php dynamic_sidebar( 'hovercraft_prefooter_bottom' ); ?>
-			<div class="clear"></div>
-		</div><!-- inner -->
-	</div><!-- prefooter-bottom -->
-<?php endif; // end hovercraft-prefooter-bottom sidebar ?>
+hovercraft_render_prefooter_widget_area( 'hovercraft_prefooter_top', 'prefooter-top' );
+hovercraft_render_prefooter_widget_area( 'hovercraft_prefooter_bottom', 'prefooter-bottom' );
