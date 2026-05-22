f16c91617d3edb6cc51199cf4c2df629fb8cbb59
Jesse Nickles <jesse@littlebizzy.com>
2026-05-22 22:37:13 +0700

    Support keyboard search trigger

--- assets/js/full-screen-search.js
+++ assets/js/full-screen-search.js
@@ -2,22 +2,42 @@
 jQuery( function( $ ) {
 
 	// open search overlay
+	function openSearchOverlay() {
+		$( '#full-screen-search' ).addClass( 'open' );
+		$( '.search-icon-wrapper' ).attr( 'aria-expanded', 'true' );
+		$( '#full-screen-search .search-input' ).focus();
+	}
+
+	// close search overlay
+	function closeSearchOverlay() {
+		$( '#full-screen-search' ).removeClass( 'open' );
+		$( '.search-icon-wrapper' ).attr( 'aria-expanded', 'false' );
+	}
+
+	// open search overlay via click
 	$( '.search-icon-wrapper' ).on( 'click', function( event ) {
 		event.preventDefault();
-		$( '#full-screen-search' ).addClass( 'open' );
-		$( '#full-screen-search .search-input' ).focus();
+		openSearchOverlay();
+	} );
+
+	// open search overlay via keyboard
+	$( '.search-icon-wrapper' ).on( 'keydown', function( event ) {
+		if ( event.key === 'Enter' || event.key === ' ' ) {
+			event.preventDefault();
+			openSearchOverlay();
+		}
 	} );
 
 	// close search overlay via button
 	$( '#full-screen-search button.close' ).on( 'click', function( event ) {
 		event.preventDefault();
-		$( '#full-screen-search' ).removeClass( 'open' );
+		closeSearchOverlay();
 	} );
 
 	// close search overlay via esc key
 	$( document ).on( 'keydown', function( event ) {
 		if ( event.key === 'Escape' ) {
-			$( '#full-screen-search' ).removeClass( 'open' );
+			closeSearchOverlay();
 		}
 	} );
 
