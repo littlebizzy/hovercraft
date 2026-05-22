f8078aa53dbcd733440e0515e7c3e79222829f49
Jesse Nickles <jesse@littlebizzy.com>
2026-05-22 23:20:47 +0700

    Add home postmain template part

---
+++ template-parts/home/postmain.php
@@ -0,0 +1,24 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+?>
+<?php if ( is_active_sidebar( 'hovercraft_home_postmain_top' ) && is_front_page() ) : ?>
+	<div id="home-postmain-top">
+		<div class="inner">
+			<?php dynamic_sidebar( 'hovercraft_home_postmain_top' ); ?>
+			<div class="clear"></div>
+		</div><!-- inner -->
+	</div><!-- home-postmain-top -->
+<?php endif; // end is_front_page ?>
+
+<?php if ( is_active_sidebar( 'hovercraft_home_postmain_bottom' ) && is_front_page() ) : ?>
+	<div id="home-postmain-bottom">
+		<div class="inner">
+			<?php dynamic_sidebar( 'hovercraft_home_postmain_bottom' ); ?>
+			<div class="clear"></div>
+		</div><!-- inner -->
+	</div><!-- home-postmain-bottom -->
+<?php endif; // end is_front_page ?>
