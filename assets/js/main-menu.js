1b048dd3c97752b0d084fd4d39e17be4467bc70a
Jesse Nickles <jesse@littlebizzy.com>
2026-05-22 22:33:19 +0700

    Improve desktop menu keyboard accessibility

--- assets/js/main-menu.js
+++ assets/js/main-menu.js
@@ -8,10 +8,29 @@ jQuery( function( $ ) {
 	}
 
 	// submenu delays
-	var delayIn  = 100; // submenu open delay
-	var delayOut = 150; // submenu close delay
+	var delayIn = 100;
+	var delayOut = 150;
 
-	// bind hover events
+	// setup submenu accessibility
+	$menu.find( '.menu-item-has-children > a' ).attr( 'aria-expanded', 'false' );
+
+	// open menu item
+	function openMenuItem( $li ) {
+		$menu.find( 'li.open' ).not( $li.parents() ).each( function() {
+			closeMenuItem( $( this ) );
+		} );
+
+		$li.addClass( 'open' );
+		$li.children( 'a' ).attr( 'aria-expanded', 'true' );
+	}
+
+	// close menu item
+	function closeMenuItem( $li ) {
+		$li.removeClass( 'open' );
+		$li.find( '.menu-item-has-children > a' ).attr( 'aria-expanded', 'false' );
+	}
+
+	// bind hover and focus events
 	$menu.find( '.menu-item-has-children' ).each( function() {
 		var $li = $( this );
 		var openTimer;
@@ -23,8 +42,7 @@ jQuery( function( $ ) {
 			clearTimeout( closeTimer );
 
 			openTimer = setTimeout( function() {
-				$menu.find( 'li.open' ).not( $li.parents() ).removeClass( 'open' );
-				$li.addClass( 'open' );
+				openMenuItem( $li );
 			}, delayIn );
 		} );
 
@@ -34,9 +52,36 @@ jQuery( function( $ ) {
 			clearTimeout( closeTimer );
 
 			closeTimer = setTimeout( function() {
-				$li.removeClass( 'open' );
+				closeMenuItem( $li );
 			}, delayOut );
 		} );
+
+		// keyboard focus in
+		$li.on( 'focusin', function() {
+			clearTimeout( openTimer );
+			clearTimeout( closeTimer );
+			openMenuItem( $li );
+		} );
+
+		// keyboard focus out
+		$li.on( 'focusout', function() {
+			clearTimeout( closeTimer );
+
+			closeTimer = setTimeout( function() {
+				if ( ! $li.find( ':focus' ).length ) {
+					closeMenuItem( $li );
+				}
+			}, delayOut );
+		} );
+	} );
+
+	// close dropdowns with esc key
+	$( document ).on( 'keydown', function( event ) {
+		if ( event.key === 'Escape' ) {
+			$menu.find( 'li.open' ).each( function() {
+				closeMenuItem( $( this ) );
+			} );
+		}
 	} );
 } );
 
