85a60561b005811564ffe7d1595850171acc6251
Jesse Nickles <jesse@littlebizzy.com>
2026-05-22 22:32:22 +0700

    Add accessible labels to header icons

--- inc/header/header-navigation.php
+++ inc/header/header-navigation.php
@@ -13,11 +13,11 @@ function hovercraft_get_header_icon_style() {
 function hovercraft_header_search_icon() {
 	$header_icon_style = hovercraft_get_header_icon_style();
 	?>
-	<div class="search-icon-wrapper">
+	<div class="search-icon-wrapper" role="button" tabindex="0" aria-label="<?php echo esc_attr__( 'Open search', 'hovercraft' ); ?>">
 		<?php if ( 'material_icons_classic' === $header_icon_style ) : ?>
-			<i class="material-icons search">search</i>
+			<i class="material-icons search" aria-hidden="true">search</i>
 		<?php elseif ( 'font_awesome_version_6' === $header_icon_style ) : ?>
-			<i class="fas fa-search"></i>
+			<i class="fas fa-search" aria-hidden="true"></i>
 		<?php endif; // end header icon style ?>
 	</div><!-- search-icon-wrapper -->
 	<?php
@@ -30,16 +30,17 @@ function hovercraft_header_cart_icon() {
 	}
 
 	$header_icon_style = hovercraft_get_header_icon_style();
+	$cart_count = WC()->cart->get_cart_contents_count();
 	?>
 	<div class="cart-icon-wrapper">
-		<a href="<?php echo esc_url( wc_get_cart_url() ); ?>">
+		<a href="<?php echo esc_url( wc_get_cart_url() ); ?>" aria-label="<?php echo esc_attr__( 'View cart', 'hovercraft' ); ?>">
 			<?php if ( 'material_icons_classic' === $header_icon_style ) : ?>
-				<i class="material-icons shopping_cart">shopping_cart</i>
+				<i class="material-icons shopping_cart" aria-hidden="true">shopping_cart</i>
 			<?php elseif ( 'font_awesome_version_6' === $header_icon_style ) : ?>
-				<i class="fas fa-shopping-cart"></i>
+				<i class="fas fa-shopping-cart" aria-hidden="true"></i>
 			<?php endif; // end header icon style ?>
 		</a>
-		<div class="notification-dot"><?php echo esc_html( WC()->cart->get_cart_contents_count() ); ?></div><!-- notification-dot -->
+		<div class="notification-dot" aria-hidden="true"><?php echo esc_html( $cart_count ); ?></div><!-- notification-dot -->
 	</div><!-- cart-icon-wrapper -->
 	<?php
 }
@@ -49,9 +50,9 @@ function hovercraft_header_menu_icon() {
 	$header_icon_style = hovercraft_get_header_icon_style();
 
 	if ( 'material_icons_classic' === $header_icon_style ) : ?>
-		<i class="material-icons menu">menu</i>
+		<i class="material-icons menu" aria-hidden="true">menu</i>
 	<?php elseif ( 'font_awesome_version_6' === $header_icon_style ) : ?>
-		<i class="fa-solid fa-bars"></i>
+		<i class="fa-solid fa-bars" aria-hidden="true"></i>
 	<?php endif; // end header icon style
 }
 
