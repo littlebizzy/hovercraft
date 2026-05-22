b0cb33d8a6f425f3f1cb079f7c42472a114d6abe
Jesse Nickles <jesse@littlebizzy.com>
2026-05-23 01:49:38 +0700

    Clean up home premain template markup

--- template-parts/content/premain.php
+++ template-parts/content/premain.php
@@ -4,24 +4,23 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
- if ( is_front_page() ) : ?>
+?>
+<?php if ( is_front_page() ) : ?>
 
-		<?php if ( is_active_sidebar( 'hovercraft_home_premain_top' ) ) : ?>
+	<?php if ( is_active_sidebar( 'hovercraft_home_premain_top' ) ) : ?>
 		<div id="home-premain-top">
 			<div class="inner">
 				<?php dynamic_sidebar( 'hovercraft_home_premain_top' ); ?>
-				<div class="clear"></div>
 			</div><!-- inner -->
 		</div><!-- home-premain-top -->
-		<?php endif; // end hovercraft-home-premain-top sidebar ?>
+	<?php endif; // end hovercraft-home-premain-top sidebar ?>
 
-		<?php if ( is_active_sidebar( 'hovercraft_home_premain_bottom' ) ) : ?>
+	<?php if ( is_active_sidebar( 'hovercraft_home_premain_bottom' ) ) : ?>
 		<div id="home-premain-bottom">
 			<div class="inner">
 				<?php dynamic_sidebar( 'hovercraft_home_premain_bottom' ); ?>
-				<div class="clear"></div>
 			</div><!-- inner -->
 		</div><!-- home-premain-bottom -->
-		<?php endif; // end hovercraft-home-premain-bottom sidebar ?>
+	<?php endif; // end hovercraft-home-premain-bottom sidebar ?>
 
-<?php endif; ?><!-- end homepage -->
+<?php endif; // end homepage ?>
