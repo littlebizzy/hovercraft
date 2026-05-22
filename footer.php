c4b7bcdec5588ae5fd8136aec81ec18ad2ebb197
Jesse Nickles <jesse@littlebizzy.com>
2026-05-22 23:21:04 +0700

    Move home postmain markup to template part

--- footer.php
+++ footer.php
@@ -7,24 +7,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 <div class="clear"></div>
 
-<?php if ( is_active_sidebar( 'hovercraft_home_postmain_top' ) && is_front_page() ) : ?>
-	<div id="home-postmain-top">
-		<div class="inner">
-			<?php dynamic_sidebar( 'hovercraft_home_postmain_top' ); ?>
-			<div class="clear"></div>
-		</div><!-- inner -->
-	</div><!-- home-postmain-top -->
-<?php endif; // end is_front_page ?>
-
-<?php if ( is_active_sidebar( 'hovercraft_home_postmain_bottom' ) && is_front_page() ) : ?>
-	<div id="home-postmain-bottom">
-		<div class="inner">
-			<?php dynamic_sidebar( 'hovercraft_home_postmain_bottom' ); ?>
-			<div class="clear"></div>
-		</div><!-- inner -->
-	</div><!-- home-postmain-bottom -->
-<?php endif; // end is_front_page ?>
-
+<?php get_template_part( 'template-parts/home/postmain' ); ?>
 <?php get_template_part( 'template-parts/footer/prefooter' ); ?>
 <?php get_template_part( 'template-parts/footer/footer-widgets' ); ?>
 <?php get_template_part( 'template-parts/footer/copyright' ); ?>
