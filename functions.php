bb58e2e264b32a26d3311c76e963f73f5c25337e
Jesse Nickles <jesse@littlebizzy.com>
2026-05-22 17:57:54 +0700

    Bump HoverCraft version to 0.3.116

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 if ( ! defined( 'HOVERCRAFT_VERSION' ) ) {
-	define( 'HOVERCRAFT_VERSION', '0.3.115' );
+	define( 'HOVERCRAFT_VERSION', '0.3.116' );
 }
 
 // title tag
