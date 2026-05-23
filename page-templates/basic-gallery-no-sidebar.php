b6066971d3edd7ac363b520566fc881497298e96
Jesse Nickles <jesse@littlebizzy.com>
2026-05-23 15:32:43 +0700

    Rewire basic gallery comments template part

--- page-templates/basic-gallery-no-sidebar.php
+++ page-templates/basic-gallery-no-sidebar.php
@@ -26,7 +26,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 					<?php get_template_part( 'template-parts/content/loop' ); ?>
 					<?php get_template_part( 'template-parts/content/gallery' ); ?>
 					<?php get_template_part( 'template-parts/content/pagination' ); ?>
-					<?php get_template_part( 'template-parts/content/comments' ); ?>
+					<?php get_template_part( 'template-parts/comments/comments' ); ?>
 				<div class="clear"></div>
 				</div><!-- content-padded -->
 				
