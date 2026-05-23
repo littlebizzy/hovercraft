8e6f331de75a06989f15d57c9a86b6942e0905de
Jesse Nickles <jesse@littlebizzy.com>
2026-05-23 17:44:22 +0700

    Move checkout coupon above order notes

--- inc/woocommerce/checkout-coupon.php
+++ inc/woocommerce/checkout-coupon.php
@@ -5,9 +5,19 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-// move checkout coupon form below checkout form
+// move checkout coupon form above order notes
 function hovercraft_move_woocommerce_checkout_coupon_form() {
 	remove_action( 'woocommerce_before_checkout_form', 'woocommerce_checkout_coupon_form', 10 );
-	add_action( 'woocommerce_after_checkout_form', 'woocommerce_checkout_coupon_form', 10 );
+	add_action( 'woocommerce_before_order_notes', 'woocommerce_checkout_coupon_form', 5 );
 }
 add_action( 'wp', 'hovercraft_move_woocommerce_checkout_coupon_form' );
+
+// rename checkout order heading
+function hovercraft_rename_woocommerce_checkout_order_heading( $translated_text, $text, $domain ) {
+	if ( is_checkout() && ! is_order_received_page() && $domain === 'woocommerce' && $text === 'Your order' ) {
+		return __( 'Order Summary', 'hovercraft' );
+	}
+
+	return $translated_text;
+}
+add_filter( 'gettext', 'hovercraft_rename_woocommerce_checkout_order_heading', 20, 3 );
