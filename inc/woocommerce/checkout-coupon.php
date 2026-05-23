240bf65a05fde790725a1d9e68ebfdaf55e55076
Jesse Nickles <jesse@littlebizzy.com>
2026-05-23 19:55:55 +0700

    Add WooCommerce guard to checkout coupon helper

--- inc/woocommerce/checkout-coupon.php
+++ inc/woocommerce/checkout-coupon.php
@@ -5,6 +5,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+// exit if woocommerce is inactive
+if ( ! class_exists( 'WooCommerce' ) ) {
+	return;
+}
+
 // disable checkout coupon form
 function hovercraft_disable_woocommerce_checkout_coupon_form() {
 	remove_action( 'woocommerce_before_checkout_form', 'woocommerce_checkout_coupon_form', 10 );
