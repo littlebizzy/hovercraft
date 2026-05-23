0c7f09256f3c083601dd69eaf1802e69e1237a7c
Jesse Nickles <jesse@littlebizzy.com>
2026-05-23 19:15:25 +0700

    Keep checkout coupon helper focused

--- inc/woocommerce/checkout-coupon.php
+++ inc/woocommerce/checkout-coupon.php
@@ -10,13 +10,3 @@ function hovercraft_disable_woocommerce_checkout_coupon_form() {
 	remove_action( 'woocommerce_before_checkout_form', 'woocommerce_checkout_coupon_form', 10 );
 }
 add_action( 'wp', 'hovercraft_disable_woocommerce_checkout_coupon_form' );
-
-// rename checkout order heading
-function hovercraft_rename_woocommerce_checkout_order_heading( $translated_text, $text, $domain ) {
-	if ( is_checkout() && ! is_order_received_page() && $domain === 'woocommerce' && $text === 'Your order' ) {
-		return __( 'Order Summary', 'hovercraft' );
-	}
-
-	return $translated_text;
-}
-add_filter( 'gettext', 'hovercraft_rename_woocommerce_checkout_order_heading', 20, 3 );
