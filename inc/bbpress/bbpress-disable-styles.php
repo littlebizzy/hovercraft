337905bfa29ebf6e6189333f0ca9ef70f444f85a
Jesse Nickles <jesse@littlebizzy.com>
2026-05-23 13:29:47 +0700

    Clean up bbPress styles helper

--- inc/bbpress/bbpress-disable-styles.php
+++ inc/bbpress/bbpress-disable-styles.php
@@ -1,12 +1,13 @@
 <?php
 
+// exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
 // disable bbpress default styles
-function hovercraft_disable_bbpress_styles() {
+function hovercraft_disable_bbpress_default_styles() {
 	wp_dequeue_style( 'bbp-default' );
 	wp_deregister_style( 'bbp-default' );
 }
-add_action( 'wp_enqueue_scripts', 'hovercraft_disable_bbpress_styles', 100 );
+add_action( 'wp_enqueue_scripts', 'hovercraft_disable_bbpress_default_styles', 100 );
