1ceaae83803aec52d1f1c2b5ef7ed6fb2c0d87ef
Jesse Nickles <jesse@littlebizzy.com>
2026-05-23 13:41:09 +0700

    Clean up bbPress reply link helper

--- inc/bbpress/bbpress-disable-reply-links.php
+++ inc/bbpress/bbpress-disable-reply-links.php
@@ -1,30 +1,38 @@
 <?php
 
+// exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+// exit if bbpress is inactive
 if ( ! function_exists( 'bbpress' ) ) {
 	return;
 }
 
-// remove all reply links from both topics and individual replies
-function hovercraft_remove_all_reply_links( $links ) {
-	if ( isset( $links['reply'] ) ) {
-		unset( $links['reply'] );
+// remove nested reply action links
+function hovercraft_remove_bbpress_reply_links( $admin_links ) {
+	if ( isset( $admin_links['reply'] ) ) {
+		unset( $admin_links['reply'] );
 	}
 
-	return $links;
+	return $admin_links;
 }
-add_filter( 'bbp_topic_admin_links', 'hovercraft_remove_all_reply_links' );
-add_filter( 'bbp_reply_admin_links', 'hovercraft_remove_all_reply_links' );
+add_filter( 'bbp_topic_admin_links', 'hovercraft_remove_bbpress_reply_links' );
+add_filter( 'bbp_reply_admin_links', 'hovercraft_remove_bbpress_reply_links' );
 
-// disable replying to individual replies, allow only replying to the main topic
-function hovercraft_disable_reply_to_individual_replies( $can_publish, $reply_id = 0, $topic_id = 0 ) {
-	if ( $reply_id && bbp_is_reply( $reply_id ) ) {
+// disable nested reply submissions
+function hovercraft_disable_bbpress_nested_replies( $can_publish, $reply_id = 0, $topic_id = 0 ) {
+	// allow replies to the main topic
+	if ( empty( $reply_id ) ) {
+		return $can_publish;
+	}
+
+	// block replies to individual replies
+	if ( bbp_is_reply( $reply_id ) ) {
 		return false;
 	}
 
 	return $can_publish;
 }
-add_filter( 'bbp_current_user_can_publish_replies', 'hovercraft_disable_reply_to_individual_replies', 10, 3 );
+add_filter( 'bbp_current_user_can_publish_replies', 'hovercraft_disable_bbpress_nested_replies', 10, 3 );
