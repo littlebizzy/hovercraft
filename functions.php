3568ac941ae0915eadb705365308b46a4d032c93
Jesse Nickles <jesse@littlebizzy.com>
2026-05-23 17:19:54 +0700

    Load checkout coupon helper

--- functions.php
+++ functions.php
@@ -116,6 +116,7 @@ require get_template_directory() . '/inc/meta-boxes/meta-box-zigzag.php';
 
 // woocommerce
 require get_template_directory() . '/inc/woocommerce/woocommerce-theme-support.php';
+require get_template_directory() . '/inc/woocommerce/checkout-coupon.php';
 require get_template_directory() . '/inc/woocommerce/quantity-select.php';
 require get_template_directory() . '/inc/woocommerce/woocommerce-wrappers.php';
 
