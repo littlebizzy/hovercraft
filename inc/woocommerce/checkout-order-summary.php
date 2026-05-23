0994ec6ad193be19ea31866815f2e29cae538eb9
Jesse Nickles <jesse@littlebizzy.com>
2026-05-23 19:57:09 +0700

    Add WooCommerce guard to checkout order summary helper

--- inc/woocommerce/checkout-order-summary.php
+++ inc/woocommerce/checkout-order-summary.php
@@ -5,6 +5,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+// exit if woocommerce is inactive
+if ( ! class_exists( 'WooCommerce' ) ) {
+	return;
+}
+
 // rename checkout order heading
 function hovercraft_rename_woocommerce_checkout_order_heading( $translated_text, $text, $domain ) {
 	if ( is_checkout() && ! is_order_received_page() && $domain === 'woocommerce' && $text === 'Your order' ) {
