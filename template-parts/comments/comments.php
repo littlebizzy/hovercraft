45839021e666a0735cd5891e2c0b4d9aa7142d20
Jesse Nickles <jesse@littlebizzy.com>
2026-05-23 16:06:53 +0700

    Improve comments template part guards

--- template-parts/comments/comments.php
+++ template-parts/comments/comments.php
@@ -1,8 +1,19 @@
 <?php
 
+// exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-// render comments template
+// exit if post is password-protected
+if ( post_password_required() ) {
+	return;
+}
+
+// exit if comments are closed and none exist
+if ( ! comments_open() && ! get_comments_number() ) {
+	return;
+}
+
+// load comments template
 comments_template();
