9cc2a452c9ac8bf462838037804e2fe5afcfa100
Jesse Nickles <jesse@littlebizzy.com>
2026-05-23 16:53:11 +0700

    Clean up Google Fonts enqueue helper

--- inc/fonts/google-fonts.php
+++ inc/fonts/google-fonts.php
@@ -1,10 +1,11 @@
 <?php
 
+// exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-// add one valid google font family
+// add one google font family
 function hovercraft_add_google_font_family( &$font_families, $font_family, $limit = 0 ) {
 	$font_family = hovercraft_normalize_font_family( $font_family );
 
@@ -22,15 +23,16 @@ function hovercraft_add_google_font_family( &$font_families, $font_family, $limi
 // collect selected google font families
 function hovercraft_get_google_font_families() {
 	$font_families = array();
+	$font_family_limit = 3;
 	$first_font_family = hovercraft_normalize_font_family( get_theme_mod( 'hovercraft_first_font_family', 'noto_sans' ) );
 
 	if ( empty( $first_font_family ) ) {
 		$first_font_family = 'noto_sans';
 	}
 
-	hovercraft_add_google_font_family( $font_families, $first_font_family, 3 );
-	hovercraft_add_google_font_family( $font_families, get_theme_mod( 'hovercraft_second_font_family', '' ), 3 );
-	hovercraft_add_google_font_family( $font_families, get_theme_mod( 'hovercraft_third_font_family', '' ), 3 );
+	hovercraft_add_google_font_family( $font_families, $first_font_family, $font_family_limit );
+	hovercraft_add_google_font_family( $font_families, get_theme_mod( 'hovercraft_second_font_family', '' ), $font_family_limit );
+	hovercraft_add_google_font_family( $font_families, get_theme_mod( 'hovercraft_third_font_family', '' ), $font_family_limit );
 
 	$multilingual_font_family = hovercraft_normalize_font_family( get_theme_mod( 'hovercraft_multilingual_font_family', '' ) );
 
@@ -44,19 +46,20 @@ function hovercraft_get_google_font_families() {
 // enqueue selected google fonts
 function hovercraft_enqueue_google_fonts() {
 	$font_variations = 'ital,wght@0,400;0,600;0,700;1,400;1,600;1,700';
-	$google_fonts = array();
+	$google_font_families = array();
 
 	foreach ( hovercraft_get_google_font_families() as $font_family ) {
-		$font_family = str_replace( ' ', '+', hovercraft_format_font_family_label( $font_family ) );
-		$google_fonts[] = 'family=' . $font_family . ':' . $font_variations;
+		$font_family_label = str_replace( ' ', '+', hovercraft_format_font_family_label( $font_family ) );
+		$google_font_families[] = 'family=' . $font_family_label . ':' . $font_variations;
 	}
 
-	$google_fonts = array_unique( $google_fonts );
-
-	if ( ! empty( $google_fonts ) ) {
-		$google_fonts_url = 'https://fonts.googleapis.com/css2?' . implode( '&', $google_fonts ) . '&display=swap';
-		wp_enqueue_style( 'hovercraft_google_fonts', esc_url_raw( $google_fonts_url ), array(), HOVERCRAFT_VERSION, 'all' );
+	if ( empty( $google_font_families ) ) {
+		return;
 	}
+
+	$google_fonts_url = 'https://fonts.googleapis.com/css2?' . implode( '&', $google_font_families ) . '&display=swap';
+
+	wp_enqueue_style( 'hovercraft_google_fonts', esc_url_raw( $google_fonts_url ), array(), HOVERCRAFT_VERSION, 'all' );
 }
 
 // load google fonts on the frontend
