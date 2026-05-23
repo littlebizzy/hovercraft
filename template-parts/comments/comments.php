20bcf4929c6a9ad71e024c62a7616c180c91ee0d
Jesse Nickles <jesse@littlebizzy.com>
2026-05-23 16:11:36 +0700

    Simplify comments template part loader

--- template-parts/comments/comments.php
+++ template-parts/comments/comments.php
@@ -1,19 +1,4 @@
 <?php
 
-// exit if accessed directly
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
-
-// exit if post is password-protected
-if ( post_password_required() ) {
-	return;
-}
-
-// exit if comments are closed and none exist
-if ( ! comments_open() && ! get_comments_number() ) {
-	return;
-}
-
 // load comments template
 comments_template();
