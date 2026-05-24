74eafca112d29c1bd445be4fa978ec9f632870dd
Jesse Nickles <jesse@littlebizzy.com>
2026-05-25 00:17:02 +0700

    Clean up basic header template

--- template-parts/header/header-basic.php
+++ template-parts/header/header-basic.php
@@ -1,15 +1,13 @@
 <?php
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
-
+// render basic header
 ?>
 <div id="header-basic">
 
+	<?php // render preheader ?>
 	<?php get_template_part( 'template-parts/header/header-preheader' ); ?>
 
+	<?php // render navigation ?>
 	<?php get_template_part( 'template-parts/header/navigation' ); ?>
 
 </div><!-- header-basic -->
-
