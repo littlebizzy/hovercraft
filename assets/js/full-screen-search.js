df28b2d7eaed033891abcae04cc2c4dcda4ca7a3
Jesse Nickles <jesse@littlebizzy.com>
2026-05-24 19:18:22 +0700

    Clean up search modal behavior

--- assets/js/full-screen-search.js
+++ assets/js/full-screen-search.js
@@ -1,37 +1,53 @@
 // full screen search
 jQuery( function( $ ) {
+	var $modal = $( '#full-screen-search' );
+	var $toggle = $( '.search-modal-toggle' );
+	var $lastFocused = $();
 
 	// open search overlay
 	function openSearchOverlay() {
-		$( '#full-screen-search' ).addClass( 'open' );
-		$( '.search-icon-wrapper' ).attr( 'aria-expanded', 'true' );
-		$( '#full-screen-search .search-input' ).focus();
+		$lastFocused = $( document.activeElement );
+		$modal.addClass( 'open' ).attr( 'aria-hidden', 'false' );
+		$toggle.attr( 'aria-expanded', 'true' );
+		$( 'body' ).addClass( 'search-modal-open' );
+
+		window.setTimeout( function() {
+			$modal.find( '.search-input' ).trigger( 'focus' );
+		}, 50 );
 	}
 
 	// close search overlay
 	function closeSearchOverlay() {
-		$( '#full-screen-search' ).removeClass( 'open' );
-		$( '.search-icon-wrapper' ).attr( 'aria-expanded', 'false' );
+		if ( ! $modal.hasClass( 'open' ) ) {
+			return;
+		}
+
+		$modal.removeClass( 'open' ).attr( 'aria-hidden', 'true' );
+		$toggle.attr( 'aria-expanded', 'false' );
+		$( 'body' ).removeClass( 'search-modal-open' );
+
+		if ( $lastFocused.length ) {
+			$lastFocused.trigger( 'focus' );
+		}
 	}
 
 	// open search overlay via click
-	$( '.search-icon-wrapper' ).on( 'click', function( event ) {
+	$toggle.on( 'click', function( event ) {
 		event.preventDefault();
 		openSearchOverlay();
 	} );
 
-	// open search overlay via keyboard
-	$( '.search-icon-wrapper' ).on( 'keydown', function( event ) {
-		if ( event.key === 'Enter' || event.key === ' ' ) {
-			event.preventDefault();
-			openSearchOverlay();
-		}
-	} );
-
 	// close search overlay via button
-	$( '#full-screen-search button.close' ).on( 'click', function( event ) {
+	$modal.find( '.search-modal-close' ).on( 'click', function( event ) {
 		event.preventDefault();
 		closeSearchOverlay();
+	} );
+
+	// close search overlay via backdrop
+	$modal.on( 'click', function( event ) {
+		if ( event.target === this ) {
+			closeSearchOverlay();
+		}
 	} );
 
 	// close search overlay via esc key
@@ -42,4 +58,3 @@ jQuery( function( $ ) {
 	} );
 
 } );
-
