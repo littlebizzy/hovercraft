c2bb89e73e6d82792a0b194c2102b766b0172d2e
Jesse Nickles <jesse@littlebizzy.com>
2026-05-24 23:24:50 +0700

    Clean up header preheader template

--- template-parts/header/header-preheader.php
+++ template-parts/header/header-preheader.php
@@ -1,21 +1,28 @@
 <?php
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+// check preheader widget areas
+$hovercraft_preheader_left_active = is_active_sidebar( 'hovercraft_preheader_left' );
+$hovercraft_preheader_right_active = is_active_sidebar( 'hovercraft_preheader_right' );
+$hovercraft_has_preheader_widgets = $hovercraft_preheader_left_active || $hovercraft_preheader_right_active;
+
+// skip empty preheader
+if ( ! $hovercraft_has_preheader_widgets ) {
+	return;
 }
 
-$preheader_left_active = is_active_sidebar( 'hovercraft_preheader_left' );
-$preheader_right_active = is_active_sidebar( 'hovercraft_preheader_right' );
-
-if ( ! $preheader_left_active && ! $preheader_right_active ) {
-	return;
+// get centered preheader widget area
+if ( $hovercraft_preheader_left_active ) {
+	$hovercraft_preheader_center_sidebar = 'hovercraft_preheader_left';
+} else {
+	$hovercraft_preheader_center_sidebar = 'hovercraft_preheader_right';
 }
 
 ?>
 <div id="preheader">
 	<div class="inner">
 
-		<?php if ( $preheader_left_active && $preheader_right_active ) : ?>
+		<?php // render split preheader widget areas ?>
+		<?php if ( $hovercraft_preheader_left_active && $hovercraft_preheader_right_active ) : ?>
 			<div class="preheader-left">
 				<?php hovercraft_dynamic_sidebar_without_title( 'hovercraft_preheader_left' ); ?>
 			</div><!-- preheader-left -->
@@ -23,11 +30,11 @@ if ( ! $preheader_left_active && ! $preheader_right_active ) {
 				<?php hovercraft_dynamic_sidebar_without_title( 'hovercraft_preheader_right' ); ?>
 			</div><!-- preheader-right -->
 		<?php else : ?>
-			<?php $preheader_sidebar = $preheader_left_active ? 'hovercraft_preheader_left' : 'hovercraft_preheader_right'; ?>
+			<?php // render centered preheader widget area ?>
 			<div class="preheader-center">
-				<?php hovercraft_dynamic_sidebar_without_title( $preheader_sidebar ); ?>
+				<?php hovercraft_dynamic_sidebar_without_title( $hovercraft_preheader_center_sidebar ); ?>
 			</div><!-- preheader-center -->
-		<?php endif; // end preheader columns ?>
+		<?php endif; // end preheader widget areas ?>
 
 	</div><!-- inner -->
 </div><!-- preheader -->
