d195aaba1947f8c893c08f4f713bbb011055147f
Jesse Nickles <jesse@littlebizzy.com>
2026-05-24 23:38:27 +0700

    Wire renamed header preheader template in basic header

--- template-parts/header/header-basic.php
+++ template-parts/header/header-basic.php
@@ -7,7 +7,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 <div id="header-basic">
 
-	<?php get_template_part( 'template-parts/header/preheader' ); ?>
+	<?php get_template_part( 'template-parts/header/header-preheader' ); ?>
 
 	<?php get_template_part( 'template-parts/header/navigation' ); ?>
 
