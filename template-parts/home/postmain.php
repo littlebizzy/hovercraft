ef9cfdb46a93180395a49352d034d8749b52e39f
Jesse Nickles <jesse@littlebizzy.com>
2026-05-24 19:29:57 +0700

    Add comments to home postmain blocks

--- template-parts/home/postmain.php
+++ template-parts/home/postmain.php
@@ -1,14 +1,16 @@
 <?php
 
+// block direct access
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+// limit output to front page
 if ( ! is_front_page() ) {
 	return;
 }
 
-// home postmain top
+// render home postmain top widget area
 if ( is_active_sidebar( 'hovercraft_home_postmain_top' ) ) : ?>
 	<div id="home-postmain-top">
 		<div class="inner">
@@ -18,7 +20,7 @@ if ( is_active_sidebar( 'hovercraft_home_postmain_top' ) ) : ?>
 <?php endif; // end home postmain top ?>
 
 <?php
-// home postmain bottom
+// render home postmain bottom widget area
 if ( is_active_sidebar( 'hovercraft_home_postmain_bottom' ) ) : ?>
 	<div id="home-postmain-bottom">
 		<div class="inner">
