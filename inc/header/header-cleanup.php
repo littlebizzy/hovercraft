13ca329701c17feab49ab2d2f5984a1014255a77
Jesse Nickles <jesse@littlebizzy.com>
2026-05-24 18:00:08 +0700

    Merge generator cleanup into header cleanup

--- inc/header/header-cleanup.php
+++ inc/header/header-cleanup.php
@@ -11,3 +11,9 @@ function hovercraft_remove_feed_links() {
 	remove_action( 'wp_head', 'feed_links_extra', 3 );
 }
 add_action( 'after_setup_theme', 'hovercraft_remove_feed_links', 99 );
+
+// remove wordpress generator output
+function hovercraft_remove_wp_generator() {
+	return '';
+}
+add_filter( 'the_generator', 'hovercraft_remove_wp_generator' );
