700f75e5986c5d3a854a84ed32175327b2f4d157
Jesse Nickles <jesse@littlebizzy.com>
2026-05-25 02:34:10 +0700

    Clean up posthero template

--- template-parts/header/posthero.php
+++ template-parts/header/posthero.php
@@ -1,13 +1,19 @@
 <?php
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+// check posthero widget area
+$hovercraft_posthero_active = is_active_sidebar( 'hovercraft_posthero' );
+
+// skip empty posthero
+if ( ! $hovercraft_posthero_active ) {
+	return;
 }
 
+// render posthero widget area
 ?>
 <div id="posthero">
 	<div class="inner">
 		<?php
+		// remove widget titles for posthero widgets
 		add_filter( 'widget_title', '__return_false' );
 		dynamic_sidebar( 'hovercraft_posthero' );
 		remove_filter( 'widget_title', '__return_false' );
