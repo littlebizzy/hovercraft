66d08cfe14536e9000ed9c7e46e427e6e7b134b7
Jesse Nickles <jesse@littlebizzy.com>
2026-05-24 23:16:10 +0700

    Clean up header branding template

--- template-parts/header/header-branding.php
+++ template-parts/header/header-branding.php
@@ -1,35 +1,35 @@
 <?php
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
+// get branding content
+$hovercraft_logo_url = hovercraft_get_header_logo_url();
+$hovercraft_site_name = get_bloginfo( 'name' );
+$hovercraft_tagline = get_bloginfo( 'description' );
+$hovercraft_show_header_text = display_header_text();
 
 ?>
 <div id="branding">
 
-<?php
-// get branding content
-$logo_url = hovercraft_get_header_logo_url();
-$tagline = get_bloginfo( 'description' );
-?>
+	<?php // render branding logo ?>
+	<?php if ( $hovercraft_logo_url ) : ?>
+		<div class="branding-media">
+			<a href="<?php echo esc_url( home_url() ); ?>" class="custom-logo-link" rel="home">
+				<img class="site-logo custom-logo" src="<?php echo esc_url( $hovercraft_logo_url ); ?>" alt="<?php echo esc_attr( $hovercraft_site_name ); ?> logo" />
+			</a>
+		</div><!-- branding-media -->
+	<?php endif; // end branding logo ?>
 
-<?php if ( $logo_url ) : ?>
-	<div class="branding-media">
-		<a href="<?php echo esc_url( home_url() ); ?>" class="custom-logo-link" rel="home">
-			<img class="site-logo custom-logo" src="<?php echo esc_url( $logo_url ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?> logo" />
-		</a>
-	</div><!-- branding-media -->
-<?php endif; // end logo-url ?>
+	<?php // render branding text ?>
+	<?php if ( $hovercraft_show_header_text ) : ?>
+		<div class="branding-text">
+			<a href="<?php echo esc_url( home_url() ); ?>" class="site-title-link" rel="home">
+				<div class="site-title"><?php echo esc_html( $hovercraft_site_name ); ?></div>
+			</a>
 
-<?php if ( display_header_text() ) : ?>
-	<div class="branding-text">
-		<a href="<?php echo esc_url( home_url() ); ?>" class="site-title-link" rel="home">
-			<div class="site-title"><?php echo esc_html( get_bloginfo( 'name' ) ); ?></div>
-		</a>
-		<?php if ( $tagline ) : ?>
-			<div class="tagline"><?php echo esc_html( $tagline ); ?></div>
-		<?php endif; // end tagline ?>
-	</div><!-- branding-text -->
-<?php endif; // end header text ?>
+			<?php // render tagline ?>
+			<?php if ( $hovercraft_tagline ) : ?>
+				<div class="tagline"><?php echo esc_html( $hovercraft_tagline ); ?></div>
+			<?php endif; // end tagline ?>
+		</div><!-- branding-text -->
+	<?php endif; // end branding text ?>
 
 </div><!-- branding -->
