9eb3fb79753c382fccb7724d393c10035cbfe182
Jesse Nickles <jesse@littlebizzy.com>
2026-05-25 00:32:58 +0700

    Clean up hero snippet template

--- template-parts/header/hero-snippet.php
+++ template-parts/header/hero-snippet.php
@@ -1,18 +1,17 @@
 <?php
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
+// check hero snippet widget area
+$hovercraft_hero_snippet_active = is_active_sidebar( 'hovercraft_hero_snippet' );
 
-// renders the header hero snippet template part
-
-?>
-<?php if ( is_active_sidebar( 'hovercraft_hero_snippet' ) ) : ?>
+// render hero snippet widgets
+if ( $hovercraft_hero_snippet_active ) : ?>
 	<?php
+	// remove widget titles for hero snippet widgets
 	add_filter( 'widget_title', '__return_false' );
 	dynamic_sidebar( 'hovercraft_hero_snippet' );
 	remove_filter( 'widget_title', '__return_false' );
 	?>
 <?php else : ?>
+	<?php // render hero snippet excerpt ?>
 	<?php the_excerpt(); ?>
-<?php endif; // end hovercraft-hero-snippet sidebar ?>
+<?php endif; // end hero snippet ?>
