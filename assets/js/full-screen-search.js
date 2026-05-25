cc3d5883ace36e36dd768f915f2b32f5d658c7f7
Jesse Nickles <jesse@littlebizzy.com>
2026-05-25 22:17:17 +0700

    Simplify search modal scroll lock

--- assets/js/full-screen-search.js
+++ assets/js/full-screen-search.js
@@ -4,34 +4,9 @@ jQuery( function( $ ) {
 	var $modal = $( '#full-screen-search' );
 	var $toggle = $( '.search-modal-toggle' );
 	var $lastFocused = $();
-	var originalBodyPaddingRight = '';
 
 	if ( ! $modal.length || ! $toggle.length ) {
 		return;
-	}
-
-	// lock page scroll
-	function lockPageScroll() {
-		var body = $body.get( 0 );
-		var scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
-		var currentPaddingRight = parseFloat( $body.css( 'padding-right' ) ) || 0;
-
-		originalBodyPaddingRight = body.style.paddingRight;
-
-		if ( 0 < scrollbarWidth ) {
-			body.style.paddingRight = currentPaddingRight + scrollbarWidth + 'px';
-		}
-
-		$body.addClass( 'search-modal-open' );
-	}
-
-	// unlock page scroll
-	function unlockPageScroll() {
-		var body = $body.get( 0 );
-
-		$body.removeClass( 'search-modal-open' );
-		body.style.paddingRight = originalBodyPaddingRight;
-		originalBodyPaddingRight = '';
 	}
 
 	// open search overlay
@@ -41,9 +16,9 @@ jQuery( function( $ ) {
 		}
 
 		$lastFocused = $( document.activeElement );
-		lockPageScroll();
 		$modal.addClass( 'open' ).attr( 'aria-hidden', 'false' );
 		$toggle.attr( 'aria-expanded', 'true' );
+		$body.addClass( 'search-modal-open' );
 
 		window.setTimeout( function() {
 			$modal.find( '.search-input' ).trigger( 'focus' );
@@ -58,7 +33,7 @@ jQuery( function( $ ) {
 
 		$modal.removeClass( 'open' ).attr( 'aria-hidden', 'true' );
 		$toggle.attr( 'aria-expanded', 'false' );
-		unlockPageScroll();
+		$body.removeClass( 'search-modal-open' );
 
 		if ( $lastFocused.length ) {
 			$lastFocused.trigger( 'focus' );
