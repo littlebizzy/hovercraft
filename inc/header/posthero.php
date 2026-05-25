fcdb6eb0a5a9be5665cf00de68e2cc851bd73e9f
Jesse Nickles <jesse@littlebizzy.com>
2026-05-25 12:37:47 +0700

    Clean up posthero helper

--- inc/header/posthero.php
+++ inc/header/posthero.php
@@ -4,16 +4,17 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-// posthero visibility helpers
-
 function hovercraft_should_show_posthero( $hero_type ) {
+	// skip inactive posthero widget area
 	if ( ! is_active_sidebar( 'hovercraft_posthero' ) ) {
 		return false;
 	}
 
-	$posthero_display = get_theme_mod( 'hovercraft_posthero_widget_display', '' );
+	// get posthero display setting
+	$hovercraft_posthero_display = get_theme_mod( 'hovercraft_posthero_widget_display', '' );
 
-	$allowed_display_options = array(
+	// get allowed posthero display options
+	$hovercraft_posthero_display_options = array(
 		'full' => array(
 			'full_hero_only',
 			'full_and_half_hero',
@@ -28,9 +29,11 @@ function hovercraft_should_show_posthero( $hero_type ) {
 		),
 	);
 
-	if ( ! isset( $allowed_display_options[ $hero_type ] ) ) {
+	// skip unknown hero type
+	if ( ! isset( $hovercraft_posthero_display_options[ $hero_type ] ) ) {
 		return false;
 	}
 
-	return in_array( $posthero_display, $allowed_display_options[ $hero_type ], true );
+	// check posthero display setting
+	return in_array( $hovercraft_posthero_display, $hovercraft_posthero_display_options[ $hero_type ], true );
 }
