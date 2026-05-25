e2b6fdb042f18bc04665bb06ad6a5d22571f0254
Jesse Nickles <jesse@littlebizzy.com>
2026-05-25 15:33:44 +0700

    Clean default header logo helper

--- inc/header/header-logo-default.php
+++ inc/header/header-logo-default.php
@@ -4,16 +4,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-// add default logo support
+// add header logo support
 function hovercraft_logo() {
-	$defaults = array(
+	$logo_defaults = array(
 		'height' => 100,
 		'width' => 250,
 		'flex-height' => true,
 		'flex-width' => true,
 	);
 
-	add_theme_support( 'custom-logo', $defaults );
+	add_theme_support( 'custom-logo', $logo_defaults );
 }
 add_action( 'after_setup_theme', 'hovercraft_logo' );
-
