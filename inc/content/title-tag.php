16b7ceaee299b1a3e0e65c3695caa7f8580316cd
Jesse Nickles <jesse@littlebizzy.com>
2026-05-25 17:47:49 +0700

    Clean title tag helper

--- inc/content/title-tag.php
+++ inc/content/title-tag.php
@@ -4,13 +4,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-// tag archive title
-
-function hovercraft_title_tag() {
-	add_theme_support( 'title-tag' );
-}
-add_action( 'after_setup_theme', 'hovercraft_title_tag' );
-
+// remove bbpress document title filters
 if ( class_exists( 'bbPress' ) ) {
 	remove_filter( 'wp_title', 'bbp_title', 10, 3 );
 	remove_filter( 'document_title_parts', 'bbp_filter_document_title', 10, 1 );
