c55d80a28c341dd654f314a1620ff58d732b5cd4
Jesse Nickles <jesse@littlebizzy.com>
2026-05-25 21:18:16 +0700

    Lightly clean admin meta collapse script

--- assets/js/admin-meta-collapse.js
+++ assets/js/admin-meta-collapse.js
@@ -1,16 +1,13 @@
 // admin meta collapse
-jQuery(function($) {
+jQuery( function( $ ) {
+	$( '.hovercraft-meta-group-header' ).on( 'click', function() {
+		var $header = $( this );
+		var $group = $header.closest( '.hovercraft-meta-group' );
+		var $body = $group.find( '.hovercraft-meta-group-body' );
+		var $toggle = $header.find( '.hovercraft-meta-group-toggle' );
+		var isOpen = $body.is( ':visible' );
 
-	$('.hovercraft-meta-group-header').on('click', function() {
-		var group = $(this).closest('.hovercraft-meta-group');
-		var body = group.find('.hovercraft-meta-group-body');
-		var toggle = $(this).find('.hovercraft-meta-group-toggle');
-
-		body.slideToggle(150);
-
-		toggle.text(
-			body.is(':visible') ? '▾' : '▸'
-		);
-	});
-
-});
+		$body.stop( true, true ).slideToggle( 150 );
+		$toggle.text( isOpen ? '▸' : '▾' );
+	} );
+} );
