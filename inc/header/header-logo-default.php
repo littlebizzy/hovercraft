448ead7d612bb1fe720351ddb0af01526d327019
Jesse Nickles <jesse@littlebizzy.com>
2026-05-25 15:07:05 +0700

    Rename default header logo helper

---
+++ inc/header/header-logo-default.php
@@ -0,0 +1,19 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+// add default logo support
+function hovercraft_logo() {
+	$defaults = array(
+		'height' => 100,
+		'width' => 250,
+		'flex-height' => true,
+		'flex-width' => true,
+	);
+
+	add_theme_support( 'custom-logo', $defaults );
+}
+add_action( 'after_setup_theme', 'hovercraft_logo' );
+
