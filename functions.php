5647b371cfb8393e88ba2897016e78f930bbf56d
Jesse Nickles <jesse@littlebizzy.com>
2026-05-25 20:39:49 +0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 if ( ! defined( 'HOVERCRAFT_VERSION' ) ) {
-	define( 'HOVERCRAFT_VERSION', '0.3.157' );
+	define( 'HOVERCRAFT_VERSION', '0.3.158' );
 }
 
 // rss feeds
