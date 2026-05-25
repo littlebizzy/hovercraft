d0f94fa7db79fc703e0ebd2e383793b81472d106
Jesse Nickles <jesse@littlebizzy.com>
2026-05-25 17:44:47 +0700

    Clean bbPress search results helper

--- inc/bbpress/bbpress-search-results.php
+++ inc/bbpress/bbpress-search-results.php
@@ -10,18 +10,11 @@ if ( ! function_exists( 'bbpress' ) ) {
 	return;
 }
 
-// include bbpress topics in search results
-function hovercraft_include_bbpress_topics_in_search( $post_type_args ) {
+// include bbpress content in search results
+function hovercraft_include_bbpress_content_in_search( $post_type_args ) {
 	$post_type_args['exclude_from_search'] = false;
 
 	return $post_type_args;
 }
-add_filter( 'bbp_register_topic_post_type', 'hovercraft_include_bbpress_topics_in_search' );
-
-// include bbpress replies in search results
-function hovercraft_include_bbpress_replies_in_search( $post_type_args ) {
-	$post_type_args['exclude_from_search'] = false;
-
-	return $post_type_args;
-}
-add_filter( 'bbp_register_reply_post_type', 'hovercraft_include_bbpress_replies_in_search' );
+add_filter( 'bbp_register_topic_post_type', 'hovercraft_include_bbpress_content_in_search' );
+add_filter( 'bbp_register_reply_post_type', 'hovercraft_include_bbpress_content_in_search' );
