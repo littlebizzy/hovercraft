45ed84f96d5c2bf574edb075d34ea6cc8cc59f7c
Jesse Nickles <jesse@littlebizzy.com>
2026-05-26 18:54:26 +0700

    Add skip link focus styling

--- inc/css/focus.php
+++ inc/css/focus.php
@@ -18,3 +18,20 @@ a:focus-visible, button:focus-visible, input:focus-visible, select:focus-visible
 input:focus-visible, select:focus-visible, textarea:focus-visible {
 	box-shadow: inset 0 0 0 1px <?php echo $default_link_color; ?> !important;
 }
+
+.skip-link:focus,
+.skip-link:focus-visible {
+	clip: auto;
+	position: fixed !important;
+	top: 10px;
+	left: 10px;
+	z-index: 999999;
+	width: auto;
+	height: auto;
+	padding: 10px 15px;
+	overflow: visible;
+	background: <?php echo $content_background_color; ?>;
+	color: <?php echo $default_link_color; ?>;
+	box-shadow: 0 0 0 2px <?php echo $default_link_color; ?>;
+	text-decoration: none;
+}
