c72f9353f133e22dc6098e0326af8c8236cbb4b5
Jesse Nickles <jesse@littlebizzy.com>
2026-05-26 17:47:11 +0700

    Normalize page template formatting

--- page-templates/mini-hero-tiles-no-sidebar.php
+++ page-templates/mini-hero-tiles-no-sidebar.php
@@ -5,43 +5,39 @@
 <?php get_template_part( 'header' ); ?>
 <?php get_template_part( 'template-parts/header/header-mini-hero' ); ?>
 
-	<?php if ( hovercraft_should_show_posthero( 'mini' ) ) : ?>
-		<?php get_template_part( 'template-parts/hero/hero-posthero' ); ?>
-	<?php endif; // end posthero ?>
+<?php if ( hovercraft_should_show_posthero( 'mini' ) ) : ?>
+	<?php get_template_part( 'template-parts/hero/hero-posthero' ); ?>
+<?php endif; // end posthero ?>
 
 <?php if ( hovercraft_should_show_main_content() ) : ?>
 
 <div id="main">
 <div class="inner">
-    
-    <div id="primary-wide">
+	<div id="primary-wide">
 
-	<div id="content-wrapper">
+		<div id="content-wrapper">
 
-	<div id="content-wide">
-			
-	<?php get_template_part( 'template-parts/misc/breadcrumbs' ); ?>
-		
-	<?php get_template_part( 'template-parts/content/loop' ); ?>
-		
-	<?php get_template_part( 'template-parts/content/tiles-wide' ); ?>
-						
-	<?php get_template_part( 'template-parts/content/pagination' ); ?>
-				
+			<div id="content-wide">
+				<?php get_template_part( 'template-parts/misc/breadcrumbs' ); ?>
+				<?php get_template_part( 'template-parts/content/loop' ); ?>
+				<?php get_template_part( 'template-parts/content/tiles-wide' ); ?>
+				<?php get_template_part( 'template-parts/content/pagination' ); ?>
+
+				<div class="clear"></div>
+			</div><!-- content-wide -->
+
+		</div><!-- content-wrapper -->
+
+		<?php get_template_part( 'template-parts/comments/comments' ); ?>
+
+		<div class="clear"></div>
+	</div><!-- primary -->
+
 	<div class="clear"></div>
-	</div><!-- content-padded -->
-		
-	</div><!-- content-wrapper -->   
-	
-	    <?php get_template_part( 'template-parts/comments/comments' ); ?>
 
-	    <div class="clear"></div>
-    </div><!-- primary -->
-        
-    <div class="clear"></div>
 </div><!-- inner -->
 </div><!-- main -->
 
-<?php endif; // end is_front_page ?>
+<?php endif; // end main content ?>
 
 <?php get_template_part( 'footer' ); ?>
