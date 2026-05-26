267ac96d98fc80a39e18ac0f3e8f77a53eb4c6aa
Jesse Nickles <jesse@littlebizzy.com>
2026-05-26 17:21:29 +0700

    Normalize page template formatting

--- page-templates/full-hero-columns-no-sidebar.php
+++ page-templates/full-hero-columns-no-sidebar.php
@@ -6,28 +6,28 @@
 <?php get_template_part( 'template-parts/header/header-full-hero' ); ?>
 </div><!-- full-hero-viewport -->
 
-	<?php if ( hovercraft_should_show_posthero( 'full' ) ) : ?>
-		<?php get_template_part( 'template-parts/hero/hero-posthero' ); ?>
-	<?php endif; // end posthero ?>
+<?php if ( hovercraft_should_show_posthero( 'full' ) ) : ?>
+	<?php get_template_part( 'template-parts/hero/hero-posthero' ); ?>
+<?php endif; // end posthero ?>
 
 <?php if ( hovercraft_should_show_main_content() ) : ?>
 
 <div id="main">
 <div class="inner">
-	
-<div id="primary-wide">
-	
-<?php get_template_part( 'template-parts/misc/breadcrumbs' ); ?>
-<?php get_template_part( 'template-parts/content/loop' ); ?>
-<?php get_template_part( 'template-parts/content/columns' ); ?>
+	<div id="primary-wide">
 
-<div class="clear"></div>
-</div><!-- primary -->
+		<?php get_template_part( 'template-parts/misc/breadcrumbs' ); ?>
+		<?php get_template_part( 'template-parts/content/loop' ); ?>
+		<?php get_template_part( 'template-parts/content/columns' ); ?>
 
-<div class="clear"></div>
+		<div class="clear"></div>
+	</div><!-- primary-wide -->
+
+	<div class="clear"></div>
+
 </div><!-- inner -->
 </div><!-- main -->
-	
+
 <?php endif; // end main content ?>
-	
+
 <?php get_template_part( 'footer' ); ?>
