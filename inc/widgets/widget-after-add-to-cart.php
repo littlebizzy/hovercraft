5f7230e5f5c8682c57846d5c0b628c2f77507120
Jesse Nickles <jesse@littlebizzy.com>
2026-05-26 20:53:33 +0700

    Add after add to cart widget area

---
+++ inc/widgets/widget-after-add-to-cart.php
@@ -0,0 +1,30 @@
+<?php
+
+// block direct access
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+// register after add to cart widget area
+function hovercraft_register_after_add_to_cart_sidebar() {
+	hovercraft_register_widget_area(
+		array(
+			'name' => 'After Add To Cart',
+			'id' => 'hovercraft_after_add_to_cart',
+			'class' => 'widget-after-add-to-cart',
+		)
+	);
+}
+add_action( 'widgets_init', 'hovercraft_register_after_add_to_cart_sidebar' );
+
+// render after add to cart widget area
+function hovercraft_render_after_add_to_cart_sidebar() {
+	if ( ! is_active_sidebar( 'hovercraft_after_add_to_cart' ) ) {
+		return;
+	}
+
+	echo '<div class="hovercraft-after-add-to-cart">';
+	dynamic_sidebar( 'hovercraft_after_add_to_cart' );
+	echo '</div><!-- hovercraft-after-add-to-cart -->';
+}
+add_action( 'woocommerce_after_add_to_cart_form', 'hovercraft_render_after_add_to_cart_sidebar' );
