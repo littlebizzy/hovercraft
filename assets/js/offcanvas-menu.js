6cc7607916fbc63259c183c85b1ca45b32a37237
Jesse Nickles <jesse@littlebizzy.com>
2026-05-26 19:18:43 +0700

    Update offcanvas-menu.js

--- assets/js/offcanvas-menu.js
+++ assets/js/offcanvas-menu.js
@@ -4,24 +4,52 @@ jQuery( function( $ ) {
 	var $menu = $( '#hovercraft-offcanvas-menu' );
 	var $overlay = $( '.hovercraft-offcanvas-overlay' );
 	var $trigger = $( '.hovercraft-offcanvas-trigger' );
+	var $lastFocused = $();
 
 	if ( ! $menu.length || ! $overlay.length || ! $trigger.length ) {
 		return;
 	}
 
-	// toggle offcanvas menu
-	$trigger.on( 'click', function() {
-		$menu.toggleClass( 'active' );
-		$body.toggleClass( 'frozen' );
-		$overlay.toggleClass( 'active' );
-		$trigger.attr( 'aria-expanded', $menu.hasClass( 'active' ) ? 'true' : 'false' );
-	} );
+	// open offcanvas menu
+	function openOffcanvasMenu() {
+		$lastFocused = $( document.activeElement );
+		$menu.addClass( 'active' );
+		$body.addClass( 'frozen' );
+		$overlay.addClass( 'active' );
+		$trigger.attr( 'aria-expanded', 'true' );
+	}
 
 	// close offcanvas menu
-	$overlay.on( 'click', function() {
+	function closeOffcanvasMenu() {
 		$menu.removeClass( 'active' );
 		$body.removeClass( 'frozen' );
 		$overlay.removeClass( 'active' );
 		$trigger.attr( 'aria-expanded', 'false' );
+
+		if ( $lastFocused.length ) {
+			$lastFocused.trigger( 'focus' );
+		}
+	}
+
+	// toggle offcanvas menu
+	$trigger.on( 'click', function() {
+		if ( $menu.hasClass( 'active' ) ) {
+			closeOffcanvasMenu();
+			return;
+		}
+
+		openOffcanvasMenu();
+	} );
+
+	// close offcanvas menu
+	$overlay.on( 'click', function() {
+		closeOffcanvasMenu();
+	} );
+
+	// close offcanvas menu keyboard
+	$( document ).on( 'keydown', function( event ) {
+		if ( 'Escape' === event.key && $menu.hasClass( 'active' ) ) {
+			closeOffcanvasMenu();
+		}
 	} );
 } );
