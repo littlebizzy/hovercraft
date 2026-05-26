30348bf7a196f63273bdf731a1382246099b2345
Jesse Nickles <jesse@littlebizzy.com>
2026-05-26 17:07:57 +0700

    Normalize page template formatting

--- page-templates/basic-columns-no-sidebar.php
+++ page-templates/basic-columns-no-sidebar.php
@@ -1,12 +1,12 @@
 <?php
 /* Template Name: Basic Columns (No Sidebar) */
 
-// basic columns page template
-
 ?>
 <?php get_template_part( 'header' ); ?>
 <?php get_template_part( 'template-parts/header/header-basic' ); ?>
+
 <?php if ( hovercraft_should_show_main_content() ) : ?>
-<?php get_template_part( 'template-parts/content/columns' ); ?>
+	<?php get_template_part( 'template-parts/content/columns' ); ?>
 <?php endif; // end main content ?>
+
 <?php get_template_part( 'footer' ); ?>
