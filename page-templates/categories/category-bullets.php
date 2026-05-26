da5e6a4c065fda178c1503c63b9033a4ddaf2590
Jesse Nickles <jesse@littlebizzy.com>
2026-05-26 17:13:15 +0700

    Normalize page template formatting

--- page-templates/categories/category-bullets.php
+++ page-templates/categories/category-bullets.php
@@ -2,10 +2,8 @@
 
 // category bullets page template
 
- get_template_part( 'header' ); ?>
+?>
+<?php get_template_part( 'header' ); ?>
 <?php get_template_part( 'template-parts/header/header-mini-hero' ); ?>
 <?php get_template_part( 'template-parts/content/bullets' ); ?>
 <?php get_template_part( 'footer' ); ?>
-
-<?php
-?>
