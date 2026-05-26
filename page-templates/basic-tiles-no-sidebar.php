4728fa792a3e7aa57e1015ffe1bb50a6e5a9a5eb
Jesse Nickles <jesse@littlebizzy.com>
2026-05-26 17:12:31 +0700

    Normalize page template formatting

--- page-templates/basic-tiles-no-sidebar.php
+++ page-templates/basic-tiles-no-sidebar.php
@@ -9,33 +9,32 @@
 
 <div id="main">
 <div class="inner">
-    
-    <div id="primary-wide">
+	<div id="primary-wide">
 
-	<div id="content-wrapper">
+		<div id="content-wrapper">
 
-	<div id="content-wide">
-			
-	<?php get_template_part( 'template-parts/misc/breadcrumbs' ); ?>
-	<?php get_template_part( 'template-parts/content/title' ); ?>
-	<?php get_template_part( 'template-parts/content/loop' ); ?>
-	<?php get_template_part( 'template-parts/content/tiles-wide' ); ?>			
-	<?php get_template_part( 'template-parts/content/pagination' ); ?>
-				
+			<div id="content-wide">
+				<?php get_template_part( 'template-parts/misc/breadcrumbs' ); ?>
+				<?php get_template_part( 'template-parts/content/title' ); ?>
+				<?php get_template_part( 'template-parts/content/loop' ); ?>
+				<?php get_template_part( 'template-parts/content/tiles-wide' ); ?>
+				<?php get_template_part( 'template-parts/content/pagination' ); ?>
+
+				<div class="clear"></div>
+			</div><!-- content-wide -->
+
+		</div><!-- content-wrapper -->
+
+		<?php get_template_part( 'template-parts/comments/comments' ); ?>
+
+		<div class="clear"></div>
+	</div><!-- primary-wide -->
+
 	<div class="clear"></div>
-	</div><!-- content-padded -->
-		
-	</div><!-- content-wrapper -->   
-	
-	    <?php get_template_part( 'template-parts/comments/comments' ); ?>
 
-	    <div class="clear"></div>
-    </div><!-- primary -->
-        
-    <div class="clear"></div>
 </div><!-- inner -->
 </div><!-- main -->
 
-<?php endif; // end is_front_page ?>
+<?php endif; // end main content ?>
 
 <?php get_template_part( 'footer' ); ?>
