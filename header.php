e46c6f6b3c155cf74a05453793fb1697b25e062e
Jesse Nickles <jesse@littlebizzy.com>
2026-05-26 18:53:47 +0700

    Add skip link

--- header.php
+++ header.php
@@ -28,6 +28,8 @@
 
 <?php wp_body_open(); ?>
 
+<a class="skip-link screen-reader-text" href="#main"><?php esc_html_e( 'Skip to content', 'hovercraft' ); ?></a>
+
 <?php get_template_part( 'template-parts/header/mobile-menu' ); ?>
 
 <div id="container"><!-- main container -->
