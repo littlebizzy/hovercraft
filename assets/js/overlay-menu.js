4bd2e0cf18cfdc643bbac13dcd02e453da3824af
Jesse Nickles <jesse@littlebizzy.com>
2026-05-26 19:11:58 +0700

    Update overlay menu expanded state

--- assets/js/overlay-menu.js
+++ assets/js/overlay-menu.js
@@ -1,16 +1,30 @@
 // overlay menu
 function hovercraftOpenNav() {
 	var menu = document.getElementById( 'hovercraft-overlay-menu' );
+	var trigger = document.querySelector( '.mobile-menu-trig:not(.hovercraft-offcanvas-trigger)' );
 
 	if ( menu ) {
 		menu.style.height = '100%';
+		menu.setAttribute( 'aria-hidden', 'false' );
+		document.body.classList.add( 'frozen' );
+	}
+
+	if ( trigger ) {
+		trigger.setAttribute( 'aria-expanded', 'true' );
 	}
 }
 
 function hovercraftCloseNav() {
 	var menu = document.getElementById( 'hovercraft-overlay-menu' );
+	var trigger = document.querySelector( '.mobile-menu-trig:not(.hovercraft-offcanvas-trigger)' );
 
 	if ( menu ) {
 		menu.style.height = '0%';
+		menu.setAttribute( 'aria-hidden', 'true' );
+		document.body.classList.remove( 'frozen' );
+	}
+
+	if ( trigger ) {
+		trigger.setAttribute( 'aria-expanded', 'false' );
 	}
 }
