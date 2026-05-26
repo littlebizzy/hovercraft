bb8189d940bb3fdd7510eddf2c5b23a7cca010f8
Jesse Nickles <jesse@littlebizzy.com>
2026-05-26 17:38:14 +0700

    Normalize page template formatting

--- page-templates/mini-hero-no-sidebar.php
+++ page-templates/mini-hero-no-sidebar.php
@@ -5,27 +5,27 @@
 <?php get_template_part( 'header' ); ?>
 <?php get_template_part( 'template-parts/header/header-mini-hero' ); ?>
 
-	<?php if ( hovercraft_should_show_posthero( 'mini' ) ) : ?>
-		<?php get_template_part( 'template-parts/hero/hero-posthero' ); ?>
-	<?php endif; // end posthero ?>
+<?php if ( hovercraft_should_show_posthero( 'mini' ) ) : ?>
+	<?php get_template_part( 'template-parts/hero/hero-posthero' ); ?>
+<?php endif; // end posthero ?>
 
 <?php if ( hovercraft_should_show_main_content() ) : ?>
 
 <div id="main">
 <div class="inner">
-    
 	<div id="primary-wide">
-	
+
 		<?php get_template_part( 'template-parts/content/content-wide' ); ?>
-	
+
 		<?php get_template_part( 'template-parts/comments/comments' ); ?>
 
 		<div class="clear"></div>
 	</div><!-- primary-wide -->
-    
-    <?php get_template_part( 'template-parts/content/pagination' ); ?>
-	
-    <div class="clear"></div>
+
+	<?php get_template_part( 'template-parts/content/pagination' ); ?>
+
+	<div class="clear"></div>
+
 </div><!-- inner -->
 </div><!-- main -->
 
