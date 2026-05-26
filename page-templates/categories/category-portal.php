74589847aa91fe93b92e653269acdd2aa7531520
Jesse Nickles <jesse@littlebizzy.com>
2026-05-26 17:20:59 +0700

    Normalize page template formatting

--- page-templates/categories/category-portal.php
+++ page-templates/categories/category-portal.php
@@ -1,19 +1,20 @@
 <?php
 
-get_template_part( 'header' ); ?>
+?>
+<?php get_template_part( 'header' ); ?>
 <?php get_template_part( 'template-parts/header/header-mini-hero' ); ?>
 
 <div id="main">
 <div class="inner">
 
 	<div id="primary-wide">
-	
+
 		<div id="content-wrapper">
-			
+
 			<?php get_template_part( 'template-parts/misc/breadcrumbs' ); ?>
-									
+
 			<div id="content-padded">
-		
+
 				<?php if ( have_posts() ) : ?>
 					<?php while ( have_posts() ) : ?>
 						<?php the_post(); ?>
@@ -22,20 +23,21 @@ get_template_part( 'header' ); ?>
 							<h4 class="entry-title"><a href="<?php echo esc_url( get_permalink() ); ?>"><?php echo esc_html( get_the_title() ); ?></a></h4>
 							<div class="entry-summary"><?php the_excerpt(); ?></div>
 						</article><!-- post-tease-archive -->
-		
+
 					<?php endwhile; // end category portal posts ?>
-				<?php endif; ?><!-- end the loop -->
-					
+				<?php endif; // end category portal loop ?>
+
 				<?php get_template_part( 'template-parts/content/pagination' ); ?>
-						
-			<div class="clear"></div>
+
+				<div class="clear"></div>
 			</div><!-- content-padded -->
+
 		</div><!-- content-wrapper -->
 
+		<div class="clear"></div>
+	</div><!-- primary-wide -->
+
 	<div class="clear"></div>
-  	</div><!-- primary -->
-    
-<div class="clear"></div>
 </div><!-- inner -->
 </div><!-- main -->
 
