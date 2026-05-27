d483e429c39b51bd6d177d9bf9fb8d8e8b57b708
Jesse Nickles <jesse@littlebizzy.com>
2026-05-27 21:52:46 +0700

    Style stacked header widget areas

--- inc/css/header/general.php
+++ inc/css/header/general.php
@@ -60,7 +60,9 @@ $navbar_background_color = get_theme_mod( 'hovercraft_navbar_background_color', 
 }
 
 @media screen and (max-width: 1199px) {
-	.header-layout-stacked-bottom {
+	.header-layout-stacked-bottom,
+	.header-center,
+	.header-aside {
 		display: none;
 	}
 }
@@ -73,6 +75,45 @@ $navbar_background_color = get_theme_mod( 'hovercraft_navbar_background_color', 
 	.header-layout-stacked-top,
 	.header-layout-stacked-bottom {
 		width: 100%;
+	}
+
+	.header-layout-stacked-top .header-layout-stacked-inner {
+		gap: 30px;
+	}
+
+	.header-center {
+		flex: 1 1 auto;
+		min-width: 0;
+		text-align: center;
+	}
+
+	.header-aside {
+		display: inline-block;
+		margin-right: 30px;
+		vertical-align: middle;
+	}
+
+	.header-center p,
+	.header-aside p {
+		margin-bottom: 0;
+	}
+
+	.header-center ul,
+	.header-aside ul {
+		margin: 0;
+		list-style-type: none;
+	}
+
+	.header-center ul li,
+	.header-aside ul li {
+		list-style-type: none;
+		display: inline-block;
+		margin: 0 0 0 10px;
+	}
+
+	.header-center ul li:first-child,
+	.header-aside ul li:first-child {
+		margin-left: 0;
 	}
 
 	.header-layout-stacked-bottom {
