2ea83f713c273067ff71411c21a5794c5c7dbacd
Jesse Nickles <jesse@littlebizzy.com>
2026-05-27 15:54:17 +0700

    Align stacked header CTAs right

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 if ( ! defined( 'HOVERCRAFT_VERSION' ) ) {
-	define( 'HOVERCRAFT_VERSION', '0.3.170' );
+	define( 'HOVERCRAFT_VERSION', '0.3.171' );
 }
 
 // rss feeds
