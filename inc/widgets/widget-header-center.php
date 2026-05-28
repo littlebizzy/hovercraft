42adfbbd85e89ccd0602391338995712bfba2b4c
Jesse Nickles <jesse@littlebizzy.com>
2026-05-28 16:41:00 +0700

    Remove duplicate header center registration

--- inc/widgets/widget-header-center.php
+++ inc/widgets/widget-header-center.php
@@ -5,18 +5,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-// register header center widget area
-function hovercraft_register_header_center_sidebar() {
-	hovercraft_register_widget_area(
-		array(
-			'name' => 'Header Center',
-			'id' => 'hovercraft_header_center',
-			'class' => 'widget-header-center',
-		)
-	);
-}
-add_action( 'widgets_init', 'hovercraft_register_header_center_sidebar' );
-
 // render header center widget area
 function hovercraft_header_center_widget_area() {
 	if ( ! is_active_sidebar( 'hovercraft_header_center' ) ) {
