f530bcbf5856b6d34e7dc49e64b7da4b0051684d
Jesse Nickles <jesse@littlebizzy.com>
2026-05-28 16:42:14 +0700

    Remove duplicate header aside registration

--- inc/widgets/widget-header-aside.php
+++ inc/widgets/widget-header-aside.php
@@ -5,18 +5,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-// register header aside widget area
-function hovercraft_register_header_aside_sidebar() {
-	hovercraft_register_widget_area(
-		array(
-			'name' => 'Header Aside',
-			'id' => 'hovercraft_header_aside',
-			'class' => 'widget-header-aside',
-		)
-	);
-}
-add_action( 'widgets_init', 'hovercraft_register_header_aside_sidebar' );
-
 // render header aside widget area
 function hovercraft_header_aside_widget_area() {
 	if ( ! is_active_sidebar( 'hovercraft_header_aside' ) ) {
