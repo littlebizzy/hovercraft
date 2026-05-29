60353c006d09455c636967ffd81f084cc9861435
Jesse Nickles <jesse@littlebizzy.com>
2026-05-29 14:04:06 +0700

    Refine mini cart summary markup

--- woocommerce/cart/mini-cart.php
+++ woocommerce/cart/mini-cart.php
@@ -12,19 +12,19 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 	<?php $hovercraft_cart_count = WC()->cart->get_cart_contents_count(); ?>
 
-	<div class="hovercraft-mini-cart-summary">
-		<a class="hovercraft-mini-cart-count" href="<?php echo esc_url( wc_get_cart_url() ); ?>">
+	<a class="hovercraft-mini-cart-summary" href="<?php echo esc_url( wc_get_cart_url() ); ?>">
+		<span class="hovercraft-mini-cart-count">
 			<span class="hovercraft-mini-cart-count-number"><?php echo esc_html( $hovercraft_cart_count ); ?></span>
 			<span class="hovercraft-mini-cart-count-label">
 				<?php echo esc_html( _n( 'item in cart', 'items in cart', $hovercraft_cart_count, 'woocommerce' ) ); ?>
 			</span><!-- hovercraft-mini-cart-count-label -->
-		</a><!-- hovercraft-mini-cart-count -->
+		</span><!-- hovercraft-mini-cart-count -->
 
-		<div class="hovercraft-mini-cart-subtotal">
-			<span><?php esc_html_e( 'Subtotal:', 'woocommerce' ); ?></span>
+		<span class="hovercraft-mini-cart-subtotal">
+			<span class="hovercraft-mini-cart-subtotal-label"><?php esc_html_e( 'Subtotal:', 'woocommerce' ); ?></span>
 			<strong><?php echo wp_kses_post( WC()->cart->get_cart_subtotal() ); ?></strong>
-		</div><!-- hovercraft-mini-cart-subtotal -->
-	</div><!-- hovercraft-mini-cart-summary -->
+		</span><!-- hovercraft-mini-cart-subtotal -->
+	</a><!-- hovercraft-mini-cart-summary -->
 
 	<p class="woocommerce-mini-cart__buttons buttons hovercraft-mini-cart-buttons">
 		<a href="<?php echo esc_url( wc_get_checkout_url() ); ?>" class="button checkout wc-forward">
