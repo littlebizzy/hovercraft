676e85294cfa155d665f81e19df8baea0b79aab2
Jesse Nickles <jesse@littlebizzy.com>
2026-05-30 17:11:29 +0700

    Align half hero header spacing rhythm

--- inc/css/hero/half.php
+++ inc/css/hero/half.php
@@ -106,14 +106,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 @media screen and (max-width: 1199px) {
 	#header-half-hero {
-		padding: 10px 20px;
+		padding: 16px 20px;
 	}
 }
 
 @media screen and (min-width: 1200px) {
 	#header-half-hero {
 		margin: 0 auto;
-		padding: 20px 0;
+		padding: 24px 0;
 	}
 }
 
