6f9e010f0589a4ba9d5e9beb3145483c65f17bf8
Jesse Nickles <jesse@littlebizzy.com>
2026-06-02 19:42:38 +0700

    Simplify comment form field customization

--- inc/comments/comment-fields.php
+++ inc/comments/comment-fields.php
@@ -8,18 +8,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 // customize comment form fields
 function hovercraft_customize_comment_form_fields( $fields ) {
 	$commenter = wp_get_current_commenter();
-	$require_name_email = get_option( 'require_name_email' );
-	$required_attribute = '';
-	$required_markup = '';
-
-	// set required field markup
-	if ( $require_name_email ) {
-		$required_attribute = ' aria-required="true" required';
-		$required_markup = '<span class="required" aria-hidden="true">*</span>';
-	}
+	$required_attribute = get_option( 'require_name_email' ) ? ' aria-required="true" required' : '';
+	$required_markup = get_option( 'require_name_email' ) ? ' <span class="required" aria-hidden="true">*</span>' : '';
 
 	$fields['author'] = sprintf(
-		'<p class="comment-form-author"><label for="comment-author">%s</label> %s<input id="comment-author" name="author" type="text" value="%s" size="30" maxlength="245" autocomplete="name"%s></p>',
+		'<p class="comment-form-author"><label for="comment-author">%s%s</label><input id="comment-author" name="author" type="text" value="%s" size="30" maxlength="245" autocomplete="name"%s></p>',
 		esc_html__( 'Name', 'hovercraft' ),
 		$required_markup,
 		esc_attr( $commenter['comment_author'] ),
@@ -27,7 +20,7 @@ function hovercraft_customize_comment_form_fields( $fields ) {
 	);
 
 	$fields['email'] = sprintf(
-		'<p class="comment-form-email"><label for="comment-email">%s</label> %s<input id="comment-email" name="email" type="email" value="%s" size="30" maxlength="100" aria-describedby="email-notes" autocomplete="email"%s></p>',
+		'<p class="comment-form-email"><label for="comment-email">%s%s</label><input id="comment-email" name="email" type="email" value="%s" size="30" maxlength="100" aria-describedby="email-notes" autocomplete="email"%s></p>',
 		esc_html__( 'Email', 'hovercraft' ),
 		$required_markup,
 		esc_attr( $commenter['comment_author_email'] ),
@@ -35,7 +28,13 @@ function hovercraft_customize_comment_form_fields( $fields ) {
 	);
 
 	if ( isset( $fields['cookies'] ) ) {
-		$fields['cookies'] = '<p class="comment-form-cookies-consent"><input id="wp-comment-cookies-consent" name="wp-comment-cookies-consent" type="checkbox" value="yes"> <label for="wp-comment-cookies-consent">' . esc_html__( 'Save my name and email in this browser for the next time I comment.', 'hovercraft' ) . '</label></p>';
+		$checked_attribute = empty( $commenter['comment_author_email'] ) ? '' : ' checked="checked"';
+
+		$fields['cookies'] = sprintf(
+			'<p class="comment-form-cookies-consent"><input id="wp-comment-cookies-consent" name="wp-comment-cookies-consent" type="checkbox" value="yes"%s> <label for="wp-comment-cookies-consent">%s</label></p>',
+			$checked_attribute,
+			esc_html__( 'Save my name and email in this browser for the next time I comment.', 'hovercraft' )
+		);
 	}
 
 	unset( $fields['url'] );
