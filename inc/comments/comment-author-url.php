6e915d6a6ff92b4dc158c252822b35f64e26aca4
Jesse Nickles <jesse@littlebizzy.com>
2026-06-03 03:14:19 +0700

    Simplify comment author URL handling

--- inc/comments/comment-author-url.php
+++ inc/comments/comment-author-url.php
@@ -6,13 +6,4 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // remove comment author url output
-function hovercraft_disable_comment_author_url( $url ) {
-	return '';
-}
-add_filter( 'get_comment_author_url', 'hovercraft_disable_comment_author_url' );
-
-// display comment authors as plain text
-function hovercraft_disable_comment_author_link( $return, $author, $comment_id ) {
-	return esc_html( $author );
-}
-add_filter( 'get_comment_author_link', 'hovercraft_disable_comment_author_link', 10, 3 );
+add_filter( 'get_comment_author_url', '__return_empty_string' );
