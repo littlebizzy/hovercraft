3f3780cbb4677e76de71d776473c8821f9d55710
Jesse Nickles <jesse@littlebizzy.com>
2026-06-03 03:15:23 +0700

    Simplify comments CSS

--- inc/css/comments.php
+++ inc/css/comments.php
@@ -10,7 +10,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 /* comments */
 
-/* remove bullets from comment lists */
 .comment-list,
 .comment-list .children {
 	list-style: none;
@@ -18,14 +17,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 	padding: 0;
 }
 
-.comment-list > li,
-.comment-list .children > li {
-	list-style: none;
-	margin: 0;
-	padding: 0;
+.comment-list .children {
+	border-left: 1px solid rgba(0, 0, 0, 0.08);
+	border-radius: 0;
+	margin-left: 2rem;
+	margin-top: 1rem;
+	padding-left: 1rem;
 }
 
-/* comment body styling */
 .comment-body {
 	border-bottom: 1px solid rgba(0, 0, 0, 0.12);
 	border-radius: 0;
@@ -33,20 +32,17 @@ if ( ! defined( 'ABSPATH' ) ) {
 	padding-bottom: 20px;
 }
 
-/* author name */
 .comment-author {
 	font-weight: 600;
 	margin-bottom: 4px;
 }
 
-/* comment meta */
 .comment-meta {
 	color: rgba(0, 0, 0, 0.62);
 	font-size: 14px;
 	margin-bottom: 10px;
 }
 
-/* comment content */
 .comment-content {
 	font-size: 16px;
 	line-height: 1.6;
@@ -71,7 +67,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 	padding: 0;
 }
 
-/* reply link */
 .comment-reply-link,
 #cancel-comment-reply-link {
 	border-radius: 0;
@@ -89,16 +84,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 	text-decoration: underline;
 }
 
-/* nested comments */
-.comment-list .children {
-	border-left: 1px solid rgba(0, 0, 0, 0.08);
-	border-radius: 0;
-	margin-left: 2rem;
-	margin-top: 1rem;
-	padding-left: 1rem;
-}
-
-/* comment anchors */
 .comment-list .comment,
 .comment-list .pingback,
 .comment-list .trackback,
@@ -106,7 +91,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 	scroll-margin-top: 120px;
 }
 
-/* pagination */
 .comment-navigation {
 	font-size: 14px;
 	margin-top: 20px;
@@ -127,19 +111,16 @@ if ( ! defined( 'ABSPATH' ) ) {
 	text-align: right;
 }
 
-/* moderation notice */
 .comment-awaiting-moderation {
 	font-style: italic;
 	margin-bottom: 10px;
 }
 
-/* post author comments */
 .bypostauthor .comment-body {
 	border-left: 3px solid rgba(0, 0, 0, 0.12);
 	padding-left: 16px;
 }
 
-/* comment form */
 #respond textarea,
 #respond input[type="text"],
 #respond input[type="email"] {
