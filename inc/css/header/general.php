3f26fae61441d6cae6863bdba898bbfa1d0b9ecc
Jesse Nickles <jesse@littlebizzy.com>
2026-06-07 15:10:52 +0700

    Add border to full hero stacked navbar

--- inc/css/header/general.php
+++ inc/css/header/general.php
@@ -158,6 +158,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 	#header-full-hero .header-layout-stacked-bottom {
 		background: transparent;
+		border-top: 1px solid rgba(255, 255, 255, 0.24);
 	}
 
 	#header-basic .header-layout-stacked-bottom {
