25d4f1fb5f20d9eed9671ae6bb6210bdb92514a8
Jesse Nickles <jesse@littlebizzy.com>
2026-06-07 13:49:42 +0700

    Bump theme constant to 0.3.222

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 if ( ! defined( 'HOVERCRAFT_VERSION' ) ) {
-	define( 'HOVERCRAFT_VERSION', '0.3.221' );
+	define( 'HOVERCRAFT_VERSION', '0.3.222' );
 }
 
 // rss feeds
@@ -78,82 +78,3 @@ require get_template_directory() . '/inc/media/sizes-insert.php';
 // navigation
 require get_template_directory() . '/inc/navigation/breadcrumbs.php';
 require get_template_directory() . '/inc/navigation/pagination.php';
-
-// taxonomy
-require get_template_directory() . '/inc/taxonomy/category-bullets.php';
-require get_template_directory() . '/inc/taxonomy/category-portal.php';
-require get_template_directory() . '/inc/taxonomy/category-hide-certain.php';
-
-// comments
-require get_template_directory() . '/inc/comments/comment-author-url.php';
-require get_template_directory() . '/inc/comments/comment-fields.php';
-require get_template_directory() . '/inc/comments/comment-reply.php';
-
-// widgets
-require get_template_directory() . '/inc/widgets/widget-areas.php';
-require get_template_directory() . '/inc/widgets/widget-tiles.php';
-require get_template_directory() . '/inc/widgets/widget-columns.php';
-require get_template_directory() . '/inc/widgets/widget-zigzag.php';
-require get_template_directory() . '/inc/widgets/widget-header-center.php';
-require get_template_directory() . '/inc/widgets/widget-header-aside.php';
-require get_template_directory() . '/inc/widgets/widget-after-add-to-cart.php';
-require get_template_directory() . '/inc/sidebar/sidebar-callout.php';
-require get_template_directory() . '/inc/sidebar/sidebar-display.php';
-
-// footer
-require get_template_directory() . '/inc/footer/footer-back-to-top.php';
-require get_template_directory() . '/inc/footer/footer-callout.php';
-
-// customizer
-require get_template_directory() . '/inc/customizer/customizer.php';
-require get_template_directory() . '/inc/customizer/categories/category-blog.php';
-require get_template_directory() . '/inc/customizer/categories/category-portal.php';
-require get_template_directory() . '/inc/customizer/categories/category-faq.php';
-require get_template_directory() . '/inc/customizer/fonts/font-helpers.php';
-require get_template_directory() . '/inc/customizer/fonts/google-fonts.php';
-require get_template_directory() . '/inc/customizer/fonts/google-fonts-multilingual.php';
-
-// css
-require get_template_directory() . '/inc/css/css-generator.php';
-
-// fonts
-require get_template_directory() . '/inc/fonts/google-fonts.php';
-
-// metadata
-require get_template_directory() . '/inc/metadata/metadata-open-graph.php';
-
-// meta boxes
-require get_template_directory() . '/inc/meta-boxes/meta-box.php';
-require get_template_directory() . '/inc/meta-boxes/meta-box-collapse-assets.php';
-require get_template_directory() . '/inc/meta-boxes/meta-box-zigzag.php';
-
-// woocommerce
-require get_template_directory() . '/inc/woocommerce/woocommerce-theme-support.php';
-require get_template_directory() . '/inc/woocommerce/woocommerce-checkout-coupon.php';
-require get_template_directory() . '/inc/woocommerce/woocommerce-checkout-order-summary.php';
-require get_template_directory() . '/inc/woocommerce/woocommerce-quantity-select.php';
-require get_template_directory() . '/inc/woocommerce/woocommerce-wrappers.php';
-
-// bbpress
-require get_template_directory() . '/inc/bbpress/bbpress-disable-reply-links.php';
-require get_template_directory() . '/inc/bbpress/bbpress-disable-styles.php';
-require get_template_directory() . '/inc/bbpress/bbpress-quick-tags.php';
-require get_template_directory() . '/inc/bbpress/bbpress-search-results.php';
-
-// shortcodes
-require get_template_directory() . '/inc/misc/shortcodes.php';
-
-// search
-require get_template_directory() . '/inc/misc/full-screen-search.php';
-
-// tags
-require get_template_directory() . '/inc/misc/tags-list.php';
-
-// license
-require_once get_template_directory() . '/inc/misc/license.php';
-
-// updates
-require get_template_directory() . '/inc/misc/git-updater.php';
-
-// utility
-require get_template_directory() . '/inc/misc/custom-post-test.php';
