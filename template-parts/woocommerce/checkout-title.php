be7d6cd722a2e62fced9f823e93e330520540e42
Jesse Nickles <jesse@littlebizzy.com>
2026-06-09 17:07:11 +0700

    Make checkout title divider intentional

--- template-parts/woocommerce/checkout-title.php
+++ template-parts/woocommerce/checkout-title.php
@@ -5,13 +5,15 @@ $hovercraft_logo_url = hovercraft_get_header_logo_url();
 $hovercraft_site_name = get_bloginfo( 'name' );
 $h1_divider_display = get_theme_mod( 'hovercraft_h1_divider_display', 'none' );
 $hovercraft_title_classes = array( 'entry-title' );
+$hovercraft_title_row_classes = array( 'checkout-title-row' );
 
 if ( $h1_divider_display === 'everywhere_possible' || $h1_divider_display === 'everywhere_except_heros' ) {
 	$hovercraft_title_classes[] = 'divide';
+	$hovercraft_title_row_classes[] = 'divide';
 }
 
 ?>
-<div class="checkout-title-row">
+<div class="<?php echo esc_attr( implode( ' ', $hovercraft_title_row_classes ) ); ?>">
 	<h1 class="<?php echo esc_attr( implode( ' ', $hovercraft_title_classes ) ); ?>"><?php echo esc_html( get_the_title() ); ?></h1>
 
 	<?php if ( $hovercraft_logo_url ) : ?>
