f096a7b531a5e95c39530263dcc86cfd42309e1a
Jesse Nickles <jesse@littlebizzy.com>
2026-06-11 17:56:21 +0700

    Set offcanvas submenu chevron to 24px

--- inc/css/header/menu-mobile.php
+++ inc/css/header/menu-mobile.php
@@ -313,8 +313,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 #hovercraft-offcanvas-menu a.menu-toggle svg {
 	display: block;
-	width: 32px;
-	height: 32px;
+	width: 24px;
+	height: 24px;
 }
 
 #hovercraft-offcanvas-menu a.menu-toggle:focus,
