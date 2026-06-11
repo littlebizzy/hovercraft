9f3ba2ecca907a4803cc6b2f738eefa5b1a95ee1
Jesse Nickles <jesse@littlebizzy.com>
2026-06-11 17:02:42 +0700

    Use local SVG for search modal close icon

--- template-parts/misc/search-modal.php
+++ template-parts/misc/search-modal.php
@@ -1,15 +1,9 @@
 <?php
 
-$hovercraft_layout_icons = get_theme_mod( 'hovercraft_layout_icons', 'material_icons_classic' );
-
 ?>
 <div id="full-screen-search" class="search-modal" role="dialog" aria-modal="true" aria-hidden="true" aria-label="<?php esc_attr_e( 'Search', 'hovercraft' ); ?>">
 	<button type="button" class="search-modal-close" aria-label="<?php esc_attr_e( 'Close search', 'hovercraft' ); ?>">
-		<?php if ( 'material_icons_classic' === $hovercraft_layout_icons ) : ?>
-			<i class="material-icons close" aria-hidden="true">close</i>
-		<?php elseif ( 'font_awesome_version_6' === $hovercraft_layout_icons ) : ?>
-			<i class="fa-solid fa-x" aria-hidden="true"></i>
-		<?php endif; // end layout icons ?>
+		<?php echo hovercraft_icon( 'close' ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
 	</button><!-- search-modal-close -->
 
 	<div class="search-modal-panel">
