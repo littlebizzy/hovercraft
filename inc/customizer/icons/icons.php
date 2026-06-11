0d89df55be53948135331a3e1cd494a84f01cabb
Jesse Nickles <jesse@littlebizzy.com>
2026-06-11 17:16:37 +0700

    Remove obsolete layout icon option

--- inc/customizer/icons/icons.php
+++ inc/customizer/icons/icons.php
@@ -11,33 +11,9 @@ $wp_customize->add_section( 'hovercraft_icons', array(
 	'priority' => 102,
 ) );
 
-// layout icons setting
-$wp_customize->add_setting( 'hovercraft_layout_icons', array(
-	'default' => 'material_icons_classic',
-	'sanitize_callback' => 'hovercraft_sanitize_select',
-) );
-
-// layout icons control
-$wp_customize->add_control( new WP_Customize_Control(
-	$wp_customize,
-	'hovercraft_layout_icons',
-	array(
-		'label' => __( 'Default Icon Elements', 'hovercraft' ),
-		'description' => __( 'Choose the icon set used for site layout and usability features.', 'hovercraft' ),
-		'section' => 'hovercraft_icons',
-		'settings' => 'hovercraft_layout_icons',
-		'type' => 'select',
-		'choices' => array(
-		'none' => 'None',
-		'material_icons_classic' => 'Material Icons',
-		'font_awesome_version_6' => 'Font Awesome 6',
-		),
-	)
-) );
-
 // material icons setting
 $wp_customize->add_setting( 'hovercraft_material_icons', array(
-	'default' => 'classic_only',
+	'default' => 'none',
 	'sanitize_callback' => 'hovercraft_sanitize_select',
 ) );
 
@@ -47,7 +23,7 @@ $wp_customize->add_control( new WP_Customize_Control(
 	'hovercraft_material_icons',
 	array(
 		'label' => __( 'Material Icons Library', 'hovercraft' ),
-		'description' => __( 'Choose which Material Icons styles should load.', 'hovercraft' ),
+		'description' => __( 'Optionally load Material Icons for custom content. Core theme icons use local SVG files.', 'hovercraft' ),
 		'section' => 'hovercraft_icons',
 		'settings' => 'hovercraft_material_icons',
 		'type' => 'select',
@@ -72,7 +48,7 @@ $wp_customize->add_control( new WP_Customize_Control(
 	'hovercraft_font_awesome',
 	array(
 		'label' => __( 'Font Awesome Library', 'hovercraft' ),
-		'description' => __( 'Choose which Font Awesome version should load.', 'hovercraft' ),
+		'description' => __( 'Optionally load Font Awesome for custom content or social brand icons. Core theme icons use local SVG files.', 'hovercraft' ),
 		'section' => 'hovercraft_icons',
 		'settings' => 'hovercraft_font_awesome',
 		'type' => 'select',
