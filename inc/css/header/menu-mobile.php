8d0a743f26e23cdd01045f0f51744d5cddd4854e
Jesse Nickles <jesse@littlebizzy.com>
2026-06-11 17:51:02 +0700

    Use rhythm size for offcanvas dropdown chevron

--- inc/css/header/menu-mobile.php
+++ inc/css/header/menu-mobile.php
@@ -313,8 +313,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 #hovercraft-offcanvas-menu a.menu-toggle svg {
 	display: block;
-	width: 24px;
-	height: 24px;
+	width: 32px;
+	height: 32px;
 }
 
 #hovercraft-offcanvas-menu a.menu-toggle:focus,
