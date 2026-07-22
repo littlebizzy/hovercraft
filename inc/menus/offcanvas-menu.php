4ede654e0b2ac2188798bf8c0adc10fbb06c088d
Jesse Nickles <jesse@littlebizzy.com>
2026-07-22 13:47:56 +0700

    Load offcanvas menu script only when selected

--- inc/menus/offcanvas-menu.php
+++ inc/menus/offcanvas-menu.php
@@ -8,6 +8,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 // offcanvas menu assets
 
 function hovercraft_offcanvas_menu() {
+	if ( 'accordion' !== get_theme_mod( 'hovercraft_mobile_menu', 'accordion' ) ) {
+		return;
+	}
+
 	wp_enqueue_script(
 		'hovercraft_offcanvas_menu',
 		esc_url_raw( get_template_directory_uri() . '/assets/js/offcanvas-menu.js' ),
@@ -17,4 +21,3 @@ function hovercraft_offcanvas_menu() {
 	);
 }
 add_action( 'wp_enqueue_scripts', 'hovercraft_offcanvas_menu' );
-
