6a7a187758dd7408bce7fe1cd0e1ce7823aa7c23
Jesse Nickles <jesse@littlebizzy.com>
2026-07-22 21:59:45 +0700

    Remove search modal jQuery dependency

--- inc/misc/full-screen-search.php
+++ inc/misc/full-screen-search.php
@@ -10,7 +10,7 @@ function hovercraft_full_screen_search() {
 	wp_enqueue_script(
 		'hovercraft_full_screen_search',
 		esc_url_raw( get_template_directory_uri() . '/assets/js/full-screen-search.js' ),
-		array( 'jquery' ),
+		array(),
 		HOVERCRAFT_VERSION,
 		true
 	);
