72ce29de022699fa645c305c535b70ca288e58db
Jesse Nickles <jesse@littlebizzy.com>
2026-07-22 14:02:18 +0700

    Read renamed mobile fallback argument

--- inc/menus/accordion-menu.php
+++ inc/menus/accordion-menu.php
@@ -26,7 +26,7 @@ add_action( 'wp_enqueue_scripts', 'hovercraft_accordion_menu' );
 function hovercraft_accordion_menu_toggle( $item_output, $item, $depth, $args ) {
 	$is_accordion_menu = 'accordion' === get_theme_mod( 'hovercraft_mobile_menu', 'accordion' );
 	$is_mobile_menu = isset( $args->theme_location ) && 'mobile-menu' === $args->theme_location;
-	$is_mobile_fallback = ! empty( $args->hovercraft_mobile_menu );
+	$is_mobile_fallback = ! empty( $args->hovercraft_mobile_fallback );
 
 	if (
 		$is_accordion_menu &&
