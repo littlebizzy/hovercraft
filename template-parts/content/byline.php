3e9a52f3041a45426b00127459ba5683bada3bdb
Jesse Nickles <jesse@littlebizzy.com>
2026-07-24 15:05:41 +0700

    Remove unscoped byline date microdata

--- template-parts/content/byline.php
+++ template-parts/content/byline.php
@@ -33,23 +33,23 @@ if ( is_singular( 'product' ) ) {
 
     if ( $hovercraft_byline_date === 'published_date_only' ) : ?>
         <span class="byline-separator">|</span>
-        <time class="date-published" datetime="<?php echo esc_attr( get_the_date( DATE_W3C ) ); ?>" itemprop="datePublished">
+        <time class="date-published" datetime="<?php echo esc_attr( get_the_date( DATE_W3C ) ); ?>">
             <?php echo esc_html( get_the_date( 'M j, Y' ) ); ?>
         </time>
 
     <?php elseif ( $hovercraft_byline_date === 'updated_date_only' ) : ?>
         <span class="byline-separator">|</span>
-        <time class="date-updated" datetime="<?php echo esc_attr( get_the_modified_date( DATE_W3C ) ); ?>" itemprop="dateModified">
+        <time class="date-updated" datetime="<?php echo esc_attr( get_the_modified_date( DATE_W3C ) ); ?>">
             <?php echo esc_html__( 'Updated', 'hovercraft' ) . ' ' . esc_html( get_the_modified_date( 'M j, Y' ) ); ?>
         </time>
 
     <?php elseif ( $hovercraft_byline_date === 'updated_and_published_dates' ) : ?>
         <span class="byline-separator">|</span>
-        <time class="date-published" datetime="<?php echo esc_attr( get_the_date( DATE_W3C ) ); ?>" itemprop="datePublished">
+        <time class="date-published" datetime="<?php echo esc_attr( get_the_date( DATE_W3C ) ); ?>">
             <?php echo esc_html( get_the_date( 'M j, Y' ) ); ?>
         </time>
         <span class="byline-date-separator" aria-hidden="true">&middot;</span>
-        <time class="date-updated" datetime="<?php echo esc_attr( get_the_modified_date( DATE_W3C ) ); ?>" itemprop="dateModified">
+        <time class="date-updated" datetime="<?php echo esc_attr( get_the_modified_date( DATE_W3C ) ); ?>">
             <?php echo esc_html__( 'Updated', 'hovercraft' ) . ' ' . esc_html( get_the_modified_date( 'M j, Y' ) ); ?>
         </time>
     <?php endif; // end byline date ?>
