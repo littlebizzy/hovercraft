ae8dc97962e0e948acd512cd9622256fa1bbd7e0
Jesse Nickles <17551083+jessuppi@users.noreply.github.com>
2026-08-20 11:01:32 +0700

    Render configured tags in shared page loop

--- template-parts/content/loop.php
+++ template-parts/content/loop.php
@@ -4,5 +4,6 @@ if ( have_posts() ) : ?>
 	<?php while ( have_posts() ) : ?>
 		<?php the_post(); ?>
 		<div class="entry-content"><?php the_content(); ?></div>
+		<?php get_template_part( 'template-parts/misc/tags' ); ?>
 	<?php endwhile; // end posts ?>
 <?php endif; ?><!-- end the loop -->
