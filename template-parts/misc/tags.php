83371db30df59c5cae7c2b40b9214fcd5ffb0423
Jesse Nickles <17551083+jessuppi@users.noreply.github.com>
2026-08-20 11:01:24 +0700

    Allow tag display on Pages

--- template-parts/misc/tags.php
+++ template-parts/misc/tags.php
@@ -7,11 +7,11 @@ $hovercraft_post_tags = get_theme_mod( 'hovercraft_post_tags', 'native_posts_onl
 		<div class="tags"><span><?php esc_html_e( 'Tags: ', 'hovercraft' ); echo hovercraft_show_tags(); ?></span></div><!-- tags -->
 	<?php endif; // end single ?>
 <?php elseif ( $hovercraft_post_tags === 'native_posts_and_pages' ) : ?>
-	<?php if ( is_single() ) : ?>
+	<?php if ( is_singular() ) : ?>
 		<?php if ( 'post' === get_post_type() || is_page() ) : ?>
 			<div class="tags"><span><?php esc_html_e( 'Tags: ', 'hovercraft' ); echo hovercraft_show_tags(); ?></span></div><!-- tags -->
 		<?php endif; // end page ?>
-	<?php endif; // end single ?>
+	<?php endif; // end singular ?>
 <?php elseif ( $hovercraft_post_tags === 'native_posts_and_custom_posts' ) : ?>
 	<?php if ( is_single() ) : ?>
 		<?php if ( 'post' === get_post_type() || hovercraft_is_custom_post_type() ) : ?>
@@ -19,11 +19,11 @@ $hovercraft_post_tags = get_theme_mod( 'hovercraft_post_tags', 'native_posts_onl
 		<?php endif; // end post type ?>
 	<?php endif; // end single ?>
 <?php elseif ( $hovercraft_post_tags === 'native_posts_and_pages_and_custom_posts' ) : ?>
-	<?php if ( is_single() ) : ?>
+	<?php if ( is_singular() ) : ?>
 		<?php if ( 'post' === get_post_type() || is_page() || hovercraft_is_custom_post_type() ) : ?>
 			<div class="tags"><span><?php esc_html_e( 'Tags: ', 'hovercraft' ); echo hovercraft_show_tags(); ?></span></div><!-- tags -->
 		<?php endif; // end page ?>
-	<?php endif; // end single ?>
+	<?php endif; // end singular ?>
 <?php endif; // end post tags ?>
 
 <?php
